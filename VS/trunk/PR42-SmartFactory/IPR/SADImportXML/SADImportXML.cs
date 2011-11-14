e36e81cf85ded7fd87a1b02b31bda466c0b5936e
mpostol <mpostol@8a3894fd-0c01-b944-991b-6a3da02cae8a>
2011-11-14 19:36:23 +0000

    [pr4-2798] Wrong lookup field used in the content types in the custom namespace - fixed wrong list name in schema files. Generated new Entities.cs Issue #: 2798

--- VS/trunk/PR42-SmartFactory/IPR/SADImportXML/SADImportXML.cs
+++ VS/trunk/PR42-SmartFactory/IPR/SADImportXML/SADImportXML.cs
@@ -50,25 +50,7 @@ namespace CAS.SmartFactory.IPR.Customs
             select enr).First<SADDocumentType>();
           entry.ReferenceNumber = document.GetNrWlasny();
           entry.Tytuł = document.MessageRootName();
-    
-          List<SADGood> cmdts = new List<SADGood>();
-          for (int i = 0; i < document.GoodsTableLength(); i++)
-          {
-            SADGood newRow = new SADGood()
-            {
-              SADDocumentIndex = entry,
-              Tytuł = String.Format("{0}: {1}", document.MessageRootName(), document.GetNrWlasny()),
-              GoodsDescription = document[i].GetDescription(),
-              PCNTariffCode = document[i].GetPCNTariffCode(),
-              GrossMass = document[i].GetGrossMass(),
-              Procedure = document[i].GetProcedure(),
-              TotalAmountInvoiced = document[i].GetTotalAmountInvoiced(),
-              ItemNo = document[i].GetItemNo()
-            };
-            cmdts.Add(newRow);
-          }
-          edc.SADGood.InsertAllOnSubmit(cmdts);
-          edc.SubmitChanges();
+          GetSADGood(document, edc, entry);
         }
       }
       catch (Exception ex)
@@ -76,8 +58,10 @@ namespace CAS.SmartFactory.IPR.Customs
         SPWeb web = properties.Web;
         SPList log = web.Lists.TryGetList("Activity Log");
         if (log == null)
+        {
+          EventLog.WriteEntry("CAS.SmartFActory", "Cannot open \"Activity Log\" list", EventLogEntryType.Error, 114);
           return;
-        EventLog.WriteEntry("CAS.SmartFActory", "Cannot open \"Activity Log\" list", EventLogEntryType.Error, 114);
+        }
         SPListItem item = log.AddItem();
         item["Title"] = "Customs document import error";
         item["Body"] = ex.Message;
@@ -90,6 +74,42 @@ namespace CAS.SmartFactory.IPR.Customs
         this.EventFiringEnabled = true;
       }
       base.ItemAdded(properties);
+    }
+    private static void GetSADDuties(CustomsDocument document, EntitiesDataContext edc, SADDocumentType entry)
+    {
+      List<SADDuties> cmdts = new List<SADDuties>();
+      for (int i = 0; i < document.GoodsTableLength(); i++)
+      {
+        SADDuties newRow = new SADDuties()
+        {
+          SADGoodLookup = entry,
+          Tytuł = String.Format("{0}: {1}", document.MessageRootName(), document.GetNrWlasny()),
+        };
+        cmdts.Add(newRow);
+      }
+      edc.SADDuties.InsertAllOnSubmit(cmdts);
+      edc.SubmitChanges();
+    }
+    private static void GetSADGood(CustomsDocument document, EntitiesDataContext edc, SADDocumentType entry)
+    {
+      List<SADGood> cmdts = new List<SADGood>();
+      for (int i = 0; i < document.GoodsTableLength(); i++)
+      {
+        SADGood newRow = new SADGood()
+        {
+          SADDocumentIndex = entry,
+          Tytuł = String.Format("{0}: {1}", document.MessageRootName(), document.GetNrWlasny()),
+          GoodsDescription = document[i].GetDescription(),
+          PCNTariffCode = document[i].GetPCNTariffCode(),
+          GrossMass = document[i].GetGrossMass(),
+          Procedure = document[i].GetProcedure(),
+          TotalAmountInvoiced = document[i].GetTotalAmountInvoiced(),
+          ItemNo = document[i].GetItemNo()
+        };
+        cmdts.Add(newRow);
+      }
+      edc.SADGood.InsertAllOnSubmit(cmdts);
+      edc.SubmitChanges();
     }
   }
 }
