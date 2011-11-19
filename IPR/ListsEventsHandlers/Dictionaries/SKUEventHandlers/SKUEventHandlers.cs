b0ed3b40333de69f6f51c23ba1d5ca70be713d47
Mariusz <mpostol@cas.eu>
2011-11-19 12:31:36 +0000

    [pr4-2827] Implement import mechanism for SKU - added to the project the handler template but not implemented. Issue #: 2827

---
+++ IPR/ListsEventsHandlers/Dictionaries/SKUEventHandlers/SKUEventHandlers.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Security.Permissions;
+using Microsoft.SharePoint;
+using Microsoft.SharePoint.Security;
+using Microsoft.SharePoint.Utilities;
+using Microsoft.SharePoint.Workflow;
+
+namespace CAS.SmartFactory.IPR.ListsEventsHandlers.Dictionaries.SKUEventHandlers
+{
+    /// <summary>
+    /// List Item Events
+    /// </summary>
+    public class SKUEventHandlers : SPItemEventReceiver
+    {
+       /// <summary>
+       /// An item was added.
+       /// </summary>
+       public override void ItemAdded(SPItemEventProperties properties)
+       {
+           base.ItemAdded(properties);
+       }
+
+
+    }
+}
