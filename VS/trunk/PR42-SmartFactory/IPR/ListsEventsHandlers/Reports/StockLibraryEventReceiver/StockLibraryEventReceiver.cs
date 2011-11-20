95797d5f86ad6464783aabe851a1f8e53eb129a9
mpostol <mpostol@8a3894fd-0c01-b944-991b-6a3da02cae8a>
2011-11-20 12:21:55 +0000

    [pr4-2823] Implement import mechanism for Stock: StockLibraryEventReceiver.cs added and registered. Issue #: 2823

---
+++ VS/trunk/PR42-SmartFactory/IPR/ListsEventsHandlers/Reports/StockLibraryEventReceiver/StockLibraryEventReceiver.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Security.Permissions;
+using Microsoft.SharePoint;
+using Microsoft.SharePoint.Security;
+using Microsoft.SharePoint.Utilities;
+using Microsoft.SharePoint.Workflow;
+
+namespace CAS.SmartFactory.IPR.ListsEventsHandlers.Reports
+{
+  /// <summary>
+  /// List Item Events
+  /// </summary>
+  public class StockLibraryEventReceiver : SPItemEventReceiver
+  {
+    /// <summary>
+    /// An item was added.
+    /// </summary>
+    public override void ItemAdded(SPItemEventProperties properties)
+    {
+      base.ItemAdded(properties);
+    }
+  }
+}
