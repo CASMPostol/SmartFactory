5d5ccabec328b9002f9edca888db9f0d46cad6cf
mpostol <mpostol@8a3894fd-0c01-b944-991b-6a3da02cae8a>
2011-11-20 13:08:21 +0000

    [pr4-2827] Implement import mechanism for SKU - added common SKU class containing serialization procedure. Issue #: 2827

---
+++ VS/trunk/PR42-SmartFactory/XML/Generated/IPR/SKU.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml;
+using System.Xml.Serialization;
+
+namespace CAS.SmartFactory.xml.IPR
+{
+  public abstract class SKU
+  {
+    static public SKU ImportDocument(System.IO.Stream documetStream)
+    {
+      Type type = null;
+      using (XmlReader reader = XmlReader.Create(documetStream))
+      {
+        if (reader.MoveToContent() != XmlNodeType.Element)
+          throw new ApplicationException("The file does not contain valid xml file.");
+        if (reader.Name.Contains("Cutfiller"))
+          type = typeof(Cutfiller);
+        else if (reader.Name.Contains("Cigarettes"))
+          type = typeof(Cigarettes);
+      }
+      if (type == null)
+        throw new ApplicationException("The file does not contain a valid SKU xml document");
+      documetStream.Seek(0, System.IO.SeekOrigin.Begin);
+      using (XmlReader reader = XmlReader.Create(documetStream, new XmlReaderSettings() { }))
+      {
+        XmlSerializer invoice = new XmlSerializer(type);
+        return (SKU)invoice.Deserialize(reader);
+      }
+    }
+  }
+}
