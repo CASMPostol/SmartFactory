054de75512b433c09129b004647c11e0bb463f4c
mpostol <mpostol@8a3894fd-0c01-b944-991b-6a3da02cae8a>
2011-11-23 13:31:07 +0000

    [pr4-2824] Implement import mechanism for invoice - done, must be tested. Issue #: 2824

---
+++ VS/trunk/PR42-SmartFactory/IPR/Entities/Batch.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CAS.SmartFactory.IPR.Entities
+{
+  public partial class Batch
+  {
+    internal static Batch GetLookup(EntitiesDataContext edc, string index)
+    {
+      Batch newBatch = null;
+      var cb =
+         from batch in edc.Batch where batch.Batch0.Contains(index) select batch;
+      if (cb.Count<Batch>() == 0)
+      {
+        newBatch = new Batch()
+        {
+          Batch0 = index,
+          BatchStatus = "Preliminary",
+          Tytuł = index
+        };
+        edc.Batch.InsertOnSubmit(newBatch);
+      }
+      return newBatch;
+    }
+
+  }
+}
