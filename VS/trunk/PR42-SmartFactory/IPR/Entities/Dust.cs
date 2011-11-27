d110a03dc3a9733b77f9deb4247d3240a5cb4a94
mpostol <mpostol@8a3894fd-0c01-b944-991b-6a3da02cae8a>
2011-11-27 22:19:13 +0000

    [pr4-2859] Implement the GetDustLookup method. - done Issue #: 2859

--- VS/trunk/PR42-SmartFactory/IPR/Entities/Dust.cs
+++ VS/trunk/PR42-SmartFactory/IPR/Entities/Dust.cs
@@ -8,15 +8,7 @@ namespace CAS.SmartFactory.IPR.Entities
     internal static Dust GetLookup(ProductType type, EntitiesDataContext edc)
     {
       Dust value = null;
-      try
-      {
-        value = (from idx in edc.Dust where idx.ProductType == type select idx).Aggregate((x, y) => (x.Wersja < y.Wersja ? y : x));
-        return value;
-      }
-      catch (ArgumentNullException)
-      {
-        value = new Dust() { DustRatio = 0, ProductType = type, Tytuł = type.ToString() }; //TODO remove in final version and replace by throwin an exception
-      }
+      value = (from idx in edc.Dust where idx.ProductType == type orderby idx.Wersja select idx).First();
       return value;
     }
   }
