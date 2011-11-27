8def83bc41a743faeda5a05f23064ab015a32204
mpostol <mpostol@8a3894fd-0c01-b944-991b-6a3da02cae8a>
2011-11-27 22:01:44 +0000

    [pr4-2878] Implement Waste.GetLookup method Issue #: 2878

--- VS/trunk/PR42-SmartFactory/IPR/Entities/Waste.cs
+++ VS/trunk/PR42-SmartFactory/IPR/Entities/Waste.cs
@@ -10,14 +10,7 @@ namespace CAS.SmartFactory.IPR.Entities
     internal static Waste GetLookup(ProductType type, EntitiesDataContext edc)
     {
       Waste value = null;
-      try
-      {
-        value = (from idx in edc.Waste where idx.ProductType == type select idx).Aggregate((x, y) => (x.Wersja < y.Wersja ? y : x));
-      }
-      catch (ArgumentNullException)
-      {
-        value = new Waste() {  WasteRatio = 0, ProductType = type, Tytuł = type.ToString() }; //TODO remove in final version and replace by throwin an exception
-      }
+      value = (from idx in edc.Waste where idx.ProductType == type orderby idx.Wersja descending select idx).First();
       return value;
     }
   }
