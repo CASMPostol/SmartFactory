5f0e5a4da4b8a951291bbafccd09036c51b70a17
mpostol <mpostol@8a3894fd-0c01-b944-991b-6a3da02cae8a>
2011-11-27 21:45:20 +0000

    [pr4-2824] Implement import mechanism for invoice: finished, any new issues will be reported separately. Issue #: 2824

--- VS/trunk/PR42-SmartFactory/IPR/Entities/Batch.cs
+++ VS/trunk/PR42-SmartFactory/IPR/Entities/Batch.cs
@@ -26,6 +26,18 @@ namespace CAS.SmartFactory.IPR.Entities
         newBatch = cb.First();
       return newBatch;
     }
+    /// <summary>
+    /// Gets the lookup.
+    /// </summary>
+    /// <param name="edc">The <see cref="EntitiesDataContext"/> instance.</param>
+    /// <param name="index">The index of the entry we are lookin for.</param>
+    /// <returns>The most recent <see cref="Batch"/> object.</returns>
+    /// <exception cref="System.ArgumentNullException">The source is null.</exception>
+    internal static Batch GetLookup(EntitiesDataContext edc, string index)
+    {
+      Batch newBatch =  (from batch in edc.Batch where batch.Batch0.Contains(index) orderby batch.Identyfikator descending select batch).First();
+      return newBatch;
+    }
     internal Disposal[] GetDisposals(EntitiesDataContext edc)
     {
       var disposals =
