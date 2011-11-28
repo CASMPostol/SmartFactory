dfbb9c65e4c225423f9c8821aca809a72aebe779
Mariusz <mpostol@cas.eu>
2011-11-28 17:24:45 +0000

    [pr4-2883] Add IPRInitializeWebsite project: Created project WebsiteManagement instead of IPRInitializeWebsite. Issue #: 2883

---
+++ WebsiteManagement/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace _pr4_2883__Add_IPRInitializeWebsite_project
+{
+  static class Program
+  {
+    /// <summary>
+    /// The main entry point for the application.
+    /// </summary>
+    [STAThread]
+    static void Main()
+    {
+      Application.EnableVisualStyles();
+      Application.SetCompatibleTextRenderingDefault(false);
+      Application.Run(new Form1());
+    }
+  }
+}
