177022653598e682c4ffe8a5ea0f9b0154f2a876
mpostol <mpostol@8a3894fd-0c01-b944-991b-6a3da02cae8a>
2011-11-30 22:37:01 +0000

    [pr4-2874] XML schema for dictionaries: Format is imported correctly. Issue #: 2874

--- VS/trunk/PR42-SmartFactory/WebsiteManagement/MainDialog.Designer.cs
+++ VS/trunk/PR42-SmartFactory/WebsiteManagement/MainDialog.Designer.cs
@@ -28,12 +28,55 @@
     /// </summary>
     private void InitializeComponent()
     {
-      this.components = new System.ComponentModel.Container();
+      this.m_OpenFileDialog = new System.Windows.Forms.OpenFileDialog();
+      this.m_ImportButton = new System.Windows.Forms.Button();
+      this.m_Status = new System.Windows.Forms.Label();
+      this.SuspendLayout();
+      // 
+      // m_OpenFileDialog
+      // 
+      this.m_OpenFileDialog.DefaultExt = "xml";
+      // 
+      // m_ImportButton
+      // 
+      this.m_ImportButton.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+      this.m_ImportButton.Location = new System.Drawing.Point(12, 12);
+      this.m_ImportButton.Name = "m_ImportButton";
+      this.m_ImportButton.Size = new System.Drawing.Size(347, 43);
+      this.m_ImportButton.TabIndex = 0;
+      this.m_ImportButton.Text = "Read Configuration";
+      this.m_ImportButton.UseVisualStyleBackColor = true;
+      this.m_ImportButton.Click += new System.EventHandler(this.m_ImportButton_Click);
+      // 
+      // m_Status
+      // 
+      this.m_Status.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+      this.m_Status.Location = new System.Drawing.Point(19, 72);
+      this.m_Status.Name = "m_Status";
+      this.m_Status.Size = new System.Drawing.Size(339, 40);
+      this.m_Status.TabIndex = 1;
+      // 
+      // MainDialog
+      // 
+      this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
       this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-      this.Text = "Form1";
+      this.ClientSize = new System.Drawing.Size(384, 161);
+      this.Controls.Add(this.m_Status);
+      this.Controls.Add(this.m_ImportButton);
+      this.MinimumSize = new System.Drawing.Size(400, 100);
+      this.Name = "MainDialog";
+      this.Text = "Smart Factory Management";
+      this.ResumeLayout(false);
+
     }
 
     #endregion
+
+    private System.Windows.Forms.OpenFileDialog m_OpenFileDialog;
+    private System.Windows.Forms.Button m_ImportButton;
+    private System.Windows.Forms.Label m_Status;
   }
 }
 
--- VS/trunk/PR42-SmartFactory/XML/Dictionaries/Configuration.partial.cs
+++ VS/trunk/PR42-SmartFactory/XML/Dictionaries/Configuration.partial.cs
@@ -1,11 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using System.IO;
+using System.Xml;
+using System.Xml.Serialization;
 
 namespace CAS.SmartFactory.xml.Dictionaries
 {
   public partial class Configuration
   {
+    static public Configuration ImportDocument(Stream documetStream)
+    {
+      using (XmlReader reader = XmlReader.Create(documetStream, new XmlReaderSettings() { }))
+      {
+        XmlSerializer serializer = new XmlSerializer(typeof(Configuration));
+        return (Configuration)serializer.Deserialize(reader);
+      }
+    }
   }
 }
