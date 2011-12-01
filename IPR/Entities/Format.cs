ed39195e823a6ca292c7726479614843ab3b928b
Mariusz <mpostol@cas.eu>
2011-12-01 17:36:49 +0000

    [pr4-2870] Format: implement GetLookup methods - rearranged the text only unimportant changes. Issue #: 2870

--- IPR/Entities/Format.cs
+++ IPR/Entities/Format.cs
@@ -8,15 +8,6 @@ namespace CAS.SmartFactory.IPR.Entities
 {
   public partial class Format
   {
-    internal static Format GetFormatLookup(string cigaretteLength, string filterSegmentLength, EntitiesDataContext edc)
-    {
-      Format frmt = (from idx in edc.Format where idx.Match(cigaretteLength, filterSegmentLength) orderby idx.Wersja descending select idx).First();
-      return frmt;
-    }
-    internal static Format GetCutfillerFormatLookup(EntitiesDataContext edc)
-    {
-      return GetFormatLookup(String.Empty, String.Empty, edc);
-    }
     private bool Match(string cigaretteLength, string filterSegmentLength)
     {
       const string frmt = "{0:d}:{1:d}";
@@ -36,6 +27,15 @@ namespace CAS.SmartFactory.IPR.Entities
         list.Add(frmt);
       };
       edc.Format.InsertAllOnSubmit(list);
+    }
+    internal static Format GetCutfillerFormatLookup(EntitiesDataContext edc)
+    {
+      return GetFormatLookup(String.Empty, String.Empty, edc);
+    }
+    internal static Format GetFormatLookup(string cigaretteLength, string filterSegmentLength, EntitiesDataContext edc)
+    {
+      Format frmt = (from idx in edc.Format where idx.Match(cigaretteLength, filterSegmentLength) orderby idx.Wersja descending select idx).First();
+      return frmt;
     }
     internal static Format GetFormatLookup(string name, EntitiesDataContext edc)
     {
