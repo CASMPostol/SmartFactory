833c642fe8ec65455398cf99f8dab4ef63f36001
Mariusz <mpostol@cas.eu>
2011-12-22 13:49:50 +0000

    [pr4-2939] Create Carrier Dashboard - finished CurrentUserWebPart - it works. Issue #: 2939

---
+++ ShepherdDashboards/CurrentUserWebPart/IUserDescriptor.cs
@@ -0,0 +1,9 @@
+﻿using System;
+namespace CAS.SmartFactory.Shepherd.Dashboards.CurrentUserWebPart
+{
+  interface IUserDescriptor
+  {
+    string Company { get; }
+    Microsoft.SharePoint.SPUser User { get; }
+  }
+}
