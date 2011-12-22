8196dcb9283b2871229a5c946672f8b7d02fcaf4
Mariusz <mpostol@cas.eu>
2011-12-22 07:13:36 +0000

    [pr4-2939] Create Carrier Dashboard - some files.
    Issue #: 2939

---
+++ ShepherdDashboards/Extensions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CAS.SmartFactory.Shepherd.Dashboards
+{
+  public static class Extensions
+  {
+    public static string ToString(this int? _val)
+    {
+      return _val.HasValue ? _val.Value.ToString() : String.Empty;
+    }
+    public static string ToString(this DateTime? _val)
+    {
+      return _val.HasValue ? _val.Value.ToString() : String.Empty;
+    }
+  }
+}
