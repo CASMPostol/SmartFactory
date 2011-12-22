1c551893f9fa9ab30916ba89d00c54bd7a3a42fc
mpostol <mpostol@8a3894fd-0c01-b944-991b-6a3da02cae8a>
2011-12-22 07:13:36 +0000

    [pr4-2939] Create Carrier Dashboard - some files.
    Issue #: 2939

---
+++ VS/trunk/PR42-SmartFactory/ShepherdDashboards/Entities/TimeSlotTimeSlot.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CAS.SmartFactory.Shepherd.Dashboards.Entities
+{
+  public partial class TimeSlotTimeSlot
+  {
+    internal static TimeSlotTimeSlot GetTimeSlot(EntitiesDataContext edc, string _id)
+    {
+      try
+      {
+        int _intid = int.Parse(_id);
+        return (
+              from idx in edc.TimeSlot
+              where idx.ShippingIndex.Identyfikator == _intid
+              orderby idx.StartTime descending
+              select idx).First();
+      }
+      catch (Exception)
+      {
+        return null;
+      }
+    }
+  }
+}
