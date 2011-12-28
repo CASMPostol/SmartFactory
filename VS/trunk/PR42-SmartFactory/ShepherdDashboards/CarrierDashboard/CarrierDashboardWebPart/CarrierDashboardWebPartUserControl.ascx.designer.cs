fc44f258de244c7c1b73869fe95f472de3b6dc3b
bwojciechowski <bwojciechowski@8a3894fd-0c01-b944-991b-6a3da02cae8a>
2011-12-28 15:47:06 +0000

    [pr4-2954] C;lean up user experience for CarrierDashboardWebPart - minor changes. Issue #: 2954

--- VS/trunk/PR42-SmartFactory/ShepherdDashboards/CarrierDashboard/CarrierDashboardWebPart/CarrierDashboardWebPartUserControl.ascx.designer.cs
+++ VS/trunk/PR42-SmartFactory/ShepherdDashboards/CarrierDashboard/CarrierDashboardWebPart/CarrierDashboardWebPartUserControl.ascx.designer.cs
@@ -22,6 +22,15 @@ namespace CAS.SmartFactory.Shepherd.Dashboards.CarrierDashboard.CarrierDashboard
         protected global::System.Web.UI.WebControls.Panel m_Panel;
         
         /// <summary>
+        /// CarrierDashboardLayout1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Table CarrierDashboardLayout1;
+        
+        /// <summary>
         /// m_StateLiteral control.
         /// </summary>
         /// <remarks>
@@ -31,13 +40,13 @@ namespace CAS.SmartFactory.Shepherd.Dashboards.CarrierDashboard.CarrierDashboard
         protected global::System.Web.UI.WebControls.Literal m_StateLiteral;
         
         /// <summary>
-        /// Label3 control.
+        /// Label6 control.
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.Label Label3;
+        protected global::System.Web.UI.WebControls.Label Label6;
         
         /// <summary>
         /// m_WarehouseTextBox control.
