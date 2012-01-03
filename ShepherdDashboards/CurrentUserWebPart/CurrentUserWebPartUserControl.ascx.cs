631a371e41ac806e0df754f81faf1771a0efa09e
Mariusz <mpostol@cas.eu>
2012-01-03 21:26:50 +0000

    [pr4-2965] Null reference exception in the CurrentUserWebPart - added some code to prevent exception if the Context is null. Issue #: 2965

--- ShepherdDashboards/CurrentUserWebPart/CurrentUserWebPartUserControl.ascx.cs
+++ ShepherdDashboards/CurrentUserWebPart/CurrentUserWebPartUserControl.ascx.cs
@@ -9,7 +9,10 @@ namespace CAS.SmartFactory.Shepherd.Dashboards.CurrentUserWebPart
     {}
     internal void DisplayUserName(IUserDescriptor _UserDescriptor)
     {
-      this.m_UserNameLiteral.Text = String.Format("Welcome {0} from {1} !", _UserDescriptor.User, _UserDescriptor.Company);
+      if (_UserDescriptor != null)
+        this.m_UserNameLiteral.Text = String.Format("Welcome {0} from {1} !", _UserDescriptor.User, _UserDescriptor.Company);
+      else
+        this.m_UserNameLiteral.Text = "Information about user is unavailable";
     }
   }
 }
