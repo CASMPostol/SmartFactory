e17dbe6c178eb190cc6358a8491ddcafe06acbbe
bwojciechowski <bwojciechowski@8a3894fd-0c01-b944-991b-6a3da02cae8a>
2012-01-05 11:05:51 +0000

    [pr4-2951] Create Carrier Daschboard page. - AlarmsAndEvents, Shipping and LoadDescription WebParts added. Issue #: 2951

---
+++ VS/trunk/PR42-SmartFactory/ShepherdDashboards/ListsWebparts/LoadDescription/LoadDescription.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.ComponentModel;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Web.UI.WebControls.WebParts;
+using Microsoft.SharePoint;
+using Microsoft.SharePoint.WebControls;
+
+namespace CAS.SmartFactory.Shepherd.Dashboards.ListsWebparts.LoadDescription
+{
+    [ToolboxItemAttribute(false)]
+    public class LoadDescription : WebPart
+    {
+        protected override void CreateChildControls()
+        {
+        }
+    }
+}
