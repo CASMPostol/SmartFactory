0ce8adc1eabb2516c4931446a846001796a94ee9
mpostol <mpostol@8a3894fd-0c01-b944-991b-6a3da02cae8a>
2012-01-05 14:06:11 +0000

    [pr4-2959] Create pilot solution installation package - added code to test the installation procedure. Issue #: 2959

--- VS/trunk/PR42-SmartFactory/XML/Test/Program.cs
+++ VS/trunk/PR42-SmartFactory/XML/Test/Program.cs
@@ -2,11 +2,20 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Windows.Forms;
+using Microsoft.SharePoint.Administration;
+using Microsoft.SharePoint;
+using System.Security.Principal;
 
 namespace CAS.SmartFactory.xml.Test
 {
+  class structure
+  {
+    public SPFarm SPFarm { get; set; }
+    public SPWebApplication SPWebApplication{ get; private set; }
+  }
   static class Program
   {
+
     /// <summary>
     /// The main entry point for the application.
     /// </summary>
@@ -15,7 +24,68 @@ namespace CAS.SmartFactory.xml.Test
     {
       Application.EnableVisualStyles();
       Application.SetCompatibleTextRenderingDefault(false);
-      Application.Run(new MainForm());
+      SPFarm farm = SPFarm.Local;
+      Console.WriteLine("Solutions in the SPFarm");
+      foreach (SPSolution item in farm.Solutions)
+      {
+        Console.WriteLine("SPFarm.Solutions" + item.Name);
+      }
+      SPSolution _Sol = farm.Solutions["ShepherdDashboards.wsp"];
+      Console.WriteLine("ShepherdDashboards.wsp status = " + _Sol.Status.ToString());
+      //foreach (var item in farm.FeatureDefinitions)
+      //{
+      //  Console.WriteLine("AvailableFeatures: " + item.DisplayName );
+      //}
+      Uri _uri = new Uri("http://casmp");
+      SPWebApplication _wa = SPWebApplication.Lookup(_uri);
+      WindowsIdentity _id = WindowsIdentity.GetCurrent();
+      Console.WriteLine("You are: " + _id.Name);
+      //_wa.Sites.Add(@"sites/AutoSite", _id.Name, "");
+      Console.WriteLine("");
+      Console.WriteLine("Sites in the web application at http://casmp");
+      foreach (SPSite _spsite in _wa.Sites)
+      {
+        Console.WriteLine(String.Format("Site url: {0} Features count: {1}", item.Url, item.Features.Count));
+        Console.WriteLine("Features in the SPSite:");
+        foreach (SPFeature _ftr in _spsite.Features)
+        {
+          Console.WriteLine(String.Format
+            ("SPFeature Definition.DisplayName={0}, Definition.Name={1}, Definition.Scope={2}, FeatureDefinitionScope={3}, Version={4}",
+             _ftr.Definition.DisplayName, _ftr.Definition.Name, _ftr.Definition.Scope, _ftr.FeatureDefinitionScope, _ftr.Version));
+        }
+        Console.WriteLine("Features in the SPSite:");
+        foreach (SPFeatureDefinition item in _spsite.FeatureDefinitions)
+        {
+          Console.WriteLine(String.Format("SPFeatureDefinition SPFeatureDefinition", item.Name);
+        }
+        Console.WriteLine("Solution in the SPSite:");
+        foreach (SPUserSolution _sl in _spsite.Solutions)
+        {
+          Console.WriteLine(String.Format("SPSolution Name={0}, Signature={1}, Status={2}", _sl.Name, _sl.Signature, _sl.Status));
+        }
+        SPWeb _wp = _spsite.RootWeb;
+        WriteSPWebInfo(_wp);
+        Console.WriteLine("Webs in the SPWeb");
+        foreach (SPWeb _spweb in _wp.Webs)
+          WriteSPWebInfo(_spweb);
+        SPList _ssg = _wp.GetCatalog(SPListTemplateType.SolutionCatalog);
+        Console.WriteLine("This web solutions in the Solution Catalog info: ");
+        foreach (SPListItem _solIdx in _ssg.Items)
+        {
+          Console.WriteLine(String.Format("Solution Name{0}, Level= {1}, Title={2}, Url={3}, ID={4}", _solIdx.Name, _solIdx.Level, _solIdx.Title, _solIdx.Url, _solIdx.ID));
+        }
+      }
+      //Application.Run(new MainForm());
+    }
+
+    private static void WriteSPWebInfo(SPWeb _wp)
+    {
+      Console.WriteLine(String.Format("SPWeb Name={0}, Language={1}, ParentWeb.Name={2}, PortalName={3}, PortalUrl={4}",
+        _wp.Name,
+        _wp.Language,
+        _wp.ParentWeb == null ? "null" : _wp.ParentWeb.Name,
+        _wp.PortalName,
+        _wp.PortalUrl));
     }
   }
 }
