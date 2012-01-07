657da5bae7b691124605cc36748b325c20163895
Mariusz <mpostol@cas.eu>
2012-01-07 21:40:38 +0000

    [pr4-2959] Create pilot solution installation package - added FeatureDeactivating to remove all installed items.
    Issue #: 2959

--- ShepherdDashboards/Features/Dashboards/Dashboards.EventReceiver.cs
+++ ShepherdDashboards/Features/Dashboards/Dashboards.EventReceiver.cs
@@ -1,15 +1,8 @@
 using System;
 using System.Runtime.InteropServices;
-using System.Security.Permissions;
 using Microsoft.SharePoint;
-using Microsoft.SharePoint.Security;
 using Microsoft.SharePoint.Navigation;
-using System.Web.UI.WebControls.WebParts;
-using System.Linq;
-using SPWebPartConnection = Microsoft.SharePoint.WebPartPages.SPWebPartConnection;
 using System.Collections.Generic;
-using System.Text;
-using WebPartPagesManagement = CAS.SmartFactory.Shepherd.Dashboards.WebPartPages.ProjectElementManagement;
 
 namespace CAS.SmartFactory.Shepherd.Dashboards.Features.Dashboards
 {
@@ -23,98 +16,92 @@ namespace CAS.SmartFactory.Shepherd.Dashboards.Features.Dashboards
   [Guid("79ed4ae0-8478-445d-996c-d84421cfe0d5")]
   public class DashboardsEventReceiver : SPFeatureReceiver
   {
-    // Uncomment the method below to handle the event raised after a feature has been activated.
-
     public override void FeatureActivated(SPFeatureReceiverProperties properties)
     {
       SPSite site = properties.Feature.Parent as SPSite;
       SPWeb _root = site.RootWeb;
-      if (site != null)
+      if (site == null)
+        throw new ApplicationException("FeatureActivated cannot get access to the Web");
+      using (Entities.EntitiesDataContext _edc = new Entities.EntitiesDataContext(_root.Url))
       {
+        Entities.Anons.WriteEntry(_edc, m_SourceClass + m_SourceFeatureActivated, "FeatureActivated strating");
         _root.Title = "Shepherd Home";
         _root.SiteLogoUrl = @"_layouts/images/ShepherdDashboards/Shepherd_50x50.png";
         _root.Update();
-      }
-      // create dropdown menu for custom site pages
-      SPNavigationNodeCollection _topNav = _root.Navigation.TopNavigationBar;
-      SPNavigationNode _topMenu = _topNav[0].Children.AddAsLast(new SPNavigationNode("Vendor", WebPartPagesManagement.CarrierDashboardURL));
-      SetupConnections(_root);
-    }
-    private static void SetupConnections(SPWeb _root)
-    {
-      SPFile file = _root.GetFile(WebPartPagesManagement.CarrierDashboardURL);
-      System.Collections.Generic.Dictionary<string, WebPart> _dict = new System.Collections.Generic.Dictionary<string, WebPart>();
-      string _phase = "starting";
-      using (Microsoft.SharePoint.WebPartPages.SPLimitedWebPartManager _pageMgr = file.GetLimitedWebPartManager(PersonalizationScope.Shared))
-      {
-        try
-        {
-          _dict = _pageMgr.WebParts.Cast<WebPart>().ToDictionary(key => key.ID); //.ForEach(wp => wpMgr.DeleteWebPart(wp));
-          _phase = "After wpMgr.WebParts.Cast";
-          SPWebPartConnection _CarrierDashboardWebPartPartner = new SPWebPartConnection
-          {
-            ConsumerID = "CarrierDashboardWebPart",
-            ProviderID = "CurrentUserWebPart",
-            ConsumerConnectionPointID = "PartnerInterconnection",
-            ProviderConnectionPointID = "CurrentUserProviderPoint",
-            ID = "CarrierDashboardWebPartPartner"
-          };
-          _phase = "After new SPWebPartConnection";
-          _pageMgr.SPWebPartConnections.Add(_CarrierDashboardWebPartPartner);
-          _phase = "After Add(_CarrierDashboardWebPartPartner)";
-          _pageMgr.SaveChanges(_dict["CarrierDashboardWebPart"]);
-          _phase = "after SaveChanges(_dict['CarrierDashboardWebPart'])";
-          _pageMgr.SaveChanges(_dict["CurrentUserWebPart"]);
-          _phase = "Finished";
-        }
-        catch (Exception ex)
-        {
-          StringBuilder _names = new StringBuilder();
-          _dict.Keys.ToList<string>().ForEach(name => _names.Append(name + ", "));
-          throw new ApplicationException(String.Format("Phase={0}, Count={1}, First={2}, Ex={3}", _phase, _dict.Count, _names.ToString(), ex.Message));
-        }
-        //SPWebPartConnection connection = new SPWebPartConnection
-        //{
-        //  ConsumerID = "CarrierDashboardWebPart",
-        //  ProviderID = "CurrentUserWebPart",
-        //  ConsumerConnectionPointID = "PartnerInterconnection",
-        //  ProviderConnectionPointID = "CurrentUserProviderPoint",
-        //  ID = "CarrierDashboardWebPartPartner"
-        //};
-
-        ////RssWebPart rssWebPart = new RssWebPart
-        //{
-        //  ID = "RssWebPart",
-        //  Title = "RSS Reader",
-        //  ItemCount = 5
-        //};
-        //FeedInputWebPart feedInput = new FeedInputWebPart
-        //{
-        //  ID = "FeedInput",
-        //  Title = "Feed input"
-        //};
-        //wpMgr.AddWebPart(feedInput, "Main", 0);
-        //wpMgr.AddWebPart(rssWebPart, "Main", 1);
-        //SPWebPartConnection connection = new SPWebPartConnection
-        //{
-        //  ConsumerID = "RssWebPart",
-        //  ProviderID = "FeedInput",
-        //  ConsumerConnectionPointID = "feed",
-        //  ProviderConnectionPointID = "",
-        //  ID = "connection"
-        //};
-        //wpMgr.SPWebPartConnections.Add(connection);
-        //wpMgr.SaveChanges(rssWebPart);
-        //wpMgr.SaveChanges(feedInput);
+        // create dropdown menu for custom site pages
+        Entities.Anons.WriteEntry(_edc, m_SourceClass + m_SourceFeatureActivated, "Navigation setup starting"); 
+        SPNavigationNodeCollection _topNav = _root.Navigation.TopNavigationBar;
+        SPNavigationNode _topMenu = _topNav[0].Children.AddAsLast(new SPNavigationNode(m_VendorMenyTitle, WebPartPages.ProjectElementManagement.CarrierDashboardURL));
+        WebPartPages.ProjectElementManagement.SetupConnections(_edc, _root);
+        Entities.Anons.WriteEntry(_edc, m_SourceClass + m_SourceFeatureActivated, "FeatureActivated finished");
       }
     }
 
     // Uncomment the method below to handle the event raised before a feature is deactivated.
-
-    //public override void FeatureDeactivating(SPFeatureReceiverProperties properties)
-    //{
-    //}
-
+    public override void FeatureDeactivating(SPFeatureReceiverProperties properties)
+    {
+      SPSite site = properties.Feature.Parent as SPSite;
+      SPWeb _root = site.RootWeb;
+      if (site == null)
+        throw new ApplicationException("FeatureDeactivating cannot get access to the Web");
+      using (Entities.EntitiesDataContext _edc = new Entities.EntitiesDataContext(_root.Url))
+      {
+        Entities.Anons.WriteEntry(_edc, "FeatureDeactivating", "Feature Deactivation starting.");
+        WebPartPages.ProjectElementManagement.RemovePages(_edc, _root);
+        RemoveNavigationEntries(_root);
+        DeleteWebParts(_edc, _root);
+        Entities.Anons.WriteEntry(_edc, "FeatureDeactivating", "Feature Deactivation finished.");
+      }
+    }
+    private static void RemoveNavigationEntries(SPWeb _root)
+    {
+      try
+      {
+        SPNavigationNodeCollection topNav = _root.Navigation.TopNavigationBar;
+        for (int i = topNav[0].Children.Count - 1; i >= 0; i--)
+          if (topNav[0].Children[i].Title == m_VendorMenyTitle)
+            topNav[0].Children[i].Delete();
+      }
+      catch (Exception ex)
+      {
+        throw new ApplicationException("Cannot remove navigation entries: " + ex.Message);
+      }
+    }
+    private const string m_VendorMenyTitle = "Vendor";
+    private static void DeleteWebParts(Entities.EntitiesDataContext _edc, SPWeb _root)
+    {
+      Entities.Anons.WriteEntry(_edc, m_SourceClass + m_SourceDeleteWebParts, "Delete Web Parts strating");
+      try
+      {
+        SPList _wpl = _root.GetCatalog(SPListTemplateType.WebPartCatalog);
+        List<SPFile> _filesToDelete = new List<SPFile>();
+        // figure out which Web Part template files need to be deleted
+        Entities.Anons.WriteEntry
+        (
+          _edc, m_SourceClass + m_SourceDeleteWebParts,
+          String.Format("Processing of the WebPartCatalog containing {0} items starting ", _wpl.ItemCount)
+        );
+        foreach (SPListItem _li in _wpl.Items)
+        {
+          bool _delete = _li.File.Name.StartsWith("ShepherdDashboards");
+          if (_delete)
+            _filesToDelete.Add(_li.File);
+          string _mess = String.Format("Title: {0}, Name: {1}, File name: {2}, deleted: {3}", _li.Title, _li.Name, _li.File.Name, _delete);
+          Entities.Anons.WriteEntry(_edc, "Processing Web Part", _mess);
+        }
+        // delete Web Part template files
+        foreach (SPFile file in _filesToDelete)
+          file.Delete();
+      }
+      catch (Exception ex)
+      {
+        Entities.Anons.WriteEntry(_edc, m_SourceClass + m_SourceDeleteWebParts, "Delete Web Parts finished with exception: " + ex.Message);
+      }
+      Entities.Anons.WriteEntry(_edc, m_SourceClass + m_SourceDeleteWebParts, "Delete Web Parts finished");
+    }
+    private const string m_SourceClass = "DashboardsEventReceiver.";
+    private const string m_SourceDeleteWebParts = "DeleteWebParts";
+    private const string m_SourceFeatureActivated = "FeatureActivated";
 
     // Uncomment the method below to handle the event raised after a feature has been installed.
 
--- ShepherdDashboards/WebPartPages/ProjectElementManagement.cs
+++ ShepherdDashboards/WebPartPages/ProjectElementManagement.cs
@@ -2,11 +2,83 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Microsoft.SharePoint;
+using System.Web.UI.WebControls.WebParts;
+using SPLimitedWebPartManager = Microsoft.SharePoint.WebPartPages.SPLimitedWebPartManager;
+using CAS.SmartFactory.Shepherd.Dashboards.Entities;
+
 
 namespace CAS.SmartFactory.Shepherd.Dashboards.WebPartPages
 {
   internal class ProjectElementManagement
   {
-    internal const string CarrierDashboardURL = @"WebPartPages/CarrierDashboard.aspx";
+    internal const string WebPartPagesFolder = "WebPartPages";
+    internal const string CarrierDashboardURL = WebPartPagesFolder + "/CarrierDashboard.aspx";
+    internal const string IDCurrentUser = "CDCurrentUser";
+    internal const string IDTimeSlots = "CDTimeSlots";
+    internal const string IDCDShipping = "CDShipping";
+    internal const string IDLoadDescription = "CDLoadDescription";
+    internal const string IDCarrierDashboardWebPart = "CDCarrierDashboardWebPart";
+    internal const string IDTransportResources = "CDTransportResources";
+    internal const string IDAlarmsAndEevents = "CDAlarmsAndEevents";
+    internal static void SetupConnections(EntitiesDataContext _edc, SPWeb _root)
+    {
+      SPFile file = _root.GetFile(CarrierDashboardURL);
+      System.Collections.Generic.Dictionary<string, WebPart> _dict = new System.Collections.Generic.Dictionary<string, WebPart>();
+      Anons.WriteEntry(_edc, m_SourceClass + m_SourceSetupConnections, "Setup connections starting");
+      string _phase = "starting";
+      using (SPLimitedWebPartManager _pageMgr = file.GetLimitedWebPartManager(PersonalizationScope.Shared))
+        try
+        {
+          _dict = _pageMgr.WebParts.Cast<WebPart>().ToDictionary(key => key.ID); //.ForEach(wp => wpMgr.DeleteWebPart(wp));
+          _phase = "After wpMgr.WebParts.Cast";
+          ConnectWebParts
+          (
+            _pageMgr,
+            _dict[IDCurrentUser],
+            _dict[IDCarrierDashboardWebPart],
+            CurrentUserWebPart.CurrentUserWebPart.CurrentUserProviderPoint,
+            CarrierDashboard.CarrierDashboardWebPart.CarrierDashboardWebPart.ConsumertIDPartnerInterconnection
+          );
+        }
+        catch (Exception ex)
+        {
+          StringBuilder _names = new StringBuilder();
+          _dict.Keys.ToList<string>().ForEach(name => _names.Append(name + ", "));
+          string _msg = String.Format("Setup connections failed in Phase={0}, Count={1}, First={2}, Ex={3}", _phase, _dict.Count, _names.ToString(), ex.Message);
+          Entities.Anons.WriteEntry(_edc, m_SourceClass + m_SourceSetupConnections, _msg);
+          //throw new ApplicationException(_msg);
+        }
+      Anons.WriteEntry(_edc, m_SourceClass + m_SourceSetupConnections, "Setup connections finished");
+    }
+    internal static void RemovePages(Entities.EntitiesDataContext _edc, SPWeb _root)
+    {
+      Anons.WriteEntry(_edc, m_SourceClass + m_SourceRemovePages, "Remove Pages starting");
+      try
+      {
+        SPFolder WebPartPagesFolder = _root.GetFolder(ProjectElementManagement.WebPartPagesFolder);
+        if (WebPartPagesFolder.Exists)
+          WebPartPagesFolder.Delete();
+        else
+          Anons.WriteEntry(_edc, m_SourceClass + m_SourceRemovePages, "Failed, the folder " + WebPartPagesFolder + "dies not exist.");
+      }
+      catch (Exception ex)
+      {
+        Anons.WriteEntry(_edc, m_SourceClass + m_SourceRemovePages, "Remove pages failed with exception: " + ex.Message);
+      }
+      Anons.WriteEntry(_edc, m_SourceClass + m_SourceRemovePages, "Remove Pages finished");
+    }
+    private const string m_SourceClass = "WebPartPages.ProjectElementManagement.";
+    private const string m_SourceRemovePages = "RemovePages";
+    private const string m_SourceSetupConnections = "SetupConnections";
+    private static void ConnectWebParts(SPLimitedWebPartManager _pageMgr, WebPart _prvdr, WebPart _cnsmr, string _providerConnectionPoints, string _consumerConnectionPoints)
+    {
+      _pageMgr.SPConnectWebParts(
+        _prvdr,
+        _pageMgr.GetProviderConnectionPoints(_prvdr)[_providerConnectionPoints],
+        _cnsmr,
+        _pageMgr.GetConsumerConnectionPoints(_cnsmr)[_consumerConnectionPoints]);
+    }
   }
 }
+
