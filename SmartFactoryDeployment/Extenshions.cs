bf452eecc3d367aeaafc77d559b13fa667ea3986
Mariusz <mpostol@cas.eu>
2012-01-13 11:12:51 +0000

    [pr4-2959] Create pilot solution installation package - added schema based cs ApplicationState.cs class generated by the xsd tool.
    Issue #: 2959

--- SmartFactoryDeployment/Extenshions.cs
+++ SmartFactoryDeployment/Extenshions.cs
@@ -6,6 +6,9 @@ using System.Windows.Forms;
 
 namespace CAS.SmartFactory.Deployment
 {
+  /// <summary>
+  /// Project extenshion definitions
+  /// </summary>
   public static class Extenshions
   {
     public static void AddMessage(this ListBox _box, string _msg)
