78a68239b5b6474483393c2e9d03aef6e3c56f19
mpostol <mpostol@8a3894fd-0c01-b944-991b-6a3da02cae8a>
2012-01-18 10:05:55 +0000

    [pr4-2986] Prepare Shepherd preliminary data loader - added script and auto-generated cs file. Issue #: 2986

---
+++ VS/trunk/PR42-SmartFactory/ShepherdImportExport/XML/PreliminaryData.xsd.cs
@@ -0,0 +1,14 @@
+﻿using System.IO;
+using System.Xml.Serialization;
+
+namespace CAS.SmartFactory.Shepherd.ImportExport.XML
+{
+  public partial class PreliminaryData
+  {
+    static public PreliminaryData ImportDocument(Stream documetStream)
+    {
+      XmlSerializer serializer = new XmlSerializer(typeof(PreliminaryData));
+      return (PreliminaryData)serializer.Deserialize(documetStream);
+    }
+  }
+}
