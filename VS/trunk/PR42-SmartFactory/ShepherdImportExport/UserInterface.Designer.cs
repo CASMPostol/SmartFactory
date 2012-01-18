a61afecb0d198fced88f1d8b40dcc8d030f881c1
mpostol <mpostol@8a3894fd-0c01-b944-991b-6a3da02cae8a>
2012-01-18 11:42:30 +0000

    [pr4-2986] Prepare Shepherd preliminary data loader - defined component do manage the file. Issue #: 2986

--- VS/trunk/PR42-SmartFactory/ShepherdImportExport/UserInterface.Designer.cs
+++ VS/trunk/PR42-SmartFactory/ShepherdImportExport/UserInterface.Designer.cs
@@ -29,11 +29,23 @@
     private void InitializeComponent()
     {
       this.components = new System.ComponentModel.Container();
+      this.m_InputFileDialog = new CAS.SmartFactory.Shepherd.ImportExport.FileDialog(this.components);
+      this.SuspendLayout();
+      // 
+      // UserInterface
+      // 
+      this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
       this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+      this.ClientSize = new System.Drawing.Size(284, 262);
+      this.Name = "UserInterface";
       this.Text = "Form1";
+      this.ResumeLayout(false);
+
     }
 
     #endregion
+
+    private FileDialog m_InputFileDialog;
   }
 }
 
