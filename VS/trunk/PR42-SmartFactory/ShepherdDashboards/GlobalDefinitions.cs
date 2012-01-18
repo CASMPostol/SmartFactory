a4f2d43cf2a6197375121a42166dcbd7a6512a9f
mpostol <mpostol@8a3894fd-0c01-b944-991b-6a3da02cae8a>
2012-01-18 13:56:48 +0000

    [pr4-2994] Customize Warehouse field in CDWP - done Issue #: 2994

---
+++ VS/trunk/PR42-SmartFactory/ShepherdDashboards/GlobalDefinitions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CAS.SmartFactory.Shepherd.Dashboards
+{
+  public class GlobalDefinitions
+  {
+    public enum Roles
+    {
+      Owner,
+      Coordinator,
+      Supervisor,
+      Operator,
+      Vendor,
+      Forwarder,
+      Escort,
+      Guard, 
+      None,
+    }
+  }
+}
