444f7e28b39b894b2fd65545bc9090feaf12b753
Mariusz <mpostol@cas.eu>
2012-01-18 11:44:13 +0000

    [pr4-2986] Prepare Shepherd preliminary data loader - defined component do manage the file - added files.
    Issue #: 2986

---
+++ ShepherdImportExport/FileDialog.Designer.cs
@@ -0,0 +1,47 @@
+﻿namespace CAS.SmartFactory.Shepherd.ImportExport
+{
+  partial class FileDialog
+  {
+    /// <summary>
+    /// Required designer variable.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary> 
+    /// Clean up any resources being used.
+    /// </summary>
+    /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+    protected override void Dispose(bool disposing)
+    {
+      if (disposing && (components != null))
+      {
+        components.Dispose();
+      }
+      base.Dispose(disposing);
+    }
+
+    #region Component Designer generated code
+
+    /// <summary>
+    /// Required method for Designer support - do not modify
+    /// the contents of this method with the code editor.
+    /// </summary>
+    private void InitializeComponent()
+    {
+      this.m_OpenFileDialog = new System.Windows.Forms.OpenFileDialog();
+      // 
+      // m_OpenFileDialog
+      // 
+      this.m_OpenFileDialog.DefaultExt = "xml";
+      this.m_OpenFileDialog.FileName = "openFileDialog1";
+      this.m_OpenFileDialog.SupportMultiDottedExtensions = true;
+      this.m_OpenFileDialog.Title = "Preliminary Data File";
+
+    }
+
+    #endregion
+
+    public System.Windows.Forms.OpenFileDialog m_OpenFileDialog;
+
+  }
+}
