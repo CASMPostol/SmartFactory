3d73c60bb2ce2a572f450ce969f983333be08b40
Mariusz <mpostol@cas.eu>
2012-01-31 22:31:09 +0000

    [pr4-2987] Add Shepherd preliminary data and create xls files - next step of modifications. Issue #: 2987

--- ShepherdImportExport/UserInterface.Designer.cs
+++ ShepherdImportExport/UserInterface.Designer.cs
@@ -19,20 +19,21 @@
     {
       this.components = new System.ComponentModel.Container();
       System.Windows.Forms.TableLayoutPanel m_tableLayoutPanel1;
+      System.Windows.Forms.Label m_CollectionURLlabel1;
       System.Windows.Forms.StatusStrip m_StatusStrip1;
       System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(UserInterface));
       this.m_TimeSlotsCreateButton = new System.Windows.Forms.Button();
-      this.label1 = new System.Windows.Forms.Label();
       this.m_URLTextBox = new System.Windows.Forms.TextBox();
-      this.button1 = new System.Windows.Forms.Button();
+      this.m_AddTimeSlotsButton = new System.Windows.Forms.Button();
       this.m_ImportDictionaries = new System.Windows.Forms.Button();
       this.m_ToolStripProgressBar = new System.Windows.Forms.ToolStripProgressBar();
       this.m_ToolStripStatusLabel = new System.Windows.Forms.ToolStripStatusLabel();
+      this.m_FileNameStatusLabel = new System.Windows.Forms.ToolStripStatusLabel();
       this.m_FileManagementComonent = new CAS.SmartFactory.Shepherd.ImportExport.FileDialog(this.components);
       this.toolStripContainer1 = new System.Windows.Forms.ToolStripContainer();
       this.m_ToolTip = new System.Windows.Forms.ToolTip(this.components);
-      this.m_FileNameStatusLabel = new System.Windows.Forms.ToolStripStatusLabel();
       m_tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+      m_CollectionURLlabel1 = new System.Windows.Forms.Label();
       m_StatusStrip1 = new System.Windows.Forms.StatusStrip();
       m_tableLayoutPanel1.SuspendLayout();
       m_StatusStrip1.SuspendLayout();
@@ -47,9 +48,9 @@
       m_tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
       m_tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
       m_tableLayoutPanel1.Controls.Add(this.m_TimeSlotsCreateButton, 0, 1);
-      m_tableLayoutPanel1.Controls.Add(this.label1, 0, 0);
+      m_tableLayoutPanel1.Controls.Add(m_CollectionURLlabel1, 0, 0);
       m_tableLayoutPanel1.Controls.Add(this.m_URLTextBox, 1, 0);
-      m_tableLayoutPanel1.Controls.Add(this.button1, 0, 2);
+      m_tableLayoutPanel1.Controls.Add(this.m_AddTimeSlotsButton, 0, 2);
       m_tableLayoutPanel1.Controls.Add(this.m_ImportDictionaries, 0, 3);
       m_tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
       m_tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
@@ -60,7 +61,7 @@
       m_tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
       m_tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
       m_tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
-      m_tableLayoutPanel1.Size = new System.Drawing.Size(597, 265);
+      m_tableLayoutPanel1.Size = new System.Drawing.Size(884, 355);
       m_tableLayoutPanel1.TabIndex = 1;
       // 
       // m_TimeSlotsCreateButton
@@ -71,39 +72,39 @@
       this.m_TimeSlotsCreateButton.TabIndex = 0;
       this.m_TimeSlotsCreateButton.Text = "Generate Time Slots";
       this.m_TimeSlotsCreateButton.UseVisualStyleBackColor = true;
-      this.m_TimeSlotsCreateButton.Click += new System.EventHandler(this.m_TimeSlotsCreateButton_Click);
+      this.m_TimeSlotsCreateButton.Click += new System.EventHandler(this.TimeSlotsCreateButton_Click);
       // 
-      // label1
+      // m_CollectionURLlabel1
       // 
-      this.label1.AutoSize = true;
-      this.label1.Dock = System.Windows.Forms.DockStyle.Left;
-      this.label1.Location = new System.Drawing.Point(3, 0);
-      this.label1.Name = "label1";
-      this.label1.Size = new System.Drawing.Size(78, 26);
-      this.label1.TabIndex = 1;
-      this.label1.Text = "Collection URL";
-      this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+      m_CollectionURLlabel1.AutoSize = true;
+      m_CollectionURLlabel1.Dock = System.Windows.Forms.DockStyle.Left;
+      m_CollectionURLlabel1.Location = new System.Drawing.Point(3, 0);
+      m_CollectionURLlabel1.Name = "m_CollectionURLlabel1";
+      m_CollectionURLlabel1.Size = new System.Drawing.Size(78, 26);
+      m_CollectionURLlabel1.TabIndex = 1;
+      m_CollectionURLlabel1.Text = "Collection URL";
+      m_CollectionURLlabel1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
       // 
       // m_URLTextBox
       // 
       this.m_URLTextBox.Dock = System.Windows.Forms.DockStyle.Fill;
       this.m_URLTextBox.Location = new System.Drawing.Point(125, 3);
       this.m_URLTextBox.Name = "m_URLTextBox";
-      this.m_URLTextBox.Size = new System.Drawing.Size(469, 20);
+      this.m_URLTextBox.Size = new System.Drawing.Size(756, 20);
       this.m_URLTextBox.TabIndex = 2;
       this.m_URLTextBox.Text = "http://casmp/";
       // 
-      // button1
+      // m_AddTimeSlotsButton
       // 
-      this.button1.AutoSize = true;
-      this.button1.Dock = System.Windows.Forms.DockStyle.Fill;
-      this.button1.Location = new System.Drawing.Point(3, 58);
-      this.button1.Name = "button1";
-      this.button1.Size = new System.Drawing.Size(116, 23);
-      this.button1.TabIndex = 3;
-      this.button1.Text = "Add TimeSlots";
-      this.button1.UseVisualStyleBackColor = true;
-      this.button1.Click += new System.EventHandler(this.button1_Click);
+      this.m_AddTimeSlotsButton.AutoSize = true;
+      this.m_AddTimeSlotsButton.Dock = System.Windows.Forms.DockStyle.Fill;
+      this.m_AddTimeSlotsButton.Location = new System.Drawing.Point(3, 58);
+      this.m_AddTimeSlotsButton.Name = "m_AddTimeSlotsButton";
+      this.m_AddTimeSlotsButton.Size = new System.Drawing.Size(116, 23);
+      this.m_AddTimeSlotsButton.TabIndex = 3;
+      this.m_AddTimeSlotsButton.Text = "Add TimeSlots";
+      this.m_AddTimeSlotsButton.UseVisualStyleBackColor = true;
+      this.m_AddTimeSlotsButton.Click += new System.EventHandler(this.AddTimeSlots_Click);
       // 
       // m_ImportDictionaries
       // 
@@ -115,7 +116,7 @@
       this.m_ImportDictionaries.TabIndex = 4;
       this.m_ImportDictionaries.Text = "Import Dictionaries";
       this.m_ImportDictionaries.UseVisualStyleBackColor = true;
-      this.m_ImportDictionaries.Click += new System.EventHandler(this.m_ImportDictionaries_Click);
+      this.m_ImportDictionaries.Click += new System.EventHandler(this.ImportDictionaries_Click);
       // 
       // m_StatusStrip1
       // 
@@ -126,7 +127,7 @@
             this.m_FileNameStatusLabel});
       m_StatusStrip1.Location = new System.Drawing.Point(0, 0);
       m_StatusStrip1.Name = "m_StatusStrip1";
-      m_StatusStrip1.Size = new System.Drawing.Size(597, 22);
+      m_StatusStrip1.Size = new System.Drawing.Size(884, 22);
       m_StatusStrip1.TabIndex = 0;
       // 
       // m_ToolStripProgressBar
@@ -139,8 +140,13 @@
       // 
       this.m_ToolStripStatusLabel.AutoSize = false;
       this.m_ToolStripStatusLabel.Name = "m_ToolStripStatusLabel";
-      this.m_ToolStripStatusLabel.Size = new System.Drawing.Size(100, 17);
+      this.m_ToolStripStatusLabel.Size = new System.Drawing.Size(200, 17);
       this.m_ToolStripStatusLabel.Text = "----";
+      // 
+      // m_FileNameStatusLabel
+      // 
+      this.m_FileNameStatusLabel.Name = "m_FileNameStatusLabel";
+      this.m_FileNameStatusLabel.Size = new System.Drawing.Size(0, 17);
       // 
       // toolStripContainer1
       // 
@@ -152,28 +158,24 @@
       // toolStripContainer1.ContentPanel
       // 
       this.toolStripContainer1.ContentPanel.Controls.Add(m_tableLayoutPanel1);
-      this.toolStripContainer1.ContentPanel.Size = new System.Drawing.Size(597, 265);
+      this.toolStripContainer1.ContentPanel.Size = new System.Drawing.Size(884, 355);
       this.toolStripContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
       this.toolStripContainer1.LeftToolStripPanelVisible = false;
       this.toolStripContainer1.Location = new System.Drawing.Point(0, 0);
       this.toolStripContainer1.Name = "toolStripContainer1";
       this.toolStripContainer1.RightToolStripPanelVisible = false;
-      this.toolStripContainer1.Size = new System.Drawing.Size(597, 312);
+      this.toolStripContainer1.Size = new System.Drawing.Size(884, 402);
       this.toolStripContainer1.TabIndex = 1;
       this.toolStripContainer1.Text = "toolStripContainer1";
-      // 
-      // m_FileNameStatusLabel
-      // 
-      this.m_FileNameStatusLabel.Name = "m_FileNameStatusLabel";
-      this.m_FileNameStatusLabel.Size = new System.Drawing.Size(0, 17);
       // 
       // UserInterface
       // 
       this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
       this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-      this.ClientSize = new System.Drawing.Size(597, 312);
+      this.ClientSize = new System.Drawing.Size(884, 402);
       this.Controls.Add(this.toolStripContainer1);
       this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+      this.MinimumSize = new System.Drawing.Size(900, 440);
       this.Name = "UserInterface";
       this.Text = "Import Data";
       m_tableLayoutPanel1.ResumeLayout(false);
@@ -195,10 +197,9 @@
     private System.Windows.Forms.Button m_TimeSlotsCreateButton;
     private System.Windows.Forms.ToolStripContainer toolStripContainer1;
     private System.Windows.Forms.ToolStripProgressBar m_ToolStripProgressBar;
-    private System.Windows.Forms.Label label1;
     private System.Windows.Forms.TextBox m_URLTextBox;
     private System.Windows.Forms.ToolStripStatusLabel m_ToolStripStatusLabel;
-    private System.Windows.Forms.Button button1;
+    private System.Windows.Forms.Button m_AddTimeSlotsButton;
     private System.Windows.Forms.Button m_ImportDictionaries;
     private System.Windows.Forms.ToolTip m_ToolTip;
     private System.Windows.Forms.ToolStripStatusLabel m_FileNameStatusLabel;
--- ShepherdImportExport/UserInterface.cs
+++ ShepherdImportExport/UserInterface.cs
@@ -1,17 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Windows.Forms;
-using CAS.SmartFactory.Shepherd.Dashboards.Entities;
-using Microsoft.SharePoint;
-using Microsoft.SharePoint.Administration;
-using System.IO;
 using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Windows.Forms;
 using CAS.SmartFactory.Shepherd.ImportExport.XML;
+using CAS.SmartFactory.Shepherd.ImportExport.Entities;
 
 namespace CAS.SmartFactory.Shepherd.ImportExport
 {
@@ -64,6 +59,7 @@ namespace CAS.SmartFactory.Shepherd.ImportExport
     {
       m_ToolStripStatusLabel.Text = _label;
       m_ToolStripProgressBar.Value = m_ToolStripProgressBar.Minimum;
+      this.Refresh();
     }
     private Stream OpenFile()
     {
@@ -78,53 +74,81 @@ namespace CAS.SmartFactory.Shepherd.ImportExport
       return m_FileManagementComonent.m_OpenFileDialog.OpenFile();
     }
     #region importing data
-    private void CreateCities(EntitiesDataContext _EDC)
+    private void CreateCountries(EntitiesDataContext _EDC, UpdateToolStripEvent _update)
     {
+      _update(this, new ProgressChangedEventArgs(1, "CreateCountries starting"));
       for (int i = 0; i < 10; i++)
       {
-        CityType _cmm = new CityType() { Tytuł = String.Format("City {0}", i) };
+        string _cn = String.Format("Country {0}", i);
+        CountryClass _cuntr = new CountryClass() { Tytuł = _cn };
+        _EDC.Country.InsertOnSubmit(_cuntr);
+        _update(this, new ProgressChangedEventArgs(1, String.Format("SubmitChanges for {0}", _cn)));
+        _EDC.SubmitChanges();
+        CreateCities(_EDC, _cuntr, _update);
+      }
+    }
+    private void CreateCities(EntitiesDataContext _EDC, CountryClass _cuntr, UpdateToolStripEvent _update)
+    {
+      _update(this, new ProgressChangedEventArgs(1, "CreateCities starting"));
+      for (int i = 0; i < 10; i++)
+      {
+        string _cn = String.Format("City {0}", i);
+        CityType _cmm = new CityType() { Tytuł = _cn, CountryName = _cuntr };
         _EDC.City.InsertOnSubmit(_cmm);
+        _update(this, new ProgressChangedEventArgs(1, String.Format("SubmitChanges for {0}", _cn)));
         _EDC.SubmitChanges();
       }
     }
-    private void CreateCommodity(EntitiesDataContext _EDC)
+    private void CreateCommodity(EntitiesDataContext _EDC, UpdateToolStripEvent _update)
     {
-      for (int i = 0; i < 10; i++)
+      _update(this, new ProgressChangedEventArgs(1, "CreateCommodity starting"));
+      for (int i = 0; i < 3; i++)
       {
-        CommodityCommodity _cmm = new CommodityCommodity() { Tytuł = String.Format("Commodity {0}", i) };
+        string _cn = String.Format("Commodity {0}", i);
+        CommodityCommodity _cmm = new CommodityCommodity() { Tytuł = _cn };
         _EDC.Commodity.InsertOnSubmit(_cmm);
+        _update(this, new ProgressChangedEventArgs(1, String.Format("SubmitChanges for {0}", _cn)));
         _EDC.SubmitChanges();
-        CraeteWareHouse(_EDC, _cmm);
+        CraeteWareHouse(_EDC, _cmm, _update);
       }
     }
-    private void CraeteWareHouse(EntitiesDataContext _EDC, CommodityCommodity _cmm)
+    private void CraeteWareHouse(EntitiesDataContext _EDC, CommodityCommodity _cmm, UpdateToolStripEvent _update)
     {
+      _update(this, new ProgressChangedEventArgs(1, "CraeteWareHouse starting"));
       Warehouse _wrs = new Warehouse() { Commodity = _cmm, Tytuł = String.Format("Warehouse {0}", _cmm.Tytuł) };
       _EDC.Warehouse.InsertOnSubmit(_wrs);
       _EDC.SubmitChanges();
-      CreateShippingPoints(_EDC, _wrs);
+      CreateShippingPoints(_EDC, _wrs, _update);
     }
-    private void CreateShippingPoints(EntitiesDataContext _EDC, Warehouse _wrs)
+    private void CreateShippingPoints(EntitiesDataContext _EDC, Warehouse _wrs, UpdateToolStripEvent _update)
     {
-      foreach (Direction _tx in Enum.GetValues(typeof(Direction)))
-      {
-        ShippingPoint _sp = new ShippingPoint() { Description = _wrs.Tytuł, Direction = _tx, Tytuł = String.Format("ShippingPoint {0}", _wrs.Tytuł), Warehouse = _wrs };
-        _EDC.ShippingPoint.InsertOnSubmit(_sp);
-        _EDC.SubmitChanges();
-        CreateTimeSlots(_EDC, _sp, UpdateToolStrip);
-      }
+      _update(this, new ProgressChangedEventArgs(1, "CreateShippingPoints starting"));
+      string _frmt = "ShippingPoint {0}";
+      ShippingPoint _sp = new ShippingPoint() { Description = _wrs.Tytuł + " Inbound", Direction = Direction.Inbound, Tytuł = String.Format(_frmt, _wrs.Tytuł), Warehouse = _wrs };
+      _EDC.ShippingPoint.InsertOnSubmit(_sp);
+      _sp = new ShippingPoint() { Description = _wrs.Tytuł + " Outbound", Direction = Direction.Outbound, Tytuł = String.Format(_frmt, _wrs.Tytuł), Warehouse = _wrs };
+      _EDC.ShippingPoint.InsertOnSubmit(_sp);
+      _sp = new ShippingPoint() { Description = _wrs.Tytuł + " BothDirections", Direction = Direction.BothDirections, Tytuł = String.Format(_frmt, _wrs.Tytuł), Warehouse = _wrs };
+      _EDC.ShippingPoint.InsertOnSubmit(_sp);
+      _EDC.SubmitChanges();
+      CreateTimeSlots(_EDC, _sp, UpdateToolStrip);
     }
     private void CreateTimeSlots(EntitiesDataContext _EDC, ShippingPoint _sp, UpdateToolStripEvent _update)
     {
-      for (DateTime _dy = DateTime.Now; _dy < DateTime.Now + TimeSpan.FromDays(25); _dy.AddDays(1))
+      _update(this, new ProgressChangedEventArgs(1, "CreateTimeSlots starting"));
+      for (int _i = 0; _i < 25; _i++)
       {
+        DateTime _dy = DateTime.Now.Date + TimeSpan.FromDays(_i);
+        _update(this, new ProgressChangedEventArgs(1, _dy.ToShortDateString()));
         if (_dy.DayOfWeek == DayOfWeek.Sunday || _dy.DayOfWeek == DayOfWeek.Saturday)
           continue;
-        List<TimeSlotTimeSlot> _ts = null;
-        for (DateTime _hr = _dy; _hr.Hour <= 16; _hr.AddHours(1))
+        List<TimeSlotTimeSlot> _ts = new List<TimeSlotTimeSlot>();
+        for (int _indx = 8; _indx <= 16; _indx++)
         {
-          _ts.Add(new TimeSlotTimeSlot() { EntryTime = _hr, EndTime = _hr.AddHours(1), ExitTime = _hr.AddHours(1), Occupied = false, ShippingPoint = _sp, StartTime = _hr });
-          _update(this, new ProgressChangedEventArgs(1, _hr.ToShortDateString()));
+          DateTime _bgn = _dy + TimeSpan.FromHours(_indx);
+          DateTime _end = _bgn + TimeSpan.FromHours(1);
+          _ts.Add(new TimeSlotTimeSlot() { EntryTime = _bgn, EndTime = _end, ExitTime = _end, Occupied = false, ShippingPoint = _sp, StartTime = _bgn });
+          _update(this, new ProgressChangedEventArgs(1, _bgn.ToShortDateString()));
         }
         _EDC.TimeSlot.InsertAllOnSubmit(_ts);
         _EDC.SubmitChanges();
@@ -132,37 +156,117 @@ namespace CAS.SmartFactory.Shepherd.ImportExport
     }
     private void ImportData(PreliminaryData cnfg, string _URL, UpdateToolStripEvent _update)
     {
+      _update(this, new ProgressChangedEventArgs(1, "ImportData starting"));
       using (EntitiesDataContext _EDC = new EntitiesDataContext(m_URLTextBox.Text.Trim()))
       {
       }
       SetDone("Done");
+    }
+    private void CreatePartners(EntitiesDataContext _EDC, UpdateToolStripEvent _update)
+    {
+      _update(this, new ProgressChangedEventArgs(1, "CreatePartners starting"));
+      ServiceType[] _stt = new ServiceType[] { ServiceType.Forwarder, ServiceType.SecurityEscortProvider, ServiceType.VendorAndForwarder };
+      string[] _pn = new string[] { "forwarder", "escort", "vendor" };
+      for (int i = 0; i <= 2; i++)
+      {
+        string _nm = String.Format("Partner {0}", i);
+        Partner _prt = new Partner() { Tytuł = _nm, ServiceType = _stt[i], ShepherdUserTitle = _pn[i] };
+        _EDC.JTIPartner.InsertOnSubmit(_prt);
+        _update(this, new ProgressChangedEventArgs(1, String.Format("SubmitChanges for {0}", _nm)));
+        _EDC.SubmitChanges();
+        CraeteTrucks(_EDC, _prt, _update);
+        CraeteDrivers(_EDC, _prt, _update);
+        if (_prt.ServiceType.Value != ServiceType.SecurityEscortProvider)
+          CraeteTrailer(_EDC, _prt, _update);
+      }
+    }
+    private void CraeteTrucks(EntitiesDataContext _EDC, Partner _prt, UpdateToolStripEvent _update)
+    {
+      _update(this, new ProgressChangedEventArgs(1, "CraeteTrucks starting"));
+      VehicleType _vt;
+      if (_prt.ServiceType.Value == ServiceType.SecurityEscortProvider)
+        _vt = VehicleType.SecurityEscortCar;
+      else
+        _vt = VehicleType.Truck;
+      for (int i = 0; i < 2; i++)
+      {
+        string _tm = String.Format("Truck {0}", i);
+        Truck _trck = new Truck() { Tytuł = _tm, VehicleType = _vt, VendorName = _prt };
+        _EDC.Truck.InsertOnSubmit(_trck);
+        _update(this, new ProgressChangedEventArgs(1, String.Format("SubmitChanges for {0}", _tm)));
+        _EDC.SubmitChanges();
+      }
+    }
+    private void CraeteTrailer(EntitiesDataContext _EDC, Partner _prt, UpdateToolStripEvent _update)
+    {
+      _update(this, new ProgressChangedEventArgs(1, "CraeteTrailer starting"));
+      for (int i = 0; i < 2; i++)
+      {
+        string _tm = String.Format("Trailer {0}", i);
+        Trailer _trck = new Trailer() { Tytuł = _tm, VendorName = _prt, };
+        _EDC.Trailer.InsertOnSubmit(_trck);
+        _update(this, new ProgressChangedEventArgs(1, String.Format("SubmitChanges for {0}", _tm)));
+        _EDC.SubmitChanges();
+      }
+    }
+    private void CraeteDrivers(EntitiesDataContext _EDC, Partner _prt, UpdateToolStripEvent _update)
+    {
+      _update(this, new ProgressChangedEventArgs(1, "CraeteDrivers starting"));
+      for (int i = 0; i < 2; i++)
+      {
+        string _tm = String.Format("Driver {0}", i);
+        Driver _drv = new Driver() { Tytuł = _tm, VendorName = _prt, IdentityDocumentNumber = "IdentityDocumentNumber" };
+        _EDC.Driver.InsertOnSubmit(_drv);
+        _update(this, new ProgressChangedEventArgs(1, String.Format("SubmitChanges for {0}", _tm)));
+        _EDC.SubmitChanges();
+      }
     }
     #endregion
-    private void m_TimeSlotsCreateButton_Click(object sender, EventArgs e)
+    #region EventHandlers
+    private void TimeSlotsCreateButton_Click(object sender, EventArgs e)
     {
-      //SPSecurity.RunWithElevatedPrivileges(delegate()
-      //  {
-      //  });
-      using (EntitiesDataContext _EDC = new EntitiesDataContext(m_URLTextBox.Text.Trim()))
+      try
       {
-        CityType.CreateCities(_EDC);
-        CreateCommodity(_EDC);
+        m_Stopwatch.Start();
+        using (EntitiesDataContext _EDC = new EntitiesDataContext(m_URLTextBox.Text.Trim()))
+        {
+          UpdateToolStrip(this, new ProgressChangedEventArgs(1, "CreateCities Starting"));
+          CreateCountries(_EDC, UpdateToolStrip);
+          UpdateToolStrip(this, new ProgressChangedEventArgs(1, "CreateCities CreateCommodity"));
+          CreateCommodity(_EDC, UpdateToolStrip);
+          CreatePartners(_EDC, UpdateToolStrip);
+        }
+        SetDone("Done");
+      }
+      catch (Exception ex)
+      {
+        SetDone(ex.Message);
       }
     }
-    private void button1_Click(object sender, EventArgs e)
+    private void AddTimeSlots_Click(object sender, EventArgs e)
     {
-      using (EntitiesDataContext _EDC = new EntitiesDataContext(m_URLTextBox.Text.Trim()))
+      m_Stopwatch.Start();
+      try
       {
-        foreach (ShippingPoint _sp in from _ei in _EDC.ShippingPoint select _ei)
-          CreateTimeSlots(_EDC, _sp, UpdateToolStrip);
+        UpdateToolStrip(this, new ProgressChangedEventArgs(1, "AddTimeSlots_Click"));
+        using (EntitiesDataContext _EDC = new EntitiesDataContext(m_URLTextBox.Text.Trim()))
+        {
+          foreach (ShippingPoint _sp in from _ei in _EDC.ShippingPoint select _ei)
+            CreateTimeSlots(_EDC, _sp, UpdateToolStrip);
+        }
+        SetDone("Done");
       }
-      SetDone("Done");
+      catch (Exception ex)
+      {
+        SetDone(ex.Message);
+      }
     }
-    private void m_ImportDictionaries_Click(object sender, EventArgs e)
+    private void ImportDictionaries_Click(object sender, EventArgs e)
     {
       Stream strm = OpenFile();
       if (strm == null)
         return;
+      m_Stopwatch.Start();
       try
       {
         using (strm)
@@ -177,7 +281,9 @@ namespace CAS.SmartFactory.Shepherd.ImportExport
       catch (Exception ex)
       {
         MessageBox.Show(ex.Message, "Exception", MessageBoxButtons.OK, MessageBoxIcon.Error);
+        SetDone(ex.Message);
       }
     }
+    #endregion
   }
 }
