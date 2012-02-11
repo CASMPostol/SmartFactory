47d8a8933f4dc8162bc060b4501f6e9578980368
mpostol <mpostol@8a3894fd-0c01-b944-991b-6a3da02cae8a>
2012-02-11 19:19:43 +0000

    [pr4-3076] Load Description WebPart: LoadDescriptionWebPart implemented Issue #: 3076

--- VS/trunk/PR42-SmartFactory/ShepherdDashboards/LoadDescriptionWebPart/LoadDescriptionWebPart.cs
+++ VS/trunk/PR42-SmartFactory/ShepherdDashboards/LoadDescriptionWebPart/LoadDescriptionWebPart.cs
@@ -1,24 +1,67 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.ComponentModel;
-using System.Web;
 using System.Web.UI;
-using System.Web.UI.WebControls;
 using System.Web.UI.WebControls.WebParts;
-using Microsoft.SharePoint;
-using Microsoft.SharePoint.WebControls;
+using CAS.SmartFactory.Shepherd.Dashboards.CarrierDashboard;
 
 namespace CAS.SmartFactory.Shepherd.Dashboards.LoadDescriptionWebPart
 {
-    [ToolboxItemAttribute(false)]
-    public class LoadDescriptionWebPart : WebPart
+  [ToolboxItemAttribute(false)]
+  public class LoadDescriptionWebPart : WebPart
+  {
+    #region private
+    // Visual Studio might automatically update this path when you change the Visual Web Part project item.
+    private const string _ascxPath = @"~/_CONTROLTEMPLATES/CAS.SmartFactory.Shepherd.Dashboards/LoadDescriptionWebPart/LoadDescriptionWebPartUserControl.ascx";
+    private LoadDescriptionWebPartUserControl m_AssociatedUserControl;
+    protected override void CreateChildControls()
     {
-        // Visual Studio might automatically update this path when you change the Visual Web Part project item.
-        private const string _ascxPath = @"~/_CONTROLTEMPLATES/CAS.SmartFactory.Shepherd.Dashboards/LoadDescriptionWebPart/LoadDescriptionWebPartUserControl.ascx";
-
-        protected override void CreateChildControls()
-        {
-            Control control = Page.LoadControl(_ascxPath);
-            Controls.Add(control);
-        }
+      m_AssociatedUserControl = (LoadDescriptionWebPartUserControl)Page.LoadControl(_ascxPath);
+      m_AssociatedUserControl.Role = Role;
+      Controls.Add(m_AssociatedUserControl);
     }
+    protected override void OnPreRender(EventArgs e)
+    {
+      m_AssociatedUserControl.SetInterconnectionData(m_ProvidesDictionary);
+      base.OnPreRender(e);
+    }
+    private Dictionary<InboundInterconnectionData.ConnectionSelector, IWebPartRow> m_ProvidesDictionary = new Dictionary<InboundInterconnectionData.ConnectionSelector, IWebPartRow>();
+    #endregion
+
+    #region public
+    public LoadDescriptionWebPart()
+    {
+      Role = GlobalDefinitions.Roles.None;
+    }
+
+    #region Personalization properties
+    /// <summary>
+    /// Gets or sets the role of the hosting dashboard.
+    /// </summary>
+    /// <value>
+    /// The role.
+    /// </value>
+    [WebBrowsable(true)]
+    [Personalizable(PersonalizationScope.Shared)]
+    [WebDisplayName("The webpart role")]
+    [WebDescription("The role of this webpart. Depending on the role the dashboard customizes" +
+      " the functionality provided to the user.")]
+    [Microsoft.SharePoint.WebPartPages.SPWebCategoryName("CAS Custom Properties")]
+    public GlobalDefinitions.Roles Role { get; set; }
+    #endregion
+
+    #region  Interconnections Providers
+    /// <summary>
+    /// Sets the shipping provider.
+    /// </summary>
+    /// <param name="_provider">The provider interface.</param>
+    [ConnectionConsumer("Shipping table interconnection", "ShippingInterconnection", AllowsMultipleConnections = false)]
+    public void SetShippingProvider(IWebPartRow _provider)
+    {
+      m_ProvidesDictionary.Add(InboundInterconnectionData.ConnectionSelector.ShippingInterconnection, _provider);
+    }
+    #endregion    
+
+    #endregion
+  }
 }
