2e3a90b88c24a5fae83ee95216646766d8eeeb54
Mariusz <mpostol@cas.eu>
2012-02-11 19:19:43 +0000

    [pr4-3076] Load Description WebPart: LoadDescriptionWebPart implemented Issue #: 3076

--- ShepherdDashboards/LoadDescriptionWebPart/LoadDescriptionWebPartUserControl.ascx.cs
+++ ShepherdDashboards/LoadDescriptionWebPart/LoadDescriptionWebPartUserControl.ascx.cs
@@ -5,10 +5,15 @@ using System.Web.UI.WebControls.WebParts;
 
 namespace CAS.SmartFactory.Shepherd.Dashboards.LoadDescriptionWebPart
 {
-    public partial class LoadDescriptionWebPartUserControl : UserControl
+  public partial class LoadDescriptionWebPartUserControl : UserControl
+  {
+    protected void Page_Load(object sender, EventArgs e)
     {
-        protected void Page_Load(object sender, EventArgs e)
-        {
-        }
     }
+    internal void SetInterconnectionData(System.Collections.Generic.Dictionary<CarrierDashboard.InboundInterconnectionData.ConnectionSelector, IWebPartRow> m_ProvidesDictionary)
+    {
+      throw new NotImplementedException();
+    }
+    internal GlobalDefinitions.Roles Role { get; set; }
+  }
 }
