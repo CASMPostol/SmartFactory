eca476fd7b1a248bf9c737c70d06b026be642223
mpostol <mpostol@8a3894fd-0c01-b944-991b-6a3da02cae8a>
2012-02-15 22:02:46 +0000

    [pr4-3076] Load Description WebPart: next step implemented edit and new. Works must be tested. Issue #: 3076

--- VS/trunk/PR42-SmartFactory/ShepherdDashboards/LoadDescriptionWebPart/StateMachineEngine.cs
+++ VS/trunk/PR42-SmartFactory/ShepherdDashboards/LoadDescriptionWebPart/StateMachineEngine.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using CAS.SmartFactory.Shepherd.Dashboards.CarrierDashboard;
 using CAS.SmartFactory.Shepherd.Dashboards.Entities;
+using System.Web.UI.WebControls;
 
 namespace CAS.SmartFactory.Shepherd.Dashboards.LoadDescriptionWebPart
 {
@@ -15,18 +16,18 @@ namespace CAS.SmartFactory.Shepherd.Dashboards.LoadDescriptionWebPart
     internal enum ControlsSet
     {
       SaveOn = 0x01, EditOn = 0x02, CancelOn = 0x04, NewOn = 0x08,
-      AbortOn = 0x10
+      DeleteOn = 0x10
     }
     internal enum InterfaceEvent { SaveClick, EditClick, CancelClick, NewClick, EnterState, AbortClick };
     internal enum InterfaceState { ViewState, EditState, NewState }
 
     #region Connection call back
-    internal void NewDataEventHandler(object sender, ShippingInterconnectionData e)
+    internal void NewDataEventHandler(object sender, ShippingInterconnectionData _InterconnectionData)
     {
       switch (CurrentMachineState)
       {
         case InterfaceState.ViewState:
-          ShowLoading(e);
+          Show(_InterconnectionData);
           break;
         case InterfaceState.EditState:
         case InterfaceState.NewState:
@@ -42,7 +43,7 @@ namespace CAS.SmartFactory.Shepherd.Dashboards.LoadDescriptionWebPart
     #region private
 
     #region Event Handlers
-    internal void NewShippingButton_Click(object sender, EventArgs e)
+    internal void NewButton_Click(object sender, EventArgs e)
     {
       switch (CurrentMachineState)
       {
@@ -61,11 +62,11 @@ namespace CAS.SmartFactory.Shepherd.Dashboards.LoadDescriptionWebPart
       switch (CurrentMachineState)
       {
         case InterfaceState.EditState:
-          UpdateLoading();
+          Update();
           CurrentMachineState = InterfaceState.ViewState;
           break;
         case InterfaceState.NewState:
-          if (this.Createloading())
+          if (this.Create())
             CurrentMachineState = InterfaceState.ViewState;
           break;
         case InterfaceState.ViewState:
@@ -80,7 +81,7 @@ namespace CAS.SmartFactory.Shepherd.Dashboards.LoadDescriptionWebPart
       {
         case InterfaceState.NewState:
         case InterfaceState.EditState:
-          ShowLoading();
+          Show();
           CurrentMachineState = InterfaceState.ViewState;
           break;
         case InterfaceState.ViewState:
@@ -103,6 +104,27 @@ namespace CAS.SmartFactory.Shepherd.Dashboards.LoadDescriptionWebPart
           break;
       }
     }
+    internal void DeleteButton_Click(object sender, EventArgs e)
+    {
+      //TODO zaimplementować
+    }
+    internal void m_LoadDescriptionGridView_SelectedIndexChanged(object sender, EventArgs e)
+    {
+      switch (CurrentMachineState)
+      {
+        case InterfaceState.ViewState:
+          GridView _gw = (GridView)sender;
+          Show(_gw.SelectedDataKey);
+          break;
+        case InterfaceState.EditState:
+        case InterfaceState.NewState:
+        default:
+          SMError(InterfaceEvent.EditClick);
+          break;
+      }
+    }
+
+
     internal void ExceptionCatched(EntitiesDataContext _EDC, string _source, string _message)
     {
       Entities.Anons _entry = new Anons(_source, _message);
@@ -114,7 +136,7 @@ namespace CAS.SmartFactory.Shepherd.Dashboards.LoadDescriptionWebPart
           break;
         case InterfaceState.EditState:
         case InterfaceState.NewState:
-          ShowLoading();
+          Show();
           CurrentMachineState = InterfaceState.ViewState;
           break;
       }
@@ -122,14 +144,15 @@ namespace CAS.SmartFactory.Shepherd.Dashboards.LoadDescriptionWebPart
     #endregion
 
     #region protected
-    protected abstract void ShowLoading(ShippingInterconnectionData _shipping);
-    protected abstract void ShowLoading();
-    protected abstract void UpdateLoading();
-    protected abstract bool Createloading();
+    protected abstract void Show(ShippingInterconnectionData _shipping);
+    protected abstract void Show();
+    protected abstract void Update();
+    protected abstract bool Create();
     protected abstract void ClearUserInterface();
     protected abstract void SetEnabled(ControlsSet _buttons);
     protected abstract void SMError(InterfaceEvent interfaceEvent);
     protected abstract InterfaceState CurrentMachineState { get; set; }
+    protected abstract void Show(DataKey dataKey);
     protected void EnterState()
     {
       switch (CurrentMachineState)
@@ -140,7 +163,7 @@ namespace CAS.SmartFactory.Shepherd.Dashboards.LoadDescriptionWebPart
         case InterfaceState.EditState:
           SetEnabled
             (
-              ControlsSet.CancelOn | ControlsSet.SaveOn | ControlsSet.AbortOn
+              ControlsSet.CancelOn | ControlsSet.SaveOn | ControlsSet.DeleteOn
             );
           break;
         case InterfaceState.NewState:
