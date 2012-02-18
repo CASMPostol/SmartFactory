e02ce765061c995df8da0ef160f3d8708074fc54
Mariusz <mpostol@cas.eu>
2012-02-18 22:57:21 +0000

    [pr4-3108] Shipping save does not work - done. First round of tests - creation shipping for inbound works. Issue #: 3108

--- ShepherdDashboards/ActionResult.cs
+++ ShepherdDashboards/ActionResult.cs
@@ -6,15 +6,16 @@ namespace CAS.SmartFactory.Shepherd.Dashboards
   internal class ActionResult : List<string>
   {
     #region public
+
+    internal bool Valid { get { return this.Count == 0; } }
     internal void AddException(Exception _excptn)
     {
-      string _frmt = "The operation has been interrupted by error {0}.";
-      Add(String.Format(_frmt, _excptn.Message));
+      string _msg = String.Format("The operation interrupted by error {0}.", _excptn.Message);
+      base.Add(String.Format(GlobalDefinitions.ErrorMessageFormat, _msg));
     }
-    internal bool Valid { get { return this.Count == 0; } }
     public new void Add(string _source)
     {
-      string _msg = String.Format("{0} must be provided", _source);
+      string _msg = String.Format("{0} must be provided.", _source);
       base.Add(String.Format(GlobalDefinitions.ErrorMessageFormat, _msg));
     }
     #endregion
