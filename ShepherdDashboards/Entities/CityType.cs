0b6279b1c87dbe6acca6807544a0d67c5750ee95
Mariusz <mpostol@cas.eu>
2012-02-19 18:28:18 +0000

    [pr4-3080] DriverTitle does not have reverse lookup - CityType.cs done - issued fixed.
    Issue #: 3080

--- ShepherdDashboards/Entities/CityType.cs
+++ ShepherdDashboards/Entities/CityType.cs
@@ -7,22 +7,6 @@ namespace CAS.SmartFactory.Shepherd.Dashboards.Entities
 {
   public partial class CityType
   {
-    internal static CityType GetdAtIndex(EntitiesDataContext edc, int? _index)
-    {
-      if (!_index.HasValue)
-        throw new ApplicationException("CityType index is null"); ;
-      try
-      {
-        return (
-              from idx in edc.City
-              where idx.Identyfikator == _index.Value
-              select idx).First();
-      }
-      catch (Exception)
-      {
-        throw new ApplicationException(String.Format("CityType item cannot be found at specified index{0}", _index.Value));
-      }
-    }
     public static void CreateCities(EntitiesDataContext _EDC)
     {
       for (int i = 0; i < 10; i++)
