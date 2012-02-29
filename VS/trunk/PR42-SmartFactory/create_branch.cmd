762700c645a6a409260156577afa5c54935410db
mpostol <mpostol@8a3894fd-0c01-b944-991b-6a3da02cae8a>
2012-02-29 09:59:56 +0000

    [pr4-3152] Release the 1.02.10 - CTP version: Created tags - improved comments in the script Issue #: 3152

--- VS/trunk/PR42-SmartFactory/create_branch.cmd
+++ VS/trunk/PR42-SmartFactory/create_branch.cmd
@@ -9,10 +9,10 @@ if "%branchtype%"=="" goto setbranch
 
 :dothejob
 svn mkdir svn://svnserver.hq.cas.com.pl/VS/%branchtype%/SmartFactory/%1  -m "created new SmartFActory tag  %1 (release folder)"
-svn copy svn://svnserver.hq.cas.com.pl/VS/trunk/PR42-SmartFactory svn://svnserver.hq.cas.com.pl/VS/%branchtype%/SmartFactory/%1/PR42-SmartFactory -m "created new CommServer tag %1 (project PR21-CommServer)"
-svn copy svn://svnserver.hq.cas.com.pl/VS/trunk/PR39-CommonResources svn://svnserver.hq.cas.com.pl/VS/%branchtype%/SmartFactory/%1/PR39-CommonResources -m "created new CommServer tag %1 (project PR39-CommonResources)"
-svn copy svn://svnserver.hq.cas.com.pl/VS/trunk/ImageLibrary svn://svnserver.hq.cas.com.pl/VS/%branchtype%/SmartFactory/%1/ImageLibrary -m "created new CommServer tag %1 (project ImageLibrary)"
-svn copy svn://svnserver.hq.cas.com.pl/VS/trunk/CommonBinaries svn://svnserver.hq.cas.com.pl/VS/%branchtype%/SmartFactory/%1/CommonBinaries -m "created new CommServer tag %1 (project CommonBinaries)"
+svn copy svn://svnserver.hq.cas.com.pl/VS/trunk/PR42-SmartFactory svn://svnserver.hq.cas.com.pl/VS/%branchtype%/SmartFactory/%1/PR42-SmartFactory -m "created new SmartFactorytag %1 (project PR42-SmartFactory)"
+svn copy svn://svnserver.hq.cas.com.pl/VS/trunk/PR39-CommonResources svn://svnserver.hq.cas.com.pl/VS/%branchtype%/SmartFactory/%1/PR39-CommonResources -m "created new SmartFactory tag %1 (project PR39-CommonResources)"
+svn copy svn://svnserver.hq.cas.com.pl/VS/trunk/ImageLibrary svn://svnserver.hq.cas.com.pl/VS/%branchtype%/SmartFactory/%1/ImageLibrary -m "created new SmartFactory tag %1 (project ImageLibrary)"
+svn copy svn://svnserver.hq.cas.com.pl/VS/trunk/CommonBinaries svn://svnserver.hq.cas.com.pl/VS/%branchtype%/SmartFactory/%1/CommonBinaries -m "created new SmartFactory tag %1 (project CommonBinaries)"
 
 goto EXIT
 
