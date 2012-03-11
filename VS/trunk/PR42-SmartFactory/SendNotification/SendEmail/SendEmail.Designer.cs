7ff53790a6de34c433e2780c416678ff6c69e429
mpostol <mpostol@8a3894fd-0c01-b944-991b-6a3da02cae8a>
2012-03-11 21:43:38 +0000

    [pr4-3127] Work-flows implementation - added new project and created the SendEmail work flow - the work flow has been tested.
    Issue #: 3127

---
+++ VS/trunk/PR42-SmartFactory/SendNotification/SendEmail/SendEmail.Designer.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.ComponentModel;
+using System.ComponentModel.Design;
+using System.Collections;
+using System.Drawing;
+using System.Reflection;
+using System.Workflow.ComponentModel.Compiler;
+using System.Workflow.ComponentModel.Serialization;
+using System.Workflow.ComponentModel;
+using System.Workflow.ComponentModel.Design;
+using System.Workflow.Runtime;
+using System.Workflow.Activities;
+using System.Workflow.Activities.Rules;
+
+namespace CAS.SmartFactorySendNotification.SendEmail
+{
+  public sealed partial class SendEmail
+  {
+    #region Designer generated code
+
+    /// <summary> 
+    /// Required method for Designer support - do not modify 
+    /// the contents of this method with the code editor.
+    /// </summary>
+    [System.Diagnostics.DebuggerNonUserCode]
+    private void InitializeComponent()
+    {
+      this.CanModifyActivities = true;
+      System.Workflow.ComponentModel.ActivityBind activitybind1 = new System.Workflow.ComponentModel.ActivityBind();
+      System.Workflow.ComponentModel.ActivityBind activitybind2 = new System.Workflow.ComponentModel.ActivityBind();
+      System.Workflow.Runtime.CorrelationToken correlationtoken1 = new System.Workflow.Runtime.CorrelationToken();
+      System.Workflow.ComponentModel.ActivityBind activitybind3 = new System.Workflow.ComponentModel.ActivityBind();
+      System.Workflow.ComponentModel.ActivityBind activitybind4 = new System.Workflow.ComponentModel.ActivityBind();
+      System.Workflow.ComponentModel.ActivityBind activitybind5 = new System.Workflow.ComponentModel.ActivityBind();
+      System.Workflow.ComponentModel.ActivityBind activitybind6 = new System.Workflow.ComponentModel.ActivityBind();
+      System.Workflow.ComponentModel.ActivityBind activitybind7 = new System.Workflow.ComponentModel.ActivityBind();
+      System.Workflow.ComponentModel.ActivityBind activitybind8 = new System.Workflow.ComponentModel.ActivityBind();
+      System.Workflow.ComponentModel.ActivityBind activitybind9 = new System.Workflow.ComponentModel.ActivityBind();
+      System.Workflow.ComponentModel.ActivityBind activitybind11 = new System.Workflow.ComponentModel.ActivityBind();
+      System.Workflow.ComponentModel.ActivityBind activitybind10 = new System.Workflow.ComponentModel.ActivityBind();
+      this.m_sendEmail = new Microsoft.SharePoint.WorkflowActions.SendEmail();
+      this.m_logToHistoryListActivity1 = new Microsoft.SharePoint.WorkflowActions.LogToHistoryListActivity();
+      this.m_onWorkflowActivated = new Microsoft.SharePoint.WorkflowActions.OnWorkflowActivated();
+      // 
+      // m_sendEmail
+      // 
+      this.m_sendEmail.BCC = null;
+      activitybind1.Name = "SendEmail";
+      activitybind1.Path = "m_sendEmail1_Body";
+      activitybind2.Name = "SendEmail";
+      activitybind2.Path = "m_sendEmail1_CC";
+      correlationtoken1.Name = "workflowToken";
+      correlationtoken1.OwnerActivityName = "SendEmail";
+      this.m_sendEmail.CorrelationToken = correlationtoken1;
+      this.m_sendEmail.Description = "Send notification about new purchase order";
+      activitybind3.Name = "SendEmail";
+      activitybind3.Path = "m_sendEmail1_From";
+      this.m_sendEmail.Headers = null;
+      this.m_sendEmail.IncludeStatus = true;
+      this.m_sendEmail.Name = "m_sendEmail";
+      activitybind4.Name = "SendEmail";
+      activitybind4.Path = "m_sendEmail1_Subject";
+      activitybind5.Name = "SendEmail";
+      activitybind5.Path = "m_sendEmail1_To";
+      this.m_sendEmail.MethodInvoking += new System.EventHandler(this.m_sendEmail1_MethodInvoking);
+      this.m_sendEmail.SetBinding(Microsoft.SharePoint.WorkflowActions.SendEmail.BodyProperty, ((System.Workflow.ComponentModel.ActivityBind)(activitybind1)));
+      this.m_sendEmail.SetBinding(Microsoft.SharePoint.WorkflowActions.SendEmail.CCProperty, ((System.Workflow.ComponentModel.ActivityBind)(activitybind2)));
+      this.m_sendEmail.SetBinding(Microsoft.SharePoint.WorkflowActions.SendEmail.FromProperty, ((System.Workflow.ComponentModel.ActivityBind)(activitybind3)));
+      this.m_sendEmail.SetBinding(Microsoft.SharePoint.WorkflowActions.SendEmail.SubjectProperty, ((System.Workflow.ComponentModel.ActivityBind)(activitybind4)));
+      this.m_sendEmail.SetBinding(Microsoft.SharePoint.WorkflowActions.SendEmail.ToProperty, ((System.Workflow.ComponentModel.ActivityBind)(activitybind5)));
+      // 
+      // m_logToHistoryListActivity1
+      // 
+      this.m_logToHistoryListActivity1.Duration = System.TimeSpan.Parse("-10675199.02:48:05.4775808");
+      this.m_logToHistoryListActivity1.EventId = Microsoft.SharePoint.Workflow.SPWorkflowHistoryEventType.WorkflowStarted;
+      activitybind6.Name = "SendEmail";
+      activitybind6.Path = "m_logToHistoryListActivity1_HistoryDescription";
+      activitybind7.Name = "SendEmail";
+      activitybind7.Path = "m_logToHistoryListActivity1_HistoryOutcome";
+      this.m_logToHistoryListActivity1.Name = "m_logToHistoryListActivity1";
+      activitybind8.Name = "SendEmail";
+      activitybind8.Path = "m_logToHistoryListActivity1_OtherData";
+      activitybind9.Name = "SendEmail";
+      activitybind9.Path = "m_logToHistoryListActivity1_UserId";
+      this.m_logToHistoryListActivity1.MethodInvoking += new System.EventHandler(this.m_logToHistoryListActivity1_MethodInvoking);
+      this.m_logToHistoryListActivity1.SetBinding(Microsoft.SharePoint.WorkflowActions.LogToHistoryListActivity.HistoryDescriptionProperty, ((System.Workflow.ComponentModel.ActivityBind)(activitybind6)));
+      this.m_logToHistoryListActivity1.SetBinding(Microsoft.SharePoint.WorkflowActions.LogToHistoryListActivity.HistoryOutcomeProperty, ((System.Workflow.ComponentModel.ActivityBind)(activitybind7)));
+      this.m_logToHistoryListActivity1.SetBinding(Microsoft.SharePoint.WorkflowActions.LogToHistoryListActivity.UserIdProperty, ((System.Workflow.ComponentModel.ActivityBind)(activitybind9)));
+      this.m_logToHistoryListActivity1.SetBinding(Microsoft.SharePoint.WorkflowActions.LogToHistoryListActivity.OtherDataProperty, ((System.Workflow.ComponentModel.ActivityBind)(activitybind8)));
+      activitybind11.Name = "SendEmail";
+      activitybind11.Path = "workflowId";
+      // 
+      // m_onWorkflowActivated
+      // 
+      this.m_onWorkflowActivated.CorrelationToken = correlationtoken1;
+      this.m_onWorkflowActivated.Description = "Send email workflow.\r\n";
+      this.m_onWorkflowActivated.EventName = "OnWorkflowActivated";
+      this.m_onWorkflowActivated.Name = "m_onWorkflowActivated";
+      activitybind10.Name = "SendEmail";
+      activitybind10.Path = "m_WorkflowProperties";
+      this.m_onWorkflowActivated.Invoked += new System.EventHandler<System.Workflow.Activities.ExternalDataEventArgs>(this.m_onWorkflowActivated_Invoked);
+      this.m_onWorkflowActivated.SetBinding(Microsoft.SharePoint.WorkflowActions.OnWorkflowActivated.WorkflowIdProperty, ((System.Workflow.ComponentModel.ActivityBind)(activitybind11)));
+      this.m_onWorkflowActivated.SetBinding(Microsoft.SharePoint.WorkflowActions.OnWorkflowActivated.WorkflowPropertiesProperty, ((System.Workflow.ComponentModel.ActivityBind)(activitybind10)));
+      // 
+      // SendEmail
+      // 
+      this.Activities.Add(this.m_onWorkflowActivated);
+      this.Activities.Add(this.m_logToHistoryListActivity1);
+      this.Activities.Add(this.m_sendEmail);
+      this.Name = "SendEmail";
+      this.CanModifyActivities = false;
+
+    }
+
+    #endregion
+
+    private Microsoft.SharePoint.WorkflowActions.SendEmail m_sendEmail;
+
+    private Microsoft.SharePoint.WorkflowActions.LogToHistoryListActivity m_logToHistoryListActivity1;
+
+    private Microsoft.SharePoint.WorkflowActions.OnWorkflowActivated m_onWorkflowActivated;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  }
+}
---
+++ VS/trunk/PR42-SmartFactory/SendNotification/SendEmail/SendEmail.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.ComponentModel;
+using System.ComponentModel.Design;
+using System.Collections;
+using System.Drawing;
+using System.Linq;
+using System.Workflow.ComponentModel.Compiler;
+using System.Workflow.ComponentModel.Serialization;
+using System.Workflow.ComponentModel;
+using System.Workflow.ComponentModel.Design;
+using System.Workflow.Runtime;
+using System.Workflow.Activities;
+using System.Workflow.Activities.Rules;
+using Microsoft.SharePoint;
+using Microsoft.SharePoint.Workflow;
+using Microsoft.SharePoint.WorkflowActions;
+using System.Xml.Serialization;
+using CAS.SmartFactorySendNotification.WorkflowData;
+using System.Xml;
+using System.IO;
+using System.Text;
+
+namespace CAS.SmartFactorySendNotification.SendEmail
+{
+  public sealed partial class SendEmail : SequentialWorkflowActivity
+  {
+    public SendEmail()
+    {
+      InitializeComponent();
+    }
+    public Guid workflowId = default(System.Guid);
+    public SPWorkflowActivationProperties m_WorkflowProperties = new SPWorkflowActivationProperties();
+    private string m_EmailSubject;
+    private string m_EmailBody;
+    private void m_onWorkflowActivated_Invoked(object sender, ExternalDataEventArgs e)
+    {
+      // deserialize initiation data; 
+      XmlSerializer serializer = new XmlSerializer(typeof(POLibraryWorkflowAssociationData));
+      XmlTextReader reader = new XmlTextReader(new StringReader(m_WorkflowProperties.InitiationData));
+      POLibraryWorkflowAssociationData _activationData = (POLibraryWorkflowAssociationData)serializer.Deserialize(reader);
+      // assign form data values to workflow fields 
+      EmailMessage(_activationData);
+    }
+    private void EmailMessage(POLibraryWorkflowAssociationData _activationData)
+    {
+      m_EmailSubject = _activationData.Title;
+      StringBuilder _body = new StringBuilder();
+      _body.AppendFormat("HistoryListUrl = {0}", m_WorkflowProperties.HistoryListUrl);
+      _body.AppendLine();
+      _body.AppendFormat("DisplayName = {0}", m_WorkflowProperties.Item.DisplayName);
+      _body.AppendLine();
+      _body.AppendFormat("File.Name = {0}", m_WorkflowProperties.Item.File.Name);
+      _body.AppendLine();
+      _body.AppendFormat("Name = {0}", m_WorkflowProperties.Item.Name);
+      _body.AppendLine();
+      _body.AppendFormat("Title = {0}", m_WorkflowProperties.Item.Title);
+      _body.AppendLine();
+      _body.AppendFormat("Url = {0}", m_WorkflowProperties.Item.Url);
+      _body.AppendLine();
+      _body.AppendFormat("Xml = {0}", m_WorkflowProperties.Item.Xml);
+      _body.AppendLine();
+      m_EmailBody = _body.ToString();
+    }
+    private void m_logToHistoryListActivity1_MethodInvoking(object sender, EventArgs e)
+    {
+      m_logToHistoryListActivity1_HistoryDescription = String.Format("Sending notification to: {0}", m_WorkflowProperties.Item["Carrier"]);
+      m_logToHistoryListActivity1_HistoryOutcome = m_WorkflowProperties.Workflow.ParentAssociation.Name;
+      m_logToHistoryListActivity1_UserId = m_WorkflowProperties.OriginatorUser.ID;
+    }
+    public Int32 m_logToHistoryListActivity1_UserId = default(System.Int32);
+    public String m_logToHistoryListActivity1_HistoryDescription = default(System.String);
+    public String m_logToHistoryListActivity1_HistoryOutcome = default(System.String);
+    public String m_logToHistoryListActivity1_OtherData = default(System.String);
+
+    private void m_sendEmail1_MethodInvoking(object sender, EventArgs e)
+    {
+      m_sendEmail1_Body = m_EmailBody;
+      m_sendEmail1_CC = m_WorkflowProperties.OriginatorEmail;
+      m_sendEmail1_From = m_WorkflowProperties.OriginatorEmail;
+      m_sendEmail1_Subject = m_EmailSubject;
+      m_sendEmail1_To = "mpostol@case.eu";
+    }
+    public String m_sendEmail1_Body = default(System.String);
+    public String m_sendEmail1_CC = default(System.String);
+    public String m_sendEmail1_From = default(System.String);
+    public String m_sendEmail1_Subject = default(System.String);
+    public String m_sendEmail1_To = default(System.String);
+
+  }
+}
