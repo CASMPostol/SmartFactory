5457c0ecdd670e48cc5fb6bb2d11e1a23b3d60f2
mpostol <mpostol@8a3894fd-0c01-b944-991b-6a3da02cae8a>
2012-03-12 21:20:21 +0000

    [pr4-3127] Work-flows implementation - added email template processing functionality - early tested - must be expanded to generate other emails

--- VS/trunk/PR42-SmartFactory/SendNotification/SendEmail/SendEmail.Designer.cs
+++ VS/trunk/PR42-SmartFactory/SendNotification/SendEmail/SendEmail.Designer.cs
@@ -28,67 +28,101 @@ namespace CAS.SmartFactorySendNotification.SendEmail
       this.CanModifyActivities = true;
       System.Workflow.ComponentModel.ActivityBind activitybind1 = new System.Workflow.ComponentModel.ActivityBind();
       System.Workflow.ComponentModel.ActivityBind activitybind2 = new System.Workflow.ComponentModel.ActivityBind();
-      System.Workflow.Runtime.CorrelationToken correlationtoken1 = new System.Workflow.Runtime.CorrelationToken();
       System.Workflow.ComponentModel.ActivityBind activitybind3 = new System.Workflow.ComponentModel.ActivityBind();
       System.Workflow.ComponentModel.ActivityBind activitybind4 = new System.Workflow.ComponentModel.ActivityBind();
       System.Workflow.ComponentModel.ActivityBind activitybind5 = new System.Workflow.ComponentModel.ActivityBind();
       System.Workflow.ComponentModel.ActivityBind activitybind6 = new System.Workflow.ComponentModel.ActivityBind();
       System.Workflow.ComponentModel.ActivityBind activitybind7 = new System.Workflow.ComponentModel.ActivityBind();
       System.Workflow.ComponentModel.ActivityBind activitybind8 = new System.Workflow.ComponentModel.ActivityBind();
+      System.Workflow.Runtime.CorrelationToken correlationtoken1 = new System.Workflow.Runtime.CorrelationToken();
       System.Workflow.ComponentModel.ActivityBind activitybind9 = new System.Workflow.ComponentModel.ActivityBind();
-      System.Workflow.ComponentModel.ActivityBind activitybind11 = new System.Workflow.ComponentModel.ActivityBind();
       System.Workflow.ComponentModel.ActivityBind activitybind10 = new System.Workflow.ComponentModel.ActivityBind();
-      this.m_sendEmail = new Microsoft.SharePoint.WorkflowActions.SendEmail();
+      System.Workflow.ComponentModel.ActivityBind activitybind11 = new System.Workflow.ComponentModel.ActivityBind();
+      System.Workflow.ComponentModel.ActivityBind activitybind13 = new System.Workflow.ComponentModel.ActivityBind();
+      System.Workflow.ComponentModel.ActivityBind activitybind12 = new System.Workflow.ComponentModel.ActivityBind();
+      this._OnFaultLogToHistoryListActivity = new Microsoft.SharePoint.WorkflowActions.LogToHistoryListActivity();
+      this.m_FaultHandlerActivity = new System.Workflow.ComponentModel.FaultHandlerActivity();
+      this.m_faultHandlersActivity = new System.Workflow.ComponentModel.FaultHandlersActivity();
       this.m_logToHistoryListActivity1 = new Microsoft.SharePoint.WorkflowActions.LogToHistoryListActivity();
+      this.m_sendEmail = new Microsoft.SharePoint.WorkflowActions.SendEmail();
       this.m_onWorkflowActivated = new Microsoft.SharePoint.WorkflowActions.OnWorkflowActivated();
       // 
-      // m_sendEmail
+      // _OnFaultLogToHistoryListActivity
       // 
-      this.m_sendEmail.BCC = null;
+      this._OnFaultLogToHistoryListActivity.Description = "On Fault Log To History List";
+      this._OnFaultLogToHistoryListActivity.Duration = System.TimeSpan.Parse("-10675199.02:48:05.4775808");
+      this._OnFaultLogToHistoryListActivity.EventId = Microsoft.SharePoint.Workflow.SPWorkflowHistoryEventType.WorkflowError;
       activitybind1.Name = "SendEmail";
-      activitybind1.Path = "m_sendEmail1_Body";
+      activitybind1.Path = "_OnFaultLogToHistoryListActivity_HistoryDescription1";
       activitybind2.Name = "SendEmail";
-      activitybind2.Path = "m_sendEmail1_CC";
-      correlationtoken1.Name = "workflowToken";
-      correlationtoken1.OwnerActivityName = "SendEmail";
-      this.m_sendEmail.CorrelationToken = correlationtoken1;
-      this.m_sendEmail.Description = "Send notification about new purchase order";
-      activitybind3.Name = "SendEmail";
-      activitybind3.Path = "m_sendEmail1_From";
-      this.m_sendEmail.Headers = null;
-      this.m_sendEmail.IncludeStatus = true;
-      this.m_sendEmail.Name = "m_sendEmail";
-      activitybind4.Name = "SendEmail";
-      activitybind4.Path = "m_sendEmail1_Subject";
-      activitybind5.Name = "SendEmail";
-      activitybind5.Path = "m_sendEmail1_To";
-      this.m_sendEmail.MethodInvoking += new System.EventHandler(this.m_sendEmail1_MethodInvoking);
-      this.m_sendEmail.SetBinding(Microsoft.SharePoint.WorkflowActions.SendEmail.BodyProperty, ((System.Workflow.ComponentModel.ActivityBind)(activitybind1)));
-      this.m_sendEmail.SetBinding(Microsoft.SharePoint.WorkflowActions.SendEmail.CCProperty, ((System.Workflow.ComponentModel.ActivityBind)(activitybind2)));
-      this.m_sendEmail.SetBinding(Microsoft.SharePoint.WorkflowActions.SendEmail.FromProperty, ((System.Workflow.ComponentModel.ActivityBind)(activitybind3)));
-      this.m_sendEmail.SetBinding(Microsoft.SharePoint.WorkflowActions.SendEmail.SubjectProperty, ((System.Workflow.ComponentModel.ActivityBind)(activitybind4)));
-      this.m_sendEmail.SetBinding(Microsoft.SharePoint.WorkflowActions.SendEmail.ToProperty, ((System.Workflow.ComponentModel.ActivityBind)(activitybind5)));
+      activitybind2.Path = "_OnFaultLogToHistoryListActivity_HistoryOutcome1";
+      this._OnFaultLogToHistoryListActivity.Name = "_OnFaultLogToHistoryListActivity";
+      this._OnFaultLogToHistoryListActivity.OtherData = "";
+      this._OnFaultLogToHistoryListActivity.UserId = -1;
+      this._OnFaultLogToHistoryListActivity.MethodInvoking += new System.EventHandler(this._OnFaultLogToHistoryListActivity_MethodInvoking);
+      this._OnFaultLogToHistoryListActivity.SetBinding(Microsoft.SharePoint.WorkflowActions.LogToHistoryListActivity.HistoryDescriptionProperty, ((System.Workflow.ComponentModel.ActivityBind)(activitybind1)));
+      this._OnFaultLogToHistoryListActivity.SetBinding(Microsoft.SharePoint.WorkflowActions.LogToHistoryListActivity.HistoryOutcomeProperty, ((System.Workflow.ComponentModel.ActivityBind)(activitybind2)));
+      // 
+      // m_FaultHandlerActivity
+      // 
+      this.m_FaultHandlerActivity.Activities.Add(this._OnFaultLogToHistoryListActivity);
+      this.m_FaultHandlerActivity.Description = "Fault Handler Activity";
+      this.m_FaultHandlerActivity.FaultType = typeof(System.Exception);
+      this.m_FaultHandlerActivity.Name = "m_FaultHandlerActivity";
+      // 
+      // m_faultHandlersActivity
+      // 
+      this.m_faultHandlersActivity.Activities.Add(this.m_FaultHandlerActivity);
+      this.m_faultHandlersActivity.Description = "Handles to deal with any errors";
+      this.m_faultHandlersActivity.Name = "m_faultHandlersActivity";
       // 
       // m_logToHistoryListActivity1
       // 
       this.m_logToHistoryListActivity1.Duration = System.TimeSpan.Parse("-10675199.02:48:05.4775808");
-      this.m_logToHistoryListActivity1.EventId = Microsoft.SharePoint.Workflow.SPWorkflowHistoryEventType.WorkflowStarted;
-      activitybind6.Name = "SendEmail";
-      activitybind6.Path = "m_logToHistoryListActivity1_HistoryDescription";
-      activitybind7.Name = "SendEmail";
-      activitybind7.Path = "m_logToHistoryListActivity1_HistoryOutcome";
+      this.m_logToHistoryListActivity1.EventId = Microsoft.SharePoint.Workflow.SPWorkflowHistoryEventType.WorkflowCompleted;
+      activitybind3.Name = "SendEmail";
+      activitybind3.Path = "m_logToHistoryListActivity1_HistoryDescription";
+      activitybind4.Name = "SendEmail";
+      activitybind4.Path = "m_logToHistoryListActivity1_HistoryOutcome";
       this.m_logToHistoryListActivity1.Name = "m_logToHistoryListActivity1";
-      activitybind8.Name = "SendEmail";
-      activitybind8.Path = "m_logToHistoryListActivity1_OtherData";
-      activitybind9.Name = "SendEmail";
-      activitybind9.Path = "m_logToHistoryListActivity1_UserId";
+      activitybind5.Name = "SendEmail";
+      activitybind5.Path = "m_logToHistoryListActivity1_OtherData";
+      activitybind6.Name = "SendEmail";
+      activitybind6.Path = "m_logToHistoryListActivity1_UserId";
       this.m_logToHistoryListActivity1.MethodInvoking += new System.EventHandler(this.m_logToHistoryListActivity1_MethodInvoking);
-      this.m_logToHistoryListActivity1.SetBinding(Microsoft.SharePoint.WorkflowActions.LogToHistoryListActivity.HistoryDescriptionProperty, ((System.Workflow.ComponentModel.ActivityBind)(activitybind6)));
-      this.m_logToHistoryListActivity1.SetBinding(Microsoft.SharePoint.WorkflowActions.LogToHistoryListActivity.HistoryOutcomeProperty, ((System.Workflow.ComponentModel.ActivityBind)(activitybind7)));
-      this.m_logToHistoryListActivity1.SetBinding(Microsoft.SharePoint.WorkflowActions.LogToHistoryListActivity.UserIdProperty, ((System.Workflow.ComponentModel.ActivityBind)(activitybind9)));
-      this.m_logToHistoryListActivity1.SetBinding(Microsoft.SharePoint.WorkflowActions.LogToHistoryListActivity.OtherDataProperty, ((System.Workflow.ComponentModel.ActivityBind)(activitybind8)));
+      this.m_logToHistoryListActivity1.SetBinding(Microsoft.SharePoint.WorkflowActions.LogToHistoryListActivity.HistoryDescriptionProperty, ((System.Workflow.ComponentModel.ActivityBind)(activitybind3)));
+      this.m_logToHistoryListActivity1.SetBinding(Microsoft.SharePoint.WorkflowActions.LogToHistoryListActivity.HistoryOutcomeProperty, ((System.Workflow.ComponentModel.ActivityBind)(activitybind4)));
+      this.m_logToHistoryListActivity1.SetBinding(Microsoft.SharePoint.WorkflowActions.LogToHistoryListActivity.UserIdProperty, ((System.Workflow.ComponentModel.ActivityBind)(activitybind6)));
+      this.m_logToHistoryListActivity1.SetBinding(Microsoft.SharePoint.WorkflowActions.LogToHistoryListActivity.OtherDataProperty, ((System.Workflow.ComponentModel.ActivityBind)(activitybind5)));
+      // 
+      // m_sendEmail
+      // 
+      this.m_sendEmail.BCC = null;
+      activitybind7.Name = "SendEmail";
+      activitybind7.Path = "m_sendEmail1_Body";
+      activitybind8.Name = "SendEmail";
+      activitybind8.Path = "m_sendEmail1_CC";
+      correlationtoken1.Name = "workflowToken";
+      correlationtoken1.OwnerActivityName = "SendEmail";
+      this.m_sendEmail.CorrelationToken = correlationtoken1;
+      this.m_sendEmail.Description = "Send notification about new purchase order";
+      activitybind9.Name = "SendEmail";
+      activitybind9.Path = "m_sendEmail1_From";
+      this.m_sendEmail.Headers = null;
+      this.m_sendEmail.IncludeStatus = true;
+      this.m_sendEmail.Name = "m_sendEmail";
+      activitybind10.Name = "SendEmail";
+      activitybind10.Path = "m_sendEmail1_Subject";
       activitybind11.Name = "SendEmail";
-      activitybind11.Path = "workflowId";
+      activitybind11.Path = "m_sendEmail1_To";
+      this.m_sendEmail.MethodInvoking += new System.EventHandler(this.m_sendEmail1_MethodInvoking);
+      this.m_sendEmail.SetBinding(Microsoft.SharePoint.WorkflowActions.SendEmail.BodyProperty, ((System.Workflow.ComponentModel.ActivityBind)(activitybind7)));
+      this.m_sendEmail.SetBinding(Microsoft.SharePoint.WorkflowActions.SendEmail.CCProperty, ((System.Workflow.ComponentModel.ActivityBind)(activitybind8)));
+      this.m_sendEmail.SetBinding(Microsoft.SharePoint.WorkflowActions.SendEmail.FromProperty, ((System.Workflow.ComponentModel.ActivityBind)(activitybind9)));
+      this.m_sendEmail.SetBinding(Microsoft.SharePoint.WorkflowActions.SendEmail.SubjectProperty, ((System.Workflow.ComponentModel.ActivityBind)(activitybind10)));
+      this.m_sendEmail.SetBinding(Microsoft.SharePoint.WorkflowActions.SendEmail.ToProperty, ((System.Workflow.ComponentModel.ActivityBind)(activitybind11)));
+      activitybind13.Name = "SendEmail";
+      activitybind13.Path = "workflowId";
       // 
       // m_onWorkflowActivated
       // 
@@ -96,17 +130,18 @@ namespace CAS.SmartFactorySendNotification.SendEmail
       this.m_onWorkflowActivated.Description = "Send email workflow.\r\n";
       this.m_onWorkflowActivated.EventName = "OnWorkflowActivated";
       this.m_onWorkflowActivated.Name = "m_onWorkflowActivated";
-      activitybind10.Name = "SendEmail";
-      activitybind10.Path = "m_WorkflowProperties";
+      activitybind12.Name = "SendEmail";
+      activitybind12.Path = "m_WorkflowProperties";
       this.m_onWorkflowActivated.Invoked += new System.EventHandler<System.Workflow.Activities.ExternalDataEventArgs>(this.m_onWorkflowActivated_Invoked);
-      this.m_onWorkflowActivated.SetBinding(Microsoft.SharePoint.WorkflowActions.OnWorkflowActivated.WorkflowIdProperty, ((System.Workflow.ComponentModel.ActivityBind)(activitybind11)));
-      this.m_onWorkflowActivated.SetBinding(Microsoft.SharePoint.WorkflowActions.OnWorkflowActivated.WorkflowPropertiesProperty, ((System.Workflow.ComponentModel.ActivityBind)(activitybind10)));
+      this.m_onWorkflowActivated.SetBinding(Microsoft.SharePoint.WorkflowActions.OnWorkflowActivated.WorkflowIdProperty, ((System.Workflow.ComponentModel.ActivityBind)(activitybind13)));
+      this.m_onWorkflowActivated.SetBinding(Microsoft.SharePoint.WorkflowActions.OnWorkflowActivated.WorkflowPropertiesProperty, ((System.Workflow.ComponentModel.ActivityBind)(activitybind12)));
       // 
       // SendEmail
       // 
       this.Activities.Add(this.m_onWorkflowActivated);
-      this.Activities.Add(this.m_logToHistoryListActivity1);
       this.Activities.Add(this.m_sendEmail);
+      this.Activities.Add(this.m_logToHistoryListActivity1);
+      this.Activities.Add(this.m_faultHandlersActivity);
       this.Name = "SendEmail";
       this.CanModifyActivities = false;
 
@@ -114,11 +149,27 @@ namespace CAS.SmartFactorySendNotification.SendEmail
 
     #endregion
 
+    private Microsoft.SharePoint.WorkflowActions.LogToHistoryListActivity _OnFaultLogToHistoryListActivity;
+
+    private FaultHandlerActivity m_FaultHandlerActivity;
+
+    private FaultHandlersActivity m_faultHandlersActivity;
+
     private Microsoft.SharePoint.WorkflowActions.SendEmail m_sendEmail;
 
     private Microsoft.SharePoint.WorkflowActions.LogToHistoryListActivity m_logToHistoryListActivity1;
 
     private Microsoft.SharePoint.WorkflowActions.OnWorkflowActivated m_onWorkflowActivated;
+
+
+
+
+
+
+
+
+
+
 
 
 
---
+++ VS/trunk/PR42-SmartFactory/SendNotification/WorkflowData/FreightPurchaseOrderTemplate.partial.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CAS.SmartFactorySendNotification.WorkflowData
+{
+  public partial class FreightPurchaseOrderTemplate
+  {
+    public string FPO2RouteGoodsHandlingPO { get; set; }
+    public string FPO2CountryTitle { get; set; }
+    public string FPO2CityTitle { get; set; }
+    public DateTime FPOLoadingDate { get; set; }
+    public string FPO2TransportUnitTypeTitle { get; set; }
+    public string FPO2CommodityTitle { get; set; }
+    public DateTime Modified { get; set; }
+    public string ModifiedBy { get; set; }
+    public Uri Encodedabsurl { get; set; }
+    public string DocumentName { get; set; }
+  }
+}
