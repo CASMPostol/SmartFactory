0b0eecaf8f356b9cd041d44bb8a983416f2c58ea
Mariusz <mpostol@cas.eu>
2012-03-12 21:20:21 +0000

    [pr4-3127] Work-flows implementation - added email template processing functionality - early tested - must be expanded to generate other emails

---
+++ SendNotification/Extensions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CAS.SmartFactorySendNotification
+{
+  public static class Extensions
+  {
+    public static string Cast2String(this object value)
+    {
+      return value == null ? String.Empty : (string)value;
+    }
+  }
+}
--- SendNotification/SendEmail/SendEmail.cs
+++ SendNotification/SendEmail/SendEmail.cs
@@ -29,38 +29,73 @@ namespace CAS.SmartFactorySendNotification.SendEmail
       InitializeComponent();
     }
     public Guid workflowId = default(System.Guid);
-    public SPWorkflowActivationProperties m_WorkflowProperties = new SPWorkflowActivationProperties();
+    public SPWorkflowActivationProperties m_WorkflowProperties = default(SPWorkflowActivationProperties);
     private string m_EmailSubject;
-    private string m_EmailBody;
+    private FreightPurchaseOrderTemplate _emailBodyObject = new FreightPurchaseOrderTemplate();
     private void m_onWorkflowActivated_Invoked(object sender, ExternalDataEventArgs e)
     {
-      // deserialize initiation data; 
-      XmlSerializer serializer = new XmlSerializer(typeof(POLibraryWorkflowAssociationData));
-      XmlTextReader reader = new XmlTextReader(new StringReader(m_WorkflowProperties.InitiationData));
-      POLibraryWorkflowAssociationData _activationData = (POLibraryWorkflowAssociationData)serializer.Deserialize(reader);
-      // assign form data values to workflow fields 
-      EmailMessage(_activationData);
+      POLibraryWorkflowAssociationData _activationData = default(POLibraryWorkflowAssociationData);
+      try
+      {
+        // deserialize initiation data; 
+        XmlSerializer serializer = new XmlSerializer(typeof(POLibraryWorkflowAssociationData));
+        XmlTextReader reader = new XmlTextReader(new StringReader(m_WorkflowProperties.InitiationData));
+        _activationData = (POLibraryWorkflowAssociationData)serializer.Deserialize(reader);
+      }
+      catch (Exception ex)
+      {
+        string _frmt = "Worflow aborted in WorkflowActivated because of error: {0}";
+        throw new ApplicationException(String.Format(_frmt, ex.Message));
+      }
+      _emailBodyObject = CreateEmailMessage(_activationData);
     }
-    private void EmailMessage(POLibraryWorkflowAssociationData _activationData)
+    private FreightPurchaseOrderTemplate CreateEmailMessage(POLibraryWorkflowAssociationData _activationData)
     {
-      m_EmailSubject = _activationData.Title;
-      StringBuilder _body = new StringBuilder();
-      _body.AppendFormat("HistoryListUrl = {0}", m_WorkflowProperties.HistoryListUrl);
-      _body.AppendLine();
-      _body.AppendFormat("DisplayName = {0}", m_WorkflowProperties.Item.DisplayName);
-      _body.AppendLine();
-      _body.AppendFormat("File.Name = {0}", m_WorkflowProperties.Item.File.Name);
-      _body.AppendLine();
-      _body.AppendFormat("Name = {0}", m_WorkflowProperties.Item.Name);
-      _body.AppendLine();
-      _body.AppendFormat("Title = {0}", m_WorkflowProperties.Item.Title);
-      _body.AppendLine();
-      _body.AppendFormat("Url = {0}", m_WorkflowProperties.Item.Url);
-      _body.AppendLine();
-      _body.AppendFormat("Xml = {0}", m_WorkflowProperties.Item.Xml);
-      _body.AppendLine();
-      m_EmailBody = _body.ToString();
+      try
+      {
+        m_EmailSubject = _activationData.Title;
+        return new FreightPurchaseOrderTemplate()
+        {
+          Encodedabsurl = new Uri((string)m_WorkflowProperties.Item["EncodedAbsUrl"]),
+          FPO2CityTitle = m_WorkflowProperties.Item["FPO2CityTitle"].Cast2String(),
+          FPO2CommodityTitle = m_WorkflowProperties.Item["FPO2CommodityTitle"].Cast2String(),
+          FPO2CountryTitle = m_WorkflowProperties.Item["FPO2CountryTitle"].Cast2String(),
+          FPO2RouteGoodsHandlingPO = m_WorkflowProperties.Item["FPO2RouteGoodsHandlingPO"].Cast2String(),
+          FPO2TransportUnitTypeTitle = m_WorkflowProperties.Item["FPO2TransportUnitTypeTitle"].Cast2String(),
+          FPOLoadingDate = (DateTime)m_WorkflowProperties.Item["FPOLoadingDate"],
+          Modified = (DateTime)m_WorkflowProperties.Item["Modified"],
+          ModifiedBy = m_WorkflowProperties.Item["Editor"].Cast2String(),
+          DocumentName = m_WorkflowProperties.Item.File.Name,
+        };
+      }
+      catch (Exception ex)
+      {
+        string _frmt = "Worflow aborted in CreateEmailMessage because of error: {0}";
+        throw new ApplicationException(String.Format(_frmt, ex.Message));
+      }
     }
+    private void m_sendEmail1_MethodInvoking(object sender, EventArgs e)
+    {
+      try
+      {
+        m_sendEmail1_Body = _emailBodyObject.TransformText();
+      }
+      catch (Exception ex)
+      {
+        string _frmt = "Worflow aborted in TransformText because of the error: {0}";
+        throw new ApplicationException(String.Format(_frmt, ex.Message));
+      }
+      m_sendEmail1_CC = m_WorkflowProperties.OriginatorEmail;
+      m_sendEmail1_From = m_WorkflowProperties.OriginatorEmail;
+      m_sendEmail1_Subject = m_EmailSubject;
+      m_sendEmail1_To = "mpostol@cas.eu";
+    }
+    public String m_sendEmail1_Body = default(System.String);
+    public String m_sendEmail1_CC = default(System.String);
+    public String m_sendEmail1_From = default(System.String);
+    public String m_sendEmail1_Subject = default(System.String);
+    public String m_sendEmail1_To = default(System.String);
+
     private void m_logToHistoryListActivity1_MethodInvoking(object sender, EventArgs e)
     {
       m_logToHistoryListActivity1_HistoryDescription = String.Format("Sending notification to: {0}", m_WorkflowProperties.Item["Carrier"]);
@@ -72,19 +107,14 @@ namespace CAS.SmartFactorySendNotification.SendEmail
     public String m_logToHistoryListActivity1_HistoryOutcome = default(System.String);
     public String m_logToHistoryListActivity1_OtherData = default(System.String);
 
-    private void m_sendEmail1_MethodInvoking(object sender, EventArgs e)
-    {
-      m_sendEmail1_Body = m_EmailBody;
-      m_sendEmail1_CC = m_WorkflowProperties.OriginatorEmail;
-      m_sendEmail1_From = m_WorkflowProperties.OriginatorEmail;
-      m_sendEmail1_Subject = m_EmailSubject;
-      m_sendEmail1_To = "mpostol@case.eu";
-    }
-    public String m_sendEmail1_Body = default(System.String);
-    public String m_sendEmail1_CC = default(System.String);
-    public String m_sendEmail1_From = default(System.String);
-    public String m_sendEmail1_Subject = default(System.String);
-    public String m_sendEmail1_To = default(System.String);
 
+
+    public String _OnFaultLogToHistoryListActivity_HistoryDescription1 = default(System.String);
+    public String _OnFaultLogToHistoryListActivity_HistoryOutcome1 = default(System.String);
+    private void _OnFaultLogToHistoryListActivity_MethodInvoking(object sender, EventArgs e)
+    {
+      _OnFaultLogToHistoryListActivity_HistoryOutcome1 = "Error";
+      _OnFaultLogToHistoryListActivity_HistoryDescription1 = m_FaultHandlerActivity.Fault.Message;
+    }
   }
 }
