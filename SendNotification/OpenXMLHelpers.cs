23bb6731fa52d4a5dad7c745a0bd751e026e38d9
Mariusz <mpostol@cas.eu>
2012-03-22 17:28:50 +0000

    [pr4-3127] Work-flows implementation - created OpenXMLHelpers.cs helpers. Issue #: 3127

---
+++ SendNotification/OpenXMLHelpers.cs
@@ -0,0 +1,30 @@
+﻿using System.IO;
+using DocumentFormat.OpenXml;
+using DocumentFormat.OpenXml.Packaging;
+using Microsoft.SharePoint;
+
+namespace CAS.SmartFactory.Shepherd.SendNotification
+{
+  internal static class OpenXMLHelpers
+  {
+    internal static SPFile AddDocument2Collection(SPFile _template, SPFileCollection _dstCollection, string _fileName)
+    {
+      SPFile _docFile = default(SPFile);
+      using (Stream _tmpStrm = _template.OpenBinaryStream())
+      using (MemoryStream _docStrm = new MemoryStream())
+      {
+        byte[] _buff = new byte[_tmpStrm.Length + 200];
+        int _leng = _tmpStrm.Read(_buff, 0, (int)_tmpStrm.Length);
+        _docStrm.Write(_buff, 0, _leng);
+        _docStrm.Position = 0;
+        WordprocessingDocument _doc = WordprocessingDocument.Open(_docStrm, true);
+        _doc.ChangeDocumentType(WordprocessingDocumentType.Document);
+        _doc.Close();
+        _docFile = _dstCollection.Add(_fileName, _docStrm, true);
+        _docStrm.Flush();
+        _docStrm.Close();
+      }
+      return _docFile;
+    }
+  }
+}
