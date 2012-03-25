cac0322ecf0064a5de24206e4ad190ddb90decaf
mpostol <mpostol@8a3894fd-0c01-b944-991b-6a3da02cae8a>
2012-03-25 19:50:54 +0000

    [pr4-3218] Escort PO - PO document - added templates to the list. The workflow shout be tested.
    Issue #: 3218

--- VS/trunk/PR42-SmartFactory/ShepherdDashboards/GlobalDefinitions.cs
+++ VS/trunk/PR42-SmartFactory/ShepherdDashboards/GlobalDefinitions.cs
@@ -23,6 +23,12 @@ namespace CAS.SmartFactory.Shepherd.Dashboards
     }
     internal const string NumberOfTimeSLotsFormat = "<b><font size=\"1\" color=\"#0072bc\"> [{0}]</font></b>";
     internal const string ErrorMessageFormat = "<font color=red>{0}</font><br/>";
+    internal const string FreightPurchaseOrderTemplate = @"Lists/FreightPOLibrary/Forms/FreightPurchaseOrderTemplate.dotx";
+    internal const string EscortPOLibraryTemplate = @"Lists/EscortPOLibrary/Forms/SecurityEscortPurchaseOrderTemplate.dotx";
+    internal const string SealProtocolLibraryTemplate = @"Lists/SealProtocolLibrary/Forms/SecuritySealProtocolTemplate.dotx";
+    internal const string FreightPOLibraryTitle = "Freight PO Library";
+    internal const string EscortPOLibraryTitle = "Escort PO Library";
+    internal const string SealProtocolLibraryTitle = "Seal Protocol Library";
     public delegate void UpdateToolStripEvent(object obj, ProgressChangedEventArgs progres);
   }
 }
