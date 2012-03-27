bb479d70f5d6cbd831888c67b344d916cbd843d9
mpostol <mpostol@8a3894fd-0c01-b944-991b-6a3da02cae8a>
2012-03-27 21:09:57 +0000

    [pr4-3065] TRWP must change the shipping state - fixed - it works Issue #: 3065

--- VS/trunk/PR42-SmartFactory/ShepherdDashboards/CarrierDashboard/TransportResources/TransportResourcesUserControl.ascx.cs
+++ VS/trunk/PR42-SmartFactory/ShepherdDashboards/CarrierDashboard/TransportResources/TransportResourcesUserControl.ascx.cs
@@ -71,7 +71,16 @@ namespace CAS.SmartFactory.Shepherd.Dashboards.CarrierDashboard.TransportResourc
       if (e.ID == m_ControlState.ShippingIdx)
         return;
       m_ControlState.ShippingIdx = e.ID;
-      UpdateUserInterface();
+      try
+      {
+        using (EntitiesDataContext edc = new EntitiesDataContext(SPContext.Current.Web.Url))
+          UpdateUserInterface(edc);
+      }
+      catch (Exception ex)
+      {
+        string _frmt = "User interface update error for shipping {0} with message: {1}";
+        this.Controls.Add(new LiteralControl(String.Format(_frmt, m_ControlState.ShippingIdx, ex.Message)));
+      }
     }
     #endregion
 
@@ -79,57 +88,48 @@ namespace CAS.SmartFactory.Shepherd.Dashboards.CarrierDashboard.TransportResourc
     /// <summary>
     /// Clears the user interface.
     /// </summary>
-    private void UpdateUserInterface()
+    private void UpdateUserInterface(EntitiesDataContext edc)
     {
       ClearUserInterface();
       if (m_ControlState.ShippingIdx.IsNullOrEmpty())
         return;
-      try
+      Shipping _Shipping = Element.GetAtIndex<Shipping>(edc.Shipping, m_ControlState.ShippingIdx);
+      Partner _prtn = Role == TransportResources.RolesSet.Carrier ? _Shipping.VendorName : _Shipping.SecurityEscortProvider;
+      if (_prtn == null)
+        return; ;
+      Dictionary<int, Driver> _drivers = _prtn.Driver.ToDictionary(x => x.Identyfikator.Value);
+      foreach (ShippingDriversTeam item in from idx in _Shipping.ShippingDriversTeam select idx)
       {
-        using (EntitiesDataContext edc = new EntitiesDataContext(SPContext.Current.Web.Url))
-        {
-          Shipping _Shipping = Element.GetAtIndex<Shipping>(edc.Shipping, m_ControlState.ShippingIdx);
-          Partner _prtn = Role == TransportResources.RolesSet.Carrier ? _Shipping.VendorName : _Shipping.SecurityEscortProvider;
-          if (_prtn == null)
-            return; ;
-          Dictionary<int, Driver> _drivers = _prtn.Driver.ToDictionary(x => x.Identyfikator.Value);
-          foreach (ShippingDriversTeam item in from idx in _Shipping.ShippingDriversTeam select idx)
-          {
-            Driver _driver = item.Driver;
-            if (Role == TransportResources.RolesSet.SecurityEscort && _driver.VendorName.ServiceType.Value != ServiceType.SecurityEscortProvider)
-              continue;
-            m_DriversTeamListBox.Items.Add(new ListItem(_driver.Tytuł, item.Identyfikator.Value.ToString()));
-            _drivers.Remove(_driver.Identyfikator.Value);
-          }
-          foreach (var item in _drivers)
-            m_DriversListBox.Items.Add(new ListItem(item.Value.Tytuł, item.Key.ToString()));
-          m_ShippingTextBox.Text = _Shipping.Tytuł;
-          m_TruckDropDown.Items.Add(new ListItem());
-          foreach (Truck _item in _prtn.Truck)
-          {
-            if (Role == TransportResources.RolesSet.SecurityEscort && _item.VendorName.ServiceType.Value != ServiceType.SecurityEscortProvider)
-              continue;
-            ListItem _li = new ListItem(_item.Tytuł, _item.Identyfikator.Value.ToString());
-            m_TruckDropDown.Items.Add(_li);
-            if (_Shipping.TruckCarRegistrationNumber == _item)
-              _li.Selected = true;
-          }
-          m_TrailerDropDown.Items.Add(new ListItem());
-          foreach (Trailer item in _prtn.Trailer)
-          {
-            ListItem _li = new ListItem(item.Tytuł, item.Identyfikator.Value.ToString());
-            m_TrailerDropDown.Items.Add(_li);
-            if (_Shipping.TrailerRegistrationNumber == item)
-              _li.Selected = true;
-          }
-        }
-        SetButtons(true);
+        Driver _driver = item.Driver;
+        if ((Role == TransportResources.RolesSet.SecurityEscort && _driver.VendorName.ServiceType.Value != ServiceType.SecurityEscortProvider) ||
+           (Role == TransportResources.RolesSet.Carrier && _driver.VendorName.ServiceType.Value == ServiceType.SecurityEscortProvider))
+          continue;
+        m_DriversTeamListBox.Items.Add(new ListItem(_driver.Tytuł, item.Identyfikator.Value.ToString()));
+        _drivers.Remove(_driver.Identyfikator.Value);
       }
-      catch (Exception ex)
+      foreach (var item in _drivers)
+        m_DriversListBox.Items.Add(new ListItem(item.Value.Tytuł, item.Key.ToString()));
+      m_ShippingTextBox.Text = _Shipping.Tytuł;
+      m_TruckDropDown.Items.Add(new ListItem());
+      foreach (Truck _item in _prtn.Truck)
       {
-        string _frmt = "User interface update error for shipping {0} with message: {1}";
-        this.Controls.Add(new LiteralControl(String.Format(_frmt, m_ControlState.ShippingIdx, ex.Message)));
+        if ((Role == TransportResources.RolesSet.SecurityEscort && _item.VendorName.ServiceType.Value != ServiceType.SecurityEscortProvider) ||
+          (Role == TransportResources.RolesSet.Carrier && _item.VendorName.ServiceType.Value == ServiceType.SecurityEscortProvider))
+          continue;
+        ListItem _li = new ListItem(_item.Tytuł, _item.Identyfikator.Value.ToString());
+        m_TruckDropDown.Items.Add(_li);
+        if (_Shipping.TruckCarRegistrationNumber == _item)
+          _li.Selected = true;
       }
+      m_TrailerDropDown.Items.Add(new ListItem());
+      foreach (Trailer item in _prtn.Trailer)
+      {
+        ListItem _li = new ListItem(item.Tytuł, item.Identyfikator.Value.ToString());
+        m_TrailerDropDown.Items.Add(_li);
+        if (_Shipping.TrailerRegistrationNumber == item)
+          _li.Selected = true;
+      }
+      SetButtons(true);
     }
     private void ClearUserInterface()
     {
@@ -162,15 +162,19 @@ namespace CAS.SmartFactory.Shepherd.Dashboards.CarrierDashboard.TransportResourc
         using (EntitiesDataContext edc = new EntitiesDataContext(SPContext.Current.Web.Url))
         {
           ShippingDriversTeam _cd = Element.GetAtIndex<ShippingDriversTeam>(edc.DriversTeam, _sel.Value);
+          Shipping _sh = _cd.ShippingIndex;
+          _cd.Driver = null;
+          _cd.ShippingIndex = null;
           edc.DriversTeam.DeleteOnSubmit(_cd);
-          _cd.ShippingIndex.CalculateState();
           edc.SubmitChanges();
+          _sh.CalculateState();
+          edc.SubmitChanges();
+          UpdateUserInterface(edc);
         }
-        UpdateUserInterface();
       }
       catch (Exception ex)
       {
-        SignalException("Remove driver error: {0}", ex);
+        SignalException("m_RemoveDriverButton_Click", "Remove driver error: {0}", ex);
       }
     }
     private void m_AddDriverButton_Click(object sender, EventArgs e)
@@ -187,16 +191,16 @@ namespace CAS.SmartFactory.Shepherd.Dashboards.CarrierDashboard.TransportResourc
               Driver = Element.GetAtIndex<Driver>(edc.Driver, _sel.Value),
               ShippingIndex = Element.GetAtIndex(edc.Shipping, m_ControlState.ShippingIdx)
             };
-          _cd.ShippingIndex.SetStateOnDrivers(m_DriversTeamListBox.Items.Count + 1, Role == TransportResources.RolesSet.Carrier);
           edc.DriversTeam.InsertOnSubmit(_cd);
+          edc.SubmitChanges();
           _cd.ShippingIndex.CalculateState();
           edc.SubmitChanges();
+          UpdateUserInterface(edc);
         }
-        UpdateUserInterface();
       }
       catch (Exception ex)
       {
-        SignalException("Add driver error: {0}", ex);
+        SignalException("m_AddDriverButton_Click", "Add driver error: {0}", ex);
       }
     }
     private void m_TruckDropDown_SelectedIndexChanged(object sender, EventArgs e)
@@ -210,15 +214,24 @@ namespace CAS.SmartFactory.Shepherd.Dashboards.CarrierDashboard.TransportResourc
         {
           Shipping _sh = Element.GetAtIndex<Shipping>(edc.Shipping, m_ControlState.ShippingIdx);
           if (String.IsNullOrEmpty(_li.Value))
-            _sh.TruckCarRegistrationNumber = null;
+            if (Role == TransportResources.RolesSet.Carrier)
+              _sh.TruckCarRegistrationNumber = null;
+            else
+              _sh.SecurityEscortCarRegistrationNumber = null;
           else
-            _sh.TruckCarRegistrationNumber = Element.GetAtIndex<Truck>(edc.Truck, _li.Value);
+          {
+            Truck _ct = Element.GetAtIndex<Truck>(edc.Truck, _li.Value);
+            if (Role == TransportResources.RolesSet.Carrier)
+              _sh.TruckCarRegistrationNumber = _ct;
+            else
+              _sh.SecurityEscortCarRegistrationNumber = _ct;
+          }
           _sh.CalculateState();
           edc.SubmitChanges();
         }
         catch (Exception _ex)
         {
-          SignalException("Truck selection error: {0}", _ex);
+          SignalException("m_TruckDropDown_SelectedIndexChanged", "Truck selection error: {0}", _ex);
         }
       }
     }
@@ -241,7 +254,7 @@ namespace CAS.SmartFactory.Shepherd.Dashboards.CarrierDashboard.TransportResourc
         }
         catch (Exception _ex)
         {
-          SignalException("Truck selection error: {0}", _ex);
+          SignalException("m_TrailerDropDown_SelectedIndexChanged", "Truck selection error: {0}", _ex);
         }
       }
     }
@@ -253,9 +266,14 @@ namespace CAS.SmartFactory.Shepherd.Dashboards.CarrierDashboard.TransportResourc
         _ret.Items.Add(new ListItem(item.Text, item.Value));
       return _ret;
     }
-    private void SignalException(string _format, Exception _ex)
+    private void SignalException(string _source, string _format, Exception _ex)
     {
-      this.Controls.Add(new LiteralControl(String.Format(_format, _ex.Message)));
+      string _msg = String.Format(_format, _ex.Message);
+      this.Controls.Add(new LiteralControl(_msg ));
+      using (EntitiesDataContext edc = new EntitiesDataContext(SPContext.Current.Web.Url))
+      {
+        Anons.WriteEntry(edc, _source, _msg);
+      }
     }
     #endregion
 
