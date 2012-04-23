0503793283526518e29f252a5170b93c5561829a
mpostol <mpostol@8a3894fd-0c01-b944-991b-6a3da02cae8a>
2012-04-23 11:13:18 +0000

    [pr4-3287] Create installation SAT package - new version 1.40.11 has been setup for all assemblies and features. Deleted old, removed form solution   feature folder. Issue #: 3287

--- VS/trunk/PR42-SmartFactory/SendNotification/Properties/AssemblyInfo.cs
+++ VS/trunk/PR42-SmartFactory/SendNotification/Properties/AssemblyInfo.cs
@@ -6,13 +6,13 @@ using System.Security;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("SendNotification")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("Shepherd Workflows")]
+[assembly: AssemblyDescription("Shepherd Workflows collection")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Microsoft")]
-[assembly: AssemblyProduct("SendNotification")]
-[assembly: AssemblyCopyright("Copyright © Microsoft 2012")]
-[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCompany("CAS")]
+[assembly: AssemblyProduct("SmartFactory Shepherd")]
+[assembly: AssemblyCopyright("Copyright © CAS2012")]
+[assembly: AssemblyTrademark("SmartFactory")]
 [assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
@@ -33,5 +33,5 @@ using System.Security;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.40.10.*")]
-[assembly: AssemblyFileVersion("1.40.10")]
+[assembly: AssemblyVersion("1.40.11.*")]
+[assembly: AssemblyFileVersion("1.40.11")]
