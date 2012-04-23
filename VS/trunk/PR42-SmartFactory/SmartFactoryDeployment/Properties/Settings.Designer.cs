c3d029792cf954fb342c8b023ef662ccedcc2fe4
mpostol <mpostol@8a3894fd-0c01-b944-991b-6a3da02cae8a>
2012-04-23 08:56:18 +0000

    [pr4-3305] SendNotification solution throws exception during deployment - deploring the solution we must set force because after retracking there are still exist the included feature and assemblies in the GAC. Improved user interface.
    Issue #: 3305

--- VS/trunk/PR42-SmartFactory/SmartFactoryDeployment/Properties/Settings.Designer.cs
+++ VS/trunk/PR42-SmartFactory/SmartFactoryDeployment/Properties/Settings.Designer.cs
@@ -25,18 +25,6 @@ namespace CAS.SmartFactory.Deployment.Properties {
         
         [global::System.Configuration.UserScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("shepherd")]
-        public string SiteCollectionURL {
-            get {
-                return ((string)(this["SiteCollectionURL"]));
-            }
-            set {
-                this["SiteCollectionURL"] = value;
-            }
-        }
-        
-        [global::System.Configuration.UserScopedSettingAttribute()]
-        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
         [global::System.Configuration.DefaultSettingValueAttribute("False")]
         public bool ManualMode {
             get {
