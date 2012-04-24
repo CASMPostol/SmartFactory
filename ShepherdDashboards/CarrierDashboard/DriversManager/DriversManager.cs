7a569e770e8770f0f1d91cf2e0aed86db0bfdd7a
bwojciechowski <bwojciechowski@8a3894fd-0c01-b944-991b-6a3da02cae8a>
2012-04-24 13:39:40 +0000

    [pr4-3311] Webparts for Driver, Truck and Trailer managment - webparts added, they are located in CarierDashboard folder. Issue #: 3311

---
+++ ShepherdDashboards/CarrierDashboard/DriversManager/DriversManager.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.ComponentModel;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Web.UI.WebControls.WebParts;
+using Microsoft.SharePoint;
+using Microsoft.SharePoint.WebControls;
+
+namespace CAS.SmartFactory.Shepherd.Dashboards.CarrierDashboard.DriversManager
+{
+    [ToolboxItemAttribute(false)]
+    public class DriversManager : WebPart
+    {
+        // Visual Studio might automatically update this path when you change the Visual Web Part project item.
+        private const string _ascxPath = @"~/_CONTROLTEMPLATES/CAS.SmartFactory.Shepherd.Dashboards.CarrierDashboard/DriversManager/DriversManagerUserControl.ascx";
+
+        protected override void CreateChildControls()
+        {
+            Control control = Page.LoadControl(_ascxPath);
+            Controls.Add(control);
+        }
+    }
+}
---
+++ ShepherdDashboards/CarrierDashboard/DriversManager/DriversManagerUserControl.ascx.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Web.UI.WebControls.WebParts;
+
+namespace CAS.SmartFactory.Shepherd.Dashboards.CarrierDashboard.DriversManager
+{
+    public partial class DriversManagerUserControl : UserControl
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+        }
+    }
+}
