86ac8cc7843bc0b1aaf0305d6ca967c84e22eb01
Mariusz <mpostol@cas.eu>
2012-05-09 09:51:34 +0000

    [pr4-3332] Remove compilation error after content type localization - next step - moving the model to the SPMetalHelper Issue #: 3332

---
+++ SPMetalHelper/Entities/Partner.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.SharePoint;
+
+namespace CAS.SmartFactory.Shepherd.Dashboards.Entities
+{
+  public partial class Partner
+  {
+    internal static Partner FindForUser(EntitiesDataContext edc, SPUser _user)
+    {
+      if (edc.Partner == null)
+        return null;
+      else
+        return edc.Partner.FirstOrDefault(idx => idx.ShepherdUserTitle.IsNullOrEmpty() ? false : idx.ShepherdUserTitle.Contains(_user.Name));
+    }
+  }
+}
