777476731087ac1782494556d73b964cfdc1f204
Mariusz <mpostol@cas.eu>
2012-05-14 14:50:15 +0000

    [pr4-3353] Localize code of the CarrierDashboard - DriversManagerUserControl done
    Issue #: 3353

--- ShepherdDashboards/CarrierDashboard/TrailerManager/StateMachine.cs
+++ ShepherdDashboards/CarrierDashboard/TrailerManager/StateMachine.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
-namespace CAS.SmartFactory.Shepherd.Dashboards.CarrierDashboard.TrailerManager
+﻿namespace CAS.SmartFactory.Shepherd.Dashboards.CarrierDashboard.TrailerManager
 {
   internal abstract class StateMachine : GenericStateMachineEngine<TrailerInterconnectionData>
   {
