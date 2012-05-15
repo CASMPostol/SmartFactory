6c0cc303c6e5f84b7bc5694c79366bc1f63ef301
Mariusz <mpostol@cas.eu>
2012-05-15 10:30:38 +0000

    [pr4-3359] Localize the SendNotification solution - ShippingStateMachine done Issue #: 3359

--- SendNotification/WorkflowDescription.cs
+++ SendNotification/WorkflowDescription.cs
@@ -10,6 +10,12 @@ namespace CAS.SmartFactory.Shepherd.SendNotification
     internal Guid WorkflowId { get; private set; }
     internal string Name { get; private set; }
     internal string Description { get; private set; }
+    /// <summary>
+    /// Initializes a new instance of the <see cref="WorkflowDescription"/> struct.
+    /// </summary>
+    /// <param name="_wrkflwId">The identifier.</param>
+    /// <param name="_name">The name.</param>
+    /// <param name="_description">The description.</param>
     public WorkflowDescription(Guid _wrkflwId, string _name, string _description)
       : this()
     {
