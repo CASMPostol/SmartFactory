9313082114cda346cd17fb161c05c95a12f5367a
Mariusz <mpostol@cas.eu>
2012-05-30 12:58:58 +0000

    [pr4-3383] Clean project names: phase 4 - removed folder IPRO.Data - seems to be useless.
    Issue #: 3383

--- XML/Customs/RequiredDocumentsDescription.cs
+++
@@ -1,9 +0,0 @@
-﻿
-namespace CAS.SmartFactory.xml.Customs
-{
-  public abstract class RequiredDocumentsDescription
-  {
-    public abstract string GetCode();
-    public abstract string GetNumber();
-  }
-}
