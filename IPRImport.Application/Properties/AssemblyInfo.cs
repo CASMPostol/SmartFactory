4860e4f996222458d3a4895fac4e62ab61850b7b
Mariusz <mpostol@cas.eu>
2012-06-01 19:40:12 +0000

    [pr4-3383] Clean project names - used T4 do create AssemblyInfo.cs - reopened to cleanup features and packages names.name.
    Issue #: 3383

--- IPRImport.Application/Properties/AssemblyInfo.cs
+++ IPRImport.Application/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("CAS.SmartFactory.Management")]
+[assembly: AssemblyTitle("CAS.SmartFactory.IPRImport")]
 [assembly: AssemblyDescription("Server site management functions")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("CAS")]
