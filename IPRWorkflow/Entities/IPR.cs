164ae0a44844d8d135320b35caa64f45b101b6f5
Mariusz <mpostol@cas.eu>
2012-06-06 08:59:37 +0000

    [pr4-3386] SADImportXML - add analysis - getting data required by the IPR from the declaration document  added regular expressions to get details from the GoodsDescription - tested using http://gskinner.com/RegExr/
    Issue #: 3386

--- IPRWorkflow/Entities/IPR.cs
+++ IPRWorkflow/Entities/IPR.cs
@@ -2,15 +2,17 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using CAS.SmartFactory.xml.Customs;
+using System.Text.RegularExpressions;
 
 namespace CAS.SmartFactory.IPR.Entities
 {
   public partial class IPR
   {
-    internal static void CreateIPRAccount(EntitiesDataContext _edc, SADDocumentType _document, Clearence _nc)
+    internal static void CreateIPRAccount(EntitiesDataContext _edc, SADDocumentType _document, Clearence _nc, CustomsDocument.DocumentType _messageType)
     {
       Consent _consent = Consent.Lookup(_edc, _document.GetConsentNo());
-      IPRData _iprdata = new IPRData(_document);
+      IPRData _iprdata = new IPRData(_document, _messageType);
       IPR _ipr = new IPR()
       {
         Cartons = _iprdata.Cartons,
@@ -47,11 +49,92 @@ namespace CAS.SmartFactory.IPR.Entities
     /// </summary>
     internal class IPRData
     {
-      private SADDocumentType _document;
-      public IPRData(SADDocumentType _document)
+      public SADGood FirstSADGood { get; private set; }
+      private void AnalizeGood(SADDocumentType _document, CustomsDocument.DocumentType _messageType)
       {
-        // TODO: Complete member initialization
-        this._document = _document;
+        SADPackage _packagex = FirstSADGood.SADPackage.First();
+        if (_messageType == CustomsDocument.DocumentType.SAD)
+          GrossMass = FirstSADGood.GrossMass.HasValue ? FirstSADGood.GrossMass.Value : _document.GrossMass.Value;
+        else if (_messageType == CustomsDocument.DocumentType.PZC)
+          GrossMass = _document.GrossMass.HasValue ? _document.GrossMass.Value : FirstSADGood.GrossMass.Value;
+        else
+          throw new CustomsDataException("IPRData.GetCartons", String.Format("Unexpected message {0} message", _messageType));
+        SADQuantity _quantity = FirstSADGood.SADQuantity.First();
+        NetMass = _quantity.NetMass.Value;
+        if (_packagex.Package.ToUpper().Contains("CT"))
+          Cartons = GrossMass - NetMass;
+        else
+          Cartons = 0;
+      }
+      private void AnalizeDutyAndVAT()
+      {
+        Duty = 0;
+        VAT = 0;
+        DutyName = string.Empty;
+        VATName = string.Empty;
+        foreach (SADDuties _duty in FirstSADGood.SADDuties)
+        {
+          switch (_duty.Tytuł)
+          {
+            //Duty
+            case "A10":
+            case "A00":
+            case "A20":
+              Duty += _duty.Amount.Value;
+              DutyName += String.Format("{0}={1:F2}; ", _duty.Tytuł, _duty.Amount.Value);
+              break;
+            //VAT
+            case "B00":
+            case "B10":
+            case "B20":
+              VAT += _duty.Amount.Value;
+              VATName += String.Format("{0}={1:F2}; ", _duty.Tytuł, _duty.Amount.Value);
+              break;
+            default:
+              break;
+          }
+        }
+        DutyPerUnit = Duty / NetMass;
+        VATPerUnit = VAT / NetMass;
+        Value = FirstSADGood.TotalAmountInvoiced.Value; 
+        UnitPrice = Value / NetMass;
+      }
+      public IPRData(SADDocumentType _document, CustomsDocument.DocumentType _messageType)
+      {
+        FirstSADGood = _document.SADGood.First();
+        AnalizeGood(_document, _messageType);
+        AnalizeDutyAndVAT();
+        InvoiceNo = (
+                        from _dx in FirstSADGood.SADRequiredDocuments
+                        let CustomsProcedureCode = _dx.Code.ToUpper()
+                        where CustomsProcedureCode.Contains("'N380") || CustomsProcedureCode.Contains("N935")
+                        select new { Number = _dx.Number }
+                     ).First().Number;
+        Match _tnm = Regex.Match(FirstSADGood.GoodsDescription, @"", RegexOptions.IgnoreCase);
+        const string UnrecognizedName = "-- unrecognized name --";
+        if (_tnm.Success && _tnm.Groups.Count == 1)
+          TobaccoName = _tnm.Captures[1].Value;
+        else
+          TobaccoName = UnrecognizedName;
+        //TODO - intialize column from GoodsDescription [pr4-3398] List: IPR - columns should be updated http://itrserver/Bugs/BugDetail.aspx?bid=3398
+        _tnm = Regex.Match(FirstSADGood.GoodsDescription, @"(?<=\WGRADE:)\W*\b(\w*)", RegexOptions.IgnoreCase);
+        string GradeName = String.Empty;
+        if (_tnm.Success && _tnm.Groups.Count == 1)
+          GradeName = _tnm.Captures[1].Value;
+        else
+          TobaccoName = UnrecognizedName;
+        _tnm = Regex.Match(FirstSADGood.GoodsDescription, @"(?<=\WSKU:)\W*\b(\d*)", RegexOptions.IgnoreCase);
+        string SKU = String.Empty;
+        if (_tnm.Success && _tnm.Groups.Count == 1)
+          SKU = _tnm.Captures[1].Value;
+        else
+          SKU = UnrecognizedName;
+        _tnm = Regex.Match(FirstSADGood.GoodsDescription, @"(?<=\WBatch:)\W*\b(\d*)", RegexOptions.IgnoreCase);
+        string Batch = String.Empty;
+        if (_tnm.Success && _tnm.Groups.Count == 1)
+          Batch = _tnm.Captures[1].Value;
+        else
+          Batch = UnrecognizedName;
       }
       public double Cartons { get; private set; }
       public double Duty { get; private set; } // TODO   http://itrserver/Bugs/BugDetail.aspx?bid=3393
