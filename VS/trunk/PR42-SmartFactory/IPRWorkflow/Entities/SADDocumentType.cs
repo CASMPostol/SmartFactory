97b551a14299d7c454c8c3f5c1548d3dd72ab055
mpostol <mpostol@8a3894fd-0c01-b944-991b-6a3da02cae8a>
2012-06-07 10:35:43 +0000

    [pr4-3386] SADImportXML - add analysis - reorganization of the code, working copy Issue #: 3386

--- VS/trunk/PR42-SmartFactory/IPRWorkflow/Entities/SADDocumentType.cs
+++ VS/trunk/PR42-SmartFactory/IPRWorkflow/Entities/SADDocumentType.cs
@@ -9,5 +9,29 @@ namespace CAS.SmartFactory.IPR.Entities
   public partial class SADDocumentType
   {
 
+    #region public
+    internal void ReExportOfGoods(EntitiesDataContext _edc, xml.Customs.CustomsDocument.DocumentType _messageType)
+    {
+      ClearenceID = FimdClearence(_edc);
+      foreach (SADGood _sg in SADGood)
+      {
+        if (_sg.Procedure.RequestedProcedure() != CustomsProcedureCodes.ReExport)
+          throw new CustomsDataException("Clearence.Create", String.Format("IE529 contains invalid customs procedure {0}", _sg.Tytuł));
+        throw new NotImplementedException();
+      }
+    }
+    internal static void ReleaseForFreeCirculation(EntitiesDataContext _edc)
+    {
+      //TODO NotImplementedException
+      throw new NotImplementedException();
+    } 
+    #endregion
+
+    #region private
+    private Clearence FimdClearence(EntitiesDataContext _edc)
+    {
+      throw new NotImplementedException();
+    }
+    #endregion
   }
 }
