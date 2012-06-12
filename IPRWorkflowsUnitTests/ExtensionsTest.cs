4e0dcd94253459a65dbc80198bcada97e4fa119f
Mariusz <mpostol@cas.eu>
2012-06-12 13:11:36 +0000

    [pr4-3405] Goods description analysis failed - tested by unit tests Issue #: 3405

--- IPRWorkflowsUnitTests/ExtensionsTest.cs
+++ IPRWorkflowsUnitTests/ExtensionsTest.cs
@@ -14,10 +14,7 @@ namespace IPRWorkflowsUnitTests
   [TestClass()]
   public class ExtensionsTest
   {
-
-
     private TestContext testContextInstance;
-
     /// <summary>
     ///Gets or sets the test context which provides
     ///information about and functionality for the current test run.
@@ -64,6 +61,7 @@ namespace IPRWorkflowsUnitTests
     //
     #endregion
 
+    const string _value = "TYTON ODZYLOWANY BRAZIL FLUE CURED VIRGINIA STRIPS                                                                                 GRADE:XBRFCB SKU:12352149 BATCH:0003814564                                       czesciowa likwidacja OGL/362010/00/002494/12";
 
     /// <summary>
     ///A test for SPValidSubstring
@@ -71,7 +69,6 @@ namespace IPRWorkflowsUnitTests
     [TestMethod()]
     public void SPValidSubstringTest()
     {
-      string _value = "TYTON ODZYLOWANY BRAZIL FLUE CURED VIRGINIA STRIPS                                                                                 GRADE:XBRFCB SKU:12352149 BATCH:0003814564                                       czesciowa likwidacja OGL/362010/00/002494/12";
       string _cchar = "\0x10";
       _value.Insert(10, _cchar);
       string actual;
@@ -79,6 +76,31 @@ namespace IPRWorkflowsUnitTests
       Assert.IsFalse(actual.Contains("  "), "Text should not contain double spaces");
       Assert.IsTrue(actual.Length <= 254, "Text is too long");
       Assert.IsTrue(actual.Min<char>( ) >= 0x20, "Text is too long");
+    }
+
+    /// <summary>
+    ///A test for GetFirstCapture
+    ///</summary>
+    [TestMethod()]
+    public void GetFirstCaptureTest()
+    {
+      string _pattern = @"\b(.*)(?=\sGRADE:)"; 
+      string expected = "TYTON ODZYLOWANY BRAZIL FLUE CURED VIRGINIA STRIPS"; 
+      string actual;
+      actual = Extensions.GetFirstCapture(Extensions.SPValidSubstring(_value), _pattern);
+      Assert.AreEqual(expected, actual);
+      _pattern = @"(?<=\WGRADE:)\W*\b(\w*)";
+      expected = "XBRFCB"; 
+      actual = Extensions.GetFirstCapture(Extensions.SPValidSubstring(_value), _pattern);
+      Assert.AreEqual(expected, actual);
+      _pattern = @"(?<=\WSKU:)\W*\b(\d*)";
+      expected = "12352149";
+      actual = Extensions.GetFirstCapture(Extensions.SPValidSubstring(_value), _pattern);
+      Assert.AreEqual(expected, actual);
+      _pattern = @"(?<=\WBatch:)\W*\b(\d*)";
+      expected = "0003814564";
+      actual = Extensions.GetFirstCapture(Extensions.SPValidSubstring(_value), _pattern);
+      Assert.AreEqual(expected, actual);
     }
   }
 }
