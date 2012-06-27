a2c68e76ecc27285c09892bc96a3af01c189d759
mpostol <mpostol@8a3894fd-0c01-b944-991b-6a3da02cae8a>
2012-06-27 13:52:53 +0000

    [pr4-2936] IPR - check the batch import mechanism - fixed mistake - compilation bug. Issue #: 2936

--- VS/trunk/PR42-SmartFactory/SPMetalHelper/Extensions.cs
+++ VS/trunk/PR42-SmartFactory/SPMetalHelper/Extensions.cs
@@ -42,7 +42,7 @@ namespace CAS.SmartFactory
     {
       return _val.HasValue ? string.Format(_format, _val.Value.ToString(CultureInfo.CurrentUICulture)) : String.Empty;
     }
-    public static const DateTime SPMinimum = new DateTime(1900, 1, 1);
+    public static DateTime SPMinimum = new DateTime(1900, 1, 1);
     internal const string UnknownEmail = "unknown@comapny.com";
     public static string UnknownIfEmpty(this String _val)
     {
