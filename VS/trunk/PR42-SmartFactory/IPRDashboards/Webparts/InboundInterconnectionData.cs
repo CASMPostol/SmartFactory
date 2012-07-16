f31b2fa3205d5c513966590ef4a2527a7db8d6a1
mpostol <mpostol@8a3894fd-0c01-b944-991b-6a3da02cae8a>
2012-07-16 07:34:04 +0000

    [pr4-3387] Move the model generation to the separate project - added reference to the SharePoint library and used common data model in the SMMetalHelper. Issue #: 3387

---
+++ VS/trunk/PR42-SmartFactory/IPRDashboards/Webparts/InboundInterconnectionData.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Data;
+using System.Web.UI.WebControls.WebParts;
+using CAS.SmartFactory.Linq.IPR;
+
+namespace CAS.SmartFactory.Shepherd.Dashboards.CarrierDashboard
+{
+
+  internal abstract class InterconnectionData<DerivedType> : CAS.SharePoint.Web.InterconnectionData<DerivedType>
+      where DerivedType: InterconnectionData<DerivedType>
+
+  {
+    internal enum ConnectionSelector
+    {
+      //TrailerInterconnection,
+      //TruckInterconnection,
+      //ShippingInterconnection,
+      //TimeSlotInterconnection,
+      //PartnerInterconnection,
+      //CityInterconnection,
+      //DriverInterconnection,
+      //RouteInterconnection,
+      //SecurityEscortCatalogInterconnection
+    }
+    internal string ID { get { return GetFieldValue( Element.IDColunmName ); } }
+    internal string Title { get { return GetFieldValue( Element.TitleColunmName ); } }
+  }
+  //internal class TrailerInterconnectionData : InterconnectionData<TrailerInterconnectionData>
+  //{
+  //  internal TrailerInterconnectionData()
+  //    : base()
+  //  { }
+  //}
+}
