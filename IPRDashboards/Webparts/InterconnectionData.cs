509c07b8e4b9b4efecf86b7976d83d31f723a03a
Mariusz <mpostol@cas.eu>
2012-07-22 09:48:13 +0000

    [pr4-3492] Create Export Finished Goods webpart - defined interconnection class - changed file name Issue #: 3492

---
+++ IPRDashboards/Webparts/InterconnectionData.cs
@@ -0,0 +1,36 @@
+﻿using CAS.SmartFactory.Linq.IPR;
+
+namespace CAS.SmartFactory.IPR.Dashboards.Webparts
+{
+
+  internal enum ConnectionSelector
+  {
+    BatchInterconnection,
+    InvoiceInterconnection,
+    InvoiceContentInterconnection
+  }
+  internal abstract class InterconnectionData<DerivedType>: CAS.SharePoint.Web.InterconnectionData<DerivedType>
+      where DerivedType: InterconnectionData<DerivedType>
+  {
+    internal string ID { get { return GetFieldValue( Element.IDColunmName ); } }
+    internal string Title { get { return GetFieldValue( Element.TitleColunmName ); } }
+  }
+  internal class BatchInterconnectionData: InterconnectionData<BatchInterconnectionData>
+  {
+    internal BatchInterconnectionData()
+      : base()
+    { }
+  }
+  internal class InvoiceInterconnectionData: InterconnectionData<InvoiceInterconnectionData>
+  {
+    internal InvoiceInterconnectionData()
+      : base()
+    { }
+  }
+  internal class InvoiceContentInterconnectionnData: InterconnectionData<InvoiceContentInterconnectionnData>
+  {
+    internal InvoiceContentInterconnectionnData()
+      : base()
+    { }
+  }
+}
