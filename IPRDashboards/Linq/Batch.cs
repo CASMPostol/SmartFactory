4a58b1396105b23a327d7cc4cc56b839f2914a41
Mariusz <mpostol@cas.eu>
2012-07-29 17:56:53 +0000

    [pr4-3540] ExportWebPart - add button Export and column Read Only - added missed files. Fixed preliminary batch creation. Issue #: 3540

---
+++ IPRDashboards/Linq/Batch.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using CAS.SharePoint;
+using CAS.SmartFactory.IPR.Dashboards;
+
+namespace CAS.SmartFactory.Linq.IPR
+{
+  public partial class Batch
+  {
+    internal void Export( double? quantity, List<ExportConsignment> consignment )
+    {
+      this.FGQuantityAvailable -= quantity.Value;
+      double _portion = quantity.Value / this.FGQuantityKUKg.Value;
+      foreach ( var _didx in this.Material )
+      {
+
+      }
+    }
+    internal ActionResult ExportPossible( double? quantity )
+    {
+      ActionResult _result = new ActionResult();
+      if ( !quantity.HasValue )
+      {
+        string _message = String.Format( Resources.NotValidValue.GetLocalizedString( GlobalDefinitions.RootResourceFileName ), "Quantity" );
+        _result.AddMessage( "ExportPossible", _message );
+        return _result;
+      }
+      else if ( this.FGQuantityAvailable.Value < quantity.Value )
+      {
+        string _message = String.Format( Resources.QuantityIsUnavailable.GetLocalizedString( GlobalDefinitions.RootResourceFileName ), this.FGQuantityAvailable.Value );
+        _result.AddMessage( "ExportPossible", _message );
+        return _result;
+      }
+      double _portion = quantity.Value / this.FGQuantityKUKg.Value;
+      foreach ( Material _didx in this.Material )
+      {
+        _didx.ExportPossible( quantity.Value, this.MaterialQuantity * _portion, _result );
+      }
+      return _result;
+    }
+  }
+}
---
+++ IPRDashboards/Linq/Material.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using CAS.SmartFactory.Linq.IPR;
+
+namespace CAS.SmartFactory.Linq.IPR
+{
+  public partial class Material
+  {
+    internal void ExportPossible( double p, double? nullable, ActionResult _result )
+    {
+      if ( this.ProductType.Value != Linq.IPR.ProductType.IPRTobacco )
+        return;
+      //foreach ( var item in this. )
+      //{
+        
+      //}
+    }
+
+  }
+}
