1883048afba5716ccc4e3adcfc64201ff09f06b0
bwojciechowski <bwojciechowski@8a3894fd-0c01-b944-991b-6a3da02cae8a>
2012-08-02 11:49:54 +0000

    [pr4-3575] Create Clearence Manager Webpart - webpart and clearence dashboard (WebPartPages/ClearenceDashboard.aspx) added. Issue #: 3575

---
+++ IPRDashboards/Webparts/ClearenceWebPart/ClearenceWebPart.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.ComponentModel;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Web.UI.WebControls.WebParts;
+using Microsoft.SharePoint;
+using Microsoft.SharePoint.WebControls;
+
+namespace CAS.SmartFactory.IPR.Dashboards.Webparts.ClearenceWebPart
+{
+    [ToolboxItemAttribute(false)]
+    public class ClearenceWebPart : WebPart
+    {
+        // Visual Studio might automatically update this path when you change the Visual Web Part project item.
+        private const string _ascxPath = @"~/_CONTROLTEMPLATES/CAS.SmartFactory.IPR.Dashboards.Webparts/ClearenceWebPart/ClearenceWebPartUserControl.ascx";
+
+        protected override void CreateChildControls()
+        {
+            Control control = Page.LoadControl(_ascxPath);
+            Controls.Add(control);
+        }
+    }
+}
