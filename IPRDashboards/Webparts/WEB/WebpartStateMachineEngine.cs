f256021ff8167169d3df99592b7378217d4601b2
Mariusz <mpostol@cas.eu>
2012-08-06 09:54:33 +0000

    [pr4-3587] Export bug: Input string was not in a correct format. - refactoring - added diagnostic instrumentation. Issue #: 3587

---
+++ IPRDashboards/Webparts/WEB/WebpartStateMachineEngine.cs
@@ -0,0 +1,22 @@
+﻿using CAS.SharePoint;
+using CAS.SharePoint.Web;
+using CAS.SmartFactory.Linq.IPR;
+
+namespace CAS.SmartFactory.IPR.Dashboards.Webparts.WEB
+{
+  internal abstract class WebpartStateMachineEngine: GenericStateMachineEngine
+  {
+    protected override void ShowActionResult( ActionResult _rslt )
+    {
+      switch ( _rslt.LastActionResult )
+      {
+        case ActionResult.Result.Success:
+        case ActionResult.Result.NotValidated:
+          break;
+        case ActionResult.Result.Exception:
+          Anons.WriteEntry( _rslt.ActionException.Source, _rslt.ActionException.Message );
+          break;
+      }
+    }
+  }
+}
--- SPMetalHelper/Linq/IPR/Entities.partial.cs
+++ SPMetalHelper/Linq/IPR/Entities.partial.cs
@@ -197,6 +197,16 @@ namespace CAS.SmartFactory.Linq.IPR
     /// <summary>
     /// Writes an entry with the given message text and application-defined event identifier to the event log list. 
     /// </summary>
+    /// <param name="source">The source denominator of the message.</param>
+    /// <param name="message">The string to write to the event log.</param>
+    internal static void WriteEntry( string source, string message )
+    {
+      using ( EntitiesDataContext edc = new EntitiesDataContext()  )
+        Anons.WriteEntry( edc, source, message );
+    }
+    /// <summary>
+    /// Writes an entry with the given message text and application-defined event identifier to the event log list. 
+    /// </summary>
     /// <param name="edc">Provides LINQ (Language Integrated Query) access to, and change tracking for, 
     /// the lists and document libraries of a Windows SharePoint Services "14" Web site.</param>
     /// <param name="source">The source denominator of the message.</param>
