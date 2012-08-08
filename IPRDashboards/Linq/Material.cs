280acb7dce0535a4d11efefe344ae2189d868253
Mariusz <mpostol@cas.eu>
2012-08-08 20:53:47 +0000

    [pr4-3580] CigaretteExportFormCollection - serialize to XML - added inheritance chain to properly initialize the collection. Issue #: 3580

--- IPRDashboards/Linq/Material.cs
+++ IPRDashboards/Linq/Material.cs
@@ -3,14 +3,15 @@ using System.Linq;
 using CAS.SharePoint;
 using System.Diagnostics;
 using System;
+using CAS.SmartFactory.xml.DocumentsFactory.CigaretteExportForm;
 
 namespace CAS.SmartFactory.Linq.IPR
 {
   public partial class Material
   {
-    internal void Export( EntitiesDataContext edc, CigaretteExportForm consignment, bool closingBatch, string invoiceNoumber, string procedure, Clearence clearence )
+    internal void Export( EntitiesDataContext edc, List<Ingredient> ingredient, bool closingBatch, string invoiceNoumber, string procedure, Clearence clearence, double portion )
     {
-      double _quantity = ( this.TobaccoQuantityKg.Value * consignment.Portion ).RountMass();
+      double _quantity = ( this.TobaccoQuantityKg.Value * portion ).RountMass();
       if ( this.ProductType.Value == Linq.IPR.ProductType.IPRTobacco )
       {
         try
@@ -19,7 +20,7 @@ namespace CAS.SmartFactory.Linq.IPR
           {
             if ( _quantity == 0 )
               break;
-            _disposal.Export( edc, ref _quantity, consignment, closingBatch, invoiceNoumber, procedure, clearence );
+            _disposal.Export( edc, ref _quantity, ingredient, closingBatch, invoiceNoumber, procedure, clearence );
           }
           string _template = "It is imposible the find the material {0} of {1} kg for invoice {2} on any IPR account";
           Anons.Assert( edc, _quantity == 0, "Material.Export", string.Format( _template, this.Batch, _quantity, invoiceNoumber ) );
@@ -36,8 +37,8 @@ namespace CAS.SmartFactory.Linq.IPR
       }
       else if ( this.ProductType.Value == Linq.IPR.ProductType.Tobacco )
       {
-        RegularIngredient _ri = new RegularIngredient( _quantity, this.SKU, this.Batch );
-        consignment.Add( _ri );
+        RegularIngredient _ri = new RegularIngredient( this.Batch, this.SKU, _quantity );
+        ingredient.Add( _ri );
       }
     }
     private List<Disposal> GetListOfDisposals()
---
+++ IPRExternalData/DocumentsFactory/CigaretteExportForm/RegularIngredient.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CAS.SmartFactory.xml.DocumentsFactory.CigaretteExportForm
+{
+  public partial class RegularIngredient
+  {
+    /// <summary>
+    /// Initializes a new instance of the <see cref="RegularIngredient"/> class.
+    /// </summary>
+    /// <param name="tobaccoBatch">The tobacco batch.</param>
+    /// <param name="tobaccoSKU">The tobacco SKU.</param>
+    /// <param name="quantity">The quantity od tobacco.</param>
+    public RegularIngredient( string tobaccoBatch, string tobaccoSKU, double quantity ) :
+      base( tobaccoBatch, tobaccoSKU, quantity )
+    { }
+    [Obsolete()]
+    public RegularIngredient()
+    { }
+  }
+}
