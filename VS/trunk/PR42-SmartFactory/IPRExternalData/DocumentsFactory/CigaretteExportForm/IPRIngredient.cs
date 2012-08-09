0de007dfc460ef7d2e91a381725dc595d256c7a3
mpostol <mpostol@8a3894fd-0c01-b944-991b-6a3da02cae8a>
2012-08-09 16:12:27 +0000

    [pr4-3607] Provide CigaretteExportFormCollection functionality - testing in progress - added test data; existing problem instead of inherit create factory - it seems that the inheritance across multiple assembly is not supported. Issue #: 3607

--- VS/trunk/PR42-SmartFactory/IPRExternalData/DocumentsFactory/CigaretteExportForm/IPRIngredient.cs
+++ VS/trunk/PR42-SmartFactory/IPRExternalData/DocumentsFactory/CigaretteExportForm/IPRIngredient.cs
@@ -7,9 +7,19 @@ namespace CAS.SmartFactory.xml.DocumentsFactory.CigaretteExportForm
 {
   public partial class IPRIngredient
   {
+    /// <summary>
+    /// Initializes a new instance of the <see cref="IPRIngredient"/> class.
+    /// </summary>
+    [Obsolete( "Use only for xml serializer" )]
+    public IPRIngredient() { }
+    /// <summary>
+    /// Initializes a new instance of the <see cref="IPRIngredient"/> class.
+    /// </summary>
+    /// <param name="tobaccoBatch">The tobacco batch.</param>
+    /// <param name="tobaccoSKU">The tobacco SKU.</param>
+    /// <param name="quantity">The quantity.</param>
     public IPRIngredient( string tobaccoBatch, string tobaccoSKU, double quantity ) :
       base( tobaccoBatch, tobaccoSKU, quantity )
     { }
-
   }
 }
