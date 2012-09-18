02c296ed4c30304f4506c6e83ef9e654c7b0d4a3
mpostol <mpostol@8a3894fd-0c01-b944-991b-6a3da02cae8a>
2012-09-18 11:34:09 +0000

    [pr4-3615] IPR material value total - use currency to calculate - main code updated.
    Issue #: 3615

--- VS/trunk/PR42-SmartFactory/IPRExternalData/DocumentsFactory/CigaretteExportForm/CigaretteExportFormCollection.xsd.cs
+++ VS/trunk/PR42-SmartFactory/IPRExternalData/DocumentsFactory/CigaretteExportForm/CigaretteExportFormCollection.xsd.cs
@@ -2,6 +2,8 @@
 using System.Xml.Serialization;
 using Microsoft.SharePoint;
 using System.Xml;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace CAS.SmartFactory.xml.DocumentsFactory.CigaretteExportForm
 {
@@ -31,6 +33,44 @@ namespace CAS.SmartFactory.xml.DocumentsFactory.CigaretteExportForm
         _docFile = destinationCollection.Add( fileName + ".xml", _docStrm, true );
       }
       return _docFile;
+    }
+  }
+  public partial class AmountOfMoney
+  {
+    /// <summary>
+    /// Initializes a new instance of the <see cref="AmountOfMoney"/> class.
+    /// </summary>
+    /// <param name="amount">The amount.</param>
+    /// <param name="currency">The currency.</param>
+    public AmountOfMoney( double amount, string currency )
+    {
+      this.Amount = amount;
+      this.Currency = currency;
+    }
+    
+    public AmountOfMoney() { }
+  }
+  public partial class TotalAmountOfMoney
+  {
+    private Dictionary<string, AmountOfMoney> _totals = new Dictionary<string, AmountOfMoney>();
+    /// <summary>
+    /// Adds the specified currency, amount pair to the totals.
+    /// </summary>
+    /// <param name="currency">The currency.</param>
+    /// <param name="amount">The amount of money.</param>
+    public void Add( string currency, double amount )
+    {
+      if ( _totals.ContainsKey( currency ) )
+        _totals[ currency ].Amount += amount;
+      else
+        _totals.Add( currency, new AmountOfMoney( amount, currency ) );
+    }
+    /// <summary>
+    /// Assigne current totals to the AmountOfMoney.
+    /// </summary>
+    public void AssignTotals()
+    {
+      this.AmountOfMoney = _totals.Values.ToArray<AmountOfMoney>();
     }
   }
 }
