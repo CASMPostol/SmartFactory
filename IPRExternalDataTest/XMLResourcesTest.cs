972bd157976f17d9eec009c54f991a816afcb95f
Mariusz <mpostol@cas.eu>
2012-09-24 07:21:15 +0000

    [pr4-3707] Export: Association of the SAD documents - SAD handling procedure modification - cumulative commit - functionality implemented, added unit test, fixed some bugs - new deployment and testing is required - added test project.
    Issue #: 3707

---
+++ IPRExternalDataTest/XMLResourcesTest.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using CAS.SmartFactory.xml;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace IPRExternalDataTest
+{
+  /// <summary>
+  ///This is a test class for XMLResourcesTest and is intended
+  ///to contain all XMLResourcesTest Unit Tests
+  ///</summary>
+  [TestClass()]
+  public class XMLResourcesTest
+  {
+
+    private TestContext testContextInstance;
+
+    /// <summary>
+    ///Gets or sets the test context which provides
+    ///information about and functionality for the current test run.
+    ///</summary>
+    public TestContext TestContext
+    {
+      get
+      {
+        return testContextInstance;
+      }
+      set
+      {
+        testContextInstance = value;
+      }
+    }
+
+    #region Additional test attributes
+    // 
+    //You can use the following additional attributes as you write your tests:
+    //
+    //Use ClassInitialize to run code before running the first test in the class
+    //[ClassInitialize()]
+    //public static void MyClassInitialize(TestContext testContext)
+    //{
+    //}
+    //
+    //Use ClassCleanup to run code after all tests in a class have run
+    //[ClassCleanup()]
+    //public static void MyClassCleanup()
+    //{
+    //}
+    //
+    //Use TestInitialize to run code before running each test
+    //[TestInitialize()]
+    //public void MyTestInitialize()
+    //{
+    //}
+    //
+    //Use TestCleanup to run code after each test has run
+    //[TestCleanup()]
+    //public void MyTestCleanup()
+    //{
+    //}
+    //
+    #endregion
+
+    /// <summary>
+    ///A test for FinishedGoodsExportFormFileName
+    ///</summary>
+    [TestMethod()]
+    public void FinishedGoodsExportFormFileNameTest()
+    {
+      int number = new Random().Next( 1, 9999999 ); 
+      string documentName = XMLResources.FinishedGoodsExportFormFileName( number );
+      Nullable<int> actual = XMLResources.GetRequiredDocumentFinishedGoodExportConsignmentNumber( documentName );
+      Assert.AreEqual( number, actual );
+    }
+  }
+}
