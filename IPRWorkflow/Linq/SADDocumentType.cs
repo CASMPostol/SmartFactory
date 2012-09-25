d99d8069b9362a19c58443ec24c1bc526d794560
Mariusz <mpostol@cas.eu>
2012-09-25 14:40:20 +0000

    [pr4-3719] Export: Association of the SAD documents - unique naming convention - removed comments from the code. Issue #: 3719

--- IPRWorkflow/Linq/SADDocumentType.cs
+++ IPRWorkflow/Linq/SADDocumentType.cs
@@ -45,7 +45,6 @@ namespace CAS.SmartFactory.Linq.IPR
       {
         if ( _sg.Procedure.RequestedProcedure() != CustomsProcedureCodes.ReExport )
           throw new IPRDataConsistencyException( "Clearence.Create", String.Format( "IE529 contains invalid customs procedure {0}", _sg.Title ), null, "Wrong customs procedure." );
-        //TODO [pr4-3719] Export: Association of the SAD documents - unique naming convention  http://itrserver/Bugs/BugDetail.aspx?bid=3719
         //TODO [pr4-3707] Export: Association of the SAD documents - SAD handling procedure modification http://itrserver/Bugs/BugDetail.aspx?bid=3707
         foreach ( SADRequiredDocuments _rdx in _sg.SADRequiredDocuments )
         {
