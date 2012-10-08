0f0429dfaf6849fdc3879cb4f9a172266bf1f757
mpostol <mpostol@8a3894fd-0c01-b944-991b-6a3da02cae8a>
2012-10-08 14:57:13 +0000

    Create model to represent the Tobacco Form document - added xml schema

---
+++ VS/trunk/PR42-SmartFactory/IPRExternalData/DocumentsFactory/TobaccoFreeCirculationForm/GoCS.ps1
@@ -0,0 +1,10 @@
+﻿Write-Host "This file creates all the c# classes for Configuration.xsd"
+Get-Location | Write-host
+$env:path += "; C:\Program Files\Microsoft SDKs\Windows\v7.1\Bin\x64\"
+#$cpath = get-location
+
+Write-host XSD processing
+xsd.exe ..\DisposalsDescription.xsd DocumentContent.xsd /N:CAS.SmartFactory.xml.DocumentsFactory.DustWasteForm /c  |write-host
+
+#set-location $cpath
+Write-host Done...
