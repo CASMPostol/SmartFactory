2320eea20995535be0b7b625092faaf31a99f00a
mpostol <mpostol@8a3894fd-0c01-b944-991b-6a3da02cae8a>
2012-10-18 08:10:22 +0000

    Tobacco clearance - selection - added selection query for all kinds of compensation materials - in progress - next step

--- VS/trunk/PR42-SmartFactory/IPRDashboards/Webparts/ClearenceWebPart/Selection.cs
+++ VS/trunk/PR42-SmartFactory/IPRDashboards/Webparts/ClearenceWebPart/Selection.cs
@@ -1,4 +1,5 @@
-﻿using System.Data;
+﻿using System;
+using System.Data;
 namespace CAS.SmartFactory.IPR.Dashboards.Webparts.ClearenceWebPart
 {
   /// <summary>
@@ -6,6 +7,20 @@ namespace CAS.SmartFactory.IPR.Dashboards.Webparts.ClearenceWebPart
   /// </summary>
   public partial class Selection
   {
+    internal partial class SelectionTableRowWraper
+    {
+      internal string DocumentNo { get; set; }
+      internal DateTime DebtDate { get; set; }
+      internal DateTime ValidTo { get; set; }
+      internal double Quantity { get; set; }
+      internal double UnitPrice { get; set; }
+      internal string Currency { get; set; }
+      internal DateTime Created { get; set; }
+      internal string Status { get; set; }
+      internal string Batch { get; set; }
+      internal string SKU { get; set; }
+      internal string ID { get; set; }
+    }
     partial class SelectionTableDataTable
     {
       internal void GetRow( Selection.SelectionTableDataTable source, string _id )
@@ -17,6 +32,23 @@ namespace CAS.SmartFactory.IPR.Dashboards.Webparts.ClearenceWebPart
         else
           _oldRow.Quantity += _slctdItem.Quantity;
         _slctdItem.Delete();
+      }
+
+      internal void NewSelectionTableRow( SelectionTableRowWraper _rowx )
+      {
+        SelectionTableRow  _nr = this.NewSelectionTableRow();
+        _nr.Batch = _rowx.Batch.Trim();
+        _nr.Created = _rowx.Created;
+        _nr.Currency = _rowx.Currency;
+        _nr.DocumentNo = _rowx.DocumentNo.Trim();
+        _nr.DebtDate = _rowx.DebtDate;
+        _nr.ID = _rowx.ID.ToString();
+        _nr.SKU = _rowx.SKU.Trim();
+        _nr.Quantity = _rowx.Quantity;
+        _nr.Status = _rowx.Status;
+        _nr.UnitPrice = _rowx.UnitPrice;
+        _nr.ValidTo = _rowx.ValidTo;
+        AddSelectionTableRow( _nr );
       }
     }
   }
