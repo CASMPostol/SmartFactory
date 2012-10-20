89d91692b0eaaf441e02c7405f7a0183a579454c
mpostol <mpostol@8a3894fd-0c01-b944-991b-6a3da02cae8a>
2012-10-20 09:59:45 +0000

    Tobacco clearance - selection - added create clearance functionality - added interconnection 9infrastructure to the webpart. Cleanup using clauses.

--- VS/trunk/PR42-SmartFactory/IPRDashboards/Webparts/ClearenceWebPart/ClearenceWebPart.cs
+++ VS/trunk/PR42-SmartFactory/IPRDashboards/Webparts/ClearenceWebPart/ClearenceWebPart.cs
@@ -1,24 +1,42 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.ComponentModel;
-using System.Web;
 using System.Web.UI;
-using System.Web.UI.WebControls;
 using System.Web.UI.WebControls.WebParts;
-using Microsoft.SharePoint;
-using Microsoft.SharePoint.WebControls;
 
 namespace CAS.SmartFactory.IPR.Dashboards.Webparts.ClearenceWebPart
 {
-    [ToolboxItemAttribute(false)]
-    public class ClearenceWebPart : WebPart
+  [ToolboxItemAttribute( false )]
+  public class ClearenceWebPart: WebPart
+  {
+    #region WebPart override
+    // Visual Studio might automatically update this path when you change the Visual Web Part project item.
+    private const string _ascxPath = @"~/_CONTROLTEMPLATES/CAS.SmartFactory.IPR.Dashboards.Webparts/ClearenceWebPart/ClearenceWebPartUserControl.ascx";
+    private ClearenceWebPartUserControl m_control;
+    protected override void CreateChildControls()
     {
-        // Visual Studio might automatically update this path when you change the Visual Web Part project item.
-        private const string _ascxPath = @"~/_CONTROLTEMPLATES/CAS.SmartFactory.IPR.Dashboards.Webparts/ClearenceWebPart/ClearenceWebPartUserControl.ascx";
-
-        protected override void CreateChildControls()
-        {
-            Control control = Page.LoadControl(_ascxPath);
-            Controls.Add(control);
-        }
+      m_control = Page.LoadControl( _ascxPath ) as ClearenceWebPartUserControl;
+      Controls.Add( m_control );
     }
+    protected override void OnPreRender( EventArgs e )
+    {
+      m_control.SetInterconnectionData( m_ProvidersDictionary );
+      base.OnPreRender( e );
+    }
+    #endregion
+
+    #region Interconnections Providers
+    /// <summary>
+    /// Sets the ClearanceInterconnection provider.
+    /// </summary>
+    /// <param name="_provider">The provider interface.</param>
+    [ConnectionConsumer( "Clearance list interconnection", "ClearanceInterconnection", AllowsMultipleConnections = false )]
+    public void SetBatchProvider( IWebPartRow _provider )
+    {
+      m_ProvidersDictionary.Add( ConnectionSelector.ClearenceInterconnection, _provider );
+    }
+    private Dictionary<ConnectionSelector, IWebPartRow> m_ProvidersDictionary = new Dictionary<ConnectionSelector, IWebPartRow>();
+    #endregion
+
+  }
 }
