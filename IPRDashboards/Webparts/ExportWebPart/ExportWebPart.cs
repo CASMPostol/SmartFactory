3201eaeb3f35a72a297563febd4ebef2aec22e8d
Mariusz <mpostol@cas.eu>
2012-10-20 09:59:45 +0000

    Tobacco clearance - selection - added create clearance functionality - added interconnection 9infrastructure to the webpart. Cleanup using clauses.

--- IPRDashboards/Webparts/ExportWebPart/ExportWebPart.cs
+++ IPRDashboards/Webparts/ExportWebPart/ExportWebPart.cs
@@ -1,12 +1,8 @@
 ﻿using System;
-using System.ComponentModel;
-using System.Web;
-using System.Web.UI;
-using System.Web.UI.WebControls;
-using System.Web.UI.WebControls.WebParts;
-using Microsoft.SharePoint;
-using Microsoft.SharePoint.WebControls;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Web.UI;
+using System.Web.UI.WebControls.WebParts;
 
 namespace CAS.SmartFactory.IPR.Dashboards.Webparts.ExportWebPart
 {
