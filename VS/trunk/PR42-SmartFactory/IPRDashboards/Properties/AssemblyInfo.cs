10037ca2dda6888ba6c01b7e1a35d5cd1a156736
mpostol <mpostol@8a3894fd-0c01-b944-991b-6a3da02cae8a>
2012-10-24 12:35:19 +0000

    Port the application to VS 2012. - added feature dependencies, changed additional references assemblies to direct,

--- VS/trunk/PR42-SmartFactory/IPRDashboards/Properties/AssemblyInfo.cs
+++ VS/trunk/PR42-SmartFactory/IPRDashboards/Properties/AssemblyInfo.cs
@@ -33,8 +33,8 @@ using System.Security;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.60.18.*")]
-[assembly: AssemblyFileVersion("1.60.18")]
+[assembly: AssemblyVersion("1.60.21.*")]
+[assembly: AssemblyFileVersion("1.60.21")]
 
 
 [assembly: System.Runtime.CompilerServices.InternalsVisibleTo( "IPRDashboardsTest, PublicKey=002400000480000094000000060200000024000052534131000400000100010015c024c9d0c88f" +
--- VS/trunk/PR42-SmartFactory/IPRWebsiteModel/Properties/AssemblyInfo.cs
+++ VS/trunk/PR42-SmartFactory/IPRWebsiteModel/Properties/AssemblyInfo.cs
@@ -7,7 +7,7 @@ using System.Security;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("IPRWebsiteModel")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription( "IPRWebsiteModel" )]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Microsoft")]
 [assembly: AssemblyProduct("IPRWebsiteModel")]
@@ -33,6 +33,6 @@ using System.Security;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion( "1.60.21.*" )]
+[assembly: AssemblyFileVersion( "1.60.21" )]
 
