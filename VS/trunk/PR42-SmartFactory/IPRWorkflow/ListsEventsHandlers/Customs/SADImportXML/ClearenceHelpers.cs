bf3c5676bc7a65a52768a76a3ea5184461cfe497
mpostol <mpostol@8a3894fd-0c01-b944-991b-6a3da02cae8a>
2012-10-26 15:08:24 +0000

    Process many invoices on IE529 message - new model - done, ready to be tested.

--- VS/trunk/PR42-SmartFactory/IPRWorkflow/ListsEventsHandlers/Customs/SADImportXML/ClearenceHelpers.cs
+++ VS/trunk/PR42-SmartFactory/IPRWorkflow/ListsEventsHandlers/Customs/SADImportXML/ClearenceHelpers.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using CAS.SharePoint;
 using CAS.SharePoint.Web;
@@ -384,31 +385,33 @@ namespace CAS.SmartFactory.IPR.Customs
     /// <summary>
     /// Res the export of goods.
     /// </summary>
-    /// <param name="_this">The _this.</param>
+    /// <param name="sadDocument">The _this.</param>
     /// <param name="_edc">The _edc.</param>
     /// <param name="_messageType">Type of the _message.</param>
     /// <exception cref="GenericStateMachineEngine.ActionResult">if operation cannot be complited.</exception>
-    private static void ReExportOfGoods( this SADDocumentType _this, Entities _edc, xml.Customs.CustomsDocument.DocumentType _messageType )
+    private static void ReExportOfGoods( this SADDocumentType sadDocument, Entities _edc, xml.Customs.CustomsDocument.DocumentType _messageType )
     {
-      Clearence _clearance = FimdClearence( _edc, _this );
+      List<Clearence> _clearanceList = FimdClearence( _edc, sadDocument );
       //TODO Define and use the reverse lookup field. 
-      _clearance.SADDocumentID = _this;
-      foreach ( var _disposal in from _dspx in _edc.Disposal where _dspx.Disposal2ClearenceIndex.Identyfikator == _clearance.Identyfikator select _dspx )
-        //TODO not sure about this.CustomsDebtDate.Value, but it is the ony one date. 
-        _disposal.Export( _edc, _this.DocumentNumber, _clearance, _this.CustomsDebtDate.Value );
-      _clearance.DocumentNo = _this.DocumentNumber;
-      _clearance.ReferenceNumber = _this.ReferenceNumber;
-      _clearance.Status = true;
-      _clearance.CreateTitle( _messageType.ToString() );
+      foreach ( Clearence _clearance in _clearanceList )
+      {
+        _clearance.SADDocumentID = sadDocument;
+        foreach ( var _disposal in from _dspx in _edc.Disposal where _dspx.Disposal2ClearenceIndex.Identyfikator == _clearance.Identyfikator select _dspx )
+          //TODO not sure about this.CustomsDebtDate.Value, but it is the ony one date. 
+          _disposal.Export( _edc, sadDocument.DocumentNumber, _clearance, sadDocument.CustomsDebtDate.Value );
+        _clearance.DocumentNo = sadDocument.DocumentNumber;
+        _clearance.ReferenceNumber = sadDocument.ReferenceNumber;
+        _clearance.Status = true;
+        _clearance.CreateTitle( _messageType.ToString() );
+      }
     }
-    private static Clearence FimdClearence( Entities _edc, SADDocumentType _this )
+    private static List<Clearence> FimdClearence( Entities _edc, SADDocumentType _this )
     {
-      Clearence _clearance = null;
+      List<Clearence> _clearance = null;
       foreach ( SADGood _sg in _this.SADGood )
       {
         if ( _sg.Procedure.RequestedProcedure() != CustomsProcedureCodes.ReExport )
           throw new IPRDataConsistencyException( "Clearence.Create", String.Format( "IE529 contains invalid customs procedure {0}", _sg.Title ), null, "Wrong customs procedure." );
-        //TODO [pr4-3707] Export: Association of the SAD documents - SAD handling procedure modification http://itrserver/Bugs/BugDetail.aspx?bid=3707
         foreach ( SADRequiredDocuments _rdx in _sg.SADRequiredDocuments )
         {
           if ( _rdx.Code != XMLResources.RequiredDocumentFinishedGoodExportConsignmentCode )
@@ -416,13 +419,13 @@ namespace CAS.SmartFactory.IPR.Customs
             int? _cleranceInt = XMLResources.GetRequiredDocumentFinishedGoodExportConsignmentNumber( _rdx.Number );
             if ( _cleranceInt.HasValue )
             {
-              _clearance = Element.GetAtIndex<Clearence>( _edc.Clearence, _cleranceInt.Value );
+              _clearance.Add( Element.GetAtIndex<Clearence>( _edc.Clearence, _cleranceInt.Value ) );
               break;
             }
           }
         } // foreach ( SADRequiredDocuments _rdx in _sg.SADRequiredDocuments )
       }//foreach ( SADGood _sg in SADGood )
-      if ( _clearance != null )
+      if ( _clearance.Count > 0 )
         return _clearance;
       string _template = "Cannot find required document code ={0} for customs document = {1}/ref={2}";
       throw GenericStateMachineEngine.ActionResult.NotValidated( String.Format( _template, _this.DocumentNumber, _this.ReferenceNumber ) );
