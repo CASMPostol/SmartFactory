add8817af2ffde5ef1d952db0150cac03c733f24
mpostol <mpostol@8a3894fd-0c01-b944-991b-6a3da02cae8a>
2012-10-30 22:20:47 +0000

    Compensation good must be recognized using the PCN code from customs message - done, to be tested.

--- VS/trunk/PR42-SmartFactory/IPRWebsiteModel/Linq/PCNCode.cs
+++ VS/trunk/PR42-SmartFactory/IPRWebsiteModel/Linq/PCNCode.cs
@@ -1,42 +1,53 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
 
 namespace CAS.SmartFactory.IPR.WebsiteModel.Linq
 {
+  /// <summary>
+  /// PCNCode
+  /// </summary>
   public partial class PCNCode
   {
     /// <summary>
     /// Adds the or get.
     /// </summary>
-    /// <param name="_edc">The _edc.</param>
-    /// <param name="productCodeNumber">The _code.</param>
+    /// <param name="entities">The entities.</param>
+    /// <param name="procedureCode">The procedure code.</param>
+    /// <param name="productCodeNumber">The product code number.</param>
+    /// <param name="title">The title.</param>
     /// <returns></returns>
-    //TODO PCN Code list - change the title http://cas_sp:11225/sites/awt/Lists/TaskList/DispForm.aspx?ID=3340
-    public static PCNCode AddOrGet( Entities _edc, ClearenceProcedure procedureCode, string productCodeNumber, string title )
+    public static PCNCode AddOrGet( Entities entities, ClearenceProcedure procedureCode, string productCodeNumber, string title )
     {
-      PCNCode _pcncode = ( from _pcnx in _edc.PCNCode where productCodeNumber.Trim().Contains( _pcnx.ProductCodeNumber ) select _pcnx ).FirstOrDefault();
+      PCNCode _pcncode = Find( entities, procedureCode, productCodeNumber );
       if ( _pcncode == null )
       {
         _pcncode = new PCNCode()
           {
             //TODO PCNCode - auto generation value of the column CompensationGood http://cas_sp:11225/sites/awt/Lists/TaskList/DispForm.aspx?ID=3341
             CompensationGood = "Tytoń",
-            Procedure = "TBD",
+            Procedure = Entities.RequestedProcedure( procedureCode ),
             ProductCodeNumber = productCodeNumber,
-            Title = String.Format( "{0} {1}", "Tytoń", productCodeNumber )
+            Title = title
           };
-        _edc.PCNCode.InsertOnSubmit( _pcncode );
+        entities.PCNCode.InsertOnSubmit( _pcncode );
       }
       return _pcncode;
     }
-    internal static PCNCode Find( Entities edc, ClearenceProcedure procedureCode, string productCodeNumber )
+    /// <summary>
+    /// Finds the specified entities.
+    /// </summary>
+    /// <param name="entities">The entities.</param>
+    /// <param name="procedureCode">The procedure code.</param>
+    /// <param name="productCodeNumber">The product code number.</param>
+    /// <returns></returns>
+    internal static PCNCode Find( Entities entities, ClearenceProcedure procedureCode, string productCodeNumber )
     {
-      PCNCode _pcn = ( from _pcnx in edc.PCNCode
-                       where _pcnx.Procedure.Contains( Entities.RequestedProcedure( procedureCode ) ) && _pcnx.ProductCodeNumber.Contains( productCodeNumber )
+      string _requestedProcedure = Entities.RequestedProcedure( procedureCode );
+      PCNCode _pcn = ( from _pcnx in entities.PCNCode
+                       where String.IsNullOrEmpty( _pcnx.Procedure ) || _pcnx.Procedure.Contains( _requestedProcedure ) &&
+                             _pcnx.ProductCodeNumber.Contains( productCodeNumber )
                        select _pcnx ).FirstOrDefault();
       return _pcn;
     }
-
   }
 }
