5232e2a566f7105b0099b73e68e57d5bd148d1eb
mpostol <mpostol@8a3894fd-0c01-b944-991b-6a3da02cae8a>
2012-10-30 20:51:04 +0000

    Import Customs Declarations - Disposals associations - fixed, to be tested.

--- VS/trunk/PR42-SmartFactory/IPRWebsiteModel/Linq/Disposal.cs
+++ VS/trunk/PR42-SmartFactory/IPRWebsiteModel/Linq/Disposal.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Linq;
+using CAS.SharePoint;
 using CAS.SharePoint.Web;
 
 namespace CAS.SmartFactory.IPR.WebsiteModel.Linq
@@ -9,6 +10,47 @@ namespace CAS.SmartFactory.IPR.WebsiteModel.Linq
   /// </summary>
   public partial class Disposal
   {
+
+    #region public
+    /// <summary>
+    /// Starts the clearance.
+    /// </summary>
+    /// <param name="clearingType">Type of the clearing.</param>
+    /// <param name="invoiceNoumber">The invoice noumber.</param>
+    /// <param name="procedure">The procedure.</param>
+    /// <param name="clearence">The clearence.</param>
+    public void StartClearance( ClearingType clearingType, string invoiceNoumber, string procedure, Clearence clearence )
+    {
+      this.Disposal2ClearenceIndex = clearence;
+      this.CustomsStatus = Linq.CustomsStatus.Started;
+      this.ClearingType = clearingType;
+      this.CustomsProcedure = procedure;
+      this.InvoiceNo = invoiceNoumber;
+      this.SetUpCalculatedColumns( ClearingType.Value );
+    }
+    /// <summary>
+    /// Calculate values of columns: Title, DutyPerSettledAmount, VATPerSettledAmount, TobaccoValue, DutyAndVAT.
+    /// </summary>
+    /// <param name="clearingType">Type of the clearing.</param>
+    public void SetUpCalculatedColumns( ClearingType clearingType )
+    {
+      string _titleTmplt = "Disposal: {0} of material {1}";
+      Title = String.Format( _titleTmplt, this.DisposalStatus.Value.ToString(), this.Disposal2IPRIndex.Batch );
+      double _portion = SettledQuantity.Value / Disposal2IPRIndex.NetMass.Value;
+      if ( clearingType == Linq.ClearingType.PartialWindingUp )
+      {
+        DutyPerSettledAmount = ( Disposal2IPRIndex.Duty.Value * _portion ).RoundCurrency();
+        VATPerSettledAmount = ( Disposal2IPRIndex.VAT.Value * _portion ).RoundCurrency();
+        TobaccoValue = ( Disposal2IPRIndex.Value.Value * _portion ).RoundCurrency();
+      }
+      else
+      {
+        DutyPerSettledAmount = GetDutyNotCleared();
+        VATPerSettledAmount = GetVATNotCleared();
+        TobaccoValue = GetPriceNotCleared();
+      }
+      DutyAndVAT = DutyPerSettledAmount.Value + VATPerSettledAmount.Value;
+    }
     internal void ClearThroughCustoms( Entities edc, string documentNo, Clearence clearence, DateTime clearanceDate, string productCodeNumber )
     {
       string _at = "starting";
@@ -25,8 +67,7 @@ namespace CAS.SmartFactory.IPR.WebsiteModel.Linq
           this.No = _lastOne.No++;
         //TODO [pr4-3737] Compensation good must be recognized using the PCN code from customs message http://cas_sp:11225/sites/awt/Lists/TaskList/DispForm.aspx?ID=1744
         PCNCode _pcn = ( from _pcnx in edc.PCNCode
-                         where _pcnx.Procedure.Contains( RequestedProcedure( clearence.ClearenceProcedure ) ) && _pcnx.ProductCodeNumber.Contains( productCodeNumber )
-                         // && this.DisposalStatus == _pcnx.CompensationGood 
+                         where _pcnx.Procedure.Contains( Entities.RequestedProcedure( clearence.ClearenceProcedure ) ) && _pcnx.ProductCodeNumber.Contains( productCodeNumber )
                          select _pcnx ).FirstOrDefault();
         this.SADDocumentNo = documentNo;
         this.SADDate = clearanceDate;
@@ -46,19 +87,22 @@ namespace CAS.SmartFactory.IPR.WebsiteModel.Linq
         throw GenericStateMachineEngine.ActionResult.Exception( _ex, String.Format( _template, this.Title, this.Identyfikator.Value, _ex.Message, _at ) );
       }
     }
-    //TODO [pr4-3737] Compensation good must be recognized using the PCN code from customs message http://cas_sp:11225/sites/awt/Lists/TaskList/DispForm.aspx?ID=1744
-    private string RequestedProcedure( ClearenceProcedure? nullable )
+    #endregion
+    
+    #region private
+    private double GetDutyNotCleared()
     {
-      throw new NotImplementedException();
+      return Disposal2IPRIndex.Duty.Value - ( from _dec in Disposal2IPRIndex.Disposal where _dec.DutyPerSettledAmount.HasValue select new { val = _dec.DutyPerSettledAmount.Value } ).Sum( itm => itm.val );
     }
-    public void StartClearance( ClearingType clearingType, string invoiceNoumber, string procedure, Clearence clearence )
+    private double GetPriceNotCleared()
     {
-      this.Disposal2ClearenceIndex = clearence;
-      this.CustomsStatus = Linq.CustomsStatus.Started;
-      this.ClearingType = clearingType;
-      this.CustomsProcedure = procedure;
-      this.InvoiceNo = invoiceNoumber;
-      this.SetUpCalculatedColumns( ClearingType.Value );
+      return Disposal2IPRIndex.IPRUnitPrice.Value - ( from _dec in Disposal2IPRIndex.Disposal where _dec.TobaccoValue.HasValue select new { val = _dec.TobaccoValue.Value } ).Sum( itm => itm.val );
     }
+    private double GetVATNotCleared()
+    {
+      return Disposal2IPRIndex.VAT.Value - ( from _dec in Disposal2IPRIndex.Disposal where _dec.VATPerSettledAmount.HasValue select new { val = _dec.VATPerSettledAmount.Value } ).Sum( itm => itm.val );
+    }
+    #endregion
+
   }
 }
--- VS/trunk/PR42-SmartFactory/IPRWebsiteModel/Linq/EntitiesDataContext.cs
+++ VS/trunk/PR42-SmartFactory/IPRWebsiteModel/Linq/EntitiesDataContext.cs
@@ -1,27 +1,122 @@
-﻿using CAS.SharePoint;
+﻿using System;
+using CAS.SharePoint;
+using Microsoft.SharePoint;
+using Microsoft.SharePoint.Linq;
+
 namespace CAS.SmartFactory.IPR.WebsiteModel.Linq
 {
   public partial class Entities
   {
     /// <summary>
+    /// Initializes a new instance of the <see cref="Entities" /> class.
+    /// </summary>
+    public Entities() : base( SPContext.Current.Web.Url ) { }
+    /// <summary>
+    /// Persists to the content database changes made by the current user to one or more lists using the specified failure mode;
+    /// or, if a concurrency conflict is found, populates the <see cref="P:Microsoft.SharePoint.Linq.DataContext.ChangeConflicts"/> property.
+    /// </summary>
+    /// <param name="mode">Specifies how the list item changing system of the LINQ to SharePoint provider will respond when it 
+    /// finds that a list item has been changed by another process since it was retrieved.
+    /// </param>
+    public void SubmitChangesSilently( RefreshMode mode )
+    {
+      try
+      {
+        SubmitChanges();
+      }
+      catch ( ChangeConflictException )
+      {
+        foreach ( ObjectChangeConflict changedListItem in this.ChangeConflicts )
+        {
+          changedListItem.Resolve( mode );
+        }
+        this.SubmitChanges();
+      }
+    }
+    /// <summary>
+    /// Resolves the change conflicts.
+    /// </summary>
+    /// <param name="_rsult">The _rsult.</param>
+    /// <exception cref="System.ApplicationException"></exception>
+    public void ResolveChangeConflicts( ActionResult _rsult )
+    {
+      string _cp = "Starting";
+      try
+      {
+        foreach ( ObjectChangeConflict _itx in this.ChangeConflicts )
+        {
+          _cp = "ObjectChangeConflict";
+          string _tmp = String.Format( "Object: {0}", _itx.Object == null ? "null" : _itx.Object.ToString() );
+          if ( _itx.MemberConflicts != null )
+          {
+            string _ft = ", Conflicts: Member.Name={0}; CurrentValue={1}; DatabaseValue={2}; OriginalValue={3}";
+            String _chnges = String.Empty;
+            foreach ( MemberChangeConflict _mid in _itx.MemberConflicts )
+            {
+              _chnges += String.Format( _ft,
+                _mid.Member == null ? "null" : _mid.Member.Name,
+                _mid.CurrentValue == null ? "null" : _mid.CurrentValue.ToString(),
+                _mid.DatabaseValue == null ? "null" : _mid.DatabaseValue.ToString(),
+                _mid.OriginalValue == null ? "null" : _mid.OriginalValue.ToString() );
+            }
+            _tmp += _chnges;
+          }
+          else
+            _tmp += "; No member details";
+          _rsult.AddMessage( "ResolveChangeConflicts at: " + _cp, _tmp );
+          _cp = "AddMessage";
+          _itx.Resolve( RefreshMode.KeepCurrentValues );
+        } //foreach (ObjectChangeConflict
+      }
+      catch ( Exception ex )
+      {
+        string _frmt = "The current operation has been interrupted in ResolveChangeConflicts at {0} by error {1}.";
+        throw new ApplicationException( String.Format( _frmt, _cp, ex.Message ) );
+      }
+    }
+    /// <summary>
+    /// String representing the requested procedure .
+    /// </summary>
+    /// <param name="procedureCode">The procedure code.</param>
+    /// <returns></returns>
+    internal static string RequestedProcedure( ClearenceProcedure? procedureCode )
+    {
+      switch ( procedureCode )
+      {
+        case ClearenceProcedure._3151:
+        case ClearenceProcedure._3171:
+          return "31";
+        case ClearenceProcedure._4051:
+        case ClearenceProcedure._4071:
+          return "40";
+        case ClearenceProcedure._5100:
+        case ClearenceProcedure._5171:
+          return "51";
+        case ClearenceProcedure._7100:
+        case ClearenceProcedure._7171:
+          return "71";
+      }
+      return string.Empty.NotAvailable();
+    }
+    /// <summary>
     /// Returns a <see cref="System.String" /> that represents this instance.
     /// </summary>
-    /// <param name="procedure">The procedure.</param>
+    /// <param name="procedureCode">The procedure code.</param>
     /// <returns>
-    /// A <see cref="System.String" /> that represents this instance.
+    /// A <see cref="System.String" /> that represents procedure code.
     /// </returns>
-    public static string ToString( ClearenceProcedure procedure )
+    public static string ToString( ClearenceProcedure procedureCode )
     {
-      switch ( procedure )
+      switch ( procedureCode )
       {
         case ClearenceProcedure._3151:
           return "3151";
         case ClearenceProcedure._3171:
           return "3171";
         case ClearenceProcedure._4051:
-          return "3151";
-        case ClearenceProcedure._4071:
           return "4051";
+        case ClearenceProcedure._4071:
+          return "4071";
         case ClearenceProcedure._5100:
           return "5100";
         case ClearenceProcedure._5171:
