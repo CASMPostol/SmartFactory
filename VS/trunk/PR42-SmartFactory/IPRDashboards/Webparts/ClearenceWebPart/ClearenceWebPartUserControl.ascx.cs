0b7da57134ff665cafcefa7f439cd2d80e58c033
mpostol <mpostol@8a3894fd-0c01-b944-991b-6a3da02cae8a>
2012-11-27 20:34:04 +0000

    Wrong status definition for Clearance list: done, TBT

--- VS/trunk/PR42-SmartFactory/IPRDashboards/Webparts/ClearenceWebPart/ClearenceWebPartUserControl.ascx.cs
+++ VS/trunk/PR42-SmartFactory/IPRDashboards/Webparts/ClearenceWebPart/ClearenceWebPartUserControl.ascx.cs
@@ -589,7 +589,6 @@ namespace CAS.SmartFactory.IPR.Dashboards.Webparts.ClearenceWebPart
         }
         SADConsignment _sadConsignment = Element.GetAtIndex<SADConsignment>( m_DataContextManagement.DataContext.SADConsignment, _sadConsignmentIdentifier );
         CurrentClearence.SADConsignmentLibraryIndex = _sadConsignment;
-        CurrentClearence.Status = true;
         foreach ( Disposal _dspsl in CurrentClearence.Disposal )
           _dspsl.CustomsStatus = CustomsStatus.Started;
         m_DataContextManagement.DataContext.SubmitChanges();
