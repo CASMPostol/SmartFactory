648ffd3297eedf94bd1ec7fcf522c14379795871
mpostol <mpostol@8a3894fd-0c01-b944-991b-6a3da02cae8a>
2012-11-28 09:30:03 +0000

    Closing of duty relief account - generate Report - added classes for content representation.

--- VS/trunk/PR42-SmartFactory/IPRExternalData/DocumentsFactory/AccountClearance/RequestContent.cs
+++ VS/trunk/PR42-SmartFactory/IPRExternalData/DocumentsFactory/AccountClearance/RequestContent.cs
@@ -28,9 +28,45 @@ namespace CAS.SmartFactory.xml.DocumentsFactory.AccountClearance {
         
         private string documentNoField;
         
-        private RequestForAccountClearance requestField;
+        private string entryDocumentNoField;
         
-        private AccountBalance balanceField;
+        private System.DateTime customsDebtDateField;
+        
+        private string sKUField;
+        
+        private string batchField;
+        
+        private string gradeField;
+        
+        private double netMassField;
+        
+        private string pCNField;
+        
+        private System.DateTime consentDateField;
+        
+        private System.DateTime validFromDateField;
+        
+        private System.DateTime validToDateField;
+        
+        private double productivityRateMinField;
+        
+        private double productivityRateMaxField;
+        
+        private double consentPeriodField;
+        
+        private string consentNoField;
+        
+        private double dutyField;
+        
+        private double dutyPerUnitField;
+        
+        private double vATField;
+        
+        private double vATPerUnitField;
+        
+        private double grossMassField;
+        
+        private double cartonsField;
         
         /// <uwagi/>
         [System.Xml.Serialization.XmlElementAttribute(DataType="date")]
@@ -54,51 +90,12 @@ namespace CAS.SmartFactory.xml.DocumentsFactory.AccountClearance {
         }
         
         /// <uwagi/>
-        public RequestForAccountClearance Request {
+        public string EntryDocumentNo {
             get {
-                return this.requestField;
+                return this.entryDocumentNoField;
             }
             set {
-                this.requestField = value;
-            }
-        }
-        
-        /// <uwagi/>
-        public AccountBalance Balance {
-            get {
-                return this.balanceField;
-            }
-            set {
-                this.balanceField = value;
-            }
-        }
-    }
-    
-    /// <uwagi/>
-    [System.CodeDom.Compiler.GeneratedCodeAttribute("xsd", "2.0.50727.3038")]
-    [System.SerializableAttribute()]
-    [System.Diagnostics.DebuggerStepThroughAttribute()]
-    [System.ComponentModel.DesignerCategoryAttribute("code")]
-    [System.Xml.Serialization.XmlTypeAttribute(Namespace="http://cas.eu/schemas/SmartFactory/xml/DocumentsFactory/AccountClearance.xsd")]
-    public partial class RequestForAccountClearance {
-        
-        private string documentNoField;
-        
-        private System.DateTime customsDebtDateField;
-        
-        private string sKUField;
-        
-        private string batchField;
-        
-        private double netMassField;
-        
-        /// <uwagi/>
-        public string DocumentNo {
-            get {
-                return this.documentNoField;
-            }
-            set {
-                this.documentNoField = value;
+                this.entryDocumentNoField = value;
             }
         }
         
@@ -134,6 +131,16 @@ namespace CAS.SmartFactory.xml.DocumentsFactory.AccountClearance {
         }
         
         /// <uwagi/>
+        public string Grade {
+            get {
+                return this.gradeField;
+            }
+            set {
+                this.gradeField = value;
+            }
+        }
+        
+        /// <uwagi/>
         public double NetMass {
             get {
                 return this.netMassField;
@@ -142,14 +149,145 @@ namespace CAS.SmartFactory.xml.DocumentsFactory.AccountClearance {
                 this.netMassField = value;
             }
         }
-    }
-    
-    /// <uwagi/>
-    [System.CodeDom.Compiler.GeneratedCodeAttribute("xsd", "2.0.50727.3038")]
-    [System.SerializableAttribute()]
-    [System.Diagnostics.DebuggerStepThroughAttribute()]
-    [System.ComponentModel.DesignerCategoryAttribute("code")]
-    [System.Xml.Serialization.XmlTypeAttribute(Namespace="http://cas.eu/schemas/SmartFactory/xml/DocumentsFactory/AccountClearance.xsd")]
-    public partial class AccountBalance {
+        
+        /// <uwagi/>
+        public string PCN {
+            get {
+                return this.pCNField;
+            }
+            set {
+                this.pCNField = value;
+            }
+        }
+        
+        /// <uwagi/>
+        public System.DateTime ConsentDate {
+            get {
+                return this.consentDateField;
+            }
+            set {
+                this.consentDateField = value;
+            }
+        }
+        
+        /// <uwagi/>
+        public System.DateTime ValidFromDate {
+            get {
+                return this.validFromDateField;
+            }
+            set {
+                this.validFromDateField = value;
+            }
+        }
+        
+        /// <uwagi/>
+        public System.DateTime ValidToDate {
+            get {
+                return this.validToDateField;
+            }
+            set {
+                this.validToDateField = value;
+            }
+        }
+        
+        /// <uwagi/>
+        public double ProductivityRateMin {
+            get {
+                return this.productivityRateMinField;
+            }
+            set {
+                this.productivityRateMinField = value;
+            }
+        }
+        
+        /// <uwagi/>
+        public double ProductivityRateMax {
+            get {
+                return this.productivityRateMaxField;
+            }
+            set {
+                this.productivityRateMaxField = value;
+            }
+        }
+        
+        /// <uwagi/>
+        public double ConsentPeriod {
+            get {
+                return this.consentPeriodField;
+            }
+            set {
+                this.consentPeriodField = value;
+            }
+        }
+        
+        /// <uwagi/>
+        public string ConsentNo {
+            get {
+                return this.consentNoField;
+            }
+            set {
+                this.consentNoField = value;
+            }
+        }
+        
+        /// <uwagi/>
+        public double Duty {
+            get {
+                return this.dutyField;
+            }
+            set {
+                this.dutyField = value;
+            }
+        }
+        
+        /// <uwagi/>
+        public double DutyPerUnit {
+            get {
+                return this.dutyPerUnitField;
+            }
+            set {
+                this.dutyPerUnitField = value;
+            }
+        }
+        
+        /// <uwagi/>
+        public double VAT {
+            get {
+                return this.vATField;
+            }
+            set {
+                this.vATField = value;
+            }
+        }
+        
+        /// <uwagi/>
+        public double VATPerUnit {
+            get {
+                return this.vATPerUnitField;
+            }
+            set {
+                this.vATPerUnitField = value;
+            }
+        }
+        
+        /// <uwagi/>
+        public double GrossMass {
+            get {
+                return this.grossMassField;
+            }
+            set {
+                this.grossMassField = value;
+            }
+        }
+        
+        /// <uwagi/>
+        public double Cartons {
+            get {
+                return this.cartonsField;
+            }
+            set {
+                this.cartonsField = value;
+            }
+        }
     }
 }
