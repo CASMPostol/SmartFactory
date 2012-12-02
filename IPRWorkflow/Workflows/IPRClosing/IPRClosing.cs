97a057f974af422e50fe25cb1362bc32b3a7a847
Mariusz <mpostol@cas.eu>
2012-12-02 21:42:12 +0000

    Workflow to close the account and generate the final report - workflow created, validation procedure implemented, work in progress.

--- IPRWorkflow/Workflows/IPRClosing/IPRClosing.cs
+++ IPRWorkflow/Workflows/IPRClosing/IPRClosing.cs
@@ -1,19 +1,8 @@
 ﻿using System;
-using System.ComponentModel;
-using System.ComponentModel.Design;
-using System.Collections;
-using System.Drawing;
 using System.Linq;
-using System.Workflow.ComponentModel.Compiler;
-using System.Workflow.ComponentModel.Serialization;
-using System.Workflow.ComponentModel;
-using System.Workflow.ComponentModel.Design;
-using System.Workflow.Runtime;
 using System.Workflow.Activities;
-using System.Workflow.Activities.Rules;
-using Microsoft.SharePoint;
+using CAS.SmartFactory.IPR.WebsiteModel.Linq;
 using Microsoft.SharePoint.Workflow;
-using Microsoft.SharePoint.WorkflowActions;
 
 namespace CAS.SmartFactory.IPR.Workflows.IPRClosing
 {
@@ -23,22 +12,40 @@ namespace CAS.SmartFactory.IPR.Workflows.IPRClosing
     {
       InitializeComponent();
     }
-
-    public Guid workflowId = default( System.Guid );
+    public Guid workflowId = new System.Guid( "{B16A9665-EFEA-4677-954B-6A1FE2A633FC}" );
     public SPWorkflowActivationProperties workflowProperties = new SPWorkflowActivationProperties();
     private void RecordValidation_ExecuteCode( object sender, EventArgs e )
     {
-
+      using ( Entities _edc = new Entities( workflowProperties.WebUrl ) )
+      {
+        WebsiteModel.Linq.IPR _record = Element.GetAtIndex<WebsiteModel.Linq.IPR>( _edc.IPR, workflowProperties.ItemId );
+        if ( _record.AccountBalance != 0 )
+        {
+          LogWarningMessageToHistory_HistoryDescription = String.Format( LogWarningMessageToHistory_HistoryDescription, "AccountBalance must be equal 0" );
+          return;
+        }
+        bool _notFinished = _record.Disposal.Where<Disposal>( v => v.CustomsStatus.Value != CustomsStatus.Finished ).Any<Disposal>();
+        if ( _notFinished )
+        {
+          LogWarningMessageToHistory_HistoryDescription = String.Format( LogWarningMessageToHistory_HistoryDescription, "All disposals must be cleared through customs." );
+          return;
+        }
+        Valid = true;
+      }
     }
     private bool Valid = false;
     private void ProcessIfValid( object sender, ConditionalEventArgs e )
     {
       e.Result = Valid;
     }
-
     private void Closeing_ExecuteCode( object sender, EventArgs e )
     {
-
+      using ( Entities _edc = new Entities( workflowProperties.WebUrl ) )
+      {
+        WebsiteModel.Linq.IPR _record = Element.GetAtIndex<WebsiteModel.Linq.IPR>( _edc.IPR, workflowProperties.ItemId );
+        _record.AccountClosed = true;
+      }
     }
+    public String LogWarningMessageToHistory_HistoryDescription = "Cannot close the IPR account because {0}, corect the content and try again.";
   }
 }
