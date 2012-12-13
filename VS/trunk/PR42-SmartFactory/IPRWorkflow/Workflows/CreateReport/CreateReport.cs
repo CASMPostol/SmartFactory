8fdd9ee8a5126439f8113637b87748559250120d
mpostol <mpostol@8a3894fd-0c01-b944-991b-6a3da02cae8a>
2012-12-13 08:46:42 +0000

    Workflow to create the reports set. - development - created workflow template.

--- VS/trunk/PR42-SmartFactory/IPRWorkflow/Workflows/CreateReport/CreateReport.cs
+++ VS/trunk/PR42-SmartFactory/IPRWorkflow/Workflows/CreateReport/CreateReport.cs
@@ -17,44 +17,56 @@ using Microsoft.SharePoint.WorkflowActions;
 
 namespace CAS.SmartFactory.IPR.Workflows.CreateReport
 {
+  /// <summary>
+  /// Create Report
+  /// </summary>
   public sealed partial class CreateReport: SequentialWorkflowActivity
   {
+    /// <summary>
+    /// Initializes a new instance of the <see cref="CreateReport" /> class.
+    /// </summary>
     public CreateReport()
     {
       InitializeComponent();
     }
 
-    public Guid workflowId = default( System.Guid );
+    public Guid workflowId = new System.Guid( "{0B735E12-FEE5-48E7-9727-D11CEE7866C6}" );
     public SPWorkflowActivationProperties workflowProperties = new SPWorkflowActivationProperties();
 
+    #region CreateListsItems
     private void CreateRecorts( object sender, EventArgs e )
     {
 
     }
+    #endregion
 
+    #region ValidationSequence
+    private void Validate( object sender, EventArgs e )
+    {
+      ValidationPaased = false;
+    }
+    private bool ValidationPaased = false;
     private void Validated( object sender, ConditionalEventArgs e )
     {
-
+      e.Result = ValidationPaased;
     }
+    #endregion
 
+    #region MainReportCreationSequence
     private void CalculateReports( object sender, EventArgs e )
     {
-
+      CalculationPassed = false;
     }
-
+    private bool CalculationPassed = false;
+    private void Calculated( object sender, ConditionalEventArgs e )
+    {
+      e.Result = CalculationPassed;
+    }
     private void GenerateReport( object sender, EventArgs e )
     {
 
     }
+    #endregion
 
-    private void Calculated( object sender, ConditionalEventArgs e )
-    {
-
-    }
-
-    private void Validate( object sender, EventArgs e )
-    {
-
-    }
   }
 }
