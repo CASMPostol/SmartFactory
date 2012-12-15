bc048f3e64539fd18867ecd3ddbd0525d9b32431
Mariusz <mpostol@cas.eu>
2012-12-15 12:46:26 +0000

    Exclude product recognition for not existing warehouses. - done

--- IPRExternalData/ERP/BatchMaterial.cs
+++ IPRExternalData/ERP/BatchMaterial.cs
@@ -7,6 +7,13 @@ namespace CAS.SmartFactory.xml.erp
 {
   public partial class BatchMaterial
   {
-    public bool IsFinishedGood { get { return this.Material_description.Trim().ToUpper().Contains( "SKU" ); } }
+    public bool IsFinishedGood
+    {
+      get
+      {
+        string _dsc = this.Material_description.Trim().ToUpper();
+        return _dsc.Contains( "SKU" ) || _dsc.Contains( "CFT" );
+      }
+    }
   }
 }
