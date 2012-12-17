2dc3efc6a9e74cf1ab5683962e7a6eef14fa079e
Mariusz <mpostol@cas.eu>
2012-12-17 16:45:32 +0000

    Create lists entries for the report; - method to create entries on the list JSOXCustomsSummary;

--- IPRWebsiteModel/Linq/JSOXCustomsSummary.cs
+++ IPRWebsiteModel/Linq/JSOXCustomsSummary.cs
@@ -4,8 +4,15 @@ namespace CAS.SmartFactory.IPR.WebsiteModel.Linq
 {
   public partial class JSOXCustomsSummary
   {
-    internal static void CreateEntries( Entities edc, JSOXLib parent )
+    /// <summary>
+    /// Creates the entries.
+    /// </summary>
+    /// <param name="edc">The <see cref="Entities"/>.</param>
+    /// <param name="parent">The parent library.</param>
+    /// <returns>Returns total sum of settled quantity fro all disposals.</returns>
+    internal static decimal CreateEntries( Entities edc, JSOXLib parent )
     {
+      decimal _ret = 0;
       List<JSOXCustomsSummary> _newEntries = new List<JSOXCustomsSummary>();
       foreach ( Disposal _dspx in Linq.Disposal.GetEntries4JSOX( edc ) )
       {
@@ -17,18 +24,21 @@ namespace CAS.SmartFactory.IPR.WebsiteModel.Linq
           JSOXIndex = parent,
           SADConsignmentDate = _dspx.Disposal2IPRIndex.CustomsDebtDate.ToString(), //TODO Wrong  type of SADConsignmentDate
           SADConsignmentNo = _dspx.Disposal2IPRIndex.DocumentNo,
-          SADDate = _dspx.SADDate, 
+          SADDate = _dspx.SADDate,
           TotalAmount = _dspx.SettledQuantity,
           Title = "Creating"
         };
+        _ret += _dspx.SettledQuantityDec;
         _dspx.JSOXCustomsSummaryIndex = _newItem;
         _newItem.CreateTitle();
+        _newEntries.Add( _newItem );
       }
+      return _ret;
     }
 
     private void CreateTitle()
     {
-     this.Title = this.SADConsignmentNo;
+      this.Title = this.SADConsignmentNo;
     }
   }
 }
