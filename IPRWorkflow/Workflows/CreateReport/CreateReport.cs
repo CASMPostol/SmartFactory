52e1fda944c243309b6b0f487f3ef26f373ed0c3
Mariusz <mpostol@cas.eu>
2012-12-18 17:27:26 +0000

    Round numbers expressing the tobacco value, duty, VAT - fixed for disposals. - fixed compilation error - wrong reference

--- IPRWorkflow/Workflows/CreateReport/CreateReport.cs
+++ IPRWorkflow/Workflows/CreateReport/CreateReport.cs
@@ -1,20 +1,7 @@
 ﻿using System;
-using System.ComponentModel;
-using System.ComponentModel.Design;
-using System.Collections;
-using System.Drawing;
-using System.Linq;
-using System.Workflow.ComponentModel.Compiler;
-using System.Workflow.ComponentModel.Serialization;
-using System.Workflow.ComponentModel;
-using System.Workflow.ComponentModel.Design;
-using System.Workflow.Runtime;
 using System.Workflow.Activities;
-using System.Workflow.Activities.Rules;
-using Microsoft.SharePoint;
-using Microsoft.SharePoint.Workflow;
-using Microsoft.SharePoint.WorkflowActions;
 using CAS.SmartFactory.IPR.WebsiteModel.Linq;
+using Microsoft.SharePoint.Workflow;
 
 namespace CAS.SmartFactory.IPR.Workflows.CreateReport
 {
