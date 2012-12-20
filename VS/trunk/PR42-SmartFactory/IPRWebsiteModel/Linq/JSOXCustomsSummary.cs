6a3e98ac5495f0aa0f5a5a2ed95991345538d656
mpostol <mpostol@8a3894fd-0c01-b944-991b-6a3da02cae8a>
2012-12-20 21:57:47 +0000

    Create lists entries for the report common part development

--- VS/trunk/PR42-SmartFactory/IPRWebsiteModel/Linq/JSOXCustomsSummary.cs
+++ VS/trunk/PR42-SmartFactory/IPRWebsiteModel/Linq/JSOXCustomsSummary.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 
 namespace CAS.SmartFactory.IPR.WebsiteModel.Linq
 {
@@ -10,19 +11,27 @@ namespace CAS.SmartFactory.IPR.WebsiteModel.Linq
     /// <param name="edc">The <see cref="Entities"/>.</param>
     /// <param name="parent">The parent library.</param>
     /// <returns>Returns total sum of settled quantity fro all disposals.</returns>
-    internal static decimal CreateEntries( Entities edc, JSOXLib parent )
+    internal static decimal CreateEntries( Entities edc, JSOXLib jSOXLib, out DateTime? nullable1, out DateTime? nullable2 )
+    {
+      throw new NotImplementedException();
+    }
+    internal static decimal CreateEntries( Entities edc, JSOXLib parent, out DateTime start, out DateTime end )
     {
       decimal _ret = 0;
       List<JSOXCustomsSummary> _newEntries = new List<JSOXCustomsSummary>();
+      start = DateTime.MaxValue;
+      end = DateTime.MinValue;
       foreach ( Disposal _dspx in Linq.Disposal.GetEntries4JSOX( edc ) )
       {
+        start = LinqIPRExtensions.Min( start, _dspx.SADDate.GetValueOrDefault( DateTime.MaxValue ) );
+        end = LinqIPRExtensions.Max( end, _dspx.SADDate.GetValueOrDefault( DateTime.MinValue ) );
         JSOXCustomsSummary _newItem = new JSOXCustomsSummary()
         {
           CompensationGood = _dspx.Disposal2PCNID.CompensationGood,
           ExportOrFreeCirculationSAD = _dspx.SADDocumentNo,
           InvoiceNo = _dspx.InvoiceNo,
           JSOXIndex = parent,
-          IntroducingSADDate = _dspx.Disposal2IPRIndex.CustomsDebtDate.Value.Date, 
+          IntroducingSADDate = _dspx.Disposal2IPRIndex.CustomsDebtDate.Value.Date,
           IntroducingSADNo = _dspx.Disposal2IPRIndex.DocumentNo,
           SADDate = _dspx.SADDate,
           TotalAmount = _dspx.SettledQuantity,
