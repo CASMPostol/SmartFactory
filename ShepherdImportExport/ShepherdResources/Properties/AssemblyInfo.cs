23aae10b8b23a18870e0212d7c138adefd04b8e5
Mariusz <mpostol@cas.eu>
2012-12-21 22:04:05 +0000

    Added Resources - I have missed them

---
+++ ShepherdImportExport/ShepherdResources/Properties/AssemblyInfo.cs
@@ -0,0 +1,37 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Security;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Shepherd Resources")]
+[assembly: AssemblyDescription("CAS Smart Factory Shepherd Localization Resources Collection")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("CAS")]
+[assembly: AssemblyProduct("CAS Smart Factory")]
+[assembly: AssemblyCopyright("Copyright © CAS 2012")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("d2899bf4-e222-407b-9fdf-9219504f17a7")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.50.0.*")]
+[assembly: AssemblyFileVersion("1.50.0")]
