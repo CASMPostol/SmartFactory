a809f6aeb6a002145a38cbf566e029e02febfa39
mpostol <mpostol@8a3894fd-0c01-b944-991b-6a3da02cae8a>
2012-12-23 22:30:27 +0000

    generate the final JSOX - JSOXReport workflow development -tested, bug fixing.

--- VS/trunk/PR42-SmartFactory/IPRWebsiteModel/Linq/JSOXLib.cs
+++ VS/trunk/PR42-SmartFactory/IPRWebsiteModel/Linq/JSOXLib.cs
@@ -30,10 +30,10 @@ namespace CAS.SmartFactory.IPR.WebsiteModel.Linq
     /// <param name="edc">The edc.</param>
     public void UpdateJSOXReport( Entities edc )
     {
-      
+
       //Introducing
-      DateTime _thisIntroducingDateStart = DateTime.MaxValue;
-      DateTime _thisIntroducingDateEnd = DateTime.MinValue;
+      DateTime _thisIntroducingDateStart = m_DateTimeMaxValue;
+      DateTime _thisIntroducingDateEnd = m_DateTimeMinValue;
       //TODO IPR reverse lookup required - summ already added IPR account
       decimal _introducingQuantity = IPR.GetIntroducingData( edc, this, out _thisIntroducingDateStart, out _thisIntroducingDateEnd );
       this.IntroducingDateStart = _thisIntroducingDateStart;
@@ -41,8 +41,8 @@ namespace CAS.SmartFactory.IPR.WebsiteModel.Linq
       this.IntroducingQuantity = _introducingQuantity.Convert2Double2Decimals();
 
       //Outbound
-      DateTime _thisOutboundDateEnd = DateTime.MinValue;
-      DateTime _thisOutboundDateStart = DateTime.MaxValue;
+      DateTime _thisOutboundDateEnd = m_DateTimeMinValue;
+      DateTime _thisOutboundDateStart = m_DateTimeMaxValue;
       //TODO JSOXCustomsSummary reverse lookup - summ already added disposals account
       decimal _outQuantity = JSOXCustomsSummary.CreateEntries( edc, this, out _thisOutboundDateStart, out _thisOutboundDateEnd );
       this.OutboundQuantity = _outQuantity.Convert2Double2Decimals();
@@ -68,6 +68,8 @@ namespace CAS.SmartFactory.IPR.WebsiteModel.Linq
 
     #region private
     private IQueryable<JSOXLib> Previous( Entities edc ) { return from _jsx in edc.JSOXLibrary orderby _jsx.BalanceDate.Value descending select _jsx; }
+    private DateTime m_DateTimeMaxValue = DateTime.Today + TimeSpan.FromDays( 3000 );
+    private DateTime m_DateTimeMinValue = DateTime.Today - TimeSpan.FromDays( 3000 );
     #endregion
 
   }
