336f9ed78f9500cd06eae8c30a27ecf74256f13a
mpostol <mpostol@8a3894fd-0c01-b944-991b-6a3da02cae8a>
2012-12-30 10:02:01 +0000

    Update and create new entries on the site - added deferred tracking functionality - must be improved.

--- VS/trunk/PR42-SmartFactory/Shepherd/SharePointLinqClient/EntityList.cs
+++ VS/trunk/PR42-SmartFactory/Shepherd/SharePointLinqClient/EntityList.cs
@@ -17,12 +17,11 @@ namespace Microsoft.SharePoint.Linq
   // Type parameters:
   //   TEntity:
   //     The content type of the list items.
-  public sealed class EntityList<TEntity>: IOrderedQueryable<TEntity>, IQueryable<TEntity>, IEnumerable<TEntity>, IOrderedQueryable, IQueryable, IEnumerable
+  public sealed class EntityList<TEntity>: EntityListData, IOrderedQueryable<TEntity>, IQueryable<TEntity>, IEnumerable<TEntity>, IOrderedQueryable, IQueryable, IEnumerable
     where TEntity: class, ITrackEntityState, ITrackOriginalValues, INotifyPropertyChanged, INotifyPropertyChanging, new()
   {
     private DataContext m_DataContext;
     private string m_ListName;
-    private SPCList m_list = default( SPCList );
     private ListItemCollection m_allItems = default( ListItemCollection );
     private List<TEntity> m_allItemsEntities = new List<TEntity>();
     internal EntityList( DataContext dataContext, string listName )
@@ -146,11 +145,11 @@ namespace Microsoft.SharePoint.Linq
     {
       if ( entity == null )
         throw new ArgumentNullException( "entity", "entity is null." );
-      ListItem item = m_list.AddItem( new ListItemCreationInformation() );
-      GetValues( entity, typeof( TEntity ), ( na, val ) => item[ na ] = val );
+      m_EntitieAssociations.Add( m_allItems.Count, null );
+      m_allListItems.Add( entity );
       entity.EntityState = EntityState.ToBeInserted;
-      item.Update();
     }
+    private List<TEntity> m_allListItems = new List<TEntity>();
     //
     // Summary:
     //     Marks the specified entities to be put in the Recycle Bin on the next call
@@ -273,36 +272,6 @@ namespace Microsoft.SharePoint.Linq
       if ( type.BaseType != typeof( Object ) )
         AssignValues( entity, type.BaseType, getValue );
     }
-    private static void GetValues( object entity, Type type, Method<string, object> assign )
-    {
-      Dictionary<string, MemberInfo> _mmbrs = GetMembers( type );
-      foreach ( MemberInfo _ax in from _pidx in _mmbrs where _pidx.Value.MemberType == MemberTypes.Property select _pidx.Value )
-        foreach ( var _cax in _ax.GetCustomAttributes( false ) )
-          if ( _cax is ColumnAttribute )
-          {
-            ColumnAttribute _ca = _cax as ColumnAttribute;
-            if ( _ca.ReadOnly )
-              continue;
-            FieldInfo _strg = _mmbrs[ _ca.Storage ] as FieldInfo;
-            object value = _strg.GetValue( entity );
-            assign( _ca.Name, value );
-          }
-          else if ( _cax is AssociationAttribute )
-          {
-            AssociationAttribute _aa = _cax as AssociationAttribute;
-            //TODO development
-          }
-      if ( type.BaseType != typeof( Object ) )
-        GetValues( entity, type.BaseType, assign );
-    }
-    private static Dictionary<string, MemberInfo> GetMembers( Type type )
-    {
-      BindingFlags _flgs = BindingFlags.DeclaredOnly | BindingFlags.Instance | BindingFlags.GetProperty | BindingFlags.GetField | BindingFlags.Public | BindingFlags.NonPublic;
-      Dictionary<string, MemberInfo> _mmbrs = ( from _midx in type.GetMembers( _flgs )
-                                                where _midx.MemberType == MemberTypes.Field || _midx.MemberType == MemberTypes.Property
-                                                select _midx ).ToDictionary<MemberInfo, string>( _mi => _mi.Name );
-      return _mmbrs;
-    }
     private void _newEntity_PropertyChanged( object sender, PropertyChangedEventArgs e )
     {
       ITrackEntityState _entity = sender as ITrackEntityState;
@@ -314,7 +283,10 @@ namespace Microsoft.SharePoint.Linq
     {
       //Do nothing
     }
-    private delegate void Method<T1, T2>( T1 arg1, T2 arg2 );
+    protected override ITrackEntityState GetEntity( int key )
+    {
+      return m_allListItems[ key ];
+    }
     #endregion
   }
 }
