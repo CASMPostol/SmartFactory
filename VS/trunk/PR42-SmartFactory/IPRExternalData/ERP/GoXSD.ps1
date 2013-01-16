16f032e3f2bfac3ab11e07e07887baae62861caf
mpostol <mpostol@8a3894fd-0c01-b944-991b-6a3da02cae8a>
2013-01-16 14:35:28 +0000

    Stock entry fatal error - fixed - new schema.

--- VS/trunk/PR42-SmartFactory/IPRExternalData/ERP/GoXSD.ps1
+++ VS/trunk/PR42-SmartFactory/IPRExternalData/ERP/GoXSD.ps1
@@ -1,8 +1,8 @@
 ﻿Write-Host "This file creates all the c# classes for ERP namespace"
 $env:path += "; C:\Program Files\Microsoft SDKs\Windows\v7.1\Bin\x64\"
-# xsd.exe Invoice.xsd /N:CAS.SmartFactory.xml.ERP /c  |write-host
-xsd.exe stock.xsd /N:CAS.SmartFactory.xml.ERP /c  |write-host
-#xsd.exe batch.xsd /N:CAS.SmartFactory.xml.ERP /c  |write-host
+# xsd.exe Invoice.xsd /N:CAS.SmartFactory.xml.erp /c  |write-host
+xsd.exe stock.xsd /N:CAS.SmartFactory.xml.erp /c  |write-host
+#xsd.exe batch.xsd /N:CAS.SmartFactory.xml.erp /c  |write-host
 #xsd.exe SKUCigarettesSchema.xsd /N:CAS.SmartFactory.xml.erp /c  |write-host
 #xsd.exe SKUCutfillerSchema.xsd /N:CAS.SmartFactory.xml.erp /c  |write-host
 Write-host Done...
