85ee4f772bd27e1888082a8d98e3882adb7d8e75
Mariusz <mpostol@cas.eu>
2013-01-23 09:31:44 +0000

    New code for consent - fixed, but procedure codes must be moved outside the code.

---
+++ IPRWebsiteModel/GlobalDefinitions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CAS.SmartFactory.IPR.WebsiteModel
+{
+  internal static class GlobalDefinitions
+  {
+    //TODO Customs Procedure Code - use separate dictionary to handle relations
+    internal static const string CustomsProcedureCode1PG1 = "1PG1";
+    internal static const string CustomsProcedureCodeC601 = "C601";
+    internal static const string CustomsProcedureCodeC600 = "C600";
+  }
+}
