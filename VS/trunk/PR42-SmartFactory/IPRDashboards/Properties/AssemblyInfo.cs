0b43a83177f3ec42a8e36fcb60b3dc1f67ab2965
mpostol <mpostol@8a3894fd-0c01-b944-991b-6a3da02cae8a>
2013-03-04 07:40:41 +0000

    Create 1.60.41 bug fix installation package - changed assemblies version.

--- VS/trunk/PR42-SmartFactory/IPRDashboards/Properties/AssemblyInfo.cs
+++ VS/trunk/PR42-SmartFactory/IPRDashboards/Properties/AssemblyInfo.cs
@@ -33,8 +33,8 @@ using System.Security;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.60.40.*")]
-[assembly: AssemblyFileVersion("1.60.40")]
+[assembly: AssemblyVersion("1.60.41.*")]
+[assembly: AssemblyFileVersion("1.60.41")]
 
 
 [assembly: System.Runtime.CompilerServices.InternalsVisibleTo( "IPRDashboardsTest, PublicKey=002400000480000094000000060200000024000052534131000400000100010015c024c9d0c88f" +
