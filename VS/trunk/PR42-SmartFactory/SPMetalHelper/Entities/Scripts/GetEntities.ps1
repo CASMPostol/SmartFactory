5ef92a617dbd5ca3ea35ebddc8c648877b0dc6da
mpostol <mpostol@8a3894fd-0c01-b944-991b-6a3da02cae8a>
2013-05-07 13:39:53 +0000

    Project cleanup, refactoring - Removed the project SPMetalHelper because we no longer use it.

--- VS/trunk/PR42-SmartFactory/SPMetalHelper/Entities/Scripts/GetEntities.ps1
+++
@@ -1,8 +0,0 @@
-﻿Write-Host "This file creates all the entities classes for the selected site"
-$env:path += "; C:\Program Files\Common Files\Microsoft Shared\Web server extensions\14\Bin"
-Get-Location | Write-host
-SPMetal.exe /web:http://casmp/sites/s42 /language:csharp /namespace:CAS.SmartFactory.Shepherd.Entities /parameters:Parameters.xml /code:..\Entities.cs| write-host
-write-host "Done . "
-
-
-
