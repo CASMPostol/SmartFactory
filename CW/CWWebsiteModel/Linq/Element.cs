bcf37793de0a3959c89d7f84920f6b21d99d9c98
Mariusz <mpostol@cas.eu>
2013-07-02 12:46:35 +0000

    Generate Sites Collection Data Model - created model

---
+++ CW/CWWebsiteModel/Linq/Element.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CAS.SmartFactory.CW.WebsiteModel.Linq
+{
+  /// <summary>
+  /// Element
+  /// </summary>
+  public partial class Element
+  {
+  }
+}
