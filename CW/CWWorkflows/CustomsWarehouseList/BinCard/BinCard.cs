018827292f36618d06f3df5ca81de5e95657353f
Mariusz <mpostol@cas.eu>
2013-07-29 11:21:01 +0000

    Workflow to create BinCard lib entry - Entities was read only - fixed.

--- CW/CWWorkflows/CustomsWarehouseList/BinCard/BinCard.cs
+++ CW/CWWorkflows/CustomsWarehouseList/BinCard/BinCard.cs
@@ -18,23 +18,37 @@ namespace CAS.SmartFactory.CW.Workflows.CustomsWarehouseList.BinCard
     public SPWorkflowActivationProperties workflowProperties = new SPWorkflowActivationProperties();
     private void CreateBinCard( object sender, EventArgs e )
     {
-      using ( Entities _entities = new Entities( workflowProperties.WebUrl ) { ObjectTrackingEnabled = false } )
+      try
       {
-        CustomsWarehouse _cw = Element.GetAtIndex<CustomsWarehouse>( _entities.CustomsWarehouse, workflowProperties.ItemId );
-        BinCardContentType _newBinCard = Factory.CreateContent( _cw );
-        if ( _cw.CWL_CW2BinCardTitle == null )
+        using ( Entities _entities = new Entities( workflowProperties.WebUrl ) )
         {
-          string _documentName = Settings.BinCardDocumentName( _entities, workflowProperties.ItemId + 1 );
-          SPFile _newFile =  File.CreateXmlFile<BinCardContentType>( workflowProperties.Web, _newBinCard, _documentName, BinCardLib.Name, BinCardContentType.StylesheetNmane );
-          BinCardLib _BinCardLibRntry = Element.GetAtIndex<BinCardLib>( _entities.BinCardLibrary, _newFile.Item.ID );
-          _cw.CWL_CW2BinCardTitle = _BinCardLibRntry;
-          _entities.SubmitChanges();
+          CustomsWarehouse _cw = Element.GetAtIndex<CustomsWarehouse>( _entities.CustomsWarehouse, workflowProperties.ItemId );
+          BinCardContentType _newBinCard = Factory.CreateContent( _cw );
+          if ( _cw.CWL_CW2BinCardTitle == null )
+          {
+            string _documentName = Settings.BinCardDocumentName( _entities, workflowProperties.ItemId );
+            SPFile _newFile = File.CreateXmlFile<BinCardContentType>( workflowProperties.Web, _newBinCard, _documentName, BinCardLib.Name, BinCardContentType.StylesheetNmane );
+            BinCardLib _BinCardLibRntry = Element.GetAtIndex<BinCardLib>( _entities.BinCardLibrary, _newFile.Item.ID );
+            _cw.CWL_CW2BinCardTitle = _BinCardLibRntry;
+            _entities.SubmitChanges();
+          }
+          else
+            File.WriteXmlFile<BinCardContentType>( workflowProperties.Item.File, _newBinCard, BinCardContentType.StylesheetNmane );
         }
-        else
-          File.WriteXmlFile<BinCardContentType>( workflowProperties.Item.File, _newBinCard, BinCardContentType.StylesheetNmane );
+        logToHistoryListActivity_HistoryOutcome = "Success";
+        logToHistoryListActivity_HistoryDescription = "Document created successfully";
+
       }
-      logToHistoryListActivity_HistoryOutcome = "Success";
-      logToHistoryListActivity_HistoryDescription = "Document created successfully";
+      catch ( CAS.SharePoint.ApplicationError _ap )
+      {
+        logToHistoryListActivity_HistoryOutcome = "ApplicationError";
+        logToHistoryListActivity_HistoryDescription = _ap.Message;
+      }
+      catch ( Exception _ex )
+      {
+        logToHistoryListActivity_HistoryOutcome = "Exeption";
+        logToHistoryListActivity_HistoryDescription = _ex.Message;
+      }
     }
     public String logToHistoryListActivity_HistoryOutcome = default( System.String );
     public String logToHistoryListActivity_HistoryDescription = default( System.String );
