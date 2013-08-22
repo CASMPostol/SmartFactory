27ee8757ed9d8c361c56b5fa183ee6f671d52e79
Mariusz <mpostol@cas.eu>
2013-08-22 13:04:44 +0000

    SAD Consignment No. - add new coulmn - handled new column on the Disposal list - once more refactoring and compiler error fix.

--- IPR/IPRWebsiteModel/Linq/SADConsignment.cs
+++ IPR/IPRWebsiteModel/Linq/SADConsignment.cs
@@ -1,12 +1,32 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿//<summary>
+//  Title   : Name of Application
+//  System  : Microsoft Visual C# .NET 2012
+//  $LastChangedDate:$
+//  $Rev:$
+//  $LastChangedBy:$
+//  $URL:$
+//  $Id:$
+//
+//  Copyright (C) 2013, CAS LODZ POLAND.
+//  TEL: +48 (42) 686 25 47
+//  mailto://techsupp@cas.eu
+//  http://www.cas.eu
+//</summary>
+      
 
 namespace CAS.SmartFactory.IPR.WebsiteModel.Linq
 {
+  /// <summary>
+  /// SADConsignment entity partial class
+  /// </summary>
   public partial class SADConsignment
   {
+    /// <summary>
+    /// returns document number.
+    /// </summary>
+    /// <param name="entities">The entities.</param>
+    /// <param name="sadConsignmentNumber">The sad consignment number.</param>
+    /// <returns></returns>
     internal static string DocumentNumber( Entities entities, int sadConsignmentNumber )
     {
       return Settings.DocumentNumber( entities, sadConsignmentNumber );
