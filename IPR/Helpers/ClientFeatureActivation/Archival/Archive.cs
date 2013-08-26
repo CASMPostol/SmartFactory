be3ee0f1e2312b73317d1d01df43f3273a25ea1a
Mariusz <mpostol@cas.eu>
2013-08-26 13:15:20 +0000

    IPR account closing - added recalculation functionality to the feature activation.

---
+++ IPR/Helpers/ClientFeatureActivation/Archival/Archive.cs
@@ -0,0 +1,34 @@
+﻿//<summary>
+//  Title   : Archive class contain collection of function supporting archival data management
+//  System  : Microsoft Visual C# .NET 2012
+//  $LastChangedDate:$
+//  $Rev:$
+//  $LastChangedBy:$
+//  $URL:$
+//  $Id:$
+//
+//  Copyright (C) 2013, CAS LODZ POLAND.
+//  TEL: +48 (42) 686 25 47
+//  mailto://techsupp@cas.eu
+//  http://www.cas.eu
+//</summary>
+
+using System;
+using CAS.SmartFactory.IPR.WebsiteModel.Linq;
+
+namespace CAS.SmartFactory.IPR.Client.FeatureActivation.Archival
+{
+  /// <summary>
+  /// Archive class contain collection of function supporting archival data management
+  /// </summary>
+  internal static class Archive
+  {
+
+    internal static void Go( Entities edc, Action<object, EntitiesChangedEventArgs> ProgressChanged )
+    {
+      //foreach ( CAS.SmartFactory.IPR.WebsiteModel.Linq.IPR _iprX in edc.IPR )
+      //  if (_iprX.AccountClosed.Value == true
+
+    }
+  }
+}
---
+++ IPR/Helpers/ClientFeatureActivation/Linq/IPR.cs
@@ -0,0 +1,41 @@
+﻿//<summary>
+//  Title   : IPR partial class
+//  System  : Microsoft Visual C# .NET 2012
+//  $LastChangedDate:$
+//  $Rev:$
+//  $LastChangedBy:$
+//  $URL:$
+//  $Id:$
+//
+//  Copyright (C) 2013, CAS LODZ POLAND.
+//  TEL: +48 (42) 686 25 47
+//  mailto://techsupp@cas.eu
+//  http://www.cas.eu
+//</summary>
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CAS.SmartFactory.IPR.WebsiteModel.Linq
+{
+  /// <summary>
+  /// IPR partial class 
+  /// </summary>
+  public sealed partial class IPR
+  {
+
+    #region public
+    internal void RecalculateClearedRecords()
+    {
+      if ( this.AccountClosed.Value )
+        throw new ApplicationException( "IPR.RecalculateClearedRecords cannot be excuted for closed account" );
+      List<Disposal> _2Calculate = ( from _dx in this.Disposal where _dx.CustomsStatus.Value == Linq.CustomsStatus.Finished orderby _dx.No.Value ascending select _dx ).ToList<Disposal>();
+      this.AccountBalance = this.NetMass;
+      foreach ( Disposal _dx in _2Calculate )
+        _dx.CalculateRemainingQuantity();
+    }
+    #endregion
+
+  }
+}
