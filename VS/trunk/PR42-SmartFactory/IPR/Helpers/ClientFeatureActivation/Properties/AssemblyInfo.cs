2265e91bef9cf72b34594ee732573bc340f3b5ce
mpostol <mpostol@8a3894fd-0c01-b944-991b-6a3da02cae8a>
2013-08-31 19:19:00 +0000

    Deploy client site IPR management console. - updated version information.

--- VS/trunk/PR42-SmartFactory/IPR/Helpers/ClientFeatureActivation/Properties/AssemblyInfo.cs
+++ VS/trunk/PR42-SmartFactory/IPR/Helpers/ClientFeatureActivation/Properties/AssemblyInfo.cs
@@ -1,36 +1,27 @@
-﻿using System.Reflection;
+﻿
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle( "ClientFeatureActivation" )]
-[assembly: AssemblyDescription( "" )]
-[assembly: AssemblyConfiguration( "" )]
-[assembly: AssemblyCompany( "Microsoft" )]
-[assembly: AssemblyProduct( "ClientFeatureActivation" )]
-[assembly: AssemblyCopyright( "Copyright © Microsoft 2013" )]
-[assembly: AssemblyTrademark( "" )]
-[assembly: AssemblyCulture( "" )]
+[assembly: AssemblyTitle("IPR Client Feature Activation")]
+[assembly: AssemblyDescription("Smart Factory IPR client site feature activation library")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("CAS")]
+[assembly: AssemblyProduct("Smart Factory")]
+[assembly: AssemblyCopyright("Copyright (c) 2000-2013 CAS LODZ POLAND")]
+[assembly: AssemblyTrademark("Smart Factory")]
+[assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible( false )]
+[assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid( "9b6c4c60-670b-444f-ae08-7cd2df5f4de4" )]
+[assembly: Guid("9FDFA43A-CE88-48CF-B1D2-66081FFDE8F8")]
+[assembly: AssemblyVersion("1.80.10.*")]
+[assembly: AssemblyFileVersion("1.80.10")]
 
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion( "1.0.0.0" )]
-[assembly: AssemblyFileVersion( "1.0.0.0" )]
