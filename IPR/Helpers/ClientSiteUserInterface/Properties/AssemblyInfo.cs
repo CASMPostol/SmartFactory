fe53aeac2cd077212a7c234261f20fcb6e48ca6b
Mariusz <mpostol@cas.eu>
2013-08-31 19:19:00 +0000

    Deploy client site IPR management console. - updated version information.

--- IPR/Helpers/ClientSiteUserInterface/Properties/AssemblyInfo.cs
+++ IPR/Helpers/ClientSiteUserInterface/Properties/AssemblyInfo.cs
@@ -1,19 +1,18 @@
-﻿using System.Reflection;
-using System.Resources;
+﻿
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
-using System.Windows;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ClientSiteUserInterface")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("IPR Client User Interface")]
+[assembly: AssemblyDescription("Smart Factory IPR client user interface application")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ClientSiteUserInterface")]
-[assembly: AssemblyCopyright("Copyright ©  2013")]
-[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCompany("CAS")]
+[assembly: AssemblyProduct("Smart Factory")]
+[assembly: AssemblyCopyright("Copyright (c) 2000-2013 CAS LODZ POLAND")]
+[assembly: AssemblyTrademark("Smart Factory")]
 [assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
@@ -21,35 +20,8 @@ using System.Windows;
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
-//In order to begin building localizable applications, set 
-//<UICulture>CultureYouAreCodingWith</UICulture> in your .csproj file
-//inside a <PropertyGroup>.  For example, if you are using US english
-//in your source files, set the <UICulture> to en-US.  Then uncomment
-//the NeutralResourceLanguage attribute below.  Update the "en-US" in
-//the line below to match the UICulture setting in the project file.
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("DFAF6384-7F9B-4BEB-82B2-BC2B4CE29742")]
+[assembly: AssemblyVersion("1.80.10.*")]
+[assembly: AssemblyFileVersion("1.80.10")]
 
-//[assembly: NeutralResourcesLanguage("en-US", UltimateResourceFallbackLocation.Satellite)]
-
-
-[assembly: ThemeInfo(
-    ResourceDictionaryLocation.None, //where theme specific resource dictionaries are located
-  //(used if a resource is not found in the page, 
-  // or application resource dictionaries)
-    ResourceDictionaryLocation.SourceAssembly //where the generic resource dictionary is located
-  //(used if a resource is not found in the page, 
-  // app, or any theme specific resource dictionaries)
-)]
-
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
--- IPR/Interoperability/Properties/AssemblyInfo.cs
+++ IPR/Interoperability/Properties/AssemblyInfo.cs
@@ -22,16 +22,6 @@ using System.Runtime.InteropServices;
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("3EABDD4B-227C-483A-A855-03C7628286FD")]
 
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.80.00.*")]
 [assembly: AssemblyFileVersion("1.80.00")]
 
