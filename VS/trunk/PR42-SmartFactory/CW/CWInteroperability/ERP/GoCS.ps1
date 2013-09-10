712bd2ac3dd30c6d5ce2810af7bb633931937509
bwojciechowski <bwojciechowski@8a3894fd-0c01-b944-991b-6a3da02cae8a>
2013-09-10 08:22:13 +0000

    Disposal Request - document import and processing - xsd, xltx and cs files for DisposalRequest processing added.

---
+++ VS/trunk/PR42-SmartFactory/CW/CWInteroperability/ERP/GoCS.ps1
@@ -0,0 +1,4 @@
+﻿Write-Host "This file creates all the c# classes for ERP namespace"
+$env:path += "; C:\Program Files (x86)\Microsoft SDKs\Windows\v8.0A\bin\NETFX 4.0 Tools"
+xsd.exe DisposalRequest.xsd /N:CAS.SmartFactory.xml.erp /c  |write-host
+Write-host Done...
