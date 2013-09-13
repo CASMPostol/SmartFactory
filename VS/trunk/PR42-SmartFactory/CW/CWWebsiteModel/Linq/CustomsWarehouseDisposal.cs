8df855dd763347cdff89c8b0c1881572e4ea95cf
mpostol <mpostol@8a3894fd-0c01-b944-991b-6a3da02cae8a>
2013-09-13 09:04:36 +0000

    Disposal Request - document import and processing - selected columns to assign - duplicated columns marked.

--- VS/trunk/PR42-SmartFactory/CW/CWWebsiteModel/Linq/CustomsWarehouseDisposal.cs
+++ VS/trunk/PR42-SmartFactory/CW/CWWebsiteModel/Linq/CustomsWarehouseDisposal.cs
@@ -7,14 +7,34 @@ namespace CAS.SmartFactory.CW.WebsiteModel.Linq
 {
   public partial class CustomsWarehouseDisposal
   {
-    private static string batch;
-    public static CustomsWarehouseDisposal Create(DisposalRequestLib parent)
+    private static double addedKg;
+    private static double declaredNetMass;
+    private static string skuDescription; //xml
+    public static CustomsWarehouseDisposal Create(DisposalRequestLib parent, Linq.CustomsWarehouse account)
     {
       return new CustomsWarehouseDisposal()
         {
-          Batch = batch,
-           CWL_CWDisposal2DisposalRequestLibraryID = parent,
-            
+          Batch = account.Batch,
+          CWL_CWDisposal2DisposalRequestLibraryID = parent,
+          CW_AddedKg = addedKg,
+          CW_DeclaredToClear = addedKg, // ??
+          CW_QuantityToClear = declaredNetMass, //TODO duplicated
+          CW_DeclaredNetMass = declaredNetMass,
+
+          Currency = account.Currency,
+          CustomsStatus = Linq.CustomsStatus.NotStarted,
+          CWL_CWDisposal2PCNTID = account.CWL_CW2PCNID,
+          CWC_EntryDate = DateTime.Today, //TODO duplcated creation date
+          Grade = account.Grade,
+          SKU = account.SKU,
+          TobaccoName = account.TobaccoName,
+          SKUDescription = skuDescription,
+          Title = "xreating", //TODO
+          CW_PackageToClear = 0,// count
+          CW_SettledGrossMass = 0, //count
+          CW_SettledNetMass = 0,//count
+          CW_PackageAvailable = 0,//TODO duplicated
+          CW_QuantityAvailable = 0,//TODO duplicated
         };
     }
 
