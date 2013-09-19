a16179107e2b15b4ee5916cc76a812e144d683bd
mpostol <mpostol@8a3894fd-0c01-b944-991b-6a3da02cae8a>
2013-09-19 05:50:47 +0000

    Disposal Request WebPart User Interface - Added testing environment.

---
+++ VS/trunk/PR42-SmartFactory/CW/Tests/SharePointProjects/TestDisposalRequest/Properties/AssemblyInfo.cs
@@ -0,0 +1,38 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Security;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("TestDisposalRequest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyProduct("TestDisposalRequest")]
+[assembly: AssemblyCopyright("Copyright © Microsoft 2013")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("23461d4d-437c-4663-8b92-38f85398e12a")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+
---
+++ VS/trunk/PR42-SmartFactory/CW/Tests/SharePointProjects/TestDisposalRequest/SilverLight/SilverlightExceptionScript.tt.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TestDisposalRequest.SilverLight
+{
+  public partial class SilverlightExceptionScript
+  {
+    public string ClientID = String.Empty;
+
+  }
+}
