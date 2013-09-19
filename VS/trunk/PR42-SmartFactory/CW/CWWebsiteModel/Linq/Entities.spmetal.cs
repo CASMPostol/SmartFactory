1a3f56c6b07ce4055662a0ed414cbc9d59b753af
mpostol <mpostol@8a3894fd-0c01-b944-991b-6a3da02cae8a>
2013-09-19 12:56:53 +0000

    Disposal Request - document import and processing - fixed ActivityLogCT.

---
+++ VS/trunk/PR42-SmartFactory/CW/CWWebsiteModel/Linq/Entities.spmetal.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.SharePoint;
+using Microsoft.SharePoint.Linq;
+
+namespace CAS.SmartFactory.CW.WebsiteModel.Linq
+{
+  public partial class Entities
+  {
+    #region public
+    /// <summary>
+    /// Initializes a new instance of the <see cref="Entities" /> class.
+    /// </summary>
+    public Entities() : this( SPContext.Current.Web.Url ) { }
+    /// <summary>
+    /// Persists to the content database changes made by the current user to one or more lists using the specified failure mode;
+    /// or, if a concurrency conflict is found, populates the <see cref="P:Microsoft.SharePoint.Linq.DataContext.ChangeConflicts"/> property.
+    /// </summary>
+    /// <param name="mode">Specifies how the list item changing system of the LINQ to SharePoint provider will respond when it 
+    /// finds that a list item has been changed by another process since it was retrieved.
+    /// </param>
+    public void SubmitChangesSilently( RefreshMode mode )
+    {
+      try
+      {
+        SubmitChanges();
+      }
+      catch ( ChangeConflictException )
+      {
+        foreach ( ObjectChangeConflict changedListItem in this.ChangeConflicts )
+          changedListItem.Resolve( mode );
+        this.SubmitChanges();
+      }
+    }
+    /// <summary>
+    /// Resolves the change conflicts.
+    /// </summary>
+    /// <param name="result">The <see cref="ActionResult"/>.</param>
+    /// <exception cref="System.ApplicationException"></exception>
+    internal void ResolveChangeConflicts( ActionResult result )
+    {
+      string _cp = "Starting";
+      try
+      {
+        foreach ( ObjectChangeConflict _itx in this.ChangeConflicts )
+        {
+          _cp = "ObjectChangeConflict";
+          string _tmp = String.Format( "Object: {0}", _itx.Object == null ? "null" : _itx.Object.ToString() );
+          if ( _itx.MemberConflicts != null )
+          {
+            string _ft = ", Conflicts: Member.Name={0}; CurrentValue={1}; DatabaseValue={2}; OriginalValue={3}";
+            String _chnges = String.Empty;
+            foreach ( MemberChangeConflict _mid in _itx.MemberConflicts )
+            {
+              _chnges += String.Format( _ft,
+                _mid.Member == null ? "null" : _mid.Member.Name,
+                _mid.CurrentValue == null ? "null" : _mid.CurrentValue.ToString(),
+                _mid.DatabaseValue == null ? "null" : _mid.DatabaseValue.ToString(),
+                _mid.OriginalValue == null ? "null" : _mid.OriginalValue.ToString() );
+            }
+            _tmp += _chnges;
+          }
+          else
+            _tmp += "; No member details";
+          result.AddMessage( "ResolveChangeConflicts at: " + _cp, _tmp );
+          _cp = "AddMessage";
+          _itx.Resolve( RefreshMode.KeepCurrentValues );
+        } //foreach (ObjectChangeConflict
+      }
+      catch ( Exception ex )
+      {
+        string _frmt = "The current operation has been interrupted in ResolveChangeConflicts at {0} by error {1}.";
+        throw new ApplicationException( String.Format( _frmt, _cp, ex.Message ) );
+      }
+    }
+    #endregion
+  }
+}
