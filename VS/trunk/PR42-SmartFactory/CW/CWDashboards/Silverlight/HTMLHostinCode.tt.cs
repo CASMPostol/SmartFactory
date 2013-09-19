c70edee1d9f8508194a5ab48cbb9d9693b682f5c
mpostol <mpostol@8a3894fd-0c01-b944-991b-6a3da02cae8a>
2013-09-19 09:57:24 +0000

    Disposal Request WebPart User Interface - added hosting webpart for DisposalRequestHost SilverLight webpart. - part 2.

---
+++ VS/trunk/PR42-SmartFactory/CW/CWDashboards/Silverlight/HTMLHostinCode.tt.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CAS.SmartFactory.CW.Dashboards.Silverlight
+{
+  public partial class HTMLHostinCode
+  {
+    public string Width = "100%";
+    public string Height = "100%";
+    public string Source = String.Empty;
+    public string ErrorScript = String.Empty;
+    public string Initparams = String.Empty;
+  }
+}
