24dbb6519aef23388cdbc3209c81d5c7b8886f63
Mariusz <mpostol@cas.eu>
2013-09-20 06:42:47 +0000

    Disposal Request WebPart User Interface - changed the structure of folder, Added interconnection properties.

---
+++ CW/CWDashboards/Webparts/DisposalRequestHost/DisposalRequestHost.cs
@@ -0,0 +1,80 @@
+﻿//<summary>
+//  Title   : Disposal Request SilverLight Host
+//  System  : Microsoft Visual C# .NET 2012
+//  $LastChangedDate:$
+//  $Rev:$
+//  $LastChangedBy:$
+//  $URL:$
+//  $Id:$
+//
+//  Copyright (C) 2013, CAS LODZ POLAND.
+//  TEL: +48 (42) 686 25 47
+//  mailto://techsupp@cas.eu
+//  http://www.cas.eu
+//</summary>
+
+using System;
+using System.ComponentModel;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Web.UI.WebControls.WebParts;
+using CAS.SmartFactory.CW.Dashboards.SharePointLib;
+using CAS.SmartFactory.CW.Dashboards.Silverlight;
+using Microsoft.SharePoint;
+using Microsoft.SharePoint.WebControls;
+
+namespace CAS.SmartFactory.CW.Dashboards.Webparts.DisposalRequestHost
+{
+  /// <summary>
+  /// Disposal Request SilverLight <see cref="WebPart"/> Host
+  /// </summary>
+  [ToolboxItemAttribute( false )]
+  public class DisposalRequestHost: WebPart
+  {
+    #region Interconnections Providers
+    /// <summary>
+    /// Sets the BatchInterconnection provider.
+    /// </summary>
+    /// <param name="_provider">The provider interface.</param>
+    [ConnectionConsumer( "Batch list interconnection", "BatchInterconnection", AllowsMultipleConnections = false )]
+    public void SetBatchProvider( IWebPartRow _provider )
+    {
+     m_ProvidersDictionary = _provider;
+    }
+    #endregion
+
+    private IWebPartRow m_ProvidersDictionary = default(IWebPartRow);
+    /// <summary>
+    /// Called by the ASP.NET page framework to notify server controls that use composition-based implementation to create any child controls they contain in preparation for posting back or rendering.
+    /// </summary>
+    protected override void CreateChildControls()
+    {
+      try
+      {
+        SilverlightWebControl _slwc = new SilverlightWebControl()
+                                          {
+                                            Source = "SiteAssets/TestDisposalRequest/DisposalRequestWebPart/DisposalRequestWebPart.xap",
+                                          };
+        Controls.Add( _slwc );
+      }
+      catch ( Exception ex )
+      {
+        this.Controls.Add( new ExceptionMessage( ex ) );
+      }
+    }
+    /// <summary>
+    /// Raises the <see cref="E:System.Web.UI.Control.PreRender" /> event.
+    /// </summary>
+    /// <param name="e">An <see cref="T:System.EventArgs" /> object that contains the event data.</param>
+    protected override void OnPreRender( EventArgs e )
+    {
+      SetInterconnectionData( m_ProvidersDictionary );
+      base.OnPreRender( e );
+    }
+    private void SetInterconnectionData( IWebPartRow m_ProvidersDictionary )
+    {
+      throw new NotImplementedException();
+    }
+  }
+}
---
+++ CW/CWDashboards/Webparts/InterconnectionData.cs
@@ -0,0 +1,40 @@
+﻿//<summary>
+//  Title   : InterconnectionData class
+//  System  : Microsoft Visual C# .NET 2012
+//  $LastChangedDate:$
+//  $Rev:$
+//  $LastChangedBy:$
+//  $URL:$
+//  $Id:$
+//
+//  Copyright (C) 2013, CAS LODZ POLAND.
+//  TEL: +48 (42) 686 25 47
+//  mailto://techsupp@cas.eu
+//  http://www.cas.eu
+//</summary>
+      
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using CAS.SmartFactory.CW.WebsiteModel.Linq;
+
+namespace CAS.SmartFactory.CW.Dashboards.Webparts
+{
+  /// <summary>
+  /// InterconnectionData class provides functionality to consume webpatrs interconnection.
+  /// </summary>
+  /// <typeparam name="DerivedType">The type of the erived type.</typeparam>
+  internal abstract class InterconnectionData<DerivedType>: CAS.SharePoint.Web.InterconnectionData<DerivedType>
+      where DerivedType: InterconnectionData<DerivedType>
+  {
+    internal string ID { get { return GetFieldValue( Element.IDColunmName ); } }
+    internal string Title { get { return GetFieldValue( Element.TitleColunmName ); } }
+  }
+  internal class DisposalRequestnterconnectionData: InterconnectionData<DisposalRequestnterconnectionData>
+  {
+    internal DisposalRequestnterconnectionData()
+      : base()
+    { }
+  }
+}
