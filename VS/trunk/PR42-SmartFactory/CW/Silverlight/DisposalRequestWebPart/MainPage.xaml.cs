d697e9e2dad9d1ecf53cb72ef8c45b4454726007
mpostol <mpostol@8a3894fd-0c01-b944-991b-6a3da02cae8a>
2013-09-24 08:10:10 +0000

    Disposal Request WebPart User Interface - development in progress

--- VS/trunk/PR42-SmartFactory/CW/Silverlight/DisposalRequestWebPart/MainPage.xaml.cs
+++ VS/trunk/PR42-SmartFactory/CW/Silverlight/DisposalRequestWebPart/MainPage.xaml.cs
@@ -32,29 +32,30 @@ namespace CAS.SmartFactory.CW.Dashboards.DisposalRequestWebPart
   /// <summary>
   /// Main page UserControl
   /// </summary>
-  public partial class MainPage : UserControl
+  public partial class MainPage: UserControl
   {
     #region public
     public MainPage()
     {
       InitializeComponent();
     }
-    public MainPage(string hiddenFieldDataName)
+    public MainPage( string hiddenFieldDataName )
       : this()
     {
+      m_at = "creator";
       m_HiddenFieldDataName = hiddenFieldDataName;
     }
     #endregion
 
-    private void UserControl_Loaded(object sender, RoutedEventArgs e)
+    private void UserControl_Loaded( object sender, RoutedEventArgs e )
     {
       try
       {
         GetData();
       }
-      catch (Exception ex)
+      catch ( Exception ex )
       {
-        MessageBox.Show(ex.Message, "Loaded error", MessageBoxButton.OK);
+        MessageBox.Show( ex.Message + " AT: " + m_at, "Loaded event error", MessageBoxButton.OK );
       }
     }
     private void GetData()
@@ -68,23 +69,31 @@ namespace CAS.SmartFactory.CW.Dashboards.DisposalRequestWebPart
           //HtmlElement hiddenField = doc.GetElementById( m_HiddenFieldDataName );
           //string _viewXml = hiddenField.GetAttribute( "value" ).ToString();
           //CamlQuery _camlQuery = new CamlQuery() { ViewXml = _viewXml };
+          m_at = "GetData";
           ClientContext clientContext = ClientContext.Current;
-          if (clientContext == null)
-            throw new ArgumentNullException("clientContext", String.Format("Cannot get the {0} ", "ClientContext"));
+          if ( clientContext == null )
+            throw new ArgumentNullException( "clientContext", String.Format( "Cannot get the {0} ", "ClientContext" ) );
+          m_at = "clientContext";
           Web _website = clientContext.Web;
-          if (_website == null)
-            throw new ArgumentNullException("_cwList", String.Format("Cannot get the {0} ", "Web"));
-          List _cwList = _website.Lists.GetByTitle(CommonDefinition.CustomsWarehouseDisposalTitle);
-          if (_cwList == null)
-            throw new ArgumentNullException("_cwList", String.Format("Cannot get the {0} list", CommonDefinition.CustomsWarehouseDisposalTitle));
-          ListItemCollection _itemsCollection = _cwList.GetItems(CamlQuery.CreateAllItemsQuery());
-          clientContext.Load(_cwList);
-          clientContext.Load(_itemsCollection);
+          if ( _website == null )
+            throw new ArgumentNullException( "_cwList", String.Format( "Cannot get the {0} ", "Web" ) );
+          m_at = "GetByTitle";
+          List _cwList = _website.Lists.GetByTitle( CommonDefinition.CustomsWarehouseDisposalTitle );
+          if ( _cwList == null )
+            throw new ArgumentNullException( "_cwList", String.Format( "Cannot get the {0} list", CommonDefinition.CustomsWarehouseDisposalTitle ) );
+          m_at = "Load cwList ";
+          ListItemCollection _itemsCollection = _cwList.GetItems( CamlQuery.CreateAllItemsQuery() );
+          m_at = "GetItems";
+          clientContext.Load( _cwList );
+          m_at = "Load itemsCollection ";
+          clientContext.Load( _itemsCollection );
+          m_at = "ExecuteQuery";
           clientContext.ExecuteQuery();
           //x_DataGridListView.ItemsSource = _itemsCollection;
-        });
+        } );
     }
     private readonly string m_HiddenFieldDataName = String.Empty;
+    private string m_at;
 
   }
 }
