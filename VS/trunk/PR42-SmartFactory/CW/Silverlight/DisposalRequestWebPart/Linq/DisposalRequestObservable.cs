c74dc75eed0fa7e04f7affde63b1f5f2feac94c1
mpostol <mpostol@8a3894fd-0c01-b944-991b-6a3da02cae8a>
2013-10-02 12:36:52 +0000

    Disposal Request WebPart User Interface - development in progress working on using auto generated Entities. rearranging the project

---
+++ VS/trunk/PR42-SmartFactory/CW/Silverlight/DisposalRequestWebPart/Linq/DisposalRequestObservable.cs
@@ -0,0 +1,60 @@
+﻿//<summary>
+//  Title   : class DisposalRequestObservable
+//  System  : Microsoft Visual C# .NET 2012
+//  $LastChangedDate:$
+//  $Rev:$
+//  $LastChangedBy:$
+//  $URL:$
+//  $Id:$
+//
+//  Copyright (C) 2013, CAS LODZ POLAND.
+//  TEL: +48 (42) 686 25 47
+//  mailto://techsupp@cas.eu
+//  http://www.cas.eu
+//</summary>
+
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Windows.Data;
+using Microsoft.SharePoint.Client;
+using System.Linq;
+using CAS.SmartFactory.CW.Dashboards.DisposalRequestWebPart.Data;
+
+namespace CAS.SmartFactory.CW.Dashboards.DisposalRequestWebPart.Linq
+{
+  /// <summary>
+  /// class DisposalRequestObservable
+  /// </summary>
+  public class DisposalRequestObservable : ObservableCollection<DisposalRequest>
+  {
+    internal void GetDataContext(EntityList<CustomsWarehouseDisposal> _list)
+    {
+      IEnumerable<IGrouping<string, CustomsWarehouseDisposal>> _requests = _list.ToList<CustomsWarehouseDisposal>().GroupBy<CustomsWarehouseDisposal, string>(x => x.);
+      foreach (IGrouping<string, CustomsWarehouseDisposal> _grx in _requests)
+      {
+        CustomsWarehouseDisposal _first = _grx.First<CustomsWarehouseDisposal>();
+        DisposalRequest _oc = new DisposalRequest()
+        {
+          AddedKg = 0,
+          DeclaredNetMass = 0,
+          Batch = _first.Key,
+          PackagesToClear = 0,
+          QuantityyToClearSum = 0,
+          QuantityyToClearSumRounded = 0,
+          RemainingOnStock = 0,
+          RemainingPackages = 0,
+          SKUDescription = _first.SKUDescription,
+          Title = "Title TBD",
+          TotalStock = 0,
+          Units = _first.Units,
+          SKU = _first.SKU,
+        };
+        foreach (CustomsWarehouseDisposal _cwdrdx in _grx)
+          _oc.GetDataContext(_cwdrdx);
+        this.Add(_oc);
+      }
+    }
+  }
+}
