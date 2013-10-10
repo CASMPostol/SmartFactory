d27ea31e91fe3c21d038700048af961b1d4d5d56
mpostol <mpostol@8a3894fd-0c01-b944-991b-6a3da02cae8a>
2013-10-10 07:12:02 +0000

    Handle Exit Sheet webpart functionality - added code to print document

--- VS/trunk/PR42-SmartFactory/CW/Silverlight/ExitSheetWebPart/App.xaml.cs
+++ VS/trunk/PR42-SmartFactory/CW/Silverlight/ExitSheetWebPart/App.xaml.cs
@@ -1,16 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Animation;
-using System.Windows.Shapes;
 
-namespace ExitSheetWebPart
+namespace CAS.SmartFactory.CW.Dashboards.ExitSheetWebPart
 {
   public partial class App : Application
   {
@@ -20,7 +11,6 @@ namespace ExitSheetWebPart
       this.Startup += this.Application_Startup;
       this.Exit += this.Application_Exit;
       this.UnhandledException += this.Application_UnhandledException;
-
       InitializeComponent();
     }
 
--- VS/trunk/PR42-SmartFactory/CW/Silverlight/ExitSheetWebPart/MainPage.xaml.cs
+++ VS/trunk/PR42-SmartFactory/CW/Silverlight/ExitSheetWebPart/MainPage.xaml.cs
@@ -1,22 +1,59 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net;
+﻿//<summary>
+//  Title   : class MainPage 
+//  System  : Microsoft Visual C# .NET 2012
+//  $LastChangedDate:$
+//  $Rev:$
+//  $LastChangedBy:$
+//  $URL:$
+//  $Id:$
+//
+//  Copyright (C) 2013, CAS LODZ POLAND.
+//  TEL: +48 (42) 686 25 47
+//  mailto://techsupp@cas.eu
+//  http://www.cas.eu
+//</summary>
+
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Animation;
-using System.Windows.Shapes;
+using System.Windows.Printing;
 
-namespace ExitSheetWebPart
+namespace CAS.SmartFactory.CW.Dashboards.ExitSheetWebPart
 {
-  public partial class MainPage : UserControl
+
+  /// <summary>
+  /// class MainPage 
+  /// </summary>
+  public partial class MainPage: UserControl
   {
     public MainPage()
     {
       InitializeComponent();
+      m_PrintDocument = new PrintDocument();
+      m_PrintDocument.PrintPage += PrintDocument_PrintPageEventHandler;
     }
+
+    #region private
+    private PrintDocument m_PrintDocument = null;
+
+    #region handlers
+    private void PrintDocument_PrintPageEventHandler( object sender, PrintPageEventArgs e )
+    {
+      e.PageVisual = x_LayoutRoot;
+      e.HasMorePages = false;
+    }
+    private void x_ButtonPrint_Click( object sender, RoutedEventArgs e )
+    {
+      try
+      {
+        m_PrintDocument.Print( "Exit Sheet" );
+      }
+      catch ( System.Exception ex )
+      {
+        MessageBox.Show( ex.Message, "Print Exception", MessageBoxButton.OK );
+      }
+    }
+    #endregion
+
+    #endregion
   }
 }
