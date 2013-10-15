6c555c10d8892be5512c2abf1677f9e36186ef21
bwojciechowski <bwojciechowski@8a3894fd-0c01-b944-991b-6a3da02cae8a>
2013-10-15 13:58:53 +0000

    Create Dashboard for Exit Sheet view & printing - development

--- CW/CWDashboards/Silverlight/SilverlightControl.cs
+++ CW/CWDashboards/Silverlight/SilverlightControl.cs
@@ -68,8 +68,8 @@ namespace CAS.SmartFactory.CW.Dashboards.Silverlight
         if ( String.IsNullOrEmpty( Source ) )
           return;
         // Ensure we have set the height and width
-        string width = ( this.Width == Unit.Empty ) ? "100%" : this.Width.ToString();
-        string height = ( this.Height == Unit.Empty ) ? "100%" : this.Height.ToString();
+        string width = ( this.Width == Unit.Empty ) ? "95%" : this.Width.ToString();
+        string height = ( this.Height == Unit.Empty ) ? "95%" : this.Height.ToString();
         SilverlightExceptionScript _exceptionScript = new SilverlightExceptionScript()
         {
           ClientID = this.ClientID
