d7304189f4ba08ddf0df1c06d34d213f86dfed9d
mpostol <mpostol@8a3894fd-0c01-b944-991b-6a3da02cae8a>
2013-10-16 13:48:00 +0000

    Disposal Request WebPart User Interface - Working on asynchronous operations on a single thread.

--- VS/trunk/PR42-SmartFactory/CW/Silverlight/DisposalRequestWebPart/Data/DataContextAsync.cs
+++ VS/trunk/PR42-SmartFactory/CW/Silverlight/DisposalRequestWebPart/Data/DataContextAsync.cs
@@ -12,19 +12,10 @@
 //  mailto://techsupp@cas.eu
 //  http://www.cas.eu
 //</summary>
-      
+
 using System;
 using System.ComponentModel;
-using System.Net;
 using System.Threading;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Documents;
-using System.Windows.Ink;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Animation;
-using System.Windows.Shapes;
 
 namespace CAS.SmartFactory.CW.Dashboards.DisposalRequestWebPart.Data
 {
@@ -32,62 +23,157 @@ namespace CAS.SmartFactory.CW.Dashboards.DisposalRequestWebPart.Data
   /// DataContextAsync
   /// </summary>
   /// <typeparam name="TContext">The type of the context.</typeparam>
-  public class DataContextAsync<TContext>
-    where TContext: DataContext, new()
+  public class DataContextAsync
   {
+    #region public
+    public bool Busy { get { return m_busy; } }
 
+    #region CreateContextAsync
     public event CreateContextAsyncCompletedEventHandler CreateContextAsyncCompletedEvent;
-    public delegate void CreateContextAsyncCompletedEventHandler( object sender, CreateContextAsyncCompletedEventArgs<TContext> e );
+    public delegate void CreateContextAsyncCompletedEventHandler( object sender, CreateContextAsyncCompletedEventArgs e );
     public void CreateContextAsync( string requestUrl )
     {
-      if ( m_AsyncOp != null )
-        throw new ArgumentException( "Task ID parameter must be unique", "taskId" );
+      if ( Busy )
+        throw new InvalidOperationException( "Context is busy" );
+      m_busy = true;
       m_OnCompletedDelegate += CreateContextAsynCompleted;
-      m_AsyncOp = AsyncOperationManager.CreateOperation( m_Counter++ );
-      m_processor.Do( () => { CreateContextWorker( requestUrl, m_AsyncOp ); } );
+      AsyncOperation m_AsyncOp = AsyncOperationManager.CreateOperation( m_Counter++ );
+      m_processor.Do( () => { CreateContextAsyncWorker( requestUrl, m_AsyncOp ); } );
     }
+    #endregion
+
+    #region GetListAsync
+    public event GetListAsyncCompletedEventHandler GetListCompleted;
+    public delegate void GetListAsyncCompletedEventHandler( object siurce, GetListAsyncCompletedEventArgs e );
+    /// <summary>
+    /// Gets the list.
+    /// </summary>
+    /// <param name="listName">Name of the list.</param>
+    public void GetListAsync<TEntity>( string listName )
+       where TEntity: class, ITrackEntityState, ITrackOriginalValues, INotifyPropertyChanged, INotifyPropertyChanging, new()
+    {
+      if ( Busy )
+        throw new InvalidOperationException( "Context is busy" );
+      m_busy = true;
+      m_OnCompletedDelegate += GetListAsyncCompleted;
+      // Create an AsyncOperation for taskId.
+      AsyncOperation _AsyncOp = AsyncOperationManager.CreateOperation( m_Counter++ );
+      m_processor.Do( () => { GetListAsyncWorker<TEntity>( listName, _AsyncOp ); } );
+    }
+    #endregion
+    public event AsyncCompletedEventHandler SubmitChangesCompleted;
+    public void SubmitChangesAsyn()
+    {
+      if ( Busy )
+        throw new InvalidOperationException( "Context is busy" );
+      m_busy = true;
+      m_OnCompletedDelegate += SubmitChangesAsynCompleted;
+      AsyncOperation _AsyncOp = AsyncOperationManager.CreateOperation( m_Counter++ );
+      m_processor.Do( () => { SubmitChangesAsynWorker( _AsyncOp ); } );
+    }
+    #endregion
+
+    #region private
+
+    #region CreateContext
     private void CreateContextAsynCompleted( object state )
     {
       if ( CreateContextAsyncCompletedEvent == null )
         return;
-      CreateContextAsyncCompletedEventArgs<TContext> e = state as CreateContextAsyncCompletedEventArgs<TContext>;
+      CreateContextAsyncCompletedEventArgs e = state as CreateContextAsyncCompletedEventArgs;
+      m_busy = false;
       CreateContextAsyncCompletedEvent( this, e );
     }
-    private void CreateContextWorker( string requestUrl, AsyncOperation asyncOp )
+    private void CreateContextAsyncWorker( string requestUrl, AsyncOperation asyncOp )
     {
-      Exception e = null;
+      Exception exception = null;
       try
       {
         m_Context.CreateContext( requestUrl );
       }
       catch ( Exception ex )
       {
-        e = ex;
+        exception = ex;
       }
-      this.CompletionMethod( m_Context, e, false, asyncOp );
+      // Package the results of the operation in a  CreateContextAsyncCompletedEventArgs.
+      CreateContextAsyncCompletedEventArgs e = new CreateContextAsyncCompletedEventArgs( m_Context, exception, false, asyncOp.UserSuppliedState );
+      OperationCompleted( asyncOp, e );
     }
-    private void CompletionMethod( TContext result, Exception exception, bool canceled, AsyncOperation asyncOp )
+    private void OperationCompleted( AsyncOperation asyncOp, object e )
     {
-      // If the task was not previously canceled, remove the task from the lifetime collection. 
-      if ( !canceled )
-      {
-        lock ( this )
-        {
-          m_AsyncOp = null;
-        }
-      }
-      // Package the results of the operation in a  GetListAsyncCompletedEventArgs.
-      CreateContextAsyncCompletedEventArgs<TContext> e = new CreateContextAsyncCompletedEventArgs<TContext>( result, exception, canceled, asyncOp.UserSuppliedState );
       // End the task. The asyncOp object is responsible for marshaling the call.
+      m_busy = false;
       asyncOp.PostOperationCompleted( m_OnCompletedDelegate, e );
       // Note that after the call to OperationCompleted, asyncOp is no longer usable, and any attempt to use it 
       // will cause an exception to be thrown.
     }
+    #endregion
+
+    #region GetListAsync
+    private void GetListAsyncCompleted( object state )
+    {
+      m_busy = false;
+      if ( GetListCompleted == null )
+        return;
+      GetListAsyncCompletedEventArgs _EventArgs = (GetListAsyncCompletedEventArgs)state;
+      GetListCompleted( this, _EventArgs );
+    }
+    /// <summary>
+    /// This method performs the actual workload to gets the list.
+    /// </summary>
+    /// <param name="listName">Name of the list.</param>
+    /// <param name="asyncOp">The _ asynchronous property.</param>
+    private void GetListAsyncWorker<TEntity>( string listName, AsyncOperation asyncOp )
+       where TEntity: class, ITrackEntityState, ITrackOriginalValues, INotifyPropertyChanged, INotifyPropertyChanging, new()
+    {
+      Exception _Exception = null;
+      EntityList<TEntity> _Result = null;
+      try
+      {
+        _Result = m_Context.GetList<TEntity>( listName );
+      }
+      catch ( Exception ex )
+      {
+        _Exception = ex;
+      }
+      // Package the results of the operation in a  GetListAsyncCompletedEventArgs.
+      GetListAsyncCompletedEventArgs e = new GetListAsyncCompletedEventArgs( _Result, _Exception, false, asyncOp.UserSuppliedState );
+      // End the task. The asyncOp object is responsible for marshaling the call.
+      asyncOp.PostOperationCompleted( m_OnCompletedDelegate, e );
+    }
+    #endregion
+
+    #region SubmitChangesAsyn
+    private void SubmitChangesAsynCompleted( object state )
+    {
+      if ( GetListCompleted == null )
+        return;
+      GetListAsyncCompletedEventArgs _EventArgs = (GetListAsyncCompletedEventArgs)state;
+      GetListCompleted( this, _EventArgs );
+    }
+    private void SubmitChangesAsynWorker( AsyncOperation asyncOp )
+    {
+      Exception _Exception = null;
+      try
+      {
+        m_Context.SubmitChanges();
+      }
+      catch ( Exception ex )
+      {
+        _Exception = ex;
+      }
+      // Package the results of the operation in a  GetListAsyncCompletedEventArgs.
+      AsyncCompletedEventArgs e = new AsyncCompletedEventArgs( _Exception, false, asyncOp.UserSuppliedState );
+      // End the task. The asyncOp object is responsible for marshaling the call.
+      asyncOp.PostOperationCompleted( m_OnCompletedDelegate, e );
+    }
+    #endregion
+
     private SendOrPostCallback m_OnCompletedDelegate;
-    private AsyncOperation m_AsyncOp = null;
+    private bool m_busy = false;
     private int m_Counter = 0;
     private DataContextAsync() { }
-    private TContext m_Context = new TContext();
+    private DataContext m_Context = new DataContext();
     private Processor m_processor = new Processor();
     private class Processor
     {
@@ -95,15 +181,19 @@ namespace CAS.SmartFactory.CW.Dashboards.DisposalRequestWebPart.Data
       {
       }
     }
+
+    #endregion
+
   }
-  public class CreateContextAsyncCompletedEventArgs<TContext>: AsyncCompletedEventArgs
-    where TContext: DataContext, new()
+  public class CreateContextAsyncCompletedEventArgs: AsyncCompletedEventArgs
   {
-    public CreateContextAsyncCompletedEventArgs( object TContext, Exception e, bool canceled, object state )
+
+    public CreateContextAsyncCompletedEventArgs( DataContext context, Exception e, bool canceled, object state )
       : base( e, canceled, state )
     {
-
+      Result = context;
     }
-    internal TContext Result { get; private set; }
+    internal DataContext Result { get; private set; }
+
   }
 }
