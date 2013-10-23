e63401b5c6863e0ed0e69e97972e4a7aca2e14d2
Mariusz <mpostol@cas.eu>
2013-10-23 08:23:43 +0000

    Disposal Request WebPart User Interface - working on save.

--- CW/Silverlight/DisposalRequestWebPart/Linq/CustomsWarehouseDisposal.cs
+++ CW/Silverlight/DisposalRequestWebPart/Linq/CustomsWarehouseDisposal.cs
@@ -1,22 +1,48 @@
-﻿using System;
-using System.Net;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Documents;
-using System.Windows.Ink;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Animation;
-using System.Windows.Shapes;
+﻿///<summary>
+//  Title   : public partial class CustomsWarehouseDisposal
+//  System  : Microsoft Visual C# .NET 2012
+//  $LastChangedDate:$
+//  $Rev:$
+//  $LastChangedBy:$
+//  $URL:$
+//  $Id:$
+//
+//  Copyright (C) 2013, CAS LODZ POLAND.
+//  TEL: +48 (42) 686 25 47
+//  mailto://techsupp@cas.eu
+//  http://www.cas.eu
+///</summary>
+
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
 
 namespace CAS.SmartFactory.CW.Dashboards.DisposalRequestWebPart.Linq
 {
   public partial class CustomsWarehouseDisposal
   {
-    internal void Dispose( ref int _packagesToDispose, System.Collections.Generic.List<CustomsWarehouse> _listCopy )
+    internal void DisposeMaterial( ref int packagesToDispose, List<CustomsWarehouse> listCopy )
     {
-      throw new NotImplementedException();
+      double _Available = this.CWL_CWDisposal2CustomsWarehouseID.TobaccoNotAllocated.Value + this.CW_SettledNetMass.Value;
+      int _2DisposePackages = Math.Min( Packages( _Available ), packagesToDispose );
+      this.CW_PackageToClear = _2DisposePackages;
+      this.CW_SettledNetMass = Quantity( _2DisposePackages );
+      this.CW_AddedKg = this.CW_DeclaredNetMass - this.CW_SettledNetMass;
+      Debug.Assert( this.CW_AddedKg > 0, "CW_AddedKg < 0" );
+      listCopy.Remove( this.CWL_CWDisposal2CustomsWarehouseID );
+      packagesToDispose -= _2DisposePackages;
     }
-
+    internal double Quantity( int packages )
+    {
+      return this.CWL_CWDisposal2CustomsWarehouseID.CW_MassPerPackage.Value * packages;
+    }
+    internal int Packages( double quantityyToClearSum )
+    {
+      return Packages( quantityyToClearSum, this.CWL_CWDisposal2CustomsWarehouseID.CW_MassPerPackage.Value );
+    }
+    internal static int Packages( double quantityyToClearSum, double massPerPackage )
+    {
+      return Convert.ToInt32( Math.Round( quantityyToClearSum / massPerPackage + 0.499999, 0 ) );
+    }
   }
 }
