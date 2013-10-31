7a267795e67671c5c952f4da8d6c7a97b4cd27c3
Mariusz <mpostol@cas.eu>
2013-10-31 15:25:28 +0000

    Disposal Request Library workflow to withdraw assigned disposals - sad message generation in progress

--- CW/CWWorkflows/DisposalRequestLibrary/ClearThroughCustoms/ClearThroughCustoms.cs
+++ CW/CWWorkflows/DisposalRequestLibrary/ClearThroughCustoms/ClearThroughCustoms.cs
@@ -1,4 +1,19 @@
-﻿using System;
+﻿//<summary>
+//  Title   : public sealed partial class ClearThroughCustoms
+//  System  : Microsoft Visual C# .NET 2012
+//  $LastChangedDate:$
+//  $Rev:$
+//  $LastChangedBy:$
+//  $URL:$
+//  $Id:$
+//
+//  Copyright (C) 2013, CAS LODZ POLAND.
+//  TEL: +48 (42) 686 25 47
+//  mailto://techsupp@cas.eu
+//  http://www.cas.eu
+//</summary>
+
+using System;
 using System.ComponentModel;
 using System.ComponentModel.Design;
 using System.Collections;
@@ -16,6 +31,7 @@ using Microsoft.SharePoint.Workflow;
 using Microsoft.SharePoint.WorkflowActions;
 using CAS.SmartFactory.CW.WebsiteModel.Linq;
 using CAS.SmartFactory.Customs.Messages.CELINA.SAD;
+using CAS.SmartFactory.Customs;
 
 namespace CAS.SmartFactory.CW.Workflows.DisposalRequestLibrary.ClearThroughCustoms
 {
@@ -36,19 +52,34 @@ namespace CAS.SmartFactory.CW.Workflows.DisposalRequestLibrary.ClearThroughCusto
         using ( Entities _entities = new Entities( workflowProperties.WebUrl ) )
         {
           DisposalRequestLib _dr = Element.GetAtIndex<DisposalRequestLib>( _entities.DisposalRequestLibrary, workflowProperties.ItemId );
-          IQueryable<IGrouping<String, CustomsWarehouseDisposal>> _clearances = from _cdx in _dr.CustomsWarehouseDisposal group _cdx by _cdx.CWL_CWDisposal2CustomsWarehouseID.Batch;
-          foreach ( var item in _clearances )
+          foreach ( var item in _dr.CustomsWarehouseDisposal )
           {
-            SADZgloszenie _zglszn = SADZgloszenie.Create();
-            SAD _sad = SAD.Create( Settings.GetParameter(_entities, SettingsEntry.OrganizationEmail), _zglszn);
+            SAD _sad = CraeteSAD( _entities, item );
           }
         }
-
       }
       catch ( Exception _ex )
       {
 
       }
+    }
+    private static SAD CraeteSAD( Entities _entities, CustomsWarehouseDisposal item )
+    {
+      SADZgloszenieTowarDokumentWymagany[] _dcs = new SADZgloszenieTowarDokumentWymagany[]
+      {
+        SADZgloszenieTowarDokumentWymagany.Create(1, "9DK8", "9DK8_123456789", "03.10.2013" ),
+        SADZgloszenieTowarDokumentWymagany.Create(2, "N935", "82131901", "" ),
+        SADZgloszenieTowarDokumentWymagany.Create(3, "5DK5", "12PL360000G000259", "PL8280001819-1111" )
+      };
+      decimal value = item.CWL_CWDisposal2CustomsWarehouseID.Value.ConvertToDecimal();
+      string reference = item.CWL_CWDisposal2CustomsWarehouseID.DocumentNo;
+      SADZgloszenieTowar[] _good = new SADZgloszenieTowar[] 
+      {
+        SADZgloszenieTowar.Create( item.CW_SettledNetMass.ConvertToDecimal(), item.CW_RemainingPackage.ConvertToDecimal(), reference, _dcs, value )
+      };
+      SADZgloszenieUC customsOffice = null;
+      SADZgloszenie _application = SADZgloszenie.Create( SADZgloszenieWartoscTowarow.Create( item.TobaccoValue.ConvertToDecimal() ), _good, customsOffice );
+      return SAD.Create( Settings.GetParameter( _entities, SettingsEntry.OrganizationEmail ), _application );
     }
   }
 }
