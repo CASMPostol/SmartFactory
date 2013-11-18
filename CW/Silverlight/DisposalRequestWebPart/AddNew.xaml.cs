e88cc32a0ed0fec25496e203e0abd1f1e26c3c61
Mariusz <mpostol@cas.eu>
2013-11-18 13:38:51 +0000

    Disposal creation - FIFO - fixed: - disposal request processing - DisposalRequestWebPart

--- CW/Silverlight/DisposalRequestWebPart/AddNew.xaml.cs
+++ CW/Silverlight/DisposalRequestWebPart/AddNew.xaml.cs
@@ -23,13 +23,13 @@ using CAS.SmartFactory.CW.Dashboards.DisposalRequestWebPart.Linq;
 
 namespace CAS.SmartFactory.CW.Dashboards.DisposalRequestWebPart
 {
-  public partial class AddNew: ChildWindow
+  public partial class AddNew : ChildWindow
   {
     public AddNew()
     {
       InitializeComponent();
     }
-    public AddNew( DataContextAsync context )
+    public AddNew(DataContextAsync context)
       : this()
     {
       m_DataContext = context;
@@ -41,79 +41,80 @@ namespace CAS.SmartFactory.CW.Dashboards.DisposalRequestWebPart
     private const string c_DoubleFormat = "F2";
     private double m_Available = 0;
     private DataContextAsync m_DataContext;
-    private void OKButton_Click( object sender, RoutedEventArgs e )
+    private void OKButton_Click(object sender, RoutedEventArgs e)
     {
       double _2dsps = 0;
-      if ( !Double.TryParse( x_TextBoxQtyToClear.Text, out _2dsps ) )
+      if (!Double.TryParse(x_TextBoxQtyToClear.Text, out _2dsps))
       {
-        MessageBox.Show( "Wrong qty to dispose", (string)this.Title, MessageBoxButton.OK );
+        MessageBox.Show("Wrong qty to dispose", (string)this.Title, MessageBoxButton.OK);
         return;
       }
-      if ( _2dsps > m_Available || _2dsps < 0 )
+      if (_2dsps > m_Available || _2dsps < 0)
       {
         x_TextBoxQtyToClear.Text = x_TextBoxTotalStock.Text;
-        string _msg = String.Format( "The entered qty is not available, {0} kg is available.", m_Available.ToString( c_DoubleFormat ) );
-        MessageBox.Show( _msg, (string)this.Title, MessageBoxButton.OK );
+        string _msg = String.Format("The entered qty is not available, {0} kg is available.", m_Available.ToString(c_DoubleFormat));
+        MessageBox.Show(_msg, (string)this.Title, MessageBoxButton.OK);
         return;
       }
-      if ( Accounts.Count == 0 )
+      if (Accounts.Count == 0)
       {
-        MessageBox.Show( "No account found. Are you sure to close the window.", (string)this.Title, MessageBoxButton.OK );
+        MessageBox.Show("No account found. Are you sure to close the window.", (string)this.Title, MessageBoxButton.OK);
         return;
       }
       ToDispose = _2dsps;
       this.DialogResult = true;
     }
-    private void CancelButton_Click( object sender, RoutedEventArgs e )
+    private void CancelButton_Click(object sender, RoutedEventArgs e)
     {
       this.DialogResult = false;
     }
-    private void ChildWindow_Loaded( object sender, RoutedEventArgs e )
+    private void ChildWindow_Loaded(object sender, RoutedEventArgs e)
     {
 
     }
-    private void x_ButtonBatchSearch_Click( object sender, RoutedEventArgs e )
+    private void x_ButtonBatchSearch_Click(object sender, RoutedEventArgs e)
     {
-      if ( m_DataContext == null )
+      if (m_DataContext == null)
       {
         this.DialogResult = false;
         return;
       }
       m_DataContext.GetListCompleted += m_DataContext_GetListCompleted;
-      m_DataContext.GetListAsync<CustomsWarehouse>( CommonDefinition.CustomsWarehouseTitle,
-                                                    CommonDefinition.GetCAMLSelectedID( x_TextBoxBatchSearch.Text, CommonDefinition.FieldBatch, CommonDefinition.CAMLTypeText )
+      m_DataContext.GetListAsync<CustomsWarehouse>(CommonDefinition.CustomsWarehouseTitle,
+                                                    CommonDefinition.GetCAMLSelectedID(x_TextBoxBatchSearch.Text, CommonDefinition.FieldBatch, CommonDefinition.CAMLTypeText)
                                                    );
     }
-    private void m_DataContext_GetListCompleted( object siurce, GetListAsyncCompletedEventArgs e )
+    private void m_DataContext_GetListCompleted(object siurce, GetListAsyncCompletedEventArgs e)
     {
       try
       {
         m_DataContext.GetListCompleted -= m_DataContext_GetListCompleted;
-        if ( e.Error != null )
+        if (e.Error != null)
         {
           x_TextBoxSelectedBatch.Text = "Not found";
           x_TextBoxTotalStock.Text = e.Error.Message;
         }
-        if ( e.Cancelled )
+        if (e.Cancelled)
         {
           x_TextBoxSelectedBatch.Text = "Not found";
           x_TextBoxTotalStock.Text = "Cancelled";
         }
         Accounts = e.Result<CustomsWarehouse>();
-        if ( Accounts.Count == 0 )
+        if (Accounts.Count == 0)
         {
           x_TextBoxSelectedBatch.Text = "Not found";
           x_TextBoxTotalStock.Text = "N/A";
           return;
         }
-        x_TextBoxSelectedBatch.Text = String.Format( "{0}/{1} accounts", Accounts.First<CustomsWarehouse>().Batch, Accounts.Count );
-        m_Available = Accounts.Sum( x => x.TobaccoNotAllocated.Value );
-        x_TextBoxTotalStock.Text = m_Available.ToString( c_DoubleFormat );
-        x_TextBoxQtyToClear.Text = 0.0.ToString( c_DoubleFormat );
+        Accounts.Sort(new Comparison<CustomsWarehouse>(CustomsWarehouse.CompareCustomsWarehouse));
+        x_TextBoxSelectedBatch.Text = String.Format("{0}/{1} accounts", Accounts.First<CustomsWarehouse>().Batch, Accounts.Count);
+        m_Available = Accounts.Sum(x => x.TobaccoNotAllocated.Value);
+        x_TextBoxTotalStock.Text = m_Available.ToString(c_DoubleFormat);
+        x_TextBoxQtyToClear.Text = 0.0.ToString(c_DoubleFormat);
       }
-      catch ( Exception ex )
+      catch (Exception ex)
       {
-        MessageBox.Show( ex.Message, "Serach exception", MessageBoxButton.OK );
+        MessageBox.Show(ex.Message, "Serach exception", MessageBoxButton.OK);
       }
     }
     #endregion
--- CW/Silverlight/DisposalRequestWebPart/Linq/CustomsWarehouse.cs
+++ CW/Silverlight/DisposalRequestWebPart/Linq/CustomsWarehouse.cs
@@ -22,21 +22,38 @@ namespace CAS.SmartFactory.CW.Dashboards.DisposalRequestWebPart.Linq
   public partial class CustomsWarehouse
   {
 
-    internal CustomsWarehouseDisposal CreateDisposal( int disposalRequestLibId, ref int packagesToDispose )
+    internal CustomsWarehouseDisposal CreateDisposal(int disposalRequestLibId, ref int packagesToDispose)
     {
-      int _TdspsePackages = Math.Min(Linq.CustomsWarehouseDisposal.Packages( this.TobaccoNotAllocated.Value, this.CW_MassPerPackage.Value ), packagesToDispose );
-      if ( _TdspsePackages == 0 )
+      int _TdspsePackages = Math.Min(Linq.CustomsWarehouseDisposal.Packages(this.TobaccoNotAllocated.Value, this.CW_MassPerPackage.Value), packagesToDispose);
+      if (_TdspsePackages == 0)
         return null;
       double _Tdspsekg = this.CW_MassPerPackage.Value * _TdspsePackages;
-      CustomsWarehouseDisposal _NewDisposal = Linq.CustomsWarehouseDisposal.Create( disposalRequestLibId, _TdspsePackages, _Tdspsekg, PackageWeight(), this );
+      CustomsWarehouseDisposal _NewDisposal = Linq.CustomsWarehouseDisposal.Create(disposalRequestLibId, _TdspsePackages, _Tdspsekg, PackageWeight(), this);
       this.TobaccoNotAllocated -= _Tdspsekg;
       packagesToDispose -= _TdspsePackages;
       return _NewDisposal;
     }
     internal double PackageWeight()
     {
-      return this.CW_PackageUnits.Value == 0 ? 0 : ( this.GrossMass.Value - this.CW_Quantity.Value ) / this.CW_PackageUnits.Value;
+      return this.CW_PackageUnits.Value == 0 ? 0 : (this.GrossMass.Value - this.CW_Quantity.Value) / this.CW_PackageUnits.Value;
     }
-
+    /// <summary>
+    /// compares two objects of the <see cref="CustomsWarehouse "/> type.
+    /// </summary>
+    /// <param name="x">The first object to compare.</param>
+    /// <param name="y">The second object to compare.</param>
+    /// <returns>
+    /// A signed integer that indicates the relative values of x and y, as shown in the following table.
+    /// Value Condition Less than 0 x is less than y.
+    /// 0 x equals y.
+    /// Greater than 0 x is greater than y.
+    /// </returns>
+    internal static int CompareCustomsWarehouse(CustomsWarehouse x, CustomsWarehouse y)
+    {
+      int _ret = DateTime.Compare(x.CustomsDebtDate.Value, y.CustomsDebtDate.Value);
+      if (_ret == 0)
+        _ret = String.Compare(x.DocumentNo, y.DocumentNo);
+      return _ret;
+    }
   }
 }
