169dfb57335555c173a787b01322c99122e2ca1c
Mariusz <mpostol@cas.eu>
2013-12-03 09:37:29 +0000

    Check List web part - handle functionality - work in progress - added missed files.

---
+++ CW/CWDashboards/Webparts/CheckListHost/CheckListWebPartDataContract.cs
@@ -0,0 +1,80 @@
+﻿//<summary>
+//  Title   : Name of Application
+//  System  : Microsoft Visual C# .NET 2012
+//  $LastChangedDate:$
+//  $Rev:$
+//  $LastChangedBy:$
+//  $URL:$
+//  $Id:$
+//
+//  Copyright (C) 2013, CAS LODZ POLAND.
+//  TEL: +48 (42) 686 25 47
+//  mailto://techsupp@cas.eu
+//  http://www.cas.eu
+//</summary>
+      
+using System;
+using System.Runtime.Serialization;
+
+namespace CAS.SmartFactory.CW.Dashboards.Webparts.CheckListHost
+{
+  /// <summary>
+  /// class CheckListWebPartDataContract
+  /// </summary>
+  [DataContract]
+  public partial class CheckListWebPartDataContract
+  {
+
+    #region public
+    [DataMember]
+    public DateTime Today
+    {
+      get { return b_DocumentDate; }
+      set { b_DocumentDate = value; }
+    }
+    [DataMember]
+    public DisposalDescription[] DisposalsList
+    {
+      get { return b_DisposalsList; }
+      set { b_DisposalsList = value; }
+    }
+    #endregion
+
+    #region private
+    private DateTime b_DocumentDate;
+    private DisposalDescription[] b_DisposalsList;
+    #endregion
+
+  }
+  /// <summary>
+  /// class DisposalDescription
+  /// </summary>
+  [DataContract]
+  public class DisposalDescription
+  {
+    #region public
+    public DateTime OGLDate
+    {
+      get { return b_OGLDate; }
+      set { b_OGLDate = value; }
+    }
+    public string OGLNumber
+    {
+      get { return b_OGLNumber; }
+      set { b_OGLNumber = value; }
+    }
+    public int PackageToClear
+    {
+      get { return b_PackageToClear; }
+      set { b_PackageToClear = value; }
+    }
+    #endregion
+
+    #region private
+    private DateTime b_OGLDate;
+    private string b_OGLNumber;
+    private int b_PackageToClear;
+    #endregion
+  }
+
+}
