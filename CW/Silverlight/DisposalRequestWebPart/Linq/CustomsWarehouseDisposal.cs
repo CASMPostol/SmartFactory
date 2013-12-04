6ab5a94e02657493afdeb9bd83d88906af7820a5
Mariusz <mpostol@cas.eu>
2013-12-04 09:55:33 +0000

    disposal column: SAD Date - done

--- CW/Silverlight/DisposalRequestWebPart/Linq/CustomsWarehouseDisposal.cs
+++ CW/Silverlight/DisposalRequestWebPart/Linq/CustomsWarehouseDisposal.cs
@@ -21,25 +21,25 @@ namespace CAS.SmartFactory.CW.Dashboards.DisposalRequestWebPart.Linq
 {
   public partial class CustomsWarehouseDisposal
   {
-    internal void DisposeMaterial( ref int packagesToDispose, List<CustomsWarehouse> listCopy )
+    internal void DisposeMaterial(ref int packagesToDispose, List<CustomsWarehouse> listCopy)
     {
-      if ( this.CustomsStatus.Value == Linq.CustomsStatus.NotStarted )
+      if (this.CustomsStatus.Value == Linq.CustomsStatus.NotStarted)
       {
         double _Available = this.CWL_CWDisposal2CustomsWarehouseID.TobaccoNotAllocated.Value + this.CW_SettledNetMass.Value;
-        int _2DisposePackages = Math.Min( Packages( _Available ), packagesToDispose );
+        int _2DisposePackages = Math.Min(Packages(_Available), packagesToDispose);
         packagesToDispose -= _2DisposePackages;
-        if ( CW_PackageToClear != _2DisposePackages )
+        if (CW_PackageToClear != _2DisposePackages)
         {
           this.CW_PackageToClear = _2DisposePackages;
-          double _diff = Quantity( _2DisposePackages ) - this.CW_SettledNetMass.Value;
+          double _diff = Quantity(_2DisposePackages) - this.CW_SettledNetMass.Value;
           this.CW_SettledNetMass += _diff;
           this.CWL_CWDisposal2CustomsWarehouseID.TobaccoNotAllocated -= _diff;
           this.CW_AddedKg = this.CW_SettledNetMass - this.CW_DeclaredNetMass;
-          Debug.Assert( this.CW_AddedKg >= 0, "CW_AddedKg <= 0" );
+          Debug.Assert(this.CW_AddedKg >= 0, "CW_AddedKg <= 0");
           CW_SettledGrossMass = _2DisposePackages * CWL_CWDisposal2CustomsWarehouseID.PackageWeight() + CW_SettledNetMass.Value;
         }
       }
-      listCopy.Remove( this.CWL_CWDisposal2CustomsWarehouseID );
+      listCopy.Remove(this.CWL_CWDisposal2CustomsWarehouseID);
     }
     internal static CustomsWarehouseDisposal Create(int disposalRequestLibId, int toDisposePackages, double toDisposeKg, double packageWeight, CustomsWarehouse cw, string customsProcedure)
     {
@@ -59,21 +59,22 @@ namespace CAS.SmartFactory.CW.Dashboards.DisposalRequestWebPart.Linq
         Title = "TBD",
         SKUDescription = "N/A",
         DisposalRequestId = disposalRequestLibId,
+        SADDate = Extensions.SPMinimum
       };
-      _newItem.UpdateTitle( DateTime.Today );
+      _newItem.UpdateTitle(DateTime.Today);
       return _newItem;
     }
-    internal double Quantity( int packages )
+    internal double Quantity(int packages)
     {
       return this.CWL_CWDisposal2CustomsWarehouseID.CW_MassPerPackage.Value * packages;
     }
-    internal int Packages( double quantity )
+    internal int Packages(double quantity)
     {
-      return Packages( quantity, this.CWL_CWDisposal2CustomsWarehouseID.CW_MassPerPackage.Value );
+      return Packages(quantity, this.CWL_CWDisposal2CustomsWarehouseID.CW_MassPerPackage.Value);
     }
-    internal static int Packages( double quantityyToClearSum, double massPerPackage )
+    internal static int Packages(double quantityyToClearSum, double massPerPackage)
     {
-      return Convert.ToInt32( Math.Round( quantityyToClearSum / massPerPackage + 0.499999, 0 ) );
+      return Convert.ToInt32(Math.Round(quantityyToClearSum / massPerPackage + 0.499999, 0));
     }
     internal void DeleteDisposal()
     {
@@ -81,16 +82,16 @@ namespace CAS.SmartFactory.CW.Dashboards.DisposalRequestWebPart.Linq
       this.CW_SettledGrossMass = 0;
       this.CW_SettledNetMass = 0;
       this.CW_AddedKg = 0;
-      Debug.Assert( this.CW_DeclaredNetMass.Value == 0, "I expect Value of this.CW_DeclaredNetMass == 0 while deleting." );
+      Debug.Assert(this.CW_DeclaredNetMass.Value == 0, "I expect Value of this.CW_DeclaredNetMass == 0 while deleting.");
       this.CW_DeclaredNetMass = 0;
       this.CW_PackageToClear = 0;
     }
     /// <summary>
     /// Updates the title.
     /// </summary>
-    internal void UpdateTitle( DateTime dateTime )
+    internal void UpdateTitle(DateTime dateTime)
     {
-      Title = String.Format( "CW-{0:D4}{1:D6}", dateTime.Year, "XXXXXX" ); //TODO Id.Value);
+      Title = String.Format("CW-{0:D4}{1:D6}", dateTime.Year, "XXXXXX"); //TODO Id.Value);
     }
 
   }
