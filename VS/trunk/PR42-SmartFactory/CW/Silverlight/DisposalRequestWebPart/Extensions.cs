7b279fc122516fd91a8038243871846ef26a39f4
mpostol <mpostol@8a3894fd-0c01-b944-991b-6a3da02cae8a>
2013-12-04 09:55:33 +0000

    disposal column: SAD Date - done

---
+++ VS/trunk/PR42-SmartFactory/CW/Silverlight/DisposalRequestWebPart/Extensions.cs
@@ -0,0 +1,27 @@
+﻿//<summary>
+//  Title   : Name of Application
+//  System  : Microsoft Visual C# .NET 2012
+//  $LastChangedDate:$
+//  $Rev:$
+//  $LastChangedBy:$
+//  $URL:$
+//  $Id:$
+//
+//  Copyright (C) 2013, CAS LODZ POLAND.
+//  TEL: +48 (42) 686 25 47
+//  mailto://techsupp@cas.eu
+//  http://www.cas.eu
+//</summary>
+      
+using System;
+
+namespace CAS.SmartFactory.CW.Dashboards.DisposalRequestWebPart
+{
+  internal static class Extensions
+  {
+    /// <summary>
+    /// The minimum date for - to avoid setting today 
+    /// </summary>
+    public static DateTime SPMinimum = new DateTime(1900, 1, 1);
+  }
+}
