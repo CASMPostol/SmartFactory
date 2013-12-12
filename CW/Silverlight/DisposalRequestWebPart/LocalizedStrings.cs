6a277619f30e93c92cef23d758247e68a1c6ea5e
bwojciechowski <bwojciechowski@8a3894fd-0c01-b944-991b-6a3da02cae8a>
2013-12-12 14:30:09 +0000

    Customs Warehouse content localization - resources for disposal request added, webpart modified.

---
+++ CW/Silverlight/DisposalRequestWebPart/LocalizedStrings.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+
+namespace CAS.SmartFactory.CW.Dashboards.DisposalRequestWebPart
+{
+  public class LocalizedStrings
+  {
+
+    private static CAS.SmartFactory.CW.Dashboards.DisposalRequestWebPart.CASSmartFactoryDisposalRequest ResourceFile = new CASSmartFactoryDisposalRequest();
+
+    public CAS.SmartFactory.CW.Dashboards.DisposalRequestWebPart.CASSmartFactoryDisposalRequest CASSmartFactoryDisposalRequest { get { return ResourceFile; } }
+
+  }
+}
