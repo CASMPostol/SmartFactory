b215f0a511dba3d3c185c9f7f1062478e3ae9815
bwojciechowski <bwojciechowski@8a3894fd-0c01-b944-991b-6a3da02cae8a>
2013-12-13 11:43:31 +0000

    Clossing the account - RequestContent - schema for xml document generation created, classes generated.

---
+++ VS/trunk/PR42-SmartFactory/CW/CWInteroperability/DocumentsFactory/AccountClearance/GoXSD.ps1
@@ -0,0 +1,11 @@
+﻿Write-Host "This file creates the schema file (xsd) for selected class"
+Get-Location | Write-host
+$env:path += "; C:\Program Files (x86)\Microsoft SDKs\Windows\v8.0A\bin\NETFX 4.0 Tools"
+# Set-Location C:\vs\Projects\JTI\Excel2XML
+#Write-host press any key to continue .....
+#Read-host
+$cpath = get-location
+Write-host CAS.SmartFactory.xml.dll processing
+set-location ..\..\bin\debug\
+xsd.exe CAS.CWInteroperability.dll /t:CAS.SmartFactory.CW.Interoperability.DocumentsFactory.RequestContent.RequestContentContentType | write-host
+set-location $cpath
