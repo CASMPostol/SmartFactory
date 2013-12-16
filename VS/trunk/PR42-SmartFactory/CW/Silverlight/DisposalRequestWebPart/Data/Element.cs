1f88595cb36dfb4e2269d0137a1eaaaf213212ef
mpostol <mpostol@8a3894fd-0c01-b944-991b-6a3da02cae8a>
2013-12-16 21:15:17 +0000

    Code refactoring - added comments and subversion key words

--- VS/trunk/PR42-SmartFactory/CW/Silverlight/DisposalRequestWebPart/Data/Element.cs
+++ VS/trunk/PR42-SmartFactory/CW/Silverlight/DisposalRequestWebPart/Data/Element.cs
@@ -1,15 +1,31 @@
-﻿
+﻿//<summary>
+//  Title   : partial class Element 
+//  System  : Microsoft Visual C# .NET 2012
+//  $LastChangedDate$
+//  $Rev$
+//  $LastChangedBy$
+//  $URL$
+//  $Id$
+//
+//  Copyright (C) 2013, CAS LODZ POLAND.
+//  TEL: +48 (42) 686 25 47
+//  mailto://techsupp@cas.eu
+//  http://www.cas.eu
+//</summary>
+      
 namespace CAS.SmartFactory.CW.Dashboards.DisposalRequestWebPart.Data
 {
   public partial class Element : ITrackEntityState, ITrackOriginalValues, System.ComponentModel.INotifyPropertyChanged, System.ComponentModel.INotifyPropertyChanging
   {
+
+    #region private
     private System.Nullable<int> _id;
     private System.Nullable<int> _version;
     private string _path;
     private EntityState _entityState;
     private System.Collections.Generic.IDictionary<string, object> _originalValues;
-
     private string _title;
+    #endregion
 
     #region Extensibility Method Definitions
     partial void OnLoaded();
@@ -17,6 +33,7 @@ namespace CAS.SmartFactory.CW.Dashboards.DisposalRequestWebPart.Data
     partial void OnCreated();
     #endregion
 
+    #region ITrackEntityState
     EntityState ITrackEntityState.EntityState
     {
       get
@@ -25,31 +42,37 @@ namespace CAS.SmartFactory.CW.Dashboards.DisposalRequestWebPart.Data
       }
       set
       {
-        if ((value != this._entityState))
+        if ( ( value != this._entityState ) )
         {
           this._entityState = value;
         }
       }
     }
+    #endregion
 
+    #region ITrackOriginalValues
     System.Collections.Generic.IDictionary<string, object> ITrackOriginalValues.OriginalValues
     {
       get
       {
-        if ((null == this._originalValues))
+        if ( ( null == this._originalValues ) )
         {
           this._originalValues = new System.Collections.Generic.Dictionary<string, object>();
         }
         return this._originalValues;
       }
     }
+    #endregion
 
+    #region ctor
     public Element()
     {
       this.OnCreated();
     }
+    #endregion
 
-    [ColumnAttribute(Name = "ID", Storage = "_id", ReadOnly = true, FieldType = "Counter")]
+    #region public
+    [ColumnAttribute( Name = "ID", Storage = "_id", ReadOnly = true, FieldType = "Counter" )]
     public System.Nullable<int> Id
     {
       get
@@ -58,16 +81,15 @@ namespace CAS.SmartFactory.CW.Dashboards.DisposalRequestWebPart.Data
       }
       set
       {
-        if ((value != this._id))
+        if ( ( value != this._id ) )
         {
-          this.OnPropertyChanging("Id", this._id);
+          this.OnPropertyChanging( "Id", this._id );
           this._id = value;
-          this.OnPropertyChanged("Id");
+          this.OnPropertyChanged( "Id" );
         }
       }
     }
-
-    [ColumnAttribute(Name = "owshiddenversion", Storage = "_version", ReadOnly = true, FieldType = "Integer")]
+    [ColumnAttribute( Name = "owshiddenversion", Storage = "_version", ReadOnly = true, FieldType = "Integer" )]
     public System.Nullable<int> Version
     {
       get
@@ -76,16 +98,15 @@ namespace CAS.SmartFactory.CW.Dashboards.DisposalRequestWebPart.Data
       }
       set
       {
-        if ((value != this._version))
+        if ( ( value != this._version ) )
         {
-          this.OnPropertyChanging("Version", this._version);
+          this.OnPropertyChanging( "Version", this._version );
           this._version = value;
-          this.OnPropertyChanged("Version");
+          this.OnPropertyChanged( "Version" );
         }
       }
     }
-
-    [ColumnAttribute(Name = "FileDirRef", Storage = "_path", ReadOnly = true, FieldType = "Lookup", IsLookupValue = true)]
+    [ColumnAttribute( Name = "FileDirRef", Storage = "_path", ReadOnly = true, FieldType = "Lookup", IsLookupValue = true )]
     public string Path
     {
       get
@@ -94,16 +115,15 @@ namespace CAS.SmartFactory.CW.Dashboards.DisposalRequestWebPart.Data
       }
       set
       {
-        if ((value != this._path))
+        if ( ( value != this._path ) )
         {
-          this.OnPropertyChanging("Path", this._path);
+          this.OnPropertyChanging( "Path", this._path );
           this._path = value;
-          this.OnPropertyChanged("Path");
+          this.OnPropertyChanged( "Path" );
         }
       }
     }
-
-    [ColumnAttribute(Name = "Title", Storage = "_title", Required = true, FieldType = "Text")]
+    [ColumnAttribute( Name = "Title", Storage = "_title", Required = true, FieldType = "Text" )]
     public string Title
     {
       get
@@ -112,41 +132,42 @@ namespace CAS.SmartFactory.CW.Dashboards.DisposalRequestWebPart.Data
       }
       set
       {
-        if ((value != this._title))
+        if ( ( value != this._title ) )
         {
-          this.OnPropertyChanging("Title", this._title);
+          this.OnPropertyChanging( "Title", this._title );
           this._title = value;
-          this.OnPropertyChanged("Title");
+          this.OnPropertyChanged( "Title" );
         }
       }
     }
-
     public event System.ComponentModel.PropertyChangedEventHandler PropertyChanged;
-
     public event System.ComponentModel.PropertyChangingEventHandler PropertyChanging;
+    #endregion
 
-    protected virtual void OnPropertyChanged(string propertyName)
+    #region protected 
+    protected virtual void OnPropertyChanged( string propertyName )
     {
-      if ((null != this.PropertyChanged))
+      if ( ( null != this.PropertyChanged ) )
       {
-        this.PropertyChanged(this, new System.ComponentModel.PropertyChangedEventArgs(propertyName));
+        this.PropertyChanged( this, new System.ComponentModel.PropertyChangedEventArgs( propertyName ) );
       }
     }
-
-    protected virtual void OnPropertyChanging(string propertyName, object value)
+    protected virtual void OnPropertyChanging( string propertyName, object value )
     {
-      if ((null == this._originalValues))
+      if ( ( null == this._originalValues ) )
       {
         this._originalValues = new System.Collections.Generic.Dictionary<string, object>();
       }
-      if ((false == this._originalValues.ContainsKey(propertyName)))
+      if ( ( false == this._originalValues.ContainsKey( propertyName ) ) )
       {
-        this._originalValues.Add(propertyName, value);
+        this._originalValues.Add( propertyName, value );
       }
-      if ((null != this.PropertyChanging))
+      if ( ( null != this.PropertyChanging ) )
       {
-        this.PropertyChanging(this, new System.ComponentModel.PropertyChangingEventArgs(propertyName));
+        this.PropertyChanging( this, new System.ComponentModel.PropertyChangingEventArgs( propertyName ) );
       }
     }
+    #endregion
+
   }
 }
