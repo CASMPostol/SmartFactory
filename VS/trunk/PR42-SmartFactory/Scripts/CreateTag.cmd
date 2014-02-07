46c8a369fd419d05aacb8e573ba065bd364e39c4
bwojciechowski <bwojciechowski@8a3894fd-0c01-b944-991b-6a3da02cae8a>
2014-02-07 09:38:43 +0000

    QA deployment - assembly inconsistency - version update

--- VS/trunk/PR42-SmartFactory/Scripts/CreateTag.cmd
+++ VS/trunk/PR42-SmartFactory/Scripts/CreateTag.cmd
@@ -7,8 +7,8 @@ rem//  $Id$
 
 
 set branchtype=tags
-set TagFolder=Rel_2_00_00
-set TagPath=svn://svnserver.hq.cas.com.pl/VS/%branchtype%/SmartFactory/rel_2_00_00
+set TagFolder=Rel_2_00_02
+set TagPath=svn://svnserver.hq.cas.com.pl/VS/%branchtype%/SmartFactory/rel_2_00_02
 set trunkPath=svn://svnserver.hq.cas.com.pl/VS/trunk
 
 svn mkdir %TagPath%  -m "created new %TagPath% (in %branchtype% folder)"
