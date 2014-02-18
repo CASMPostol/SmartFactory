40ce84d0e067478e0cd8bc7b6064422b7923ed3b
mpostol <mpostol@8a3894fd-0c01-b944-991b-6a3da02cae8a>
2014-02-18 15:28:25 +0000

    Modify workflow for AccountBalance Report - IPR - done, moved code from CW to common library.

--- VS/trunk/PR42-SmartFactory/IPR/IPRWorkflow/Workflows/IPRClosing/IPRClosing.cs
+++ VS/trunk/PR42-SmartFactory/IPR/IPRWorkflow/Workflows/IPRClosing/IPRClosing.cs
@@ -1,9 +1,25 @@
-﻿using System;
+﻿//<summary>
+//  Title   : class IPRClosing
+//  System  : Microsoft Visual C# .NET 2012
+//  $LastChangedDate$
+//  $Rev$
+//  $LastChangedBy$
+//  $URL$
+//  $Id$
+//
+//  Copyright (C) 2014, CAS LODZ POLAND.
+//  TEL: +48 (42) 686 25 47
+//  mailto://techsupp@cas.eu
+//  http://www.cas.eu
+//</summary>
+
+using System;
 using System.Linq;
 using System.Workflow.Activities;
-using CAS.SmartFactory.IPR.DocumentsFactory;
 using CAS.SmartFactory.IPR.WebsiteModel.Linq;
+using CAS.SmartFactory.xml.DocumentsFactory;
 using CAS.SmartFactory.xml.DocumentsFactory.AccountClearance;
+using Microsoft.SharePoint;
 using Microsoft.SharePoint.Workflow;
 using IPRClass = CAS.SmartFactory.IPR.WebsiteModel.Linq.IPR;
 
@@ -12,7 +28,7 @@ namespace CAS.SmartFactory.IPR.Workflows.IPRClosing
   /// <summary>
   /// IPR Closing Workflow
   /// </summary>
-  public sealed partial class IPRClosing: SequentialWorkflowActivity
+  public sealed partial class IPRClosing : SequentialWorkflowActivity
   {
     /// <summary>
     /// Initializes a new instance of the <see cref="IPRClosing" /> class.
@@ -24,77 +40,62 @@ namespace CAS.SmartFactory.IPR.Workflows.IPRClosing
     /// <summary>
     /// The workflow id
     /// </summary>
-    public Guid workflowId = new System.Guid( "{B16A9665-EFEA-4677-954B-6A1FE2A633FC}" );
+    public Guid workflowId = new System.Guid("{B16A9665-EFEA-4677-954B-6A1FE2A633FC}");
     /// <summary>
     /// The workflow properties
     /// </summary>
     public SPWorkflowActivationProperties workflowProperties = new SPWorkflowActivationProperties();
-    private void RecordValidation_ExecuteCode( object sender, EventArgs e )
+    private void Closeing_ExecuteCode(object sender, EventArgs e)
     {
       try
       {
-        using ( Entities _edc = new Entities( workflowProperties.WebUrl ) )
+        bool Valid = true;
+        using (Entities _edc = new Entities(workflowProperties.WebUrl))
         {
-          WebsiteModel.Linq.IPR _record = Element.GetAtIndex<WebsiteModel.Linq.IPR>( _edc.IPR, workflowProperties.ItemId );
-          if ( _record.AccountBalance != 0 )
+          IPRClass _record = Element.GetAtIndex<WebsiteModel.Linq.IPR>(_edc.IPR, workflowProperties.ItemId);
+          if (_record.AccountBalance != 0)
           {
-            LogWarningMessageToHistory_HistoryDescription = String.Format( LogWarningMessageToHistory_HistoryDescription, "AccountBalance must be equal 0" );
-            return;
+            LogFinalMessageToHistory_HistoryOutcome = "Closing error";
+            LogFinalMessageToHistory_HistoryOutcome = String.Format(LogWarningTemplate, "AccountBalance must be equal 0");
+            Valid = false;
           }
-          bool _notFinished = _record.Disposal.Where<Disposal>( v => v.CustomsStatus.Value != CustomsStatus.Finished ).Any<Disposal>();
-          if ( _notFinished )
+          bool _notFinished = _record.Disposal.Where<Disposal>(v => v.CustomsStatus.Value != CustomsStatus.Finished).Any<Disposal>();
+          if (_notFinished)
           {
-            LogWarningMessageToHistory_HistoryDescription = String.Format( LogWarningMessageToHistory_HistoryDescription, "All disposals must be cleared through customs." );
-            return;
+            LogFinalMessageToHistory_HistoryOutcome = "Closing error";
+            LogFinalMessageToHistory_HistoryOutcome = String.Format(LogWarningTemplate, "All disposals must be cleared through customs.");
+            Valid = false;
           }
-          Valid = true;
-        }
-      }
-      catch ( Exception ex )
-      {
-        LogWarningMessageToHistory_HistoryOutcome = "Validation fatal error";
-        string _patt = "Cannot validate the IPR account because of fata error {0} at {1}";
-        LogWarningMessageToHistory_HistoryDescription = String.Format( _patt, ex.Message, ex.StackTrace );
-      }
-    }
-    private bool Valid = false;
-    private void ProcessIfValid( object sender, ConditionalEventArgs e )
-    {
-      e.Result = Valid;
-    }
-    private void Closeing_ExecuteCode( object sender, EventArgs e )
-    {
-      try
-      {
-        using ( Entities _edc = new Entities( workflowProperties.WebUrl ) )
-        {
-          IPRClass _record = Element.GetAtIndex<WebsiteModel.Linq.IPR>( _edc.IPR, workflowProperties.ItemId );
-          string _documentName = Settings.RequestForAccountClearenceDocumentName( _edc, _record.Id.Value );
-          RequestContent _content = DocumentsFactory.AccountClearanceFactory.CreateRequestContent( _record, _record.Id.Value, _documentName );
-          int _id = SPDocumentFactory.Prepare( this.workflowProperties.Web, _content, _documentName );
-          WebsiteModel.Linq.IPRLib _document = Element.GetAtIndex<WebsiteModel.Linq.IPRLib>( _edc.IPRLibrary, _id );
-          _record.IPRLibraryIndex = _document;
-          _record.AccountClosed = true;
-          _record.ClosingDate = DateTime.Today.Date;
-          _document.DocumentNo = _record.Title;
+          string _documentName = Settings.RequestForAccountClearenceDocumentName(_edc, _record.Id.Value);
+          RequestContent _content = DocumentsFactory.AccountClearanceFactory.CreateRequestContent(_record, _record.Id.Value, _documentName);
+          if (_record.IPRLibraryIndex != null)
+            CAS.SharePoint.DocumentsFactory.File.WriteXmlFile<RequestContent>(this.workflowProperties.Web, _record.IPRLibraryIndex.Id.Value, Entities.IPRLibraryName, _content, DocumentNames.RequestForAccountClearenceName);
+          else
+          {
+            SPFile _docFile = CAS.SharePoint.DocumentsFactory.File.CreateXmlFile<RequestContent>(this.workflowProperties.Web, _content, _documentName, Entities.IPRLibraryName, DocumentNames.RequestForAccountClearenceName);
+            WebsiteModel.Linq.IPRLib _document = Element.GetAtIndex<WebsiteModel.Linq.IPRLib>(_edc.IPRLibrary, _docFile.Item.ID);
+            _document.DocumentNo = _record.Title;
+            _record.IPRLibraryIndex = _document;
+          }
+          if (Valid)
+          {
+            _record.AccountClosed = true;
+            _record.ClosingDate = DateTime.Today.Date;
+          }
           _edc.SubmitChanges();
         }
       }
-      catch ( Exception ex )
+      catch (Exception ex)
       {
         LogFinalMessageToHistory_HistoryOutcome = "Closing fatal error";
-        string _patt = "Cannot close the IPR account because of fata error {0} at {1}";
-        LogFinalMessageToHistory_HistoryDescription = String.Format( _patt, ex.Message, ex.StackTrace );
+        string _patt = "Cannot close the IPR account because of fatal error {0} at {1}";
+        LogFinalMessageToHistory_HistoryDescription = String.Format(_patt, ex.Message, ex.StackTrace);
       }
     }
     /// <summary>
     /// The log warning message to history_ history description
     /// </summary>
-    public String LogWarningMessageToHistory_HistoryDescription = "Cannot close the IPR account because {0}, corect the content and try again.";
-    /// <summary>
-    /// The log warning message to history_ history outcome
-    /// </summary>
-    public String LogWarningMessageToHistory_HistoryOutcome = "Closing failed";
+    public String LogWarningTemplate = "Cannot close the IPR account because {0}, correct the content and try again.";
     /// <summary>
     /// The log final message to history_ history description
     /// </summary>
@@ -103,5 +104,6 @@ namespace CAS.SmartFactory.IPR.Workflows.IPRClosing
     /// The log final message to history_ history outcome
     /// </summary>
     public String LogFinalMessageToHistory_HistoryOutcome = "Finished successfully";
+
   }
 }
