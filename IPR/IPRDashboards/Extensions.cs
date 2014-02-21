6f4829035edfdecc02d6848701a74103fd55c832
bwojciechowski <bwojciechowski@8a3894fd-0c01-b944-991b-6a3da02cae8a>
2014-02-21 08:51:12 +0000

    Add IPRDashboards.EventReceiver - EventReceiver added (with feature activation)

---
+++ IPR/IPRDashboards/Extensions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CAS.SmartFactory.IPR.Dashboards
+{
+  public static class Extensions
+  {
+    #region public
+
+    public static string GetIPRLocalizationExpresion(this string val)
+    {
+      string _frmt = "$Resources:{0},{1}";
+      return String.Format(_frmt, GlobalDefinitions.IPRResourceFileName, val);
+    }
+    #endregion
+  }
+}
---
+++ IPR/IPRDashboards/WebPartPages/ProjectElementManagement.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CAS.SmartFactory.IPR.Dashboards.WebPartPages
+{
+  using SPLimitedWebPartManager = Microsoft.SharePoint.WebPartPages.SPLimitedWebPartManager;
+
+  internal class ProjectElementManagement
+  {
+    // TopNavigation Elements Title
+    internal static string MenuIPRBookTitle = "IprBookTitle".GetIPRLocalizationExpresion();
+    internal static string MenuIPRClosedBookTitle = "IprClosedBookTitle".GetIPRLocalizationExpresion();
+    internal static string MenuIPRBookCustomsOfficeTitle = "IPRBookCustomsOfficeTitle".GetIPRLocalizationExpresion();
+    internal static string MenuIPRClosedBookCustomsOfficeTitle = "IPRClosedBookCustomsOfficeTitle".GetIPRLocalizationExpresion();
+    internal static string MenuClearanceTitle = "ClearanceTitle".GetIPRLocalizationExpresion();
+    internal static string MenuClearanceViewTitle = "ClearanceViewTitle".GetIPRLocalizationExpresion();
+    internal static string MenuExportTitle = "ExportTitle".GetIPRLocalizationExpresion();
+    internal static string MenuBatchTitle = "BatchTitle".GetIPRLocalizationExpresion();
+    internal static string MenuStocksTitle = "StocksTitle".GetIPRLocalizationExpresion();
+    internal static string MenuReportsTitle = "ReportsTitle".GetIPRLocalizationExpresion();
+    
+
+    // DashboardsURL
+    internal const string WebPartPagesFolder = "WebPartPages";
+    internal const string URLIPRBookDashboard = WebPartPagesFolder + "/IPRBookDashboard.aspx";
+    internal const string URLIPRBookClosedDashboard = WebPartPagesFolder + "/IPRBookClosedDashboard.aspx";
+    internal const string URLIPRBookCustomsOfficeDashboard = WebPartPagesFolder + "/IPRBookCustomsOfficeDashboard.aspx";
+    internal const string URLIPRBookClosedCustomsOfficeDashboard = WebPartPagesFolder + "/IPRBookClosedCustomsOfficeDashboard.aspx";
+    internal const string URLClearenceDashboard = WebPartPagesFolder + "/ClearenceDashboard.aspx";
+    internal const string URLClearenceViewDashboard = WebPartPagesFolder + "/ClearenceViewDashboard.aspx";
+    internal const string URLExportDashboard = WebPartPagesFolder + "/ExportDashboard.aspx";
+    internal const string URLBatchDashboard = WebPartPagesFolder + "/BatchDashboard.aspx";
+    internal const string URLStocksDashboard = WebPartPagesFolder + "/StocksDashboard.aspx";
+    internal const string URLReportsDashboard = WebPartPagesFolder + "/ReportsDashboard.aspx";
+
+  }
+}
