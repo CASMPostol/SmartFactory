001015cfd7695289b9892c0d0e0ff9f44d493439
bwojciechowski <bwojciechowski@8a3894fd-0c01-b944-991b-6a3da02cae8a>
2014-03-05 09:19:02 +0000

    Create CW Dashboards Feature Event Receiver. - created and tested.

---
+++ VS/trunk/PR42-SmartFactory/CW/CWDashboards/Extensions.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CAS.SmartFactory.CW.Dashboards
+{
+  /// <summary>
+  /// class Extensions
+  /// </summary>
+  public static class Extensions
+  {
+    #region public
+
+    /// <summary>
+    /// Gets the ipr localization expression.
+    /// </summary>
+    /// <param name="val">The value.</param>
+    /// <returns></returns>
+    public static string GetCWLocalizationExpression(this string val)
+    {
+      string _frmt = "$Resources:{0},{1}";
+      return String.Format(_frmt, GlobalDefinitions.CWResourceFileName, val);
+    }
+    #endregion
+  }
+}
---
+++ VS/trunk/PR42-SmartFactory/CW/CWDashboards/GlobalDefinitions.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CAS.SmartFactory.CW.Dashboards
+{
+  internal class GlobalDefinitions
+  {
+    internal const string RootResourceFileName = "";
+    internal const string CWResourceFileName = "CASSmartFactoryCWCode";
+  }
+}
