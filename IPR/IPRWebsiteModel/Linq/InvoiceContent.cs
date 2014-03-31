5335045273116115978e75ff746999c8e60f10d2
Mariusz <mpostol@cas.eu>
2014-03-31 12:41:02 +0000

    Requirement: Intermediate batch: CigaretteExportForm - correction. - Added anchor for the adjust procedure

--- IPR/IPRWebsiteModel/Linq/InvoiceContent.cs
+++ IPR/IPRWebsiteModel/Linq/InvoiceContent.cs
@@ -37,6 +37,8 @@ namespace CAS.SmartFactory.IPR.WebsiteModel.Linq
           continue;
         Material _mtrl = _dsp.Disposal2MaterialIndex;
         decimal _2Add = _mtrl.CalculatedQuantity( this ) - _gx.Sum<Disposal>( v => v.SettledQuantityDec );
+        //TODO it could cause the closed accounts will have account balance != 0 
+        //TODO if current settled quantity < previous value it could also cause that old account will have holes that makes them difficult to be closed.
         IEnumerable<Disposal> _sorted = from _dx in _gx
                                         orderby _dx.SettledQuantityDec ascending
                                         select _dx;
