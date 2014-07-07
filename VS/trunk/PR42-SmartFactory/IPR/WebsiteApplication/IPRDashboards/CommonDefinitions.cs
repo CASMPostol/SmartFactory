136ec3f1446eb1f0e9e8b0e33237f5b8bee6d08e
mpostol <mpostol@8a3894fd-0c01-b944-991b-6a3da02cae8a>
2014-07-07 13:10:59 +0000

    IPRClientSiteUserInterface: implement MVVM model for user interface - code refactoring - tt file updated

--- VS/trunk/PR42-SmartFactory/IPR/WebsiteApplication/IPRDashboards/CommonDefinitions.cs
+++ VS/trunk/PR42-SmartFactory/IPR/WebsiteApplication/IPRDashboards/CommonDefinitions.cs
@@ -1,1 +1,15 @@
-﻿ErrorGeneratingOutput
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CAS.SmartFactory.IPR.Dashboards
+{
+  internal class CommonDefinitions
+  {
+    //TODO  [pr4-3550] Add localization to the application http://itrserver/Bugs/BugDetail.aspx?bid=3550
+    internal const string RootResourceFileName = "";
+    internal const string IPRSADConsignmentLibraryTitle = "SAD Consignment";
+  }
+}
+
--- VS/trunk/PR42-SmartFactory/IPR/WebsiteApplication/IPRDashboards/Properties/AssemblyInfo.cs
+++ VS/trunk/PR42-SmartFactory/IPR/WebsiteApplication/IPRDashboards/Properties/AssemblyInfo.cs
@@ -1,1 +1,45 @@
-﻿ErrorGeneratingOutput
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Security;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("CAS SmartFactory IPR Dashboards")]
+[assembly: AssemblyDescription( "IPR Dashboards" )]
+[assembly: AssemblyConfiguration( "" )]
+[assembly: AssemblyCompany("CAS")]
+[assembly: AssemblyProduct("Smart Factory")]
+[assembly: AssemblyCopyright("Copyright (c) 2000-2014 CAS LODZ POLAND")]
+[assembly: AssemblyTrademark("Smart Factory")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid( "c3e386b9-7f0d-4b3b-a1e8-62ee6a5db102" )]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("2.00.21.*")]
+[assembly: AssemblyFileVersion("2.00.21")]
+
+
+[assembly: System.Runtime.CompilerServices.InternalsVisibleTo( "IPRDashboardsTest, PublicKey=002400000480000094000000060200000024000052534131000400000100010015c024c9d0c88f" +
+                                                                                            "bd6a2775206bd59cc647fc8d595ffecd9dbfae9cb1a1481cec83e46af1cd31de69c109ae0735b0" +
+                                                                                            "9198f1862ea4ed3f1849c2ce98450a72b4f6f3701ea6e051b0852c0ba6c41fbe16ea1f31862359" +
+                                                                                            "261eb3342916d6e2593b1946b703ff2829e34e25fdc4182f409616db70cd97ef87fd3083c6e5dd" +
+                                                                                            "fc3089b1" )]
+
