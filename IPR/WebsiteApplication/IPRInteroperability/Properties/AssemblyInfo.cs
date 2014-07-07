902d0563986b0d247b24cad5ec7aedfa22cb8aaa
Mariusz <mpostol@cas.eu>
2014-07-07 13:10:59 +0000

    IPRClientSiteUserInterface: implement MVVM model for user interface - code refactoring - tt file updated

--- IPR/WebsiteApplication/IPRInteroperability/Properties/AssemblyInfo.cs
+++ IPR/WebsiteApplication/IPRInteroperability/Properties/AssemblyInfo.cs
@@ -1,1 +1,27 @@
-﻿ErrorGeneratingOutput
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("IPR Interoperability")]
+[assembly: AssemblyDescription("Smart Factory IPR SAP, CELINE, WMS. Office interconnection helpers")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("CAS")]
+[assembly: AssemblyProduct("Smart Factory")]
+[assembly: AssemblyCopyright("Copyright (c) 2000-2014 CAS LODZ POLAND")]
+[assembly: AssemblyTrademark("Smart Factory")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("3EABDD4B-227C-483A-A855-03C7628286FD")]
+
+[assembly: AssemblyVersion("2.00.21.*")]
+[assembly: AssemblyFileVersion("2.00.21")]
+
--- IPR/WebsiteApplication/IPRWebsite/Properties/AssemblyInfo.cs
+++ IPR/WebsiteApplication/IPRWebsite/Properties/AssemblyInfo.cs
@@ -1,1 +1,27 @@
-﻿ErrorGeneratingOutput
+﻿
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("IPR Website")]
+[assembly: AssemblyDescription("IPR Website Collection Definitions")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("CAS")]
+[assembly: AssemblyProduct("Smart Factory")]
+[assembly: AssemblyCopyright("Copyright (c) 2000-2014 CAS LODZ POLAND")]
+[assembly: AssemblyTrademark("Smart Factory")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("65cefe18-d7fb-4189-ac46-5acfd7be80d8")]
+[assembly: AssemblyVersion("2.00.21.*")]
+[assembly: AssemblyFileVersion("2.00.21")]
+
