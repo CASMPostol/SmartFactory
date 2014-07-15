0ac89e3cc2839fb5b58c9ef736ba47b36c237cbb
bwojciechowski <bwojciechowski@8a3894fd-0c01-b944-991b-6a3da02cae8a>
2014-07-15 10:27:24 +0000

    SQL database - entities for client - entities generated.

---
+++ IPR/Library/ClientDataManagement/Linq2SQL/Scripts/GetEntities.ps1
@@ -0,0 +1,5 @@
+﻿Write-Host "This file creates all the entities classes for the selected SQL database"
+$env:path += "; C:\Program Files (x86)\Microsoft SDKs\Windows\v7.0A\Bin"
+Get-Location | Write-host
+SQLMetal.exe /server:CAS_SP\MSSQL_01 /database:IPRDEV /namespace:CAS.SmartFactory.IPR.Client.DataManagement.Linq2SQL /code:..\Entities.cs /language:csharp | write-host
+write-host "Done . "
