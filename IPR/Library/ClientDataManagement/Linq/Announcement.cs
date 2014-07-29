505d9c78d4fdf815ac4f514c34e2a1a52afdcdf3
Mariusz <mpostol@cas.eu>
2014-07-29 09:47:27 +0000

    ClientSiteUserInterface: Add tracing mechanism - In progress - BW: add mapping on the new branch task

---
+++ IPR/Library/ClientDataManagement/Linq/Announcement.cs
@@ -0,0 +1,19 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CAS.SmartFactory.IPR.Client.DataManagement.Linq
+{
+  
+  public partial class Announcement
+  {
+    internal override Dictionary<string, string> GetMappings()
+    {
+      Dictionary<string, string> _ret = base.GetMappings();
+      _ret.Add("SPPropert", "SQLProperty");
+      return _ret;
+    }
+  }
+}
