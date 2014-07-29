27621a0bb19fe03444f648d68f7b05211431c252
mpostol <mpostol@8a3894fd-0c01-b944-991b-6a3da02cae8a>
2014-07-29 09:47:27 +0000

    ClientSiteUserInterface: Add tracing mechanism - In progress - BW: add mapping on the new branch task

---
+++ VS/trunk/PR42-SmartFactory/IPR/Library/ClientDataManagement/Linq/Item.cs
@@ -0,0 +1,36 @@
+﻿//<summary>
+//  Title   : partial class Item
+//  System  : Microsoft VisulaStudio 2013 / C#
+//  $LastChangedDate:$
+//  $Rev:$
+//  $LastChangedBy:$
+//  $URL:$
+//  $Id:$
+//
+//  Copyright (C) 2014, CAS LODZ POLAND.
+//  TEL: +48 (42) 686 25 47
+//  mailto://techsupp@cas.eu
+//  http://www.cas.eu
+//</summary>
+      
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CAS.SmartFactory.IPR.Client.DataManagement.Linq
+{
+  /// <summary>
+  /// SharePoint Item Entity class
+  /// </summary>
+  public partial class Item
+  {
+    internal virtual Dictionary<string, string> GetMappings()
+    {
+      return new Dictionary<string, string>()
+      { 
+        {"SPProperty", "SQLProperty"} 
+      };
+    }
+  }
+}
---
+++ VS/trunk/PR42-SmartFactory/IPR/Library/ClientDataManagement/SynchronizationContent.cs
@@ -0,0 +1,43 @@
+﻿using CAS.SmartFactory.IPR.Client.DataManagement.Linq;
+using CAS.SmartFactory.IPR.Client.DataManagement.Linq2SQL;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+
+namespace CAS.SmartFactory.IPR.Client.DataManagement
+{
+  /// <summary>
+  /// Helpers to synchronize SQL content with the SharePoint website
+  /// </summary>
+  public static class SynchronizationContent
+  {
+    public struct ArchiveSettings
+    {
+      public string SiteURL;
+      public string ConnectionString;
+    }
+    public static void Go(ArchiveSettings settings, Action<object, ProgressChangedEventArgs> progressChanged)
+    {
+      IPRDEV _entities = Connect2SQL(settings, progressChanged);
+      using (Entities edc = new Entities(settings.SiteURL))
+      {
+
+      }
+    }
+    private static IPRDEV Connect2SQL(ArchiveSettings settings, Action<object, ProgressChangedEventArgs> progressChanged)
+    {
+      progressChanged(settings, new ProgressChangedEventArgs(1, String.Format("Attempt to connect to SQL at: {0}", settings.ConnectionString)));
+      System.Data.IDbConnection _connection = new SqlConnection(settings.ConnectionString);
+      IPRDEV _entities = new IPRDEV(_connection);
+      if (_entities.DatabaseExists())
+        progressChanged(settings, new ProgressChangedEventArgs(1, "The specified database can be opened."));
+      else
+        progressChanged(settings, new ProgressChangedEventArgs(1, "The specified database cannot be opened."));
+      return _entities;
+    }
+
+  }
+}
