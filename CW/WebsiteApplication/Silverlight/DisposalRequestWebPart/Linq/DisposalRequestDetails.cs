9ae949904a782f9e804136d985f3743646aff570
bwojciechowski <bwojciechowski@8a3894fd-0c01-b944-991b-6a3da02cae8a>
2014-08-01 13:16:53 +0000

    Interface for CW - FIFO exception

---
+++ CW/WebsiteApplication/Silverlight/DisposalRequestWebPart/Linq/DisposalRequestDetails.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+
+namespace CAS.SmartFactory.CW.Dashboards.DisposalRequestWebPart.Linq
+{
+  public class DisposalRequestDetails: DisposalRequest
+  {
+
+    private string b_DocumentNumber;
+
+    public string DocumentNumber
+    {
+      get
+      {
+        return b_DocumentNumber;
+      }
+      set
+      {
+        RaiseHandler<string>(value, ref b_DocumentNumber, "DocumentNumber", this);
+      }
+    } 
+                
+  }
+}
