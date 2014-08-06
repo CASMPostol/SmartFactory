de34e7030ab19c8610adaea065351dabf131903c
Mariusz <mpostol@cas.eu>
2014-08-06 10:46:15 +0000

    SPExplorer: all column in the SQL script have NOT NULL attribute - fixed additional tables must be generated to separate files.

---
+++ IPR/Tools/Tests/IPRArchivalDtabase/dbo/Tables/ArchivingOperationLogs.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[ArchivingOperationLogs] (
+    [ID]                     INT IDENTITY(1,1) NOT NULL,
+    [Operation]              NVARCHAR(255)   NOT NULL,
+    [Date]                   DATETIME        NOT NULL,
+    [UserName]               NVARCHAR(255)   NOT NULL,
+      CONSTRAINT [PK_ArchivingOperationLogs_ID] PRIMARY KEY CLUSTERED ([ID] ASC) 
+);
