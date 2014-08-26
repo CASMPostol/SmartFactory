734c653c2f4e9b340d5e9095ba901db97f066232
bwojciechowski <bwojciechowski@8a3894fd-0c01-b944-991b-6a3da02cae8a>
2014-08-26 11:06:00 +0000

    Improve parameters.xml generation - parameters and entities update.

--- VS/trunk/PR42-SmartFactory/IPR/WebsiteApplication/IPRWebsiteModel/Linq/Scripts/GetEntities.ps1
+++ VS/trunk/PR42-SmartFactory/IPR/WebsiteApplication/IPRWebsiteModel/Linq/Scripts/GetEntities.ps1
@@ -1,5 +1,5 @@
 ﻿Write-Host "This file creates all the entities classes for the selected site"
 $env:path += "; C:\Program Files\Common Files\Microsoft Shared\Web server extensions\14\Bin"
 Get-Location | Write-host
-SPMetal.exe /web:http://cas_bw/sites/i07 /language:csharp /namespace:CAS.SmartFactory.IPR.WebsiteModel.Linq /parameters:Parameters.xml /code:..\Entities.cs| write-host
+SPMetal.exe /web:http://cas_sp:11225/sites/ipr3 /language:csharp /namespace:CAS.SmartFactory.IPR.WebsiteModel.Linq /parameters:Parameters.xml /code:..\Entities.cs| write-host
 write-host "Done . "
