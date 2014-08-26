a660f05ddc9c6b9e65c2d3a57c5aefcbcbdd4c01
Mariusz <mpostol@cas.eu>
2014-08-26 19:23:07 +0000

    IPR ClientSiteUserInterface: Derived content types are not read from SP - testing in progress - fixed problem wit content types Id comparison.

--- IPR/Tools/Tests/IPRClientEntityTests/StartWithEqualityComparerTest.cs
+++ IPR/Tools/Tests/IPRClientEntityTests/StartWithEqualityComparerTest.cs
@@ -11,17 +11,24 @@ namespace CAS.SmartFactory.IPR.Client.DataManagement.Tests
     [TestMethod]
     public void DictionaryTest()
     {
-      Dictionary<string, string> _dic = new Dictionary<string, string>(new StartWithEqualityComparer());
+      StartWithEqualityComparer _cmprr = new StartWithEqualityComparer();
+      Dictionary<string, string> _dic = new Dictionary<string, string>(_cmprr);
       _dic.Add(m_ShortString, m_ShortString);
-      //_dic.Add(m_ShortString + m_ShortString, m_ShortString);
       _dic.Add(m_LongString, m_LongString);
-      //_dic.Add(m_LongString, m_LongString);
-      Assert.IsTrue(_dic.ContainsKey(m_LongString));
+      _cmprr.LockComparer();
+      Assert.IsTrue(_dic.ContainsKey(m_ShortString));
+      Assert.AreEqual<string>(m_ShortString, _dic[m_ShortString]);
+      Assert.IsTrue(_dic.ContainsKey(m_LongString + m_ShortString));
+      Assert.AreEqual<string>(m_LongString, _dic[m_LongString + m_ShortString]);
+      Assert.IsTrue(_dic.ContainsKey(m_LongString + "dadadasasasad"));
+      Assert.AreEqual<string>(m_LongString, _dic[m_LongString + "0988776iujhkjhkjhjk"]);
     }
     [TestMethod]
     public void StartWithEqualityComparerTestMethod()
     {
       StartWithEqualityComparer _comp = new StartWithEqualityComparer();
+      _comp.GetHashCode(m_ShortString);
+      _comp.LockComparer();
       Assert.IsTrue(_comp.Equals(m_ShortString, m_LongString));
       Assert.IsTrue(_comp.Equals(m_LongString, m_ShortString));
 
