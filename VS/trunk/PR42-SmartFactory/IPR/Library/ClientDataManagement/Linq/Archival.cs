7cb22e3f8ba32e43949bf84d266dd5d5bb1676ae
bwojciechowski <bwojciechowski@8a3894fd-0c01-b944-991b-6a3da02cae8a>
2014-08-28 10:00:37 +0000

    Add Activation 2.10 functionality - added partial classes to the interface supporting Archival bit.

--- VS/trunk/PR42-SmartFactory/IPR/Library/ClientDataManagement/Linq/Archival.cs
+++ VS/trunk/PR42-SmartFactory/IPR/Library/ClientDataManagement/Linq/Archival.cs
@@ -17,5 +17,22 @@ namespace CAS.SmartFactory.IPR.Client.DataManagement.Linq
 {
   
   public partial class ReportsCommonPart : IArchival { }
-
+  public partial class Batch : IArchival { }
+  public partial class Clearence : IArchival { }
+  public partial class Disposal : IArchival { }
+  public partial class InvoiceContent : IArchival { }
+  public partial class IPR : IArchival { }
+  public partial class Material : IArchival { }
+  public partial class SADConsignment : IArchival { }
+  public partial class SADDocumentLib : IArchival { }
+  public partial class SADDocumentType : IArchival { }
+  public partial class SADDuties : IArchival { }
+  public partial class SADGood : IArchival { }
+  public partial class SADPackage : IArchival { }
+  public partial class SADQuantity : IArchival { }
+  public partial class SADRequiredDocuments : IArchival { }
+  public partial class SKUCommonPart : IArchival { }
+  public partial class StockEntry : IArchival { }
+  public partial class StockLib : IArchival { }
+  
 }
