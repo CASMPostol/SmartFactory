16bd1739ccbf681e699bcf7a3b17d9ccbfb0c056
mpostol <mpostol@8a3894fd-0c01-b944-991b-6a3da02cae8a>
2014-08-28 07:49:21 +0000

    Add Activation 2.10 functionality - added interface and placeholder for definition of the inheritance declaration.

--- VS/trunk/PR42-SmartFactory/IPR/Library/ClientDataManagement/Linq/Batch.cs
+++ VS/trunk/PR42-SmartFactory/IPR/Library/ClientDataManagement/Linq/Batch.cs
@@ -1,7 +1,19 @@
-﻿using System;
+﻿//<summary>
+//  Title   : public partial class Batch
+//  System  : Microsoft VisulaStudio 2013 / C#
+//  $LastChangedDate$
+//  $Rev$
+//  $LastChangedBy$
+//  $URL$
+//  $Id$
+//
+//  Copyright (C) 2014, CAS LODZ POLAND.
+//  TEL: +48 (42) 686 25 47
+//  mailto://techsupp@cas.eu
+//  http://www.cas.eu
+//</summary>
+
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 
 namespace CAS.SmartFactory.IPR.Client.DataManagement.Linq
 {
