324a49bcdab68ead20be0d8ad0891de72ac85acc
mpostol <mpostol@8a3894fd-0c01-b944-991b-6a3da02cae8a>
2014-10-07 12:52:41 +0000

    Interface for CW - FIFO exception - development in progress - added properties to DisposalRequestDetails.

--- VS/trunk/PR42-SmartFactory/CW/WebsiteApplication/Silverlight/DisposalRequestWebPart/Linq/DisposalRequestDetails.cs
+++ VS/trunk/PR42-SmartFactory/CW/WebsiteApplication/Silverlight/DisposalRequestWebPart/Linq/DisposalRequestDetails.cs
@@ -18,313 +18,302 @@ namespace CAS.SmartFactory.CW.Dashboards.DisposalRequestWebPart.Linq
   /// <summary>
   /// DisposalRequest details view model
   /// </summary>
-  public class DisposalRequestDetails: CAS.Common.ComponentModel.PropertyChangedBase
+  public class DisposalRequestDetails : CAS.Common.ComponentModel.PropertyChangedBase
   {
-    ///// <summary>
-    ///// Gets or sets the SKU.
-    ///// </summary>
-    ///// <value>
-    ///// The SKU.
-    ///// </value>
-    //public String SKU
-    //{
-    //  get
-    //  {
-    //    return this._sKU;
-    //  }
-    //  set
-    //  {
-    //    if ((value != this._sKU))
-    //    {
-    //      this.OnPropertyChanging("SKU", this._sKU);
-    //      this._sKU = value;
-    //      this.OnPropertyChanged("SKU");
-    //    }
-    //  }
-    //}
-    ///// <summary>
-    ///// Gets or sets the sku description.
-    ///// </summary>
-    ///// <value>
-    ///// The sku description.
-    ///// </value>
-    //public String SKUDescription
-    //{
-    //  get
-    //  {
-    //    return this._sKUDescription;
-    //  }
-    //  set
-    //  {
-    //    if ((value != this._sKUDescription))
-    //    {
-    //      this.OnPropertyChanging("SKUDescription", this._sKUDescription);
-    //      this._sKUDescription = value;
-    //      this.OnPropertyChanged("SKUDescription");
-    //    }
-    //  }
-    //}
-    ///// <summary>
-    ///// Gets or sets the batch.
-    ///// </summary>
-    ///// <value>
-    ///// The batch.
-    ///// </value>
-    //public String Batch
-    //{
-    //  get
-    //  {
-    //    return this._Batch;
-    //  }
-    //  set
-    //  {
-    //    if ((value != this._Batch))
-    //    {
-    //      this.OnPropertyChanging("Batch", this._Batch);
-    //      this._Batch = value;
-    //      this.OnPropertyChanged("Batch");
-    //    }
-    //  }
-    //}
-    ///// <summary>
-    ///// Gets or sets the total stock.
-    ///// </summary>
-    ///// <value>
-    ///// The total stock.
-    ///// </value>
-    //public double TotalStock
-    //{
-    //  get
-    //  {
-    //    return this._totalStock;
-    //  }
-    //  set
-    //  {
-    //    if ((value != this._totalStock))
-    //    {
-    //      this.OnPropertyChanging("TotalStock", this._totalStock);
-    //      this._totalStock = value;
-    //      this.OnPropertyChanged("TotalStock");
-    //    }
-    //  }
-    //}
-    ///// <summary>
-    ///// Gets or sets the mass per package.
-    ///// </summary>
-    ///// <value>
-    ///// The mass per package.
-    ///// </value>
-    //public double MassPerPackage
-    //{
-    //  get
-    //  {
-    //    return this._massPerPackage;
-    //  }
-    //  set
-    //  {
-    //    if ((value != this._massPerPackage))
-    //    {
-    //      this.OnPropertyChanging("MassPerPackage", this._massPerPackage);
-    //      this._massPerPackage = value;
-    //      this.OnPropertyChanged("MassPerPackage");
-    //    }
-    //  }
-    //}
-    ///// <summary>
-    ///// Gets or sets the declared net mass.
-    ///// </summary>
-    ///// <value>
-    ///// The declared net mass.
-    ///// </value>
-    //public double DeclaredNetMass
-    //{
-    //  get
-    //  {
-    //    return this._declaredNetMass;
-    //  }
-    //  set
-    //  {
-    //    if ((value != this._declaredNetMass))
-    //    {
-    //      this.OnPropertyChanging("CW_SettledNetMass", this._declaredNetMass);
-    //      this._declaredNetMass = value;
-    //      this.OnPropertyChanged("CW_SettledNetMass");
-    //    }
-    //  }
-    //}
-    ///// <summary>
-    ///// Gets or sets the added kg.
-    ///// </summary>
-    ///// <value>
-    ///// The added kg.
-    ///// </value>
-    //public double AddedKg
-    //{
-    //  get
-    //  {
-    //    return this._addedKg;
-    //  }
-    //  set
-    //  {
-    //    if ((value != this._addedKg))
-    //    {
-    //      this.OnPropertyChanging("AddedKg", this._addedKg);
-    //      this._addedKg = value;
-    //      this.OnPropertyChanged("AddedKg");
-    //    }
-    //  }
-    //}
-    ///// <summary>
-    ///// Gets or sets the quantityy automatic clear sum.
-    ///// </summary>
-    ///// <value>
-    ///// The quantityy automatic clear sum.
-    ///// </value>
-    //public double QuantityyToClearSum
-    //{
-    //  get
-    //  {
-    //    return this._quantityyToClearSum;
-    //  }
-    //  set
-    //  {
-    //    if ((value != this._quantityyToClearSum))
-    //    {
-    //      this.OnPropertyChanging("QuantityyToClearSum", this._quantityyToClearSum);
-    //      this._quantityyToClearSum = value;
-    //      this.OnPropertyChanged("QuantityyToClearSum");
-    //    }
-    //  }
-    //}
-    ///// <summary>
-    ///// Gets or sets the quantityy automatic clear sum rounded.
-    ///// </summary>
-    ///// <value>
-    ///// The quantityy automatic clear sum rounded.
-    ///// </value>
-    //public double QuantityyToClearSumRounded
-    //{
-    //  get
-    //  {
-    //    return this._quantityyToClearSumRounded;
-    //  }
-    //  set
-    //  {
-    //    if ((value != this._quantityyToClearSumRounded))
-    //    {
-    //      this.OnPropertyChanging("QuantityyToClearSumRounded", this._quantityyToClearSumRounded);
-    //      this._quantityyToClearSumRounded = value;
-    //      this.OnPropertyChanged("QuantityyToClearSumRounded");
-    //    }
-    //  }
-    //}
-    ///// <summary>
-    ///// Gets or sets the remaining configuration stock.
-    ///// </summary>
-    ///// <value>
-    ///// The remaining configuration stock.
-    ///// </value>
-    //public double RemainingOnStock
-    //{
-    //  get
-    //  {
-    //    return this._remainingOnStock;
-    //  }
-    //  set
-    //  {
-    //    if ((value != this._remainingOnStock))
-    //    {
-    //      this.OnPropertyChanging("RemainingOnStock", this._remainingOnStock);
-    //      this._remainingOnStock = value;
-    //      this.OnPropertyChanged("RemainingOnStock");
-    //    }
-    //  }
-    //}
-    ///// <summary>
-    ///// Gets or sets the units.
-    ///// </summary>
-    ///// <value>
-    ///// The units.
-    ///// </value>
-    //public string Units
-    //{
-    //  get
-    //  {
-    //    return this._units;
-    //  }
-    //  set
-    //  {
-    //    if ((value != this._units))
-    //    {
-    //      this.OnPropertyChanging("Units", this._units);
-    //      this._units = value;
-    //      this.OnPropertyChanged("Units");
-    //    }
-    //  }
-    //}
-    ///// <summary>
-    ///// Gets or sets the remaining packages.
-    ///// </summary>
-    ///// <value>
-    ///// The remaining packages.
-    ///// </value>
-    //public double RemainingPackages
-    //{
-    //  get
-    //  {
-    //    return this._remainingPackages;
-    //  }
-    //  set
-    //  {
-    //    if ((value != this._remainingPackages))
-    //    {
-    //      this.OnPropertyChanging("RemainingPackages", this._remainingPackages);
-    //      this._remainingPackages = value;
-    //      this.OnPropertyChanged("RemainingPackages");
-    //    }
-    //  }
-    //}
-    ///// <summary>
-    ///// Gets or sets the packages automatic clear.
-    ///// </summary>
-    ///// <value>
-    ///// The packages automatic clear.
-    ///// </value>
-    //public int PackagesToDispose
-    //{
-    //  get
-    //  {
-    //    return this._packagesToClear;
-    //  }
-    //  set
-    //  {
-    //    if ((value != this._packagesToClear))
-    //    {
-    //      this.OnPropertyChanging("PackagesToDispose", this._packagesToClear);
-    //      this._packagesToClear = value;
-    //      this.OnPropertyChanged("PackagesToDispose");
-    //    }
-    //  }
-    //}
-    ///// <summary>
-    ///// Gets or sets the customs procedure.
-    ///// </summary>
-    ///// <value>
-    ///// The customs procedure.
-    ///// </value>
-    //public string CustomsProcedure
-    //{
-    //  get { return b_customsProcedure; }
-    //  set
-    //  {
-    //    if ((value != this.b_customsProcedure))
-    //    {
-    //      this.OnPropertyChanging("CustomsProcedure", this._packagesToClear);
-    //      this.b_customsProcedure = value;
-    //      this.OnPropertyChanged("CustomsProcedure");
-    //    }
-    //  }
-    //}
+
+    #region public UI properties
+    /// <summary>
+    /// Gets or sets the SKU.
+    /// </summary>
+    /// <value>
+    /// The SKU.
+    /// </value>
+    public string SKU
+    {
+      get
+      {
+        return b_SKU;
+      }
+      set
+      {
+        RaiseHandler<string>(value, ref b_SKU, "SKU", this);
+      }
+    }
+    /// <summary>
+    /// Gets or sets the SKU description.
+    /// </summary>
+    /// <value>
+    /// The SKU description.
+    /// </value>
+    public string SKUDescription
+    {
+      get
+      {
+        return b_SKUDescription;
+      }
+      set
+      {
+        RaiseHandler<string>(value, ref b_SKUDescription, "SKUDescription", this);
+      }
+    }
+    /// <summary>
+    /// Gets or sets the batch.
+    /// </summary>
+    /// <value>
+    /// The batch.
+    /// </value>
+    public string Batch
+    {
+      get
+      {
+        return b_Batch;
+      }
+      set
+      {
+        RaiseHandler<string>(value, ref b_Batch, "Batch", this);
+      }
+    }
+    /// <summary>
+    /// Gets or sets the total stock.
+    /// </summary>
+    /// <value>
+    /// The total stock.
+    /// </value>
+    public double TotalStock
+    {
+      get
+      {
+        return b_TotalStock;
+      }
+      set
+      {
+        RaiseHandler<double>(value, ref b_TotalStock, "TotalStock", this);
+      }
+    }
+    /// <summary>
+    /// Gets or sets the mass per package.
+    /// </summary>
+    /// <value>
+    /// The mass per package.
+    /// </value>
+    public double MassPerPackage
+    {
+      get
+      {
+        return b_MassPerPackage;
+      }
+      set
+      {
+        RaiseHandler<double>(value, ref b_MassPerPackage, "MassPerPackage", this);
+      }
+    }
+    /// <summary>
+    /// Gets or sets the declared net mass.
+    /// </summary>
+    /// <value>
+    /// The declared net mass.
+    /// </value>
+    public double DeclaredNetMass
+    {
+      get
+      {
+        return b_DeclaredNetMass;
+      }
+      set
+      {
+        RaiseHandler<double>(value, ref b_DeclaredNetMass, "DeclaredNetMass", this);
+      }
+    }
+    /// <summary>
+    /// Gets or sets the added kg.
+    /// </summary>
+    /// <value>
+    /// The added kg.
+    /// </value>
+    public double AddedKg
+    {
+      get
+      {
+        return b_AddedKg;
+      }
+      set
+      {
+        RaiseHandler<double>(value, ref b_AddedKg, "AddedKg", this);
+      }
+    }
+    /// <summary>
+    /// Gets or sets the quantity automatic clear sum.
+    /// </summary>
+    /// <value>
+    /// The quantity automatic clear sum.
+    /// </value>
+    public double QuantityyToClearSum
+    {
+      get
+      {
+        return b_QuantityyToClearSum;
+      }
+      set
+      {
+        RaiseHandler<double>(value, ref b_QuantityyToClearSum, "QuantityyToClearSum", this);
+      }
+    }
+    /// <summary>
+    /// Gets or sets the quantity automatic clear sum rounded.
+    /// </summary>
+    /// <value>
+    /// The quantity automatic clear sum rounded.
+    /// </value>
+    public double QuantityyToClearSumRounded
+    {
+      get
+      {
+        return b_QuantityyToClearSumRounded;
+      }
+      set
+      {
+        RaiseHandler<double>(value, ref b_QuantityyToClearSumRounded, "QuantityyToClearSumRounded", this);
+      }
+    }
+    /// <summary>
+    /// Gets or sets the remaining configuration stock.
+    /// </summary>
+    /// <value>
+    /// The remaining configuration stock.
+    /// </value>
+    public double RemainingOnStock
+    {
+      get
+      {
+        return b_RemainingOnStock;
+      }
+      set
+      {
+        RaiseHandler<double>(value, ref b_RemainingOnStock, "RemainingOnStock", this);
+      }
+    }
+    /// <summary>
+    /// Gets or sets the units.
+    /// </summary>
+    /// <value>
+    /// The units.
+    /// </value>
+    public string Units
+    {
+      get
+      {
+        return b_Units;
+      }
+      set
+      {
+        RaiseHandler<string>(value, ref b_Units, "Units", this);
+      }
+    }
+    /// <summary>
+    /// Gets or sets the remaining packages.
+    /// </summary>
+    /// <value>
+    /// The remaining packages.
+    /// </value>
+    public string RemainingPackages
+    {
+      get
+      {
+        return b_RemainingPackages;
+      }
+      set
+      {
+        RaiseHandler<string>(value, ref b_RemainingPackages, "RemainingPackages", this);
+      }
+    }
+    /// <summary>
+    /// Gets or sets the packages automatic clear.
+    /// </summary>
+    /// <value>
+    /// The packages automatic clear.
+    /// </value>
+    public int PackagesToDispose
+    {
+      get
+      {
+        return b_PackagesToDispose;
+      }
+      set
+      {
+        RaiseHandler<int>(value, ref b_PackagesToDispose, "PackagesToDispose", this);
+      }
+    }
+    /// <summary>
+    /// Gets or sets the customs procedure.
+    /// </summary>
+    /// <value>
+    /// The customs procedure.
+    /// </value>
+    public string CustomsProcedure
+    {
+      get
+      {
+        return b_CustomsProcedure;
+      }
+      set
+      {
+        RaiseHandler<string>(value, ref b_CustomsProcedure, "CustomsProcedure", this);
+      }
+    }
+    /// <summary>
+    /// Gets or sets the customs document number.
+    /// </summary>
+    /// <value>
+    /// The customs document number.
+    /// </value>
+    public string DocumentNumber
+    {
+      get
+      {
+        return b_DocumentNumber;
+      }
+      set
+      {
+        RaiseHandler<string>(value, ref b_DocumentNumber, "DocumentNumber", this);
+      }
+    }
+    /// <summary>
+    /// Gets or sets the sequence number.
+    /// </summary>
+    /// <value>
+    /// The sequence number.
+    /// </value>
+    public int SequenceNumber
+    {
+      get
+      {
+        return b_SequenceNumber;
+      }
+      set
+      {
+        RaiseHandler<int>(value, ref b_SequenceNumber, "SequenceNumber", this);
+      }
+    } 
+    #endregion
+
+    #region backing fields
+    private string b_SKU;
+    private string b_SKUDescription;
+    private string b_Batch;
+    private double b_TotalStock;
+    private double b_MassPerPackage;
+    private double b_DeclaredNetMass;
+    private double b_AddedKg;
+    private double b_QuantityyToClearSum;
+    private double b_QuantityyToClearSumRounded;
+    private double b_RemainingOnStock;
+    private string b_Units;
+    private string b_RemainingPackages;
+    private int b_PackagesToDispose;
+    private string b_CustomsProcedure;
+    private string b_DocumentNumber;
+    private int b_SequenceNumber;
+    #endregion
 
   }
 }
