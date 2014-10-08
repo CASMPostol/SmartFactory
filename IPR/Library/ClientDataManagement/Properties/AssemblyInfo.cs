94206007e77e5e56f44df566953ddf8449ff4f8b
Mariusz <mpostol@cas.eu>
2014-10-08 12:23:28 +0000

    Create tag, close this sprint rel. 2.10.00 - deployed version 2.10.08 for testing in the QA environment.

--- IPR/Library/ClientDataManagement/Properties/AssemblyInfo.cs
+++ IPR/Library/ClientDataManagement/Properties/AssemblyInfo.cs
@@ -22,8 +22,8 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("9FDFA43A-CE88-48CF-B1D2-66081FFDE8F8")]
-[assembly: AssemblyVersion("2.10.07.*")]
-[assembly: AssemblyFileVersion("2.10.07")]
+[assembly: AssemblyVersion("2.10.08.*")]
+[assembly: AssemblyFileVersion("2.10.08")]
 [assembly: InternalsVisibleToAttribute("IPRClientEntityTests, PublicKey=0024000004800000940000000602000000240000525341310004000001000100b3cff1bec656a6" +
                                                                        "e3784c75cacce8ee3bc695d0ce135d820c86465eb49b2353f0438d28ad9a458e5350c0c141393d" +
                                                                        "d9ad606bd9f4e0a8e1b0c49fa37835630b16798ece56df81c756f4c6bd37f07d68cc91ebfa7be7" +
