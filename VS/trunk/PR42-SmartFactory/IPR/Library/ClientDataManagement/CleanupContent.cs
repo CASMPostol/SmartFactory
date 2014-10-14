418d3763ebb3ff6f08acbee58fadc884e52f4b62
mpostol <mpostol@8a3894fd-0c01-b944-991b-6a3da02cae8a>
2014-10-14 11:23:31 +0000

    ClientSiteUserInterface - add functionality to archive all lists - added cleanup of Materials, fixed problem with cleanup of Invoice content, fixed problem with Invoice library to Clearance broken lookup.

--- VS/trunk/PR42-SmartFactory/IPR/Library/ClientDataManagement/CleanupContent.cs
+++ VS/trunk/PR42-SmartFactory/IPR/Library/ClientDataManagement/CleanupContent.cs
@@ -45,13 +45,14 @@ namespace CAS.SmartFactory.IPR.Client.DataManagement
         progress(null, new ProgressChangedEventArgs(1, String.Format("There are {0} StockLib entries", _slList.Count)));
         progress(null, new ProgressChangedEventArgs(1, "Buffering StockEntry entries"));
         List<StockEntry> _stockEntryAll = _spedc.StockEntry.ToList<StockEntry>().ToList<StockEntry>();
-        IEnumerable<StockEntry> _seToBeDeleted = _stockEntryAll.Where<StockEntry>(x => x.StockLibraryIndex == null);
+        List<StockEntry> _seToBeDeleted = _stockEntryAll.Where<StockEntry>(x => x.StockLibraryIndex == null).ToList<StockEntry>();
         //Delete not associated StockEntries
         progress(null, new ProgressChangedEventArgs(1, String.Format("There are {0} StockEntry to be deleted because they are not associated with StockLib.", _seToBeDeleted.Count<StockEntry>())));
         _spedc.StockEntry.Delete<StockEntry, NSLinq2SQL.History>
           (_seToBeDeleted, null, x => _sqledc.StockEntry.GetAt<NSLinq2SQL.StockEntry>(x), (id, listName) => _sqledc.ArchivingLogs.AddLog(id, listName, Extensions.UserName()),
             siteURL, x => _sqledc.History.AddHistoryEntry(x));
         Link2SQLExtensions.SubmitChanges(_spedc, _sqledc, progress);
+        _seToBeDeleted.Clear();
         //Delete StockLib and associated StockEntry
         List<StockLib> _slToBeDeleted = new List<StockLib>();
         foreach (StockLib _selX in _slList)
@@ -59,10 +60,9 @@ namespace CAS.SmartFactory.IPR.Client.DataManagement
           if (_selX.Stock2JSOXLibraryIndex != null)
             continue;
           _slToBeDeleted.Add(_selX);
-          _seToBeDeleted = _stockEntryAll.Where(x => x.StockLibraryIndex == _selX);
+          _seToBeDeleted.AddRange(_stockEntryAll.Where(x => x.StockLibraryIndex == _selX));
         }
-        int _entries = _seToBeDeleted.Count<StockEntry>();
-        progress(null, new ProgressChangedEventArgs(1, String.Format("To be deleted {0} StockLib and associated {1} StockEntry entries.", _slToBeDeleted.Count, _entries)));
+        progress(null, new ProgressChangedEventArgs(1, String.Format("To be deleted {0} StockLib and associated {1} StockEntry entries.", _slToBeDeleted.Count, _seToBeDeleted.Count())));
         _spedc.StockEntry.Delete<StockEntry, NSLinq2SQL.History>
           (_seToBeDeleted, null, x => _sqledc.StockEntry.GetAt<NSLinq2SQL.StockEntry>(x), (id, listName) => _sqledc.ArchivingLogs.AddLog(id, listName, Extensions.UserName()),
            siteURL, x => _sqledc.History.AddHistoryEntry(x));
@@ -72,6 +72,18 @@ namespace CAS.SmartFactory.IPR.Client.DataManagement
         Link2SQLExtensions.SubmitChanges(_spedc, _sqledc, progress);
         //Update Activities Log
         Linq2SQL.ArchivingOperationLogs.UpdateActivitiesLogs(_sqledc, Linq2SQL.ArchivingOperationLogs.OperationName.Cleanup, progress);
+      }
+      //Delete all not associated BatchMaterial
+      using (Entities _spedc = new Entities(siteURL))
+      {
+        progress(null, new ProgressChangedEventArgs(1, "Buffering Material entries"));
+        List<Material> _mtrl2BeDeleted = _spedc.Material.ToList<Material>();
+        _mtrl2BeDeleted = _mtrl2BeDeleted.Where<Material>(x => x.Material2BatchIndex == null && x.Disposal.ToList<Disposal>().Count == 0).ToList<Material>();
+        progress(null, new ProgressChangedEventArgs(1, String.Format("There are {0} Material entries to be deleted because they are not associated with Batch and Disposal.", _mtrl2BeDeleted.Count())));
+        _spedc.Material.Delete<Material, NSLinq2SQL.History>
+          (_mtrl2BeDeleted, null, x => _sqledc.Material.GetAt<NSLinq2SQL.Material>(x), (id, listName) => _sqledc.ArchivingLogs.AddLog(id, listName, Extensions.UserName()),
+           siteURL, x => _sqledc.History.AddHistoryEntry(x));
+        Link2SQLExtensions.SubmitChanges(_spedc, _sqledc, progress);
       }
       progress(null, new ProgressChangedEventArgs(1, "Finished Cleanup Content Operation."));
     }
