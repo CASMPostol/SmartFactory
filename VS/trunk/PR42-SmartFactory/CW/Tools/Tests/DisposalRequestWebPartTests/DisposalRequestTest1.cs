6a481ae8c03c82038aab46f1d3bdf5d5b64841b4
mpostol <mpostol@8a3894fd-0c01-b944-991b-6a3da02cae8a>
2014-10-17 13:08:34 +0000

    Interface for CW - FIFO exception - DisposalRequestDetails - added project to run unit tests.

---
+++ VS/trunk/PR42-SmartFactory/CW/Tools/Tests/DisposalRequestWebPartTests/DisposalRequestTest1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CAS.SmartFactory.CW.Dashboards.DisposalRequestWebPart.Linq;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CAS.SmartFactory.CW.Dashboards.DisposalRequestWebPart.Test
+{
+  [TestClass]
+  public class DisposalRequestTest1
+  {
+    private static List<CustomsWarehouse> listOfAccounts = new List<CustomsWarehouse>();
+    private static List<CustomsWarehouseDisposal> listOfDisposals = new List<CustomsWarehouseDisposal>();
+    private static IEnumerable<IGrouping<string, CustomsWarehouseDisposal>> groupOfDisposals = null;
+
+    [ClassInitialize]
+    public static void ClassInitializeMethod(TestContext context)
+    {
+      groupOfDisposals = listOfDisposals.GroupBy<CustomsWarehouseDisposal, string>(x => x.CWL_CWDisposal2CustomsWarehouseID.Batch);
+    }
+
+    [TestMethod]
+    [ExpectedException(typeof(InvalidOperationException))]
+    public void CreateDisposalRequestInstanceWrongDataTest()
+    {
+    List<CustomsWarehouse> _listOfAccounts = new List<CustomsWarehouse>();
+    List<CustomsWarehouseDisposal> _listOfDisposals = new List<CustomsWarehouseDisposal>();
+    IEnumerable<IGrouping<string, CustomsWarehouseDisposal>> groupOfDisposals = null;
+    groupOfDisposals = _listOfDisposals.GroupBy<CustomsWarehouseDisposal, string>(x => x.CWL_CWDisposal2CustomsWarehouseID.Batch);
+    DisposalRequest _newItem = DisposalRequest.Create(_listOfAccounts, groupOfDisposals.First());
+
+    }
+  }
+}
---
+++ VS/trunk/PR42-SmartFactory/CW/WebsiteApplication/Silverlight/DisposalRequestWebPart/CommonDefinition.cs
@@ -0,0 +1,28 @@
+﻿//<summary>
+//  Title   : class CommonDefinition
+//  System  : Microsoft Visual C# .NET 2012
+//  $LastChangedDate: 2013-10-07 12:20:37 +0200 (Mon, 07 Oct 2013) $
+//  $Rev: 9810 $
+//  $LastChangedBy: mpostol $
+//  $URL: svn://svnserver.hq.cas.com.pl/VS/trunk/PR42-SmartFactory/CW/WebsiteApplication/Silverlight/DisposalRequestWebPart/CommonDefinition.tt $
+//  $Id: CommonDefinition.tt 9810 2013-10-07 10:20:37Z mpostol $
+//
+//  Copyright (C) 2013, CAS LODZ POLAND.
+//  TEL: +48 (42) 686 25 47
+//  mailto://techsupp@cas.eu
+//  http://www.cas.eu
+//</summary>
+
+namespace CAS.SmartFactory.CW.Dashboards.DisposalRequestWebPart
+{
+  /// <summary>
+  /// CommonDefinition for multiproject environment
+  /// </summary>
+  internal partial class CommonDefinition
+  {
+    internal const string CustomsWarehouseDisposalTitle = "Customs Warehouse Disposal";
+    internal const string DisposalRequestLibraryTitle = "Disposal Request Library";
+    internal const string HiddenFieldDataParameterName = "controlId";
+    internal const string ColumnNameId = "ID";
+  }
+}
