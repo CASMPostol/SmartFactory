caf9db6e8a6345cd472407020b09c0db6cdb054c
mpostol <mpostol@8a3894fd-0c01-b944-991b-6a3da02cae8a>
2014-10-17 13:19:43 +0000

    Prepare testing data - created placeholder for the test data - the data must be added.

--- VS/trunk/PR42-SmartFactory/CW/Tools/Tests/DisposalRequestWebPartTests/DisposalRequestTest1.cs
+++ VS/trunk/PR42-SmartFactory/CW/Tools/Tests/DisposalRequestWebPartTests/DisposalRequestTest1.cs
@@ -17,18 +17,32 @@ namespace CAS.SmartFactory.CW.Dashboards.DisposalRequestWebPart.Test
     public static void ClassInitializeMethod(TestContext context)
     {
       groupOfDisposals = listOfDisposals.GroupBy<CustomsWarehouseDisposal, string>(x => x.CWL_CWDisposal2CustomsWarehouseID.Batch);
+      CustomsWarehouse _newCW = new CustomsWarehouse()
+      {
+        //TODO
+      };
+      listOfAccounts.Add(_newCW);
+      CustomsWarehouseDisposal _newCWD = new CustomsWarehouseDisposal()
+      {
+        CWL_CWDisposal2CustomsWarehouseID = _newCW,
+        //TODO
+      };
+      listOfDisposals.Add(_newCWD);
     }
 
     [TestMethod]
     [ExpectedException(typeof(InvalidOperationException))]
     public void CreateDisposalRequestInstanceWrongDataTest()
     {
-    List<CustomsWarehouse> _listOfAccounts = new List<CustomsWarehouse>();
-    List<CustomsWarehouseDisposal> _listOfDisposals = new List<CustomsWarehouseDisposal>();
-    IEnumerable<IGrouping<string, CustomsWarehouseDisposal>> groupOfDisposals = null;
-    groupOfDisposals = _listOfDisposals.GroupBy<CustomsWarehouseDisposal, string>(x => x.CWL_CWDisposal2CustomsWarehouseID.Batch);
-    DisposalRequest _newItem = DisposalRequest.Create(_listOfAccounts, groupOfDisposals.First());
-
+      List<CustomsWarehouse> _listOfAccounts = new List<CustomsWarehouse>();
+      List<CustomsWarehouseDisposal> _listOfDisposals = new List<CustomsWarehouseDisposal>();
+      IEnumerable<IGrouping<string, CustomsWarehouseDisposal>> _groupOfDisposals = _listOfDisposals.GroupBy<CustomsWarehouseDisposal, string>(x => x.CWL_CWDisposal2CustomsWarehouseID.Batch);
+      DisposalRequest _newItem = DisposalRequest.Create(_listOfAccounts, _groupOfDisposals.First());
+    }
+    [TestMethod]
+    public void CreateDisposalRequestInstanceDemoDataTest()
+    {
+      DisposalRequest _newItem = DisposalRequest.Create(listOfAccounts, groupOfDisposals.First());
     }
   }
 }
