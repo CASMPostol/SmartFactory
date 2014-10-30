0941d64f953576007875ce009d5683f9c1e36bc2
bwojciechowski <bwojciechowski@8a3894fd-0c01-b944-991b-6a3da02cae8a>
2014-10-30 08:10:14 +0000

    Data model development

--- IPR/WebsiteApplication/IPRWebsiteModel/Linq/Scripts/GetEntities.ps1
+++ IPR/WebsiteApplication/IPRWebsiteModel/Linq/Scripts/GetEntities.ps1
@@ -1,5 +1,5 @@
 ﻿Write-Host "This file creates all the entities classes for the selected site"
 $env:path += "; C:\Program Files\Common Files\microsoft shared\Web Server Extensions\14\BIN"
 Get-Location | Write-host
-SPMetal.exe /web:http://casbw/sites/iprmodel /language:csharp /namespace:CAS.SmartFactory.IPR.WebsiteModel.Linq /parameters:Parameters.xml /code:..\Entities.cs| write-host
+SPMetal.exe /web:http://casbw:11224/sites/iprmodel /language:csharp /namespace:CAS.SmartFactory.IPR.WebsiteModel.Linq /parameters:Parameters.xml /code:..\Entities.cs| write-host
 write-host "Done . "
