c25e98e466db11a9cc23fade16cd806168de39a6
mpostol <mpostol@8a3894fd-0c01-b944-991b-6a3da02cae8a>
2014-11-03 10:13:47 +0000

    Create tag, close this sprint rel. 2.10.00 - changed tag version.

--- VS/trunk/PR42-SmartFactory/Scripts/CreateTag.cmd
+++ VS/trunk/PR42-SmartFactory/Scripts/CreateTag.cmd
@@ -7,8 +7,8 @@ rem//  $Id$
 
 
 set branchtype=tags
-set TagFolder=Rel_SHR_2_20_10
-set TagPath=svn://svnserver.hq.cas.com.pl/VS/%branchtype%/SmartFactory/rel_SHR_2_20_10
+set TagFolder=Rel_IPR_2_20_20
+set TagPath=svn://svnserver.hq.cas.com.pl/VS/%branchtype%/SmartFactory/rel_IPR_2_20_20
 set trunkPath=svn://svnserver.hq.cas.com.pl/VS/trunk
 
 svn mkdir %TagPath%  -m "created new %TagPath% (in %branchtype% folder)"
