d3794b888fd8e9475a91a48488a14e3352c2a3a5
mpostol <mpostol@8a3894fd-0c01-b944-991b-6a3da02cae8a>
2014-11-03 07:21:29 +0000

    Updated code to new data entities definition. - fixed all compiler errors. - added missed file.

---
+++ VS/trunk/PR42-SmartFactory/IPR/WebsiteApplication/IPRWebsiteModel/Linq/InvoiceLib.cs
@@ -0,0 +1,36 @@
+﻿//<summary>
+//  Title   : InvoiceLib
+//  System  : Microsoft VisulaStudio 2013 / C#
+//  $LastChangedDate:$
+//  $Rev:$
+//  $LastChangedBy:$
+//  $URL:$
+//  $Id:$
+//
+//  Copyright (C) 2014, CAS LODZ POLAND.
+//  TEL: +48 (42) 686 25 47
+//  mailto://techsupp@cas.eu
+//  http://www.cas.eu
+//</summary>
+
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CAS.SmartFactory.IPR.WebsiteModel.Linq
+{
+  /// <summary>
+  /// InvoiceLib Entity
+  /// </summary>
+  public partial class InvoiceLib
+  {
+    /// <summary>
+    /// Reverse lookup returning associated <see cref="InvoiceContent"/> collection.
+    /// </summary>
+    /// <param name="entities">The entities.</param>
+    /// <returns></returns>
+    public IEnumerable<InvoiceContent> InvoiceContent(Entities entities)
+    {
+      return entities.InvoiceContent.Where<InvoiceContent>(x => x.InvoiceIndex == this);
+    }
+  }
+}
