fecbe1933b7b67d440765ee13ccc6c14e93b05e1
bwojciechowski <bwojciechowski@8a3894fd-0c01-b944-991b-6a3da02cae8a>
2014-11-04 13:29:38 +0000

    CW Data model development - removing all reversed lookup - data model improvement

--- VS/trunk/PR42-SmartFactory/CW/WebsiteApplication/CWWebsiteModel/Linq/Scripts/GetEntities.ps1
+++ VS/trunk/PR42-SmartFactory/CW/WebsiteApplication/CWWebsiteModel/Linq/Scripts/GetEntities.ps1
@@ -1,5 +1,5 @@
 ﻿Write-Host "This file creates all the entities classes for the selected site"
 $env:path += "; C:\Program Files\Common Files\Microsoft Shared\Web server extensions\14\Bin"
 Get-Location | Write-host
-SPMetal.exe /web:http://cas_sp:11225/sites/cw53 /language:csharp /namespace:CAS.SmartFactory.CW.WebsiteModel.Linq /parameters:Parameters.xml /code:..\Entities.cs| write-host
+SPMetal.exe /web:http://casbw:11224/sites/iprmodel /language:csharp /namespace:CAS.SmartFactory.CW.WebsiteModel.Linq /parameters:Parameters.xml /code:..\Entities.cs| write-host
 write-host "Done . "
