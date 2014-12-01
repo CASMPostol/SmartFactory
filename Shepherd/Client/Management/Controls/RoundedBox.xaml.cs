af51e455e2ec16f30596a1b165c3fde51863d224
Mariusz <mpostol@cas.eu>
2014-12-01 14:38:46 +0000

    SHR Client Implement state machine for the user interface - Shell development - added code and resources - in progress.

---
+++ Shepherd/Client/Management/Controls/RoundedBox.xaml.cs
@@ -0,0 +1,33 @@
+//<summary>
+//  Title   : RoundedBox
+//  System  : Microsoft VisulaStudio 2013 / C#
+//  $LastChangedDate:$
+//  $Rev:$
+//  $LastChangedBy:$
+//  $URL:$
+//  $Id:$
+//
+//  Copyright (C) 2014, CAS LODZ POLAND.
+//  TEL: +48 (42) 686 25 47
+//  mailto://techsupp@cas.eu
+//  http://www.cas.eu
+//</summary>
+
+using System.Windows.Controls;
+
+namespace CAS.SmartFactory.Shepherd.Client.Management.Controls
+{
+  /// <summary>
+  /// Interaction logic for RoundedBox.xaml
+  /// </summary>
+  public partial class RoundedBox : UserControl
+  {
+    /// <summary>
+    /// Initializes a new instance of the <see cref="RoundedBox"/> class.
+    /// </summary>
+    public RoundedBox()
+    {
+      InitializeComponent();
+    }
+  }
+}
