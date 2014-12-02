7d693b3da84e58918f67331ccd45500dfebe3e3d
mpostol <mpostol@8a3894fd-0c01-b944-991b-6a3da02cae8a>
2014-12-02 16:02:04 +0000

    SHR Client Implement state machine for the user interface - Shell development - in progress.

---
+++ VS/trunk/PR42-SmartFactory/Shepherd/Client/Management/ApplicationIBootstrapper.Desktop.cs
@@ -0,0 +1,34 @@
+﻿//<summary>
+//  Title   : ApplicationIBootstrapper - adopted from StockTraderRI
+//  System  : Microsoft VisulaStudio 2013 / C#
+//  $LastChangedDate:$
+//  $Rev:$
+//  $LastChangedBy:$
+//  $URL:$
+//  $Id:$
+//
+//  Copyright (C) 2014, CAS LODZ POLAND.
+//  TEL: +48 (42) 686 25 47
+//  mailto://techsupp@cas.eu
+//  http://www.cas.eu
+//</summary>
+      
+using Microsoft.Practices.Prism.Logging;
+using Microsoft.Practices.Prism.MefExtensions;
+
+namespace CAS.SmartFactory.Shepherd.Client.Management
+{
+  /// <summary>
+  /// ApplicationIBootstrapper part that defines the logger 
+  /// </summary>
+  internal partial class ApplicationIBootstrapper 
+    {
+      private readonly EnterpriseLibraryLoggerAdapter _logger = new EnterpriseLibraryLoggerAdapter();
+
+      protected override ILoggerFacade CreateLogger()
+      {
+        return _logger;
+      }
+
+    }
+}
---
+++ VS/trunk/PR42-SmartFactory/Shepherd/Client/Management/ApplicationIBootstrapper.cs
@@ -0,0 +1,48 @@
+﻿using CAS.SmartFactory.Shepherd.Client.Management.Infrastructure.Behaviors;
+using Microsoft.Practices.Prism.MefExtensions;
+using System.ComponentModel.Composition.Hosting;
+using System.Windows;
+
+namespace CAS.SmartFactory.Shepherd.Client.Management
+{
+  internal partial class ApplicationIBootstrapper : MefBootstrapper
+  {
+    protected override void ConfigureAggregateCatalog()
+    {
+      this.AggregateCatalog.Catalogs.Add(new AssemblyCatalog(typeof(ApplicationIBootstrapper).Assembly));
+      //this.AggregateCatalog.Catalogs.Add(new AssemblyCatalog(typeof(StockTraderRICommands).Assembly));
+      //this.AggregateCatalog.Catalogs.Add(new AssemblyCatalog(typeof(MarketModule).Assembly));
+      //this.AggregateCatalog.Catalogs.Add(new AssemblyCatalog(typeof(PositionModule).Assembly));
+      //this.AggregateCatalog.Catalogs.Add(new AssemblyCatalog(typeof(WatchModule).Assembly));
+      //this.AggregateCatalog.Catalogs.Add(new AssemblyCatalog(typeof(NewsModule).Assembly));
+    }
+
+    protected override void ConfigureContainer()
+    {
+      base.ConfigureContainer();
+    }
+
+    protected override void InitializeShell()
+    {
+      base.InitializeShell();
+
+      Application.Current.MainWindow = (Shell)this.Shell;
+      Application.Current.MainWindow.Show();
+    }
+
+    protected override Microsoft.Practices.Prism.Regions.IRegionBehaviorFactory ConfigureDefaultRegionBehaviors()
+    {
+      var factory = base.ConfigureDefaultRegionBehaviors();
+
+      factory.AddIfMissing("AutoPopulateExportedViewsBehavior", typeof(AutoPopulateExportedViewsBehavior));
+
+      return factory;
+    }
+
+    protected override DependencyObject CreateShell()
+    {
+      return this.Container.GetExportedValue<Shell>();
+    }
+
+  }
+}
