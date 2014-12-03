1b4c31fc00ec43f57cbbd7d28467e4974e693360
bwojciechowski <bwojciechowski@8a3894fd-0c01-b944-991b-6a3da02cae8a>
2014-12-03 11:41:32 +0000

    Shepherd Client - generate entities. - entities added.

---
+++ Shepherd/Client/Management/Entities/Scripts/GetEntities.ps1
@@ -0,0 +1,8 @@
+﻿Write-Host "This file creates all the entities classes for the selected site"
+$env:path += "; C:\Program Files\Common Files\microsoft shared\Web Server Extensions\14\BIN"
+Get-Location | Write-host
+SPMetal.exe /web:http://casbw/sites/shepherd /parameters:Parameters.xml /language:csharp /namespace:CAS.SmartFactory.Shepherd.Client.Management /code:..\Entities.cs| write-host
+write-host "Done . "
+
+
+
