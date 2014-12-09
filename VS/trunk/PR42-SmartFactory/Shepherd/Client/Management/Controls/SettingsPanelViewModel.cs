4f107b68d35036ddd45f4640bfe38246a4d69d28
mpostol <mpostol@8a3894fd-0c01-b944-991b-6a3da02cae8a>
2014-12-09 14:20:47 +0000

    SHR Client Implement state machine for the user interface - Shell development - development in progress - modified CASCommon - new version created.

--- VS/trunk/PR42-SmartFactory/Shepherd/Client/Management/Controls/SettingsPanelViewModel.cs
+++ VS/trunk/PR42-SmartFactory/Shepherd/Client/Management/Controls/SettingsPanelViewModel.cs
@@ -1,11 +1,27 @@
-﻿using CAS.Common.ComponentModel;
+﻿//<summary>
+//  Title   : SettingsPanelViewModel
+//  System  : Microsoft VisualStudio 2013 / C#
+//  $LastChangedDate:$
+//  $Rev:$
+//  $LastChangedBy:$
+//  $URL:$
+//  $Id:$
+//
+//  Copyright (C) 2014, CAS LODZ POLAND.
+//  TEL: +48 (42) 686 25 47
+//  mailto://techsupp@cas.eu
+//  http://www.cas.eu
+//</summary>
+
+using CAS.Common.ComponentModel;
+using CAS.Common.ViewModel.Wizard;
 using CAS.SmartFactory.Shepherd.Client.Management.StateMachines;
 using System.ComponentModel.Composition;
 
 namespace CAS.SmartFactory.Shepherd.Client.Management.Controls
 {
   [Export]
-  public class SettingsPanelViewModel: PropertyChangedBase
+  public class SettingsPanelViewModel: PropertyChangedBase, IViewModelContext
   {
     public SettingsPanelViewModel()
     {
@@ -44,18 +60,27 @@ namespace CAS.SmartFactory.Shepherd.Client.Management.Controls
         RaiseHandler<string>(value, ref b_SQLServer, "SQLServer", this);
       }
     }
+    /// <summary>
+    /// Sets the master shell view model. It activates the state <see cref="SetupDataDialogMachine"/>
+    /// </summary>
+    /// <value>The master shell view model <see cref="ShellViewModel"/>.</value>
     [Import]
     public ShellViewModel MasterShellViewModel
     {
       set
       {
-        value.EnterState<SetupDataDialogMachine>();
+        SetupDataDialogMachineLocal _myState =  value.EnterState<SetupDataDialogMachineLocal>(this);
       }
     }
     //vars
     private string b_URL = string.Empty;
     private string b_DatabaseName = string.Empty;
     private string b_SQLServer = string.Empty;
+    private class SetupDataDialogMachineLocal : SetupDataDialogMachine<SettingsPanelViewModel>
+    {
+      
+
+    }
     //methods
     private void RestoreSettings()
     {
