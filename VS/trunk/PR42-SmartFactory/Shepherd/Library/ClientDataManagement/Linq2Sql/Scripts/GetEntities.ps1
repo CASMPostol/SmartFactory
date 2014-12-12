3e09a36f3e18fc51e4ce0d4cc3c7c0e638dc733a
bwojciechowski <bwojciechowski@8a3894fd-0c01-b944-991b-6a3da02cae8a>
2014-12-12 13:17:07 +0000

    Shepherd Management Client - Linq2Sql entities - missing fields added, entities updated.

--- VS/trunk/PR42-SmartFactory/Shepherd/Library/ClientDataManagement/Linq2Sql/Scripts/GetEntities.ps1
+++ VS/trunk/PR42-SmartFactory/Shepherd/Library/ClientDataManagement/Linq2Sql/Scripts/GetEntities.ps1
@@ -1,5 +1,5 @@
 ﻿Write-Host "This file creates all the entities classes for the selected SQL database"
 $env:path += "; C:\Program Files (x86)\Microsoft SDKs\Windows\v7.0A\Bin"
 Get-Location | Write-host
-SQLMetal.exe /server:CASBW /database:SHRARCHIVE /namespace:CAS.SmartFactory.Shepherd.Client.Management.Linq2SQL /code:..\Entities.cs /language:csharp /context:SHRARCHIVE | write-host
+SQLMetal.exe /server:CASBW /database:SHRARCHIVE /namespace:CAS.SmartFactory.Shepherd.Client.DataManagement.Linq2SQL /code:..\Entities.cs /language:csharp /context:SHRARCHIVE | write-host
 write-host "Done . "
