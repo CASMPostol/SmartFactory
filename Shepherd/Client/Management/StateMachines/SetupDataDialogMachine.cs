0d8b3de3d45ad4989b3fe323e186c34bf5f13b65
Mariusz <mpostol@cas.eu>
2014-12-15 16:20:46 +0000

    Implement SetupDataDialogMachine - added connectivity check functionality.

--- Shepherd/Client/Management/StateMachines/SetupDataDialogMachine.cs
+++ Shepherd/Client/Management/StateMachines/SetupDataDialogMachine.cs
@@ -15,6 +15,9 @@
 
 using CAS.Common.ViewModel.Wizard;
 using CAS.Common.ViewModel.Wizard.ButtonsPanelStateTemplates;
+using CAS.SmartFactory.Shepherd.Client.DataManagement.Linq2SQL;
+using NsSPLinq = CAS.SmartFactory.Shepherd.Client.DataManagement.Linq;
+using CAS.SmartFactory.Shepherd.Client.Management.Services;
 using System;
 using System.ComponentModel;
 using System.Data.SqlClient;
@@ -41,7 +44,7 @@ namespace CAS.SmartFactory.Shepherd.Client.Management.StateMachines
       m_ButtonsTemplate.LeftButtonVisibility = System.Windows.Visibility.Visible;
       m_ButtonsTemplate.LeftMiddleButtonTitle = "Archive";
       m_ButtonsTemplate.LeftMiddleButtonVisibility = System.Windows.Visibility.Visible;
-      m_StateMachineActionsArray = new Action<object>[4]; 
+      m_StateMachineActionsArray = new Action<object>[4];
       m_StateMachineActionsArray[m_ButtonsTemplate.ConnectPosition] = x => this.OnConnectCommand();
       m_StateMachineActionsArray[m_ButtonsTemplate.CancelPosition] = x => this.OnCancellation();
       m_StateMachineActionsArray[(int)StateMachineEventIndex.LeftButtonEvent] = x => this.OnRouteEditCommand();
@@ -93,23 +96,21 @@ namespace CAS.SmartFactory.Shepherd.Client.Management.StateMachines
     {
       e.Cancel = false;
       e.Result = null;
-      //string _connectionString = this.GetConnectionString();
-      //ReportProgress(this, new ProgressChangedEventArgs(1, String.Format("Connection string {0}", _connectionString)));
-      //System.Data.IDbConnection _connection = new SqlConnection(_connectionString);
-      //IPRDEV _entities = new IPRDEV(_connection);
-      //WorkerReturnData m_WorkerReturnData = new WorkerReturnData();
-      //if (_entities.DatabaseExists())
-      //{
-      //  ReportProgress(this, new ProgressChangedEventArgs(1, "The specified database exists."));
-      //  GetLastOperation(_entities, ArchivingOperationLogs.OperationName.Cleanup, ref m_WorkerReturnData.CleanupLastRunBy, ref m_WorkerReturnData.CleanupLastRunDate);
-      //  GetLastOperation(_entities, ArchivingOperationLogs.OperationName.Synchronization, ref m_WorkerReturnData.SyncLastRunBy, ref m_WorkerReturnData.SyncLastRunDate);
-      //  GetLastOperation(_entities, ArchivingOperationLogs.OperationName.Archiving, ref m_WorkerReturnData.ArchivingLastRunBy, ref m_WorkerReturnData.ArchivingLastRunDate);
-      //}
-      //else
-      //  ReportProgress(this, new ProgressChangedEventArgs(1, "The specified database cannot be opened."));
-      //NsSPLinq.Settings.GetCurrentContentVersion(Properties.Settings.Default.SiteURL, ref m_WorkerReturnData.CurrentContentVersion, ReportProgress);
-      //ReportProgress(this, new ProgressChangedEventArgs(1, String.Format("The current version of the site content is: {0}", m_WorkerReturnData.CurrentContentVersion.ToString())));
-      //e.Result = m_WorkerReturnData;
+      string _connectionString = this.GetConnectionString();
+      ReportProgress(this, new ProgressChangedEventArgs(1, String.Format("Connection string {0}", _connectionString)));
+      System.Data.IDbConnection _connection = new SqlConnection(_connectionString);
+      SHRARCHIVE _entities = new SHRARCHIVE(_connection);
+      if (_entities.DatabaseExists())
+      {
+        ReportProgress(this, new ProgressChangedEventArgs(1, "The specified database exists."));
+        GetLastOperation(_entities, ArchivingOperationLogs.OperationName.Cleanup, x => ConnectionData.ThisInstance.CleanupLastRunBy = x, x => ConnectionData.ThisInstance.CleanupLastRunDate = x);
+        GetLastOperation(_entities, ArchivingOperationLogs.OperationName.Synchronization, x => ConnectionData.ThisInstance.SyncLastRunBy = x, x => ConnectionData.ThisInstance.SyncLastRunDate = x);
+        GetLastOperation(_entities, ArchivingOperationLogs.OperationName.Archiving, x => ConnectionData.ThisInstance.ArchivingLastRunBy = x, x => ConnectionData.ThisInstance.ArchivingLastRunDate);
+        ConnectionData.ThisInstance.SQLConnected = true;
+      }
+      else
+        ReportProgress(this, new ProgressChangedEventArgs(1, "The specified database cannot be opened."));
+      ConnectionData.ThisInstance.SPConnected = NsSPLinq.Connectivity.TestConnection(SharePointServerURL, x => ReportProgress(this, x));
     }
     /// <summary>
     /// Called when worker task has been completed.
@@ -138,7 +139,7 @@ namespace CAS.SmartFactory.Shepherd.Client.Management.StateMachines
     /// Gets the URL of the SharePoint application website.
     /// </summary>
     /// <value>The URL.</value>
-    protected abstract string URL { get; }
+    protected abstract string SharePointServerURL { get; }
     /// <summary>
     /// Gets the name of the SQL database containing backup of SharePoint application data content.
     /// </summary>
@@ -150,7 +151,7 @@ namespace CAS.SmartFactory.Shepherd.Client.Management.StateMachines
     /// <value>The SQL server address.</value>
     protected abstract string SQLServer { get; }
     #endregion
-    
+
     #region private
     private void OnConnectCommand()
     {
@@ -164,7 +165,20 @@ namespace CAS.SmartFactory.Shepherd.Client.Management.StateMachines
     {
       throw new NotImplementedException();
     }
-
+    private static void GetLastOperation(SHRARCHIVE entities, ArchivingOperationLogs.OperationName operationName, Func<string, string> RunBy, Func<string, string> RunDate)
+    {
+      ArchivingOperationLogs _recentActions = ArchivingOperationLogs.GetRecentActions(entities, operationName);
+      if (_recentActions != null)
+      {
+        RunBy(_recentActions.UserName);
+        RunDate(_recentActions.Date.ToString("G"));
+      }
+      else
+      {
+        RunBy(Properties.Settings.Default.RunByUnknown);
+        RunDate(Properties.Settings.Default.RunDateUnknown);
+      }
+    }
     private ConnectCancelTemplate m_ButtonsTemplate = null;
     private Action<object>[] m_StateMachineActionsArray;
     private string GetConnectionString()
