eff7badb8a81fa8f6ba6511e7f5960e6292d2df0
mpostol <mpostol@8a3894fd-0c01-b944-991b-6a3da02cae8a>
2014-12-15 11:56:07 +0000

    SHR Client Implement States Navigations - buttons usage tests - issue: buttons functionality must be changed

--- VS/trunk/PR42-SmartFactory/Shepherd/Client/Management/StateMachines/SetupDataDialogMachine.cs
+++ VS/trunk/PR42-SmartFactory/Shepherd/Client/Management/StateMachines/SetupDataDialogMachine.cs
@@ -16,6 +16,8 @@
 using CAS.Common.ViewModel.Wizard;
 using CAS.Common.ViewModel.Wizard.ButtonsPanelStateTemplates;
 using System;
+using System.ComponentModel;
+using System.Data.SqlClient;
 
 /// <summary>
 /// The StateMachines namespace.
@@ -34,15 +36,16 @@ namespace CAS.SmartFactory.Shepherd.Client.Management.StateMachines
     /// </summary>
     public SetupDataDialogMachine()
     {
+      m_ButtonsTemplate = new ConnectCancelTemplate();
+      m_ButtonsTemplate.LeftButtonTitle = "Route edit";
+      m_ButtonsTemplate.LeftButtonVisibility = System.Windows.Visibility.Visible;
+      m_ButtonsTemplate.LeftMiddleButtonTitle = "Archive";
+      m_ButtonsTemplate.LeftMiddleButtonVisibility = System.Windows.Visibility.Visible;
       m_StateMachineActionsArray = new Action<object>[4]; 
-      //{
-      //  x => throw new NotImplementedException("This action is not handled")
-      //}
-          //{ x => m_ActionExecuted[(int)StateMachineEventIndex.LeftButtonEvent] = true , 
-          //  x => m_ActionExecuted[(int)StateMachineEventIndex.LeftMiddleButtonEvent] = true ,
-          //  x => m_ActionExecuted[(int)StateMachineEventIndex.RightMiddleButtonEvent] = true ,
-          //  x => m_ActionExecuted[(int)StateMachineEventIndex.RightButtonEvent] = true 
-          //};
+      m_StateMachineActionsArray[m_ButtonsTemplate.ConnectPosition] = x => this.OnConnectCommand();
+      m_StateMachineActionsArray[m_ButtonsTemplate.CancelPosition] = x => this.OnCancellation();
+      m_StateMachineActionsArray[(int)StateMachineEventIndex.LeftButtonEvent] = x => this.OnRouteEditCommand();
+      m_StateMachineActionsArray[(int)StateMachineEventIndex.LeftMiddleButtonEvent] = x => this.OnArchiveCommand();
     }
 
     #region IAbstractMachineState
@@ -53,9 +56,8 @@ namespace CAS.SmartFactory.Shepherd.Client.Management.StateMachines
     /// <exception cref="System.NotImplementedException"></exception>
     public override Action<object>[] StateMachineActionsArray
     {
-      get { throw new System.NotImplementedException(); }
+      get { return m_StateMachineActionsArray; }
     }
-
     #endregion
 
     /// <summary>
@@ -64,8 +66,22 @@ namespace CAS.SmartFactory.Shepherd.Client.Management.StateMachines
     public override void OnEnteringState()
     {
       base.OnEnteringState();
+      Context.EnabledEvents = StateMachineEvents.RightButtonEvent | StateMachineEvents.RightMiddleButtonEvent;
     }
-
+    public override void OnCancellation()
+    {
+      base.OnCancellation();
+    }
+    public override void OnException(Exception exception)
+    {
+      base.OnException(exception);
+      m_ButtonsTemplate.OnlyTemplate();
+    }
+    public override void OnExitingState()
+    {
+      base.OnExitingState();
+      //Context.SaveSettings();
+    }
     #region BackgroundWorkerMachine
     /// <summary>
     /// Handles the DoWork event of the BackgroundWorker control.
@@ -75,7 +91,25 @@ namespace CAS.SmartFactory.Shepherd.Client.Management.StateMachines
     /// <exception cref="System.NotImplementedException"></exception>
     protected override void BackgroundWorker_DoWork(object sender, System.ComponentModel.DoWorkEventArgs e)
     {
-      throw new NotImplementedException();
+      e.Cancel = false;
+      e.Result = null;
+      //string _connectionString = this.GetConnectionString();
+      //ReportProgress(this, new ProgressChangedEventArgs(1, String.Format("Connection string {0}", _connectionString)));
+      //System.Data.IDbConnection _connection = new SqlConnection(_connectionString);
+      //IPRDEV _entities = new IPRDEV(_connection);
+      //WorkerReturnData m_WorkerReturnData = new WorkerReturnData();
+      //if (_entities.DatabaseExists())
+      //{
+      //  ReportProgress(this, new ProgressChangedEventArgs(1, "The specified database exists."));
+      //  GetLastOperation(_entities, ArchivingOperationLogs.OperationName.Cleanup, ref m_WorkerReturnData.CleanupLastRunBy, ref m_WorkerReturnData.CleanupLastRunDate);
+      //  GetLastOperation(_entities, ArchivingOperationLogs.OperationName.Synchronization, ref m_WorkerReturnData.SyncLastRunBy, ref m_WorkerReturnData.SyncLastRunDate);
+      //  GetLastOperation(_entities, ArchivingOperationLogs.OperationName.Archiving, ref m_WorkerReturnData.ArchivingLastRunBy, ref m_WorkerReturnData.ArchivingLastRunDate);
+      //}
+      //else
+      //  ReportProgress(this, new ProgressChangedEventArgs(1, "The specified database cannot be opened."));
+      //NsSPLinq.Settings.GetCurrentContentVersion(Properties.Settings.Default.SiteURL, ref m_WorkerReturnData.CurrentContentVersion, ReportProgress);
+      //ReportProgress(this, new ProgressChangedEventArgs(1, String.Format("The current version of the site content is: {0}", m_WorkerReturnData.CurrentContentVersion.ToString())));
+      //e.Result = m_WorkerReturnData;
     }
     /// <summary>
     /// Called when worker task has been completed.
@@ -84,14 +118,11 @@ namespace CAS.SmartFactory.Shepherd.Client.Management.StateMachines
     /// <exception cref="System.NotImplementedException"></exception>
     protected override void RunWorkerCompleted(object result)
     {
-      throw new NotImplementedException();
+      Context.EnabledEvents = StateMachineEvents.LeftButtonEvent | StateMachineEvents.LeftMiddleButtonEvent | StateMachineEvents.RightButtonEvent | StateMachineEvents.RightMiddleButtonEvent;
     }
-    #endregion
-
-    #region private
-    protected abstract string URL { get; }
-    protected abstract string DatabaseName { get; }
-    protected abstract string SQLServer { get; }
+    /// <summary>
+    ///  Called when only cancel button must be active - after starting background worker.
+    /// </summary>
     protected override void OnlyCancelActive()
     {
       m_ButtonsTemplate.OnlyCancel();
@@ -100,8 +131,46 @@ namespace CAS.SmartFactory.Shepherd.Client.Management.StateMachines
     {
       get { return m_ButtonsTemplate; }
     }
-    private ConnectCancelTemplate m_ButtonsTemplate = new ConnectCancelTemplate();
+    #endregion
+
+    #region abstract
+    /// <summary>
+    /// Gets the URL of the SharePoint application website.
+    /// </summary>
+    /// <value>The URL.</value>
+    protected abstract string URL { get; }
+    /// <summary>
+    /// Gets the name of the SQL database containing backup of SharePoint application data content.
+    /// </summary>
+    /// <value>The name of the database.</value>
+    protected abstract string DatabaseName { get; }
+    /// <summary>
+    /// Gets the SQL server part of the connection string.
+    /// </summary>
+    /// <value>The SQL server address.</value>
+    protected abstract string SQLServer { get; }
+    #endregion
+    
+    #region private
+    private void OnConnectCommand()
+    {
+      this.RunAsync();
+    }
+    private object OnArchiveCommand()
+    {
+      throw new NotImplementedException();
+    }
+    private object OnRouteEditCommand()
+    {
+      throw new NotImplementedException();
+    }
+
+    private ConnectCancelTemplate m_ButtonsTemplate = null;
     private Action<object>[] m_StateMachineActionsArray;
+    private string GetConnectionString()
+    {
+      return String.Format(Properties.Settings.Default.ConnectionString, SQLServer, DatabaseName);
+    }
     #endregion
 
   }
