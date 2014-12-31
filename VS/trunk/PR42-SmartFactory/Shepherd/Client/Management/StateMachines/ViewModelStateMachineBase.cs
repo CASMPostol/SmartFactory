516735e30165dafb613f2810425550eac618edad
mpostol <mpostol@8a3894fd-0c01-b944-991b-6a3da02cae8a>
2014-12-31 11:42:50 +0000

    Implement RouteEditMachineState - fixed navigation problem. Open issues - after navigating to the UI must be cleared.

--- VS/trunk/PR42-SmartFactory/Shepherd/Client/Management/StateMachines/ViewModelStateMachineBase.cs
+++ VS/trunk/PR42-SmartFactory/Shepherd/Client/Management/StateMachines/ViewModelStateMachineBase.cs
@@ -12,16 +12,29 @@
 //  mailto://techsupp@cas.eu
 //  http://www.cas.eu
 //</summary>
-      
+
 using CAS.Common.ViewModel.Wizard;
+using Microsoft.Practices.Prism.Logging;
+using Microsoft.Practices.Prism.Regions;
 using System.ComponentModel.Composition;
 
 namespace CAS.SmartFactory.Shepherd.Client.Management.StateMachines
 {
-  public class ViewModelStateMachineBase<StateType> : ViewModelBase<StateType>
-    where StateType : IAbstractMachineState, new()
+  /// <summary>
+  /// Class ViewModelStateMachineBase - base ViewModel for all View classes hosting a state the state machine <see cref=""/>
+  /// </summary>
+  /// <typeparam name="StateType">The type of the state.</typeparam>
+  public class ViewModelStateMachineBase<StateType> : ViewModelBase<StateType>, INavigationAware, ILoggerFacade
+    where StateType : class, IAbstractMachineState, new()
   {
-
+    /// <summary>
+    /// Initializes a new instance of the <see cref="ViewModelStateMachineBase{StateType}"/> class.
+    /// </summary>
+    /// <param name="loggerFacade">The logger facade class.</param>
+    public ViewModelStateMachineBase(ILoggerFacade loggerFacade)
+    {
+      m_ILoggerFacade = loggerFacade;
+    }
     /// <summary>
     /// Sets the master shell view model. It activates the state <typeparamref name="StateType"/>
     /// </summary>
@@ -31,9 +44,47 @@ namespace CAS.SmartFactory.Shepherd.Client.Management.StateMachines
     {
       set
       {
-        base.EnterState(value);
+        m_ShellViewModel = value;
       }
     }
+
+    #region INavigationAware
+    /// <summary>
+    /// Called when the implementer has been navigated to.
+    /// </summary>
+    /// <param name="navigationContext">The navigation context.</param>
+    public virtual void OnNavigatedTo(NavigationContext navigationContext)
+    {
+      base.EnterState(m_ShellViewModel);
+    }
+    public virtual bool IsNavigationTarget(NavigationContext navigationContext)
+    {
+      return true;
+    }
+    public virtual void OnNavigatedFrom(NavigationContext navigationContext)
+    {
+      base.ExitState();
+    }
+    #endregion
+
+
+    #region ILoggerFacade
+    /// <summary>
+    /// Write a new log entry with the specified category and priority.
+    /// </summary>
+    /// <param name="message">Message body to log.</param>
+    /// <param name="category">Category of the entry.</param>
+    /// <param name="priority">The priority of the entry.</param>
+    public void Log(string message, Category category, Priority priority)
+    {
+      m_ILoggerFacade.Log(message, category, priority);
+    }
+    #endregion
+
+    #region private
+    private ShellViewModel m_ShellViewModel;
+    private ILoggerFacade m_ILoggerFacade;
+    #endregion
 
   }
 }
