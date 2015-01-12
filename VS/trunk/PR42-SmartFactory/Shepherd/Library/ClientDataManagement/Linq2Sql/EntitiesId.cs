d11f8e37d9fb7356ad3d9e4ce146cc9eb840a1f2
bwojciechowski <bwojciechowski@8a3894fd-0c01-b944-991b-6a3da02cae8a>
2015-01-12 11:05:34 +0000

    Shepherd Management Client - Linq2Sql entities - EntitiesId.cs created and added.

---
+++ VS/trunk/PR42-SmartFactory/Shepherd/Library/ClientDataManagement/Linq2Sql/EntitiesId.cs
@@ -0,0 +1,38 @@
+﻿using CAS.SharePoint.Client.Link2SQL;
+
+namespace CAS.SmartFactory.Shepherd.Client.DataManagement.Linq2SQL
+{
+  public partial class AlarmsAndEvents : IItem { }
+  public partial class ArchivingLogs : IArchivingLogs, IId { }
+  public partial class ArchivingOperationLogs : IId { }
+  public partial class BusinessDescription : IItem { }
+  public partial class Carrier : IItem { }
+  public partial class CarrierPerformanceReport : IItem { }
+  public partial class City : IItem { }
+  public partial class Commodity : IItem { }
+  public partial class Country : IItem { }
+  public partial class Currency : IItem { }
+  public partial class DestinationMarket : IItem { }
+  public partial class Driver : IItem { }
+  public partial class DriversTeam : IItem { }
+  public partial class EscortPOLibrary : IItem { }
+  public partial class FreightPayer : IItem { }
+  public partial class FreightPOLibrary : IItem { }
+  public partial class LoadDescription : IItem { }
+  public partial class Market : IItem { }
+  public partial class Partner : IItem { }
+  public partial class Route : IItem { }
+  public partial class SAPDestinationPlant : IItem { }
+  public partial class ScheduleTemplate : IItem { }
+  public partial class SealProtocolLibrary : IItem { }
+  public partial class SecurityEscortRoute : IItem { }
+  public partial class ShipmentType : IItem { }
+  public partial class Shipping : IItem { }
+  public partial class ShippingPoint : IItem { }
+  public partial class TimeSlot : IItem { }
+  public partial class TimeSlotsTemplate : IItem { }
+  public partial class Trailer : IItem { }
+  public partial class TransportUnitType : IItem { }
+  public partial class Truck : IItem { }
+  public partial class Warehouse : IItem { }
+}
