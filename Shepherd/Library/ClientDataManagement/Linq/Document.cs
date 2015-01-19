c7e2f15bb8e2fe8b2cab33d0983661327d063ca6
Mariusz <mpostol@cas.eu>
2015-01-19 15:21:56 +0000

    Implement ArchiveMachineState - development in progress - work on synchronization

---
+++ Shepherd/Library/ClientDataManagement/Linq/Document.cs
@@ -0,0 +1,35 @@
+﻿//<summary>
+//  Title   : partial class Document
+//  System  : Microsoft VisulaStudio 2013 / C#
+//  $LastChangedDate$
+//  $Rev$
+//  $LastChangedBy$
+//  $URL$
+//  $Id$
+//
+//  Copyright (C) 2014, CAS LODZ POLAND.
+//  TEL: +48 (42) 686 25 47
+//  mailto://techsupp@cas.eu
+//  http://www.cas.eu
+//</summary>
+
+using System.Collections.Generic;
+
+namespace CAS.SmartFactory.Shepherd.Client.DataManagement.Linq
+{
+  /// <summary>
+  /// Class Document
+  /// </summary>
+  public partial class Document
+  {
+    /// <summary>
+    /// Gets the mappings the key is SQL property name, the value is SP property name.
+    /// </summary>
+    internal new static Dictionary<string, string> GetMappings()
+    {
+      Dictionary<string, string> _ret = Item.GetMappings();
+      _ret.Add("FileName", "Name");
+      return _ret;
+    }
+  }
+}
--- Shepherd/Library/ClientDataManagement/SynchronizationContent.cs
+++ Shepherd/Library/ClientDataManagement/SynchronizationContent.cs
@@ -15,6 +15,7 @@
 
 using System;
 using System.ComponentModel;
+using CAS.SharePoint.Client.SP2SQLInteroperability;
 
 namespace CAS.SmartFactory.Shepherd.Client.DataManagement
 {
@@ -32,20 +33,47 @@ namespace CAS.SmartFactory.Shepherd.Client.DataManagement
     /// <param name="trace">An action to trace progress in a log stream.</param>
     public static void DoSynchronizationContent(string URL, string sqlConnectionString, Action<ProgressChangedEventArgs> reportProgress, Action<String> trace)
     {
-      bool _breakingIssueEncountered = false;
       reportProgress(new ProgressChangedEventArgs(1, String.Format("Starting DoSynchronizationContent URL: {0}, connection string: {1}", URL, sqlConnectionString)));
       trace("Establishing connection with the SP site and SQL database.");
       using (Linq.Entities _edc = new Linq.Entities(trace, URL))
       {
         using (Linq2SQL.SHRARCHIVE _sqledc = Linq2SQL.SHRARCHIVE.Connect2SQL(sqlConnectionString, y => trace(y)))
         {
-          if (false)
-            _breakingIssueEncountered = true;
+          Synchronizer.Synchronize<Linq2SQL.Commodity, Linq.Commodity>(_sqledc.Commodity, _edc.Commodity, (x, y) => reportProgress(y), Linq.Commodity.GetMappings(), false);
+          //Warehouse
+          //Partner
+          //Currency
+          //FreightPayer
+          //BusinessDescription
+          //SecurityEscortRoute
+          //SealProtocolLibrary
+          //Country
+          //City
+          //EscortPOLibrary
+          //FreightPOLibrary
+          //Carrier
+          //SAPDestinationPlant
+          //ShipmentType
+          //TransportUnitType
+          //Route
+          //Truck
+          //Trailer
+          //Shipping
+          //AlarmsAndEvents
+          //CarrierPerformanceReport
+          //Market
+          //DestinationMarket
+          //Driver
+          //DriversTeam
+          //LoadDescription
+          //ShippingPoint
+          //ScheduleTemplate
+          //TimeSlot
+          //TimeSlotsTemplate
+
         }
       }
       reportProgress(new ProgressChangedEventArgs(1, "Finished DoSynchronizationContent"));
-      if (_breakingIssueEncountered)
-        throw new ApplicationException("DoSynchronizationContent has encountered breaking inconsistency - review the log and remove problems to pass to next phase.");
     }
   }
 }
