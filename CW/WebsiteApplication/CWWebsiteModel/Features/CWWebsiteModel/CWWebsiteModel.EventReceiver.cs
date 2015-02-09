d5427e9d509ad81b2b55ab60ef7e284b46b8d8a9
Mariusz <mpostol@cas.eu>
2015-02-09 10:26:25 +0000

    Diagnostic tracing - added registration and underregistration of the areas and categories.

--- CW/WebsiteApplication/CWWebsiteModel/Features/CWWebsiteModel/CWWebsiteModel.EventReceiver.cs
+++ CW/WebsiteApplication/CWWebsiteModel/Features/CWWebsiteModel/CWWebsiteModel.EventReceiver.cs
@@ -13,13 +13,13 @@
 //  http://www.cas.eu
 //</summary>
 
-using System;
-using System.Runtime.InteropServices;
-using CAS.SharePoint.Common.Logging;
 using CAS.SharePoint.Common.ServiceLocation;
+using CAS.SmartFactory.Customs.Account;
 using Microsoft.Practices.ServiceLocation;
 using Microsoft.SharePoint;
 using Microsoft.SharePoint.Administration;
+using System;
+using System.Runtime.InteropServices;
 
 namespace CAS.SmartFactory.CW.WebsiteModel.Features.CWWebsiteModel
 {
@@ -29,8 +29,8 @@ namespace CAS.SmartFactory.CW.WebsiteModel.Features.CWWebsiteModel
   /// <remarks>
   /// The GUID attached to this class may be used during packaging and should not be modified.
   /// </remarks>
-  [Guid( "acaa7339-77af-4a13-9a56-6c09b05da2f4" )]
-  public class CWWebsiteModelEventReceiver: SPFeatureReceiver
+  [Guid("acaa7339-77af-4a13-9a56-6c09b05da2f4")]
+  public class CWWebsiteModelEventReceiver : SPFeatureReceiver
   {
     //public override void FeatureActivated(SPFeatureReceiverProperties properties)
     //{
@@ -43,22 +43,23 @@ namespace CAS.SmartFactory.CW.WebsiteModel.Features.CWWebsiteModel
     /// Occurs after a Feature is installed.
     /// </summary>
     /// <param name="properties">An <see cref="T:Microsoft.SharePoint.SPFeatureReceiverProperties" /> object that represents the properties of the event.</param>
-    public override void FeatureInstalled( SPFeatureReceiverProperties properties )
+    public override void FeatureInstalled(SPFeatureReceiverProperties properties)
     {
       try
       {
-        if ( !System.Diagnostics.EventLog.SourceExists( SourceName ) )
-          System.Diagnostics.EventLog.CreateEventSource( new System.Diagnostics.EventSourceCreationData( SourceName, "Application" ) );
+        if (!System.Diagnostics.EventLog.SourceExists(SourceName))
+          System.Diagnostics.EventLog.CreateEventSource(new System.Diagnostics.EventSourceCreationData(SourceName, "Application"));
+        WebsiteModelExtensions.RegisterLoggerSource();
+        WebsiteModelExtensions.TraceEvent
+          (String.Format("CAS.SmartFactory.CW.WebsiteModel FeatureInstalled: {0}", properties.Definition.DisplayName), 54, TraceSeverity.High, WebsiteModelExtensions.LoggingCategories.FeatureActivation);
         IServiceLocator _serviceLocator = SharePointServiceLocator.GetCurrent();
-        ILogger logger = _serviceLocator.GetInstance<ILogger>();
-        logger.TraceToDeveloper( String.Format( "CAS.SmartFactory.CW.WebsiteModel FeatureInstalled: {0}", properties.Definition.DisplayName ), TraceSeverity.High );
         IServiceLocatorConfig _typeMappings = _serviceLocator.GetInstance<IServiceLocatorConfig>();
         _typeMappings.RegisterTypeMapping<CAS.SmartFactory.Customs.Account.ICWAccountFactory, CAS.SmartFactory.CW.WebsiteModel.Linq.Account.CWAccountData>();
-        logger.TraceToDeveloper( "CAS.SmartFactory.CW.WebsiteModel TypeMapping registered", TraceSeverity.High );
+        WebsiteModelExtensions.TraceEvent("CAS.SmartFactory.CW.WebsiteModel TypeMapping registered", 59, TraceSeverity.High, WebsiteModelExtensions.LoggingCategories.FeatureActivation);
       }
-      catch ( Exception  _ex)
+      catch (Exception _ex)
       {
-        System.Diagnostics.EventLog.WriteEvent( SourceName, new System.Diagnostics.EventInstance( 63, 0 ) { EntryType = System.Diagnostics.EventLogEntryType.Error }, _ex );
+        System.Diagnostics.EventLog.WriteEvent(SourceName, new System.Diagnostics.EventInstance(63, 0) { EntryType = System.Diagnostics.EventLogEntryType.Error }, _ex);
         throw;
       }
     }
@@ -66,27 +67,29 @@ namespace CAS.SmartFactory.CW.WebsiteModel.Features.CWWebsiteModel
     /// Occurs when a Feature is uninstalled.
     /// </summary>
     /// <param name="properties">An <see cref="T:Microsoft.SharePoint.SPFeatureReceiverProperties" /> object that represents the properties of the event.</param>
-    public override void FeatureUninstalling( SPFeatureReceiverProperties properties )
+    public override void FeatureUninstalling(SPFeatureReceiverProperties properties)
     {
       try
       {
         // Get the ServiceLocatorConfig service from the service locator.
+        WebsiteModelExtensions.TraceEvent
+          (String.Format("CAS.SmartFactory.CW.WebsiteModel FeatureUninstalling: {0}", properties.Definition.DisplayName), 76, TraceSeverity.Monitorable, WebsiteModelExtensions.LoggingCategories.FeatureActivation);
         IServiceLocator _serviceLocator = SharePointServiceLocator.GetCurrent();
-        ILogger logger = _serviceLocator.GetInstance<ILogger>();
-        logger.TraceToDeveloper( String.Format( "CAS.SmartFactory.CW.WebsiteModel FeatureUninstalling: {0}", properties.Definition.DisplayName ), TraceSeverity.Monitorable );
         IServiceLocatorConfig _typeMappings = _serviceLocator.GetInstance<IServiceLocatorConfig>();
-        _typeMappings.RemoveTypeMapping<CAS.SmartFactory.Customs.Account.ICWAccountFactory>( null );
-        logger.TraceToDeveloper( "CAS.SmartFactory.CW.WebsiteModel TypeMapping removed: ICWAccountFactory", TraceSeverity.High );
+        _typeMappings.RemoveTypeMapping<ICWAccountFactory>(null);
+        WebsiteModelExtensions.TraceEvent
+          ("CAS.SmartFactory.CW.WebsiteModel TypeMapping removed: ICWAccountFactory", 82, TraceSeverity.High, WebsiteModelExtensions.LoggingCategories.FeatureActivation);
+        WebsiteModelExtensions.UnregisterLoggerSource();
       }
-      catch ( Exception _ex)
+      catch (Exception _ex)
       {
-        System.Diagnostics.EventLog.WriteEvent( SourceName, new System.Diagnostics.EventInstance( 63, 0 ) { EntryType = System.Diagnostics.EventLogEntryType.Error }, _ex );
+        System.Diagnostics.EventLog.WriteEvent(SourceName, new System.Diagnostics.EventInstance(63, 0) { EntryType = System.Diagnostics.EventLogEntryType.Error }, _ex);
         throw;
       }
     }
     //public override void FeatureUpgrading(SPFeatureReceiverProperties properties, string upgradeActionName, System.Collections.Generic.IDictionary<string, string> parameters)
     //{
     //}
-    internal static string SourceName = "CAS.SmartFactory.CW.WebsiteModel";
+    internal static string SourceName = "Application";
   }
 }
