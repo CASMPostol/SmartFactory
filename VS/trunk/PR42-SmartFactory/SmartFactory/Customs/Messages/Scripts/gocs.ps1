68a9a1cb4052ae713f001816d887bab8c7a2ed91
bwojciechowski <bwojciechowski@8a3894fd-0c01-b944-991b-6a3da02cae8a>
2015-02-18 14:48:01 +0000

    Schema for sad collection generated.

--- VS/trunk/PR42-SmartFactory/SmartFactory/Customs/Messages/Scripts/gocs.ps1
+++ VS/trunk/PR42-SmartFactory/SmartFactory/Customs/Messages/Scripts/gocs.ps1
@@ -1,21 +1,22 @@
 ﻿Write-Host "This file creates all the c# classes for xnl schemas"
 Get-Location | Write-host
-$env:path += "; C:\Program Files\Microsoft SDKs\Windows\v7.1\Bin\x64\"
+$env:path += "; C:\Program Files (x86)\Microsoft SDKs\Windows\v8.0A\bin\NETFX 4.0 Tools"
 # Set-Location C:\vs\Projects\JTI\Excel2XML
 # Write-host press any key to continue .....
 # Read-host
 $cpath = get-location
 # Write-host CELINA processing
 set-location ..\CELINA
-xsd.exe SADw2r0.xsd xmldsig-core-schema.xsd /N:CAS.SmartFactory.Customs.Messages.CELINA.SAD /c  |write-host
-xsd.exe CLNEw2r0.xsd xmldsig-core-schema.xsd /N:CAS.SmartFactory.Customs.Messages.CELINA.CLNE /c  |write-host
-xsd.exe PZCw2r0.xsd xmldsig-core-schema.xsd /N:CAS.SmartFactory.Customs.Messages.CELINA.PZC /c  |write-host
+#xsd.exe SADw2r0.xsd xmldsig-core-schema.xsd /N:CAS.SmartFactory.Customs.Messages.CELINA.SAD /c  |write-host
+#xsd.exe CLNEw2r0.xsd xmldsig-core-schema.xsd /N:CAS.SmartFactory.Customs.Messages.CELINA.CLNE /c  |write-host
+#xsd.exe PZCw2r0.xsd xmldsig-core-schema.xsd /N:CAS.SmartFactory.Customs.Messages.CELINA.PZC /c  |write-host
 # xsd.exe DSw2r0.xsd  xmldsig-core-schema.xsd /N:CAS.SmartFactory.Customs.Messages.CELINA /c  |write-host
 # xsd.exe PWDw2r0.xsd xmldsig-core-schema.xsd /N:CAS.SmartFactory.Customs.Messages.CELINA /c  |write-host
+xsd.exe SADCollection.xsd /N:CAS.SmartFactory.Customs.Messages.CELINA.SAD.SADCollection /c  |write-host
 
-Write-host ECS processing
-set-location ..\ECS
-xsd.exe IE529_v1-0.xsd xmldsig-core-schema.xsd /N:CAS.SmartFactory.Customs.Messages.ECS /c  |write-host
+#Write-host ECS processing
+#set-location ..\ECS
+#xsd.exe IE529_v1-0.xsd xmldsig-core-schema.xsd /N:CAS.SmartFactory.Customs.Messages.ECS /c  |write-host
 
 set-location $cpath
 Write-host Done...
