9a07581a8575a08dc3830e916e706304380d5c33
bwojciechowski <bwojciechowski@8a3894fd-0c01-b944-991b-6a3da02cae8a>
2015-02-19 09:45:12 +0000

    Closing many accounts - worflow + form added - to be developed. New nuget package for customs 2.30.02 published.

---
+++ VS/trunk/PR42-SmartFactory/CW/WebsiteApplication/CWWorkflows/CustomsWarehouseList/CloseManyAccounts/CloseManyAccountsForm.aspx.designer.cs
@@ -0,0 +1,32 @@
+﻿namespace CAS.SmartFactory.CW.Workflows.CustomsWarehouseList.CloseManyAccounts
+{
+  public partial class CloseManyAccountsForm
+  {
+    /// <summary>
+    /// form1 Control.
+    /// </summary>
+    /// <remarks>
+    /// Auto-generated field.
+    /// To modify move field declaration from designer file to code-behind file.
+    /// </remarks>
+    protected global::System.Web.UI.HtmlControls.HtmlForm form1;
+
+    /// <summary>
+    /// StartWorkflow Control.
+    /// </summary>
+    /// <remarks>
+    /// Auto-generated field.
+    /// To modify move field declaration from designer file to code-behind file.
+    /// </remarks>
+    protected global::System.Web.UI.WebControls.Button StartWorkflow;
+
+    /// <summary>
+    /// Cancel Control.
+    /// </summary>
+    /// <remarks>
+    /// Auto-generated field.
+    /// To modify move field declaration from designer file to code-behind file.
+    /// </remarks>
+    protected global::System.Web.UI.WebControls.Button Cancel;
+  }
+}
--- VS/trunk/PR42-SmartFactory/SmartFactory/Customs/Properties/AssemblyInfo.cs
+++ VS/trunk/PR42-SmartFactory/SmartFactory/Customs/Properties/AssemblyInfo.cs
@@ -20,6 +20,6 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("519191f7-7957-43b8-94a8-5f89473dfe47")]
-[assembly: AssemblyVersion("2.30.00.*")]
-[assembly: AssemblyFileVersion("2.30.00")]
+[assembly: AssemblyVersion("2.30.02.*")]
+[assembly: AssemblyFileVersion("2.30.02")]
 
