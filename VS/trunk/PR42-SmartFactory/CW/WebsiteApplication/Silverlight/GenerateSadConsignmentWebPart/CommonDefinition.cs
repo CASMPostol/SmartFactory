ebe8fb5f222f45beb83305a89879b03111d1b578
mpostol <mpostol@8a3894fd-0c01-b944-991b-6a3da02cae8a>
2015-02-23 12:48:26 +0000

    Save all SAD Consignment for one Request using one 'button' - added client site code - TBT.

---
+++ VS/trunk/PR42-SmartFactory/CW/WebsiteApplication/Silverlight/GenerateSadConsignmentWebPart/CommonDefinition.cs
@@ -0,0 +1,28 @@
+﻿//<summary>
+//  Title   : class CommonDefinition
+//  System  : Microsoft Visual C# .NET 2012
+//  $LastChangedDate: 2014-10-22 15:02:32 +0200 (Śr, 22 paź 2014) $
+//  $Rev: 10905 $
+//  $LastChangedBy: mpostol $
+//  $URL: svn://svnserver.hq.cas.com.pl/VS/trunk/PR42-SmartFactory/CW/WebsiteApplication/Silverlight/ExitSheetWebPart/CommonDefinition.tt $
+//  $Id: CommonDefinition.tt 10905 2014-10-22 13:02:32Z mpostol $
+//
+//  Copyright (C) 2013, CAS LODZ POLAND.
+//  TEL: +48 (42) 686 25 47
+//  mailto://techsupp@cas.eu
+//  http://www.cas.eu
+//</summary>
+
+namespace CAS.SmartFactory.CW.Dashboards.GenerateSadConsignmentWebPart
+{
+  /// <summary>
+  /// CommonDefinition for multiproject environment
+  /// </summary>
+  internal partial class CommonDefinition
+  {
+    internal const string CustomsWarehouseDisposalTitle = "Customs Warehouse Disposal";
+    internal const string DisposalRequestLibraryTitle = "Disposal Request Library";
+    internal const string HiddenFieldDataParameterName = "controlId";
+    internal const string ColumnNameId = "ID";
+  }
+}
