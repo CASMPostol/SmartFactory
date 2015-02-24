c5ebf40f84636ef81e3da88a13ad9c87543e3bfa
Mariusz <mpostol@cas.eu>
2015-02-24 14:17:36 +0000

    Closing many accounts - working copy.

--- CW/WebsiteApplication/CWWorkflows/CustomsWarehouseList/CloseManyAccounts/CloseManyAccounts.cs
+++ CW/WebsiteApplication/CWWorkflows/CustomsWarehouseList/CloseManyAccounts/CloseManyAccounts.cs
@@ -14,6 +14,8 @@ using System.Workflow.Activities.Rules;
 using Microsoft.SharePoint;
 using Microsoft.SharePoint.Workflow;
 using Microsoft.SharePoint.WorkflowActions;
+using CAS.SmartFactory.CW.WebsiteModel.Linq;
+using CAS.SharePoint.Serialization;
 
 namespace CAS.SmartFactory.CW.Workflows.CustomsWarehouseList.CloseManyAccounts
 {
@@ -26,10 +28,50 @@ namespace CAS.SmartFactory.CW.Workflows.CustomsWarehouseList.CloseManyAccounts
 
     public Guid workflowId = default(System.Guid);
     public SPWorkflowActivationProperties workflowProperties = new SPWorkflowActivationProperties();
-
-    private void onWorkflowActivated1_Invoked(object sender, ExternalDataEventArgs e)
+    InitializationFormData m_InitializationData = null;
+    private void GetParameters(object sender, ExternalDataEventArgs e)
     {
+      try
+      {
+        m_InitializationData = JsonSerializer.Deserialize<InitializationFormData>(workflowProperties.InitiationData);
+      }
+      catch (Exception ex)
+      {
+        string _frmt = "Worflow aborted in StartLogToHistory because of the error: {0}";
+        throw new ApplicationException(String.Format(_frmt, ex.Message));
+      }
+    }
+    private void DoCloseManyAccount(object sender, EventArgs e)
+    {
+      using (Entities _entities = new Entities(workflowProperties.WebUrl))
+      {
+
+      }
 
     }
+    private void OnStartingLogToHistory(object sender, EventArgs e)
+    {
+      StartingHistoryListActivity_HistoryOutcome = "Starting";
+      StartingHistoryListActivity_HistoryDescription = "Starting closing the accounts: " + String.Join(", ", m_InitializationData.AccountsArray.Select<int, string>(x => x.ToString()).ToArray<string>());
+    }
+    private void onFinishedHistoryListActivity(object sender, EventArgs e)
+    {
+      StartingHistoryListActivity_HistoryOutcome = "Starting";
+      StartingHistoryListActivity_HistoryDescription = "Starting closing the accounts:";
+    }
+    private void onExceptionHandleristoryListActivity(object sender, EventArgs e)
+    {
+      ExceptionHandleristoryListActivity_HistoryDescription1 = "Aborted by exception";
+      ExceptionHandleristoryListActivity_HistoryOutcome = "Exception";
+    }
+    public String StartingHistoryListActivity_HistoryDescription = default(System.String);
+    public String StartingHistoryListActivity_HistoryOutcome = default(System.String);
+    public String FinishedHistoryListActivity_HistoryDescription = default(System.String);
+    public String FinishedHistoryListActivity_HistoryOutcome = default(System.String);
+    public String ExceptionHandleristoryListActivity_HistoryDescription1 = default(System.String);
+    public String ExceptionHandleristoryListActivity_HistoryOutcome = default(System.String);
+
+
+
   }
 }
