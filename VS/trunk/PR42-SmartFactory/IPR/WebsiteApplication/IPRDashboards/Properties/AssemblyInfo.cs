f7ce251e8900f79e89a4c2ea6324b90127de0b5c
mpostol <mpostol@8a3894fd-0c01-b944-991b-6a3da02cae8a>
2015-05-28 14:59:02 +0000

    Release IPR 2.60.21 - changed version.

--- VS/trunk/PR42-SmartFactory/IPR/WebsiteApplication/IPRDashboards/Properties/AssemblyInfo.cs
+++ VS/trunk/PR42-SmartFactory/IPR/WebsiteApplication/IPRDashboards/Properties/AssemblyInfo.cs
@@ -33,8 +33,8 @@ using System.Security;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.60.20.*")]
-[assembly: AssemblyFileVersion("2.60.20")]
+[assembly: AssemblyVersion("2.60.21.*")]
+[assembly: AssemblyFileVersion("2.60.21")]
 
 
 [assembly: System.Runtime.CompilerServices.InternalsVisibleTo( "IPRDashboardsTest, PublicKey=002400000480000094000000060200000024000052534131000400000100010015c024c9d0c88f" +
