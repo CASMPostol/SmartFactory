af67f8d19e2551126154f050a02a64cd79addc3e
Mariusz <mpostol@cas.eu>
2016-01-20 15:02:56 +0000

    Port SmartFactory to VS2015 - SmartFactory updated version information.
    Issue #: 4770

--- Scripts/CreateTag.cmd
+++ Scripts/CreateTag.cmd
@@ -7,8 +7,8 @@ rem//  $Id$
 
 
 set branchtype=tags
-set TagFolder=Rel_IPRCW_2_60_23
-set TagPath=svn://svnserver.hq.cas.com.pl/VS/%branchtype%/SmartFactory/rel_IPRCW_2_60_23
+set TagFolder=Rel_2_60_25
+set TagPath=svn://svnserver.hq.cas.com.pl/VS/%branchtype%/SmartFactory/rel_2_60_25
 set trunkPath=svn://svnserver.hq.cas.com.pl/VS/trunk
 
 svn mkdir %TagPath%  -m "created new %TagPath% (in %branchtype% folder)"
