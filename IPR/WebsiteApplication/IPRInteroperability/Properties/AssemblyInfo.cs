d894b6d482c24a94912ecd5984f022e5d30d720b
Mariusz <mpostol@cas.eu>
2016-01-20 14:54:05 +0000

    Port SmartFactory to VS2015 - IPR updated version information.
    Issue #: 4770

--- IPR/WebsiteApplication/IPRInteroperability/Properties/AssemblyInfo.cs
+++ IPR/WebsiteApplication/IPRInteroperability/Properties/AssemblyInfo.cs
@@ -1,27 +1,16 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
 [assembly: AssemblyTitle("IPR Interoperability")]
 [assembly: AssemblyDescription("Smart Factory IPR SAP, CELINE, WMS. Office interconnection helpers")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("CAS")]
 [assembly: AssemblyProduct("Smart Factory")]
-[assembly: AssemblyCopyright("Copyright (c) 2000-2015 CAS LODZ POLAND")]
+[assembly: AssemblyCopyright("Copyright (c) 2000-2016 CAS LODZ POLAND")]
 [assembly: AssemblyTrademark("Smart Factory")]
 [assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("3EABDD4B-227C-483A-A855-03C7628286FD")]
-
-[assembly: AssemblyVersion("2.60.23.*")]
-[assembly: AssemblyFileVersion("2.60.23")]
+[assembly: AssemblyVersion("2.60.25.*")]
+[assembly: AssemblyFileVersion("2.60.25")]
 
--- IPR/WebsiteApplication/IPRWorkflow/Properties/AssemblyInfo.cs
+++ IPR/WebsiteApplication/IPRWorkflow/Properties/AssemblyInfo.cs
@@ -2,36 +2,16 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
 [assembly: AssemblyTitle("IPR Workflows")]
 [assembly: AssemblyDescription("Smart Factory IPR workflows collection.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("CAS")]
 [assembly: AssemblyProduct("Smart Factory")]
-[assembly: AssemblyCopyright("Copyright (c) 2000-2015 CAS LODZ POLAND")]
+[assembly: AssemblyCopyright("Copyright (c) 2000-2016 CAS LODZ POLAND")]
 [assembly: AssemblyTrademark("Smart Factory")]
 [assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("c4d9bbc4-c123-4aab-b1e6-9906b7b51bc6")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.60.23.*")]
-[assembly: AssemblyFileVersion("2.60.23")]
+[assembly: AssemblyVersion("2.60.25.*")]
+[assembly: AssemblyFileVersion("2.60.25")]
 
