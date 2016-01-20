3b9755e4035e0b13261487ca8633384f4a97f10d
Mariusz <mpostol@cas.eu>
2016-01-20 14:15:04 +0000

    Port SmartFactory to VS2015 - CW - updated version information. Issue #: 4770

--- CW/WebsiteApplication/Silverlight/DisposalRequestWebPart/Properties/AssemblyInfo.cs
+++ CW/WebsiteApplication/Silverlight/DisposalRequestWebPart/Properties/AssemblyInfo.cs
@@ -2,24 +2,16 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
 [assembly: AssemblyTitle("CW Disposal Request Web Part")]
 [assembly: AssemblyDescription("CAS Smart Factory Customs Warehousing Disposal Request WebPart")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("CAS")]
 [assembly: AssemblyProduct("Smart Factory")]
-[assembly: AssemblyCopyright("Copyright (c) 2000-2015 CAS LODZ POLAND")]
+[assembly: AssemblyCopyright("Copyright (c) 2000-2016 CAS LODZ POLAND")]
 [assembly: AssemblyTrademark("Smart Factory")]
 [assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
-// The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("6F935305-C1D7-41C4-93E5-3C8BF4955231")]
-[assembly: AssemblyVersion("2.20.30.*")]
-[assembly: AssemblyFileVersion("2.20.30")]
+[assembly: AssemblyVersion("2.20.31.*")]
+[assembly: AssemblyFileVersion("2.20.31")]
 
